e55d6930b6bbc20c47dbef5940d11952790e2217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 14:03:44 +0000

    Add package pybibtexer/0.0.3/pybibtexer-0.0.3.tar.gz

---
+++ packages/pybibtexer/pybibtexer-0.0.3.tar.gz/pybibtexer-0.0.3/pybibtexer/main/basic_input.py
@@ -0,0 +1,88 @@
+import json
+import os
+from typing import Any, Dict
+
+
+class BasicInput(object):
+    """Basic input.
+
+    Args:
+        full_json_c (str): The conference json file
+        full_json_j (str): The journal json file
+        options (Dict[str, Any]): Options.
+
+    Attributes:
+
+        full_abbr_article_dict (Dict[str, str]): Full abbr article dict.
+        full_abbr_inproceedings_dict (Dict[str, str]): Full abbr inproceedings dict.
+        full_names_in_json (str): Full names in json.
+        abbr_names_in_json (str): Abbr names in json.
+
+        options (Dict[str, Any]): Options.
+
+    Notes:
+        The structure of full_json_c follows the format {"publisher": {"conferences": {"abbr": {}}}},
+            while full_json_j adheres to the format {"publisher": {"journals": {"abbr": {}}}}.
+    """
+
+    def __init__(self, full_json_c: str, full_json_j: str, options: Dict[str, Any]) -> None:
+        self.full_json_c = full_json_c
+        self.full_json_j = full_json_j
+
+        # bib/core
+        self._initialize_middlewares(options)
+
+        self.options = options
+
+    # bib/core
+    def _initialize_middlewares(self, options: Dict[str, Any]) -> None:
+        if os.path.isfile(self.full_json_c):
+            with open(self.full_json_c, "r") as f:
+                try:
+                    json_dict = json.loads(f.read())
+                except Exception as e:
+                    print(e)
+                    json_dict = {}
+
+                full_abbr_inproceedings_dict = {}
+                for flag in ["conferences", "Conferences", "CONFERENCES", "conference", "Conference", "CONFERENCE"]:
+                    full_abbr_inproceedings_dict = {p: json_dict[p].get(flag, {}) for p in json_dict}
+                    if full_abbr_inproceedings_dict:
+                        break
+        else:
+            full_abbr_inproceedings_dict = {}
+
+        if os.path.isfile(self.full_json_j):
+            with open(self.full_json_j, "r") as f:
+                try:
+                    json_dict = json.loads(f.read())
+                except Exception as e:
+                    print(e)
+                    json_dict = {}
+
+                full_abbr_article_dict = {}
+                for flag in ["journals", "Journals", "JOURNALS", "journal", "Journal", "JOURNAL"]:
+                    full_abbr_article_dict = {p: json_dict[p].get("journals", {}) for p in json_dict}
+                    if full_abbr_article_dict:
+                        break
+        else:
+            full_abbr_article_dict = {}
+
+        self.full_abbr_inproceedings_dict = full_abbr_inproceedings_dict
+        self.full_abbr_article_dict = full_abbr_article_dict
+
+        full_names_in_json = options.get("full_names_in_json", "names_full")
+        if not full_names_in_json:
+            full_names_in_json = "names_full"
+        self.full_names_in_json = full_names_in_json
+
+        abbr_names_in_json = options.get("abbr_names_in_json", "names_abbr")
+        if not abbr_names_in_json:
+            abbr_names_in_json = "names_abbr"
+        self.abbr_names_in_json = abbr_names_in_json
+
+        options["full_abbr_article_dict"] = self.full_abbr_article_dict
+        options["full_abbr_inproceedings_dict"] = self.full_abbr_inproceedings_dict
+
+        options["full_names_in_json"] = self.full_names_in_json
+        options["abbr_names_in_json"] = self.abbr_names_in_json
