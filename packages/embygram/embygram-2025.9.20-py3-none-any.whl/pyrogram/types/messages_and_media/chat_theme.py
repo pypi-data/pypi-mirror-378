cd5f5ed647625482fac6216154eb30cc202a5925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 16:04:39 +0000

    Add package embygram/2025.9.20/embygram-2025.9.20-py3-none-any.whl

---
+++ packages/embygram/embygram-2025.9.20-py3-none-any.whl/pyrogram/types/messages_and_media/chat_theme.py
@@ -0,0 +1,40 @@
+#  Pyrogram - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#
+#  This file is part of Pyrogram.
+#
+#  Pyrogram is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Pyrogram is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
+
+from pyrogram import raw
+
+from ..object import Object
+
+
+class ChatTheme(Object):
+    """A theme in the chat has been changed.
+
+    Parameters:
+        name (``str``):
+            Theme name.
+    """
+
+    def __init__(self, *, name: str):
+        super().__init__()
+
+        self.name = name
+
+    @staticmethod
+    def _parse(action: "raw.types.MessageActionSetChatTheme") -> "ChatTheme":
+        if isinstance(action.theme, raw.types.ChatTheme):
+            return ChatTheme(name=action.theme.emoticon)
