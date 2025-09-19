12c8723847225bc515ad53f263f8e024af2e8ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 14:04:01 +0000

    Add package svc-infra/0.1.317/svc_infra-0.1.317.tar.gz

---
+++ packages/svc-infra/svc_infra-0.1.317.tar.gz/svc_infra-0.1.317/src/svc_infra/api/fastapi/__init__.py
@@ -0,0 +1,13 @@
+from .dual_router import DualAPIRouter, dualize_router
+from .setup import APIVersionSpec, ServiceInfo, setup_service_api
+from .sugar import easy_service_app, quick_service_api
+
+__all__ = [
+    "DualAPIRouter",
+    "dualize_router",
+    "ServiceInfo",
+    "APIVersionSpec",
+    "setup_service_api",
+    "quick_service_api",
+    "easy_service_app",
+]
