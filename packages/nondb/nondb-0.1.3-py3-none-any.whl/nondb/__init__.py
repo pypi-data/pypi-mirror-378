2df7df687940a001652f1b283debdd4ecaec661b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 15:04:35 +0000

    Add package nondb/0.1.3/nondb-0.1.3-py3-none-any.whl

---
+++ packages/nondb/nondb-0.1.3-py3-none-any.whl/nondb/__init__.py
@@ -0,0 +1,6 @@
+"""nodb is a filesystem-based json storage system."""
+
+from .NonDB import NonDB  # noqa: F401
+from .NonIndex import NonIndex  # noqa: F401
+from .NonTable import NonTable  # noqa: F401
+from .Storage import Storage  # noqa: F401
