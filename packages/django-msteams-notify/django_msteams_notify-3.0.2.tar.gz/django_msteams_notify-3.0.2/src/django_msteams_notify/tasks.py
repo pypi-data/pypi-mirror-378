5ff2a62a7b337f4f8cb98473be0c30eacfcfb0bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 15:04:43 +0000

    Add package django-msteams-notify/3.0.2/django_msteams_notify-3.0.2.tar.gz

---
+++ packages/django-msteams-notify/django_msteams_notify-3.0.2.tar.gz/django_msteams_notify-3.0.2/src/django_msteams_notify/tasks.py
@@ -0,0 +1,12 @@
+from celery import shared_task
+from django_msteams_notify.models import TeamsNotification
+from django_msteams_notify.utils import send_teams_message
+
+@shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, max_retries=5)
+def send_teams_notification_task(self, notification_id):
+    """Send a Teams notification asynchronously with retries."""
+    try:
+        notification = TeamsNotification.objects.get(id=notification_id)
+    except TeamsNotification.DoesNotExist:
+        return False
+    return send_teams_message(notification)
