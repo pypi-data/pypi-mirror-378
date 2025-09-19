029b3d11fb6bb5e826d43d7e5007a503eef456e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 14:04:41 +0000

    Add package test-mypip-setup/0.2.1/test_mypip_setup-0.2.1-py3-none-any.whl

---
+++ packages/test-mypip-setup/test_mypip_setup-0.2.1-py3-none-any.whl/test_mypip_setup/__init__.py
@@ -0,0 +1,11 @@
+"""
+test_mypip_setup - 一个示例Python包
+"""
+
+__version__ = "0.2.1"
+__author__ = "Your Name"
+__email__ = "your.email@example.com"
+
+from .test2 import fun1,fun2
+
+__all__ = ["fun1","fun2"]
