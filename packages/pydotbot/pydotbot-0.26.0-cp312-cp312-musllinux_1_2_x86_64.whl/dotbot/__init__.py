b4c41f5a212f64bd73c9628127cbcc582fe9f451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 15:04:20 +0000

    Add package pydotbot/0.26.0/pydotbot-0.26.0-cp312-cp312-musllinux_1_2_x86_64.whl

---
+++ packages/pydotbot/pydotbot-0.26.0-cp312-cp312-musllinux_1_2_x86_64.whl/dotbot/__init__.py
@@ -0,0 +1,25 @@
+"""Pydotbot module."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+from dotbot.serial_interface import get_default_port
+
+SERIAL_PORT_DEFAULT = get_default_port()
+SERIAL_BAUDRATE_DEFAULT = 1000000
+DOTBOT_ADDRESS_DEFAULT = "FFFFFFFFFFFFFFFF"  # Broadcast by default
+GATEWAY_ADDRESS_DEFAULT = "0000000000000000"
+NETWORK_ID_DEFAULT = "0000"
+CONTROLLER_HTTP_PROTOCOL_DEFAULT = "http"
+CONTROLLER_HTTP_HOSTNAME_DEFAULT = "localhost"
+CONTROLLER_HTTP_PORT_DEFAULT = "8000"
+CONTROLLER_ADAPTER_DEFAULT = "serial"
+MQTT_HOST_DEFAULT = "localhost"
+MQTT_PORT_DEFAULT = 1883
+
+
+def pydotbot_version() -> str:
+    """Returns the version of the pydotdot package."""
+    try:
+        return version("pydotbot")
+    except PackageNotFoundError:
+        return "unknown"
