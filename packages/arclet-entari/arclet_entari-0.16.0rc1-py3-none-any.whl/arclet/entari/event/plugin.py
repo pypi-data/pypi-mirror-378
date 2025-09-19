4a28bec2c534a47ca2b2b9bc05af1e43342dc905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 15:04:42 +0000

    Add package arclet-entari/0.16.0rc1/arclet_entari-0.16.0rc1-py3-none-any.whl

---
+++ packages/arclet-entari/arclet_entari-0.16.0rc1-py3-none-any.whl/arclet/entari/event/plugin.py
@@ -0,0 +1,18 @@
+from arclet.letoderea import make_event
+
+
+@make_event(name="entari.event/plugin/loaded_success")
+class PluginLoadedSuccess:
+    name: str
+
+
+@make_event(name="entari.event/plugin/loaded_failed")
+class PluginLoadedFailed:
+    name: str
+    error: Exception | None = None
+    """若没有异常信息，说明该插件加载失败的原因是插件不存在。"""
+
+
+@make_event(name="entari.event/plugin/unloaded")
+class PluginUnloaded:
+    name: str
