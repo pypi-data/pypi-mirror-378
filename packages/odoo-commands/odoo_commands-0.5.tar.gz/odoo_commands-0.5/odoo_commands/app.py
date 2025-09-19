455075cd1d01eff0d916e1c9b4377c448a7397c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 14:03:42 +0000

    Add package odoo-commands/0.5/odoo_commands-0.5.tar.gz

---
+++ packages/odoo-commands/odoo_commands-0.5.tar.gz/odoo_commands-0.5/odoo_commands/app.py
@@ -0,0 +1,16 @@
+import typer
+from odoo_commands.createdb import create_database_command
+from odoo_commands.nighly import download_odoo
+
+app = typer.Typer()
+
+app.command(name='createdb')(create_database_command)
+app.command(name='nightly')(download_odoo)
+
+@app.command()
+def hello(name: str):
+    print(f"Hello {name}")
+
+
+if __name__ == "__main__":
+    app()
