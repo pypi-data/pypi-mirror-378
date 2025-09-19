8078b5bc6c79eecabd49a76f0c36da29b9f6975c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 15:04:34 +0000

    Add package hapla/0.33.0/hapla-0.33.0.tar.gz

---
+++ packages/hapla/hapla-0.33.0.tar.gz/hapla-0.33.0/hapla/predict.py
@@ -0,0 +1,229 @@
+"""
+hapla.
+Haplotype clustering using pre-estimated cluster medians.
+"""
+
+__author__ = "Jonas Meisner"
+
+# Libraries
+import os
+from datetime import datetime
+from time import time
+from hapla import __version__
+
+
+##### hapla predict #####
+def main(args, deaf):
+	print("-----------------------------------")
+	print(f"hapla by Jonas Meisner (v{__version__})")
+	print(f"hapla predict using {args.threads} thread(s)")
+	print("-----------------------------------\n")
+	
+	# Check input
+	assert args.vcf is not None, "Please provide phased genotype file (--bcf or --vcf)!"
+	assert os.path.isfile(f"{args.vcf}"), "VCF/BCF file doesn't exist!"
+	assert os.path.isfile(f"{args.vcf}.csi") or os.path.isfile(f"{args.vcf}.tbi"), "VCF/BCF index doesn't exist!"
+	assert args.ref is not None, "Please provide pre-estimated reference haplotype cluster medians (--ref)!"
+	assert os.path.isfile(f"{args.ref}.bcm"), "bcm file doesn't exist!"
+	assert os.path.isfile(f"{args.ref}.win"), "win file doesn't exist!"
+	assert os.path.isfile(f"{args.ref}.wix"), "wix file doesn't exist!"
+	assert args.threads > 0, "Please select a valid number of threads!"
+	start = time()
+
+	# Control threads of external numerical libraries
+	os.environ["MKL_NUM_THREADS"] = str(args.threads)
+	os.environ["MKL_MAX_THREADS"] = str(args.threads)
+	os.environ["OMP_NUM_THREADS"] = str(args.threads)
+	os.environ["OMP_MAX_THREADS"] = str(args.threads)
+	os.environ["NUMEXPR_NUM_THREADS"] = str(args.threads)
+	os.environ["NUMEXPR_MAX_THREADS"] = str(args.threads)
+	os.environ["OPENBLAS_NUM_THREADS"] = str(args.threads)
+	os.environ["OPENBLAS_MAX_THREADS"] = str(args.threads)
+
+	# Create log-file of used arguments
+	full = vars(args)
+	with open(f"{args.out}.log", "w") as log:
+		log.write(f"hapla v{__version__}\n")
+		log.write("hapla predict\n")
+		log.write(f"Time: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}\n")
+		log.write(f"Directory: {os.getcwd()}\n")
+		log.write("Options:\n")
+		for key in full:
+			if full[key] != deaf[key]:
+				if type(full[key]) is bool:
+					log.write(f"\t--{key}\n")
+				else:
+					log.write(f"\t--{key} {full[key]}\n")
+	del full, deaf
+
+	# Import numerical libraries and cython functions
+	import numpy as np
+	from cyvcf2 import VCF
+	from math import ceil
+	from hapla import reader_cy
+	from hapla import memory_cy
+	from hapla import shared_cy
+
+	# Initiate VCF and extract sample list
+	print("\rLoading VCF/BCF file...", end="")
+	v_file = VCF(args.vcf, threads=min(args.threads, 4))
+	s_list = np.array(v_file.samples).reshape(-1, 1)
+	N = 2*s_list.shape[0]
+	M = v_file.num_records
+	B = ceil(N/4)
+
+	# Allocate arrays
+	if args.memory:
+		G = np.zeros((M, B), dtype=np.uint8)
+	else:
+		G = np.zeros((M, N), dtype=np.uint8)
+	v_vec = np.zeros(M, dtype=np.uint32)
+
+	# Read variants into matrix
+	v_file = VCF(args.vcf, threads=min(args.threads, 4))
+	for j, variant in enumerate(v_file):
+		V = variant.genotype.array()
+		if args.memory:
+			memory_cy.predBit(G[j], V, N//2)
+		else:
+			reader_cy.predVar(G[j], V, N//2)
+		v_vec[j] = variant.POS
+	chrom = str(variant.CHROM) # Extract chromosome information
+	del V, v_file
+	print(f"\rLoaded phased genotype data: {N} haplotypes and {M} SNPs.")
+
+	# Load window information from reference
+	w_mat = np.genfromtxt(f"{args.ref}.win", dtype=np.str_, skip_header=1)
+	assert w_mat[0,0] == chrom, "Chromosome name differ between files!"
+	assert int(w_mat[0,1]) == v_vec[0], "Positions differ between files!"
+	assert int(w_mat[-1,2]) == v_vec[-1], "Positions differ between files!"
+	s_vec = w_mat[:,1].astype(np.uint32)
+	b_vec = w_mat[:,4].astype(np.uint32)
+	k_vec = w_mat[:,5].astype(np.uint32)
+	W = k_vec.shape[0]
+
+	# Haplotype cluster medians
+	with open(f"{args.ref}.bcm", "rb") as f:
+		# Check magic numbers
+		magic = np.fromfile(f, dtype=np.uint8, count=3)
+		assert np.allclose(magic, np.array([7, 9, 13], dtype=np.uint8)), "Magic number doesn't match file format!"
+		R_arr = np.fromfile(f, dtype=np.uint8)
+		
+	# Load window setup files
+	w_vec = np.genfromtxt(f"{args.ref}.wix", dtype=np.uint32)
+	assert np.allclose(v_vec[w_vec], s_vec), "SNP set doesn't match!"
+	del v_vec, magic
+
+	# Containers
+	Z = np.zeros((W, N), dtype=np.uint8) # Haplotype cluster alleles
+
+	# Clustering
+	B = 0
+	K = 0
+	print(f"Clustering {W} windows.")
+	for w in np.arange(W):
+		print(f"\rWindow {w+1}/{W}", end="")
+		b_win = int(b_vec[w])
+		k_win = int(k_vec[w])
+
+		# Load haplotype window
+		R_mat = R_arr[B:(B + k_win*b_win)]
+		R_mat.shape = (k_win, b_win)
+		X = np.zeros((N, R_mat.shape[1]), dtype=np.uint8)
+		if args.memory:
+			memory_cy.predictBit(G, X, w_vec[w])
+		else:
+			reader_cy.predictHap(G, X, w_vec[w])
+		
+		# Cluster assignment
+		shared_cy.predictCluster(X, R_mat, Z, k_vec[w], w)
+
+		# Update counter
+		B += k_win*b_win
+		K += k_win
+	del G, X, R_mat, R_arr, w_vec
+	print(".\n")
+
+	# Save hapla output and print info
+	h_win = ["#CHROM", "START", "END", "LENGTH", "SIZE", "K"]
+	with open(f"{args.out}.bca", "wb") as f:
+		np.array([7, 9, 13], dtype=np.uint8).tofile(f) # Add magic numbers
+		Z.tofile(f) # Save haplotype cluster assignments to binary file
+	np.savetxt(f"{args.out}.ids", s_list, fmt="%s")
+	np.savetxt(f"{args.out}.win", w_mat, fmt="%s", delimiter="\t", comments="", header="\t".join(h_win))
+	print("\rSaved haplotype clusters in binary hapla format:\n" + \
+		f"- {args.out}.bca\n" + \
+		f"- {args.out}.ids\n" + \
+		f"- {args.out}.win\n")
+
+	# Save haplotype cluster assignments in binary PLINK format
+	if args.plink:
+		print("\rGenerating binary PLINK output.", end="")
+		B = ceil(N/8)
+		K_tot = np.sum(k_vec, dtype=int)
+		P_mat = np.zeros((K_tot, 3), dtype=np.uint32)
+		Z_bin = np.zeros((K_tot, B), dtype=np.uint8)
+		c_vec = np.insert(np.cumsum(k_vec[:-1], dtype=np.uint32), 0, 0)
+		reader_cy.convertPlink(Z, Z_bin, P_mat, k_vec, c_vec, b_vec)
+		
+		# Save .bed file including magic numbers
+		with open(f"{args.out}.bed", "w") as bfile:
+			np.array([108, 27, 1], dtype=np.uint8).tofile(bfile)
+			Z_bin.tofile(bfile)
+		del c_vec, b_vec, Z_bin, Z
+
+		# Save .bim file
+		tmp = np.array([f"{chrom}_W{w}_K{k}_B{l}" for w,k,l in P_mat])
+		bim = np.hstack((
+			np.array([chrom]).repeat(K_tot).reshape(-1, 1),
+			tmp.reshape(-1, 1),
+			np.zeros((K_tot, 1), dtype=np.uint32),
+			s_vec.repeat(k_vec).reshape(-1, 1),
+			np.array(["K"]).repeat(K_tot).reshape(-1, 1),
+			np.zeros((K_tot, 1), dtype=np.uint32)
+		))
+		np.savetxt(f"{args.out}.bim", bim, fmt="%s", delimiter="\t")
+		del k_vec, s_vec, bim, tmp, P_mat
+		
+		# Save .fam file
+		if args.duplicate_fid:
+			s_list = s_list.repeat(2, axis=1)
+		else:
+			s_list = np.hstack((np.zeros((N//2, 1), dtype=np.uint8), s_list))
+		fam = np.hstack((
+			s_list,
+			np.zeros((N//2, 3), dtype=np.uint8),
+			np.full((N//2, 1), -9, dtype=np.int8)
+		))
+		np.savetxt(f"{args.out}.fam", fam, fmt="%s", delimiter="\t")
+
+		# Print info
+		print("\rSaved haplotype cluster alleles in binary PLINK format:\n" + \
+			f"- {args.out}.bed\n" + \
+			f"- {args.out}.bim\n" + \
+			f"- {args.out}.fam\n")
+		del fam, s_list
+
+	# Print elapsed time for computation
+	t_tot = time() - start
+	t_min = int(t_tot//60)
+	t_sec = int(t_tot - t_min*60)
+	print(f"Total elapsed time: {t_min}m{t_sec}s")
+
+	# Write to log-file
+	with open(f"{args.out}.log", "a") as log:
+		log.write("\nSaved haplotype clusters in binary hapla format:\n" + \
+			f"- {args.out}.bca\n" + \
+			f"- {args.out}.ids\n" + \
+			f"- {args.out}.win\n")
+		if args.plink:
+			log.write("\nSaved haplotype cluster alleles in binary PLINK format:\n" + \
+				f"- {args.out}.bed\n" + \
+				f"- {args.out}.bim\n" + \
+				f"- {args.out}.fam\n")
+		log.write(f"\nTotal elapsed time: {t_min}m{t_sec}s\n")
+
+
+
+##### Main exception #####
+assert __name__ != "__main__", "Please use the 'hapla predict' command!"
---
+++ packages/hapla/hapla-0.33.0.tar.gz/hapla-0.33.0/hapla/shared_cy.pyx
@@ -0,0 +1,181 @@
+# cython: language_level=3, boundscheck=False, wraparound=False, initializedcheck=False, cdivision=True
+cimport numpy as np
+from cython.parallel import prange
+from libc.stdint cimport uint8_t, uint32_t
+
+ctypedef uint8_t u8
+ctypedef uint32_t u32
+ctypedef float f32
+
+
+##### hapla - analyses on haplotype cluster assignments #####
+### Inline functions
+# Center function
+cdef inline void _center(
+		const u8* z, f32* x, const f32 u, const Py_ssize_t N
+	) noexcept nogil:
+	cdef:
+		size_t i
+	for i in range(N):
+		x[i] = <f32>z[i] - u
+
+# Standardize function
+cdef inline void _standardize(
+		const u8* z, f32* x, const f32 u, const f32 a, const Py_ssize_t N
+	) noexcept nogil:
+	cdef:
+		size_t i
+	for i in range(N):
+		x[i] = (<f32>z[i] - u)*a
+
+# Calculate Hamming distance
+cdef inline u32 hammingPred(
+		const u8* X, const u8* R, const Py_ssize_t M
+	) noexcept nogil:
+	cdef:
+		size_t j
+		u32 dist = 0
+	for j in range(M):
+		if X[j] != R[j] and X[j] != 9: # Ignore missing
+			dist += 1
+	return dist
+
+
+
+### hapla struct
+# Extract aggregated haplotype cluster counts
+cpdef void haplotypeAggregate(
+		u8[:,::1] Z, u8[:,::1] Z_agg, f32[::1] p, const u32[::1] k_vec, const u32[::1] c_vec
+	) noexcept nogil:
+	cdef:
+		Py_ssize_t W = Z.shape[0]
+		Py_ssize_t N = Z.shape[1]
+		size_t c, i, l, s, w
+		u8* z
+		f32 d = 1.0/<f32>N
+	for w in prange(W, schedule='guided'):
+		s = c_vec[w]
+		for c in range(k_vec[w]):
+			l = s + c
+			z = &Z_agg[l,0]
+			for i in range(N):
+				z[i >> 1] += 1 if Z[w,i] == c else 0
+				p[l] += 1.0 if Z[w,i] == c else 0.0
+			p[l] *= d
+
+# Estimate haplotype cluster frequencies
+cpdef void estimateFreq(
+		u8[:,::1] Z, f32[::1] p, const u32[::1] k_vec, const u32[::1] c_vec
+	) noexcept nogil:
+	cdef:
+		Py_ssize_t W = Z.shape[0]
+		Py_ssize_t N = Z.shape[1]
+		size_t c, i, l, s, w
+		u8* z
+		f32 d = 1.0/<f32>N
+	for w in prange(W, schedule='guided'):
+		s = c_vec[w]
+		z = &Z[w,0]
+		for c in range(k_vec[w]):
+			l = s + c
+			for i in range(N):
+				p[l] += 1.0 if z[i] == c else 0.0
+			p[l] *= d
+
+# Center expanded batch haplotype cluster assignment matrix
+cpdef void centerZ(
+		u8[:,::1] Z_agg, f32[:,::1] X, const f32[::1] p, const int S
+	) noexcept nogil:
+	cdef:
+		Py_ssize_t M = X.shape[0]
+		Py_ssize_t N = X.shape[1]
+		size_t j, l
+		f32 u
+	for j in prange(M, schedule='guided'):
+		l = S + j
+		u = 2.0*p[l]
+		_center(&Z_agg[l,0], &X[j,0], u, N)
+
+# Standardize expanded chunk of haplotype cluster assignment matrix
+cpdef void chunkZ(
+		u8[:,::1] Z_agg, f32[:,::1] X, const f32[::1] p, const f32[::1] a
+	) noexcept nogil:
+	cdef:
+		Py_ssize_t M = X.shape[0]
+		Py_ssize_t N = X.shape[1]
+		size_t j
+		f32 d, u
+	for j in prange(M, schedule='guided'):
+		u = 2.0*p[j]
+		_standardize(&Z_agg[j,0], &X[j,0], u, a[j], N)
+
+# Standardize 
+cpdef void memoryC(
+		u8[:,::1] Z, f32[:,::1] X, const f32[::1] p, const f32[::1] a, const u32[::1] k_vec, const u32[::1] c_vec
+	) noexcept nogil:
+	cdef:
+		Py_ssize_t W = Z.shape[0]
+		Py_ssize_t N = X.shape[1]
+		size_t c, i, l, s, w
+		u8* z
+		f32 u, d
+		f32* x
+	for w in prange(W, schedule='guided'):
+		s = c_vec[w] - c_vec[0]
+		z = &Z[w,0]
+		for c in range(k_vec[w]):
+			l = s + c
+			u = p[l]
+			d = a[l]
+			x = &X[l,0]
+			for i in range(N):
+				x[i] = (1.0 - u)*d if z[2*i] == c else (0.0 - u)*d
+				x[i] += (1.0 - u)*d if z[2*i + 1] == c else (0.0 - u)*d
+
+
+
+### hapla admix
+# Center batch haplotype cluster assignment matrix
+cpdef void centerC(
+		u8[:,::1] Z, f32[:,::1] X, const f32[::1] p, const u32[::1] k_vec, const u32[::1] c_vec
+	) noexcept nogil:
+	cdef:
+		Py_ssize_t W = Z.shape[0]
+		Py_ssize_t N = X.shape[1]
+		size_t c, i, l, s, w
+		u8* z
+		f32 u
+		f32* x
+	for w in prange(W, schedule='guided'):
+		s = c_vec[w] - c_vec[0]
+		z = &Z[w,0]
+		for c in range(k_vec[w]):
+			l = s + c
+			u = p[l]
+			x = &X[l,0]
+			for i in range(N):
+				x[i] = (1.0 - u) if z[2*i] == c else (0.0 - u)
+				x[i] += (1.0 - u) if z[2*i + 1] == c else (0.0 - u)
+
+
+
+### hapla predict
+# Haplotype cluster assignment based on pre-estimated medians
+cpdef void predictCluster(
+		u8[:,::1] X, const u8[:,::1] R, u8[:,::1] Z, const u32 K, const int w
+	) noexcept nogil:
+	cdef:
+		Py_ssize_t N = X.shape[0]
+		Py_ssize_t M = X.shape[1]
+		size_t c, d, i, k, z
+		u8* h
+	for i in prange(N):
+		h = &X[i,0]
+		z = 0
+		c = hammingPred(h, &R[0,0], M)
+		for k in range(1, K):
+			d = hammingPred(h, &R[k,0], M)
+			if d <= c:
+				z = k
+				c = d
+		Z[w,i] = z
