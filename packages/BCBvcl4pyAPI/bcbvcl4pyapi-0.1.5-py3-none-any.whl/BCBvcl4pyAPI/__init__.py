5bf5b703fdee32dbfe62ca58663b601435b2abbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 15:04:34 +0000

    Add package BCBvcl4pyAPI/0.1.5/bcbvcl4pyapi-0.1.5-py3-none-any.whl

---
+++ packages/BCBvcl4pyAPI/bcbvcl4pyapi-0.1.5-py3-none-any.whl/BCBvcl4pyAPI/__init__.py
@@ -0,0 +1,4 @@
+from .BCBvcl4pyAPI import TStringList, TList, DynamicArray
+from .HyperDynamicArrayAPI import HyperDynamicArray
+
+__all__ = ["TStringList", "TList", "DynamicArray", "HyperDynamicArray"]
