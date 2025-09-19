87e5bf766dc24f88f9d4768d7471f4ef1b8adc2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 15:04:15 +0000

    Add package sisaptools/1.18.1/sisaptools-1.18.1.tar.gz

---
+++ packages/sisaptools/sisaptools-1.18.1.tar.gz/sisaptools-1.18.1/sisaptools/services.py
@@ -0,0 +1,23 @@
+# -*- coding: utf8 -*-
+
+"""
+Importació de services des de json.
+"""
+
+import json
+import os
+
+
+json_path = os.path.dirname(os.path.abspath(__file__))
+json_file = '{}/services.json'.format(json_path)
+json_data = json.load(open(json_file))
+
+DB_INSTANCES = json_data['db_instances']
+DB_INSTANCES["p2262"]["port"] = int(os.environ.get("DATABASE_PORT", 3307))
+DB_CREDENTIALS = json_data['db_credentials']
+DB_FALLBACKS = json_data['db_fallbacks']
+REDIS_INSTANCES = json_data['redis_instances']
+MAIL_SERVERS = json_data['mail_servers']
+SSH_SERVERS = json_data['ssh_servers']
+TRELLO_CREDENTIALS = json_data['trello']
+WEKAN_CREDENTIALS = json_data['wekan']
