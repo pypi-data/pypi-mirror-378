bac4dc5d928ecbd089dde62b761081534e5be14d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 14:04:46 +0000

    Add package KDinkar-Pattern/0.1.0/KDinkar_Pattern-0.1.0.tar.gz

---
+++ packages/KDinkar-Pattern/KDinkar_Pattern-0.1.0.tar.gz/KDinkar_Pattern-0.1.0/KDinkar_Pattern/__init__.py
@@ -0,0 +1,3 @@
+from .pattern import (pyramid, right_angle, left_angle)
+
+__all__ = ["pyramid", "right_angle", "left_angle"]
---
+++ packages/KDinkar-Pattern/KDinkar_Pattern-0.1.0.tar.gz/KDinkar_Pattern-0.1.0/setup.py
@@ -0,0 +1,21 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="KDinkar_Pattern",
+    version="0.1.0",
+    author="K Dinkar",
+    author_email="vineetkotari98@gmail.com",
+    description="Sample Package Printing",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/vineet12kotari/dinkar_package",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
