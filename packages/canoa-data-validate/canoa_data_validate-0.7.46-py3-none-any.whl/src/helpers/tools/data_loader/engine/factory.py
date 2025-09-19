3669a6787eea560dc60d04cb1a05422a955dbee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 14:04:36 +0000

    Add package canoa-data-validate/0.7.46/canoa_data_validate-0.7.46-py3-none-any.whl

---
+++ packages/canoa-data-validate/canoa_data_validate-0.7.46-py3-none-any.whl/src/helpers/tools/data_loader/engine/factory.py
@@ -0,0 +1,29 @@
+#  Copyright (c) 2025 Mário Carvalho (https://github.com/MarioCarvalhoBr).
+
+# File: data_loader/factory.py
+"""
+Factory Method para instanciar leitores adequados.
+"""
+
+from pathlib import Path
+from ..common.exceptions import ReaderNotFoundError
+from ..strategies.header import HeaderStrategy
+from ..readers.csv_reader import CSVReader
+from ..readers.excel_reader import ExcelReader
+from ..readers.qml_reader import QMLReader
+
+
+class ReaderFactory:
+    _registry = {
+        ".csv": CSVReader,
+        ".xlsx": ExcelReader,
+        ".qml": QMLReader,
+    }
+
+    @classmethod
+    def get_reader(cls, file_path: Path, header_strategy: HeaderStrategy):
+        ext = file_path.suffix.lower()
+        reader_cls = cls._registry.get(ext)
+        if not reader_cls:
+            raise ReaderNotFoundError(f"Nenhum leitor para extensão '{ext}'")
+        return reader_cls(file_path, header_strategy)
