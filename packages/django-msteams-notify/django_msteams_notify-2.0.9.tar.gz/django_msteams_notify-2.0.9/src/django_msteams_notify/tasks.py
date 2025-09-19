7eeef6cc30b057974db6b3ea5464e0989fcb92a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 14:04:15 +0000

    Add package django-msteams-notify/2.0.9/django_msteams_notify-2.0.9.tar.gz

---
+++ packages/django-msteams-notify/django_msteams_notify-2.0.9.tar.gz/django_msteams_notify-2.0.9/src/django_msteams_notify/tasks.py
@@ -0,0 +1,11 @@
+from celery import shared_task
+from django_msteams_notify.models import TeamsNotification
+from django_msteams_notify.utils import send_teams_message
+
+@shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, max_retries=5)
+def send_teams_notification_task(self, notification_id):
+    notification = TeamsNotification.objects.get(id=notification_id)
+    success = send_teams_message(notification)
+    if not success:
+        raise Exception(f"Failed to send Teams notification ID {notification_id}")
+    return True
