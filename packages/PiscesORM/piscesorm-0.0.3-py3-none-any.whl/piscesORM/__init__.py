a77007b4c257f3aa1aa73ea0a5c7859c6f46d9c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 15:04:08 +0000

    Add package PiscesORM/0.0.3/piscesorm-0.0.3-py3-none-any.whl

---
+++ packages/PiscesORM/piscesorm-0.0.3-py3-none-any.whl/piscesORM/__init__.py
@@ -0,0 +1,12 @@
+from . import column
+from . import table
+from . import operator
+from . import generator
+from . import engine
+from . import errors
+from . import lock
+from . import session
+from . import _setting
+
+__version__ = "0.0.3"
+setting = _setting.setting
