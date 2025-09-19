e697224507ad39f3e686a2216b17c079c95335aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 14:04:36 +0000

    Add package superb-ai-onprem/0.7.2/superb_ai_onprem-0.7.2.tar.gz

---
+++ packages/superb-ai-onprem/superb_ai_onprem-0.7.2.tar.gz/superb_ai_onprem-0.7.2/spb_onprem/contents/params/__init__.py
@@ -0,0 +1,10 @@
+
+from .create import create_variables
+from .get_download_url import get_download_url_params
+from .delete_content import delete_content_params
+
+__all__ = (
+    "create_variables",
+    "get_download_url_params",
+    "delete_content_params",
+)
