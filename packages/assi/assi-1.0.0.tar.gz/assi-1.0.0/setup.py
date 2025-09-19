2998cf11fcd9ba2534e11dae9f9fb1a9b850826d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 16:05:00 +0000

    Add package assi/1.0.0/assi-1.0.0.tar.gz

---
+++ packages/assi/assi-1.0.0.tar.gz/assi-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="assi",
+    version="1.0.0",
+    description="A Super assistant in Python! There a lot of thing, And 1.0.0 is expand to Everything be Usable !",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
