edf7ee5d8b8bef8c48db97c597ce94a95779627a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 16:04:28 +0000

    Add package whitebox-plugin-flight-management/0.1.7/whitebox_plugin_flight_management-0.1.7-py3-none-any.whl

---
+++ packages/whitebox-plugin-flight-management/whitebox_plugin_flight_management-0.1.7-py3-none-any.whl/whitebox_plugin_flight_management/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.utils import timezone
+
+
+class FlightSessionQuerySet(models.QuerySet):
+    def active(self):
+        return self.filter(ended_at__isnull=True)
+
+    def current(self):
+        return self.active().first()
+
+    async def acurrent(self):
+        return await self.active().afirst()
+
+
+class FlightSession(models.Model):
+    started_at = models.DateTimeField(default=timezone.now)
+    ended_at = models.DateTimeField(null=True, blank=True)
+
+    objects = FlightSessionQuerySet.as_manager()
+
+    @property
+    def is_active(self):
+        return self.ended_at is None
+
+
+class FlightSessionRecording(models.Model):
+    flight_session = models.ForeignKey(
+        FlightSession,
+        on_delete=models.CASCADE,
+        related_name="recordings",
+    )
+
+    created_at = models.DateTimeField(default=timezone.now)
+    file = models.FileField()
