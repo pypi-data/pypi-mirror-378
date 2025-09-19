bc5c50ac1f639ceb08dadef98693ee09684468f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 14:04:14 +0000

    Add package scikit-robot/0.2.19/scikit_robot-0.2.19-py3-none-any.whl

---
+++ packages/scikit-robot/scikit_robot-0.2.19-py3-none-any.whl/skrobot/apps/visualize_urdf.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import argparse
+import os.path as osp
+import time
+
+import skrobot
+from skrobot.model import RobotModel
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Visualize URDF')
+    parser.add_argument('input_urdfpath', type=str, help='Input URDF path')
+    parser.add_argument(
+        '--viewer', type=str,
+        choices=['trimesh', 'pyrender'], default='pyrender',
+        help='Choose the viewer type: trimesh or pyrender')
+    parser.add_argument(
+        '--interactive', '-i',
+        action='store_true',
+        help='enter interactive shell'
+    )
+    args = parser.parse_args()
+
+    if args.viewer == 'trimesh':
+        viewer = skrobot.viewers.TrimeshSceneViewer(update_interval=0.1)
+    elif args.viewer == 'pyrender':
+        viewer = skrobot.viewers.PyrenderViewer(update_interval=0.1)
+    robot_model = RobotModel.from_urdf(
+        osp.abspath(args.input_urdfpath))
+    viewer.add(robot_model)
+    viewer.show()
+    if args.interactive:
+        try:
+            import IPython
+        except Exception as e:
+            print("IPython is not installed. {}".format(e))
+            return
+        IPython.embed()
+    else:
+        while viewer.is_active:
+            viewer.redraw()
+            time.sleep(0.1)
+        viewer.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/scikit-robot/scikit_robot-0.2.19-py3-none-any.whl/skrobot/interfaces/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+
+from . import _pybullet as pybullet
+from ._pybullet import PybulletRobotInterface
+
+from .ros import *
