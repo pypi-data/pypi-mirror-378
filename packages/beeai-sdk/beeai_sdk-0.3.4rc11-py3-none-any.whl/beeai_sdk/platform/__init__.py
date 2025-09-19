66accbfdf18ab2999ef102ad4d11ce394f0b753f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 15:04:30 +0000

    Add package beeai-sdk/0.3.4rc11/beeai_sdk-0.3.4rc11-py3-none-any.whl

---
+++ packages/beeai-sdk/beeai_sdk-0.3.4rc11-py3-none-any.whl/beeai_sdk/platform/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2025 © BeeAI a Series of LF Projects, LLC
+# SPDX-License-Identifier: Apache-2.0
+
+from .client import *
+from .configuration import *
+from .file import *
+from .model_provider import *
+from .provider import *
+from .vector_store import *
