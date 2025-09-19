4edc922896c5cf9d876715c93b82db97c13f5c42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 16:04:53 +0000

    Add package metbit/8.7.3/metbit-8.7.3-py3-none-any.whl

---
+++ packages/metbit/metbit-8.7.3-py3-none-any.whl/metbit/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from .metbit import opls_da, pca
+from .utility import UnivarStats, Normalise
+from .lazy_opls_da import lazy_opls_da
+from .spec_norm import *
+from .peak_processe import peak_chops
+from .STOCSY import STOCSY
+from .ui_stocsy import STOCSY_app
+from .ui_picky_peak import pickie_peak
+from .take_intensity import *
+from .nmr_preprocess import nmr_preprocessing
+from .calibrate import calibrate
+from .annotate_peak import annotate_peak
+
+
+
+# -*- coding: utf-8 -*-
+
+__author__ = "aeiwz"
+__email__ = "theerayut_aeiw_123@hotmail.com"
+__maintainer__ = "aeiwz"
+__status__ = "Development"
+__copyright__ = "Copyright 2024, Theerayut"
