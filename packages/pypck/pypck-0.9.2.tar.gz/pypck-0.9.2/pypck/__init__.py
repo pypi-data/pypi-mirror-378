5c347f85b17e8550e90f29c9fd18d5ec1bc5c811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 14:04:51 +0000

    Add package pypck/0.9.2/pypck-0.9.2.tar.gz

---
+++ packages/pypck/pypck-0.9.2.tar.gz/pypck-0.9.2/pypck/__init__.py
@@ -0,0 +1,21 @@
+"""Init file for pypck."""
+
+from pypck import (
+    connection,
+    helpers,
+    inputs,
+    lcn_addr,
+    lcn_defs,
+    module,
+    pck_commands,
+)
+
+__all__ = [
+    "connection",
+    "inputs",
+    "helpers",
+    "lcn_addr",
+    "lcn_defs",
+    "module",
+    "pck_commands",
+]
