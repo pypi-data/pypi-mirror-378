152545710003e4d6c0fc2b119733ee5f541442b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 15:04:35 +0000

    Add package breathe-design/1.0.0/breathe_design-1.0.0-py3-none-any.whl

---
+++ packages/breathe-design/breathe_design-1.0.0-py3-none-any.whl/breathe_design/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "1.0.0"
+__date__ = "19/09/2025"
+
+from .api_interface import *  # NOQA
+from .cycler import *  # NOQA
+from .plots import *  # NOQA
+from .dynamic_analysis import *  # NOQA
+from .utilities import *  # NOQA
+from .format import *  # NOQA
+from .results import *  # NOQA
