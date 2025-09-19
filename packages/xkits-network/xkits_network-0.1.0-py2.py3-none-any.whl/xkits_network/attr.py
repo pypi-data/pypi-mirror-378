1a2593a0b92ade1e6901d09361cd47d9fefabb45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 14:04:39 +0000

    Add package xkits-network/0.1.0/xkits_network-0.1.0-py2.py3-none-any.whl

---
+++ packages/xkits-network/xkits_network-0.1.0-py2.py3-none-any.whl/xkits_network/attr.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+
+__project__ = "xkits-network"
+__version__ = "0.1.0"
+__urlhome__ = "https://github.com/bondbox/xnetwork/"
+__description__ = "Network module"
+
+# author
+__author__ = "Mingzhe Zou"
+__author_email__ = "zoumingzhe@outlook.com"
