e8905ba7867019a2b3d97a7942bdfbc92bb70051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 14:04:10 +0000

    Add package wuying-agentbay-sdk/0.8.0/wuying_agentbay_sdk-0.8.0.tar.gz

---
+++ packages/wuying-agentbay-sdk/wuying_agentbay_sdk-0.8.0.tar.gz/wuying_agentbay_sdk-0.8.0/agentbay/config.py
@@ -0,0 +1,54 @@
+import json
+import os
+from typing import Any, Dict
+import dotenv
+from pathlib import Path
+from agentbay.logger import get_logger
+
+# Initialize logger for this module
+logger = get_logger("config")
+
+
+def default_config() -> Dict[str, Any]:
+    """Return the default configuration"""
+    return {
+        "region_id": "cn-shanghai",
+        "endpoint": "wuyingai.cn-shanghai.aliyuncs.com",
+        "timeout_ms": 60000,
+    }
+
+
+# Browser data path constant
+BROWSER_DATA_PATH = "/tmp/agentbay_browser"
+
+
+"""
+The SDK uses the following precedence order for configuration (highest to lowest):
+1. Explicitly passed configuration in code.
+2. Environment variables.
+3. .env file.
+4. Default configuration.
+"""
+
+
+def load_config(cfg) -> Dict[str, Any]:
+    if cfg is not None:
+        config = {
+            "region_id": cfg.region_id,
+            "endpoint": cfg.endpoint,
+            "timeout_ms": cfg.timeout_ms,
+        }
+    else:
+        config = default_config()
+        try:
+            env_path = Path(os.getcwd()) / ".env"
+            dotenv.load_dotenv(env_path)
+        except:
+            logger.warning("Failed to load .env file")
+        if region_id := os.getenv("AGENTBAY_REGION_ID"):
+            config["region_id"] = region_id
+        if endpoint := os.getenv("AGENTBAY_ENDPOINT"):
+            config["endpoint"] = endpoint
+        if timeout_ms := os.getenv("AGENTBAY_TIMEOUT_MS"):
+            config["timeout_ms"] = int(timeout_ms)
+    return config
