40ae1effee32bc353ab67179b5a4696126404f2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 14:04:37 +0000

    Add package nvchecker/2.19/nvchecker-2.19.tar.gz

---
+++ packages/nvchecker/nvchecker-2.19.tar.gz/nvchecker-2.19/tests/test_aur.py
@@ -0,0 +1,26 @@
+# MIT licensed
+# Copyright (c) 2013-2020 lilydjwg <lilydjwg@gmail.com>, et al.
+
+import pytest
+pytestmark = [pytest.mark.asyncio,
+              pytest.mark.needs_net]
+
+@pytest.mark.flaky(reruns=10)
+async def test_aur(get_version):
+    assert await get_version("ssed", {
+        "source": "aur",
+    }) == "3.62-2"
+
+@pytest.mark.flaky(reruns=10)
+async def test_aur_strip_release(get_version):
+    assert await get_version("ssed", {
+        "source": "aur",
+        "strip_release": 1,
+    }) == "3.62"
+
+@pytest.mark.flaky(reruns=10)
+async def test_aur_use_last_modified(get_version):
+    assert await get_version("ssed", {
+        "source": "aur",
+        'use_last_modified': True,
+    }) == "3.62-2-20150725052412"
