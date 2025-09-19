fcb46be19f40180d27d9289339d594d5c3df9a6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 14:03:50 +0000

    Add package GridSeisPy/0.1.8/gridseispy-0.1.8-py3-none-any.whl

---
+++ packages/GridSeisPy/gridseispy-0.1.8-py3-none-any.whl/GridSeisPy/__init__.py
@@ -0,0 +1,9 @@
+"""
+SeisPy is a Python library for seismic data processing.
+"""
+
+__version__ = "0.1.8"
+
+from .seis_data import SeisData
+from .horiz import Horiz
+from .mapping import TraceField, BinField, CVDFile, Trace
