b321af7f6c2ee2dd339acb0b679a1f6742aec06b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 16:04:47 +0000

    Add package unimi-crop-sensing/0.9/unimi_crop_sensing-0.9.tar.gz

---
+++ packages/unimi-crop-sensing/unimi_crop_sensing-0.9.tar.gz/unimi_crop_sensing-0.9/crop_sensing/main.py
@@ -0,0 +1,54 @@
+import zed_manager
+import find_plant
+import cobot_manager
+import create_plc
+import cv2
+
+# testing parameters
+linux_ip = "192.168.5.6"
+plants_number = 10
+
+# This function is used to test the functionalities of the crop sensing module
+def main():
+
+    # Initialize the ZED camera
+    zed = zed_manager.zed_init()
+    
+    # Start loop
+    frame = 1
+    for frame in range(100):
+        # Capture the environment with the ZED camera
+        image, depth_map, normal_map, point_cloud = zed_manager.get_zed_image(zed, save=True)
+
+        # Filter the plants from the background
+        mask = find_plant.filter_plants(image, save_mask=True)
+        
+        # Divide the plants into clusters
+        masks, bounding_boxes = find_plant.segment_plants(mask, plants_number)
+        find_plant.save_clustered_image(image, bounding_boxes)
+        
+        # Save bounding boxes to a txt file in crop_sensing/data
+        log_file = "crop_sensing/data/log.txt"
+        with open(log_file, "w") as f:
+            for i, bbox in enumerate(bounding_boxes):
+                f.write(f"Frame {frame}: {bbox}\n")
+        
+        # Extract the 3D points from the clusters
+        for m in masks:
+            bbxpts = find_plant.plot_3d_bbox(m, point_cloud)
+        frame += 1
+        
+    # Communicate the bounding boxes to the cobot (only if the cobot is operated in another machine)
+    #cobot_manager.send_cobot_map(linux_ip, bbxpts)
+
+    zed.close()
+    
+    # Create point cloud (this will create a .ply file by taking a video of the environment)
+    #create_plc.record_and_save(plant_name='piantina1', frames=300)
+
+
+
+if __name__ == "__main__":
+    main()
+    
+
