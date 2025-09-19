7f9354418cf87517e4a3f0fca0c49d9054a6daf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 16:04:41 +0000

    Add package furthrmind/0.2.6/furthrmind-0.2.6.tar.gz

---
+++ packages/furthrmind/furthrmind-0.2.6.tar.gz/furthrmind-0.2.6/furthrmind/utils.py
@@ -0,0 +1,38 @@
+from functools import wraps
+from types import MethodType
+
+def furthr_wrap(force_list=False):
+    def decorator(function):
+        @wraps(function)
+        def wrapper(*args, **kws):
+            response = function(*args, **kws)
+            data = None
+            if response.status_code in [200, 400, 401, 403, 500]:
+                data = response.json()
+            
+            if response.status_code in [404, 500]:
+                print("error", response.status_code)
+                if data is not None:
+                    print("message", data["message"])
+                    raise ValueError(f"Server returned an error, {data['message']}")
+                else:
+                    raise ValueError("Server returned an error")
+
+            if data["status"] == "error":
+                print("error", data["message"])
+                return
+            else:
+                if force_list:
+                    return data["results"]
+                if len(data["results"]) == 1:
+                    return data["results"][0]
+                else:
+                    return data["results"]
+        return wrapper
+    return decorator
+
+def instance_overload(self, methods):
+    """ Adds instance overloads for one or more classmethods"""
+    for name in methods:
+        if hasattr(self, name):
+            setattr(self, name, MethodType(getattr(self, name).__func__, self))
