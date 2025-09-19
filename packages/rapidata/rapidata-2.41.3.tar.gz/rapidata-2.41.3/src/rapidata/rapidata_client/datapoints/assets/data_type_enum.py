b588e4a63123ef25adb4aeebb48016eadcad97b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 16:04:53 +0000

    Add package rapidata/2.41.3/rapidata-2.41.3.tar.gz

---
+++ packages/rapidata/rapidata-2.41.3.tar.gz/rapidata-2.41.3/src/rapidata/rapidata_client/datapoints/assets/data_type_enum.py
@@ -0,0 +1,8 @@
+class RapidataDataTypes:
+    # deprecated use Literal["media", "text"] instead
+    MEDIA = "media"  # any form of image, video or audio
+    TEXT = "text"
+
+    @classmethod
+    def _possible_values(cls):
+        return [cls.MEDIA, cls.TEXT]
---
+++ packages/rapidata/rapidata-2.41.3.tar.gz/rapidata-2.41.3/src/rapidata/rapidata_client/datapoints/metadata/_media_asset_metadata.py
@@ -0,0 +1,23 @@
+from rapidata.api_client.models.prompt_asset_metadata_input import (
+    PromptAssetMetadataInput,
+)
+from rapidata.api_client.models.url_asset_input import UrlAssetInput
+from rapidata.rapidata_client.datapoints.metadata._base_metadata import Metadata
+from rapidata.api_client.models.prompt_asset_metadata_input_asset import (
+    PromptAssetMetadataInputAsset,
+)
+
+
+class MediaAssetMetadata(Metadata):
+
+    def __init__(self, url: str):
+        super().__init__()
+        self._url = url
+
+    def to_model(self):
+        return PromptAssetMetadataInput(
+            _t="PromptAssetMetadataInput",
+            asset=PromptAssetMetadataInputAsset(
+                actual_instance=UrlAssetInput(_t="UrlAssetInput", url=self._url)
+            ),
+        )
