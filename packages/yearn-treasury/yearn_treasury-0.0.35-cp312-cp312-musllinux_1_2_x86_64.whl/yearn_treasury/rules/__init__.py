515c90ca930a01765824b7d7032c0221df05c8e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-19 14:04:14 +0000

    Add package yearn-treasury/0.0.35/yearn_treasury-0.0.35-cp312-cp312-musllinux_1_2_x86_64.whl

---
+++ packages/yearn-treasury/yearn_treasury-0.0.35-cp312-cp312-musllinux_1_2_x86_64.whl/yearn_treasury/rules/__init__.py
@@ -0,0 +1,6 @@
+from yearn_treasury.rules.cost_of_revenue import *
+from yearn_treasury.rules.expense import *
+from yearn_treasury.rules.ignore import *
+from yearn_treasury.rules.other_expense import *
+from yearn_treasury.rules.other_income import *
+from yearn_treasury.rules.revenue import *
