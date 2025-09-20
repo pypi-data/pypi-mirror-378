0ce8d2f5f4e557772ede0897593276fe8aeb8389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 04:05:35 +0000

    Add package snakemake-software-deployment-plugin-conda/0.3.2/snakemake_software_deployment_plugin_conda-0.3.2.tar.gz

---
+++ packages/snakemake-software-deployment-plugin-conda/snakemake_software_deployment_plugin_conda-0.3.2.tar.gz/snakemake_software_deployment_plugin_conda-0.3.2/CHANGELOG.md
@@ -0,0 +1,40 @@
+# Changelog
+
+## [0.3.2](https://github.com/snakemake/snakemake-software-deployment-plugin-conda/compare/v0.3.1...v0.3.2) (2025-09-19)
+
+
+### Bug Fixes
+
+* update to latest interface ([72f4ca9](https://github.com/snakemake/snakemake-software-deployment-plugin-conda/commit/72f4ca90971f594bb1a86428063d74b00c7c89c1))
+* update to latest interface ([e806317](https://github.com/snakemake/snakemake-software-deployment-plugin-conda/commit/e806317efc9ebe1660cfb01425f97075e0eb66aa))
+
+## [0.3.1](https://github.com/snakemake/snakemake-software-deployment-plugin-conda/compare/v0.3.0...v0.3.1) (2025-09-15)
+
+
+### Bug Fixes
+
+* capture STDOUT of conda info commands ([314e43a](https://github.com/snakemake/snakemake-software-deployment-plugin-conda/commit/314e43a02b8ce0c546d08144bab7660043fa38dc))
+* use latest interface ([c6b2217](https://github.com/snakemake/snakemake-software-deployment-plugin-conda/commit/c6b22178b4ee6b6a15b2e82839d7ea2d681db0a3))
+* use proper env prefix ([7893471](https://github.com/snakemake/snakemake-software-deployment-plugin-conda/commit/7893471eee3379fc5dcebf3e74b594b7317d23b1))
+
+## [0.3.0](https://github.com/snakemake/snakemake-software-deployment-plugin-conda/compare/v0.2.0...v0.3.0) (2025-09-12)
+
+
+### Features
+
+* complete support for named environments ([#7](https://github.com/snakemake/snakemake-software-deployment-plugin-conda/issues/7)) ([b38aecd](https://github.com/snakemake/snakemake-software-deployment-plugin-conda/commit/b38aecdc93e3058d219e2b7fe8bdfcee9d59a690))
+
+## [0.2.0](https://github.com/snakemake/snakemake-software-deployment-plugin-conda/compare/v0.1.0...v0.2.0) (2025-05-16)
+
+
+### Features
+
+* add pypi support via uv ([#2](https://github.com/snakemake/snakemake-software-deployment-plugin-conda/issues/2)) ([728c71a](https://github.com/snakemake/snakemake-software-deployment-plugin-conda/commit/728c71a8d4d62334ddb516acb23c992b5bcb656e))
+* support pinfiles ([#5](https://github.com/snakemake/snakemake-software-deployment-plugin-conda/issues/5)) ([58faba4](https://github.com/snakemake/snakemake-software-deployment-plugin-conda/commit/58faba47e8bb782dea27427f8419e4e5b47a6b20))
+
+## 0.1.0 (2025-03-08)
+
+
+### Bug Fixes
+
+* update to latest interface ([0159285](https://github.com/snakemake/snakemake-software-deployment-plugin-conda/commit/015928520a4d87bb545cd2c1ab21f45f36466738))
---
+++ packages/snakemake-software-deployment-plugin-conda/snakemake_software_deployment_plugin_conda-0.3.2.tar.gz/snakemake_software_deployment_plugin_conda-0.3.2/tests/test_plugin.py
@@ -0,0 +1,95 @@
+import os
+from pathlib import Path
+from typing import Optional, Type
+from snakemake_interface_software_deployment_plugins.tests import (
+    TestSoftwareDeploymentBase,
+)
+from snakemake_interface_software_deployment_plugins import (
+    EnvSpecBase,
+    EnvBase,
+    EnvSpecSourceFile,
+)
+from snakemake_interface_software_deployment_plugins.settings import (
+    SoftwareDeploymentSettingsBase,
+)
+from snakemake_software_deployment_plugin_conda import (
+    Env,
+    EnvSpec,
+)
+
+
+# There can be multiple subclasses of SoftwareDeploymentProviderBase here.
+# This way, you can implement multiple test scenarios.
+# For each subclass, the test suite tests the environment activation and execution
+# within, and, if applicable, environment deployment and archiving.
+class Test(TestSoftwareDeploymentBase):
+    __test__ = True  # activate automatic testing
+
+    def get_env_spec(self) -> EnvSpecBase:
+        return EnvSpec(
+            envfile=EnvSpecSourceFile(Path(__file__).parent / "test_env.yaml")
+        )
+
+    def get_env_cls(self) -> Type[EnvBase]:
+        # Return the environment class that should be tested.
+        return Env
+
+    def get_settings_cls(self) -> Optional[Type[SoftwareDeploymentSettingsBase]]:
+        # Return the settings class that should be used for this plugin.
+        return None
+
+    def get_settings(
+        self,
+    ) -> Optional[SoftwareDeploymentSettingsBase]:
+        # If your plugin has settings, return a valid settings object here.
+        # Otherwise, return None.
+        return None
+
+    def get_test_cmd(self) -> str:
+        # Return a test command that should be executed within the environment
+        # with exit code 0 (i.e. without error).
+        return "stress-ng --cpu 1 --timeout 1s"
+
+
+# TODO requires https://github.com/conda/rattler/pull/1206 to be released
+class TestPinned(Test):
+    def get_env_spec(self) -> EnvSpecBase:
+        return EnvSpec(
+            envfile=EnvSpecSourceFile(Path(__file__).parent / "test_env_pinned.yaml")
+        )
+
+
+class TestPypi(Test):
+    def get_env_spec(self) -> EnvSpecBase:
+        return EnvSpec(
+            envfile=EnvSpecSourceFile(Path(__file__).parent / "test_env_pypi.yaml")
+        )
+
+    def get_test_cmd(self) -> str:
+        # Return a test command that should be executed within the environment
+        # with exit code 0 (i.e. without error).
+        return "which python; python -c 'import humanfriendly'"
+
+
+class TestNamed(Test):
+    __test__ = os.environ.get("TEST_NAMED_ENV") == "1"
+
+    def get_env_spec(self) -> EnvSpecBase:
+        return EnvSpec(name="test-env")
+
+    def get_test_cmd(self) -> str:
+        # Return a test command that should be executed within the environment
+        # with exit code 0 (i.e. without error).
+        return "stress-ng --cpu 1 --timeout 1s"
+
+
+class TestDirectory(Test):
+    __test__ = os.environ.get("TEST_DIRECTORY_ENV") == "1"
+
+    def get_env_spec(self) -> EnvSpecBase:
+        return EnvSpec(directory=Path(os.environ["TEST_ENV_DIR"]))
+
+    def get_test_cmd(self) -> str:
+        # Return a test command that should be executed within the environment
+        # with exit code 0 (i.e. without error).
+        return "stress-ng --cpu 1 --timeout 1s"
