c4ad3c7964672c85c8e0339c2b8cac73aafbb216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 15:05:07 +0000

    Add package pygremlinbox-epl-2-0/1.0.1/pygremlinbox_epl_2_0-1.0.1-py3-none-any.whl

---
+++ packages/pygremlinbox-epl-2-0/pygremlinbox_epl_2_0-1.0.1-py3-none-any.whl/pygremlinbox_epl_2_0/__init__.py
@@ -0,0 +1,7 @@
+"""PyGremlinBox EPL-2.0 - Supply chain security testing package."""
+
+__version__ = "0.1.0"
+
+from .licence_reader import retrieve_licence_content, get_licence_identifier, get_package_metadata
+
+__all__ = ["retrieve_licence_content", "get_licence_identifier", "get_package_metadata"]
