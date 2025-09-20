4286203a43602c014675b1cfb262f9ddd0840fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 05:05:01 +0000

    Add package psdtags/2025.9.19/psdtags-2025.9.19-py3-none-any.whl

---
+++ packages/psdtags/psdtags-2025.9.19-py3-none-any.whl/psdtags/__init__.py
@@ -0,0 +1,17 @@
+# psdtags/__init__.py
+
+from .psdtags import *
+from .psdtags import __all__, __doc__, __version__
+
+
+def _set_module() -> None:
+    """Set __module__ attribute for all public objects."""
+    globs = globals()
+    module = globs['__name__']
+    for item in __all__:
+        obj = globs[item]
+        if hasattr(obj, '__module__'):
+            obj.__module__ = module
+
+
+_set_module()
