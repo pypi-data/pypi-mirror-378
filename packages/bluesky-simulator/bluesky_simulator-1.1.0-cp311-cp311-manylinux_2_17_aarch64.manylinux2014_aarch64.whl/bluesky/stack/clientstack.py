4eff381b4fe06cedc56d00cecd87484cb85b7a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 03:12:24 +0000

    Add package bluesky-simulator/1.1.0/bluesky_simulator-1.1.0-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/bluesky-simulator/bluesky_simulator-1.1.0-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/bluesky/stack/clientstack.py
@@ -0,0 +1,172 @@
+from pathlib import Path
+import subprocess
+import traceback
+import math
+
+import bluesky as bs
+from bluesky.core.signal import Signal
+from bluesky.stack.stackbase import Stack, forward, stack
+from bluesky.stack.cmdparser import Command, command, commandgroup
+from bluesky.stack import argparser
+
+
+# Globals
+_sig_echo = Signal('echo')
+
+
+def init():
+    ''' client-side stack initialisation. '''
+    # Display Help text on start of program
+    stack("ECHO BlueSky Console Window: Enter HELP or ? for info.\n"
+          "Or select IC to Open a scenario file.")
+
+
+def process():
+    ''' Client-side stack processing. '''
+    # Process stack of commands
+    for cmdline in Stack.commands():
+        success = True
+        echotext = ''
+        echoflags = bs.BS_OK
+
+        # Get first argument from command line and check if it's a command
+        cmd, argstring = argparser.getnextarg(cmdline)
+        cmdu = cmd.upper()
+        cmdobj = Command.cmddict.get(cmdu)
+
+        # Proceed if a command object was found
+        if cmdobj:
+            try:
+                # Call the command, passing the argument string
+                success, echotext = cmdobj(argstring)
+                if not success:
+                    if not argstring:
+                        echotext = echotext or cmdobj.brieftext()
+                    else:
+                        echoflags = bs.BS_FUNERR
+                        echotext = f'Syntax error: {echotext or cmdobj.brieftext()}'
+
+            except Exception as e:
+                success = False
+                echoflags = bs.BS_ARGERR
+                header = '' if not argstring else e.args[0] if e.args else 'Argument error.'
+                echotext = f'{header}\nUsage:\n{cmdobj.brieftext()}'
+                traceback.print_exc()
+
+        # ----------------------------------------------------------------------
+        # ZOOM command (or use ++++  or --  to zoom in or out)
+        # ----------------------------------------------------------------------
+        elif cmdu[0] in ("+", "=", "-"):
+            # = equals + (same key)
+            nplus = cmdu.count("+") + cmdu.count("=")
+            nmin = cmdu.count("-")
+            fac = math.sqrt(2) ** (nplus - nmin)
+            cmdu = 'ZOOM'
+            cmdobj = Command.cmddict.get(cmdu)
+            if cmdobj:
+                cmdobj(f'IN {fac}')
+
+        elif Stack.sender_id is None:
+            # If sender_id is None, this stack command originated from the gui. Send it on to the sim
+            forward(target_id=bs.net.act_id)
+        # -------------------------------------------------------------------
+        # Command not found
+        # -------------------------------------------------------------------
+        else:
+            success = False
+            echoflags = bs.BS_CMDERR
+            if not argstring:
+                echotext = f'Unknown command or aircraft: {cmd}'
+            else:
+                echotext = f'Unknown command: {cmd}'
+
+        # Always return on command
+        if echotext:
+            echo(echotext, echoflags, Stack.sender_id)
+
+    # Clear the processed commands
+    Stack.cmdstack.clear()
+
+
+@command(annotations='string')
+def echo(text='', flags=0, sender_id=None):
+        ''' Echo
+
+            Clien-side implementation of ECHO emits the same
+            signal as the one triggered by incoming echo
+            messages.    
+        '''
+        _sig_echo.emit(text, flags, sender_id)
+
+
+@commandgroup(name='HELP', aliases=('?',))
+def showhelp(cmd: 'txt' = '', subcmd: 'txt' = ''):
+    """ HELP: Display general help text or help text for a specific command,
+        or dump command reference in file when command is >filename.
+
+        Arguments:
+        - cmd: Argument can refer to:
+            - Command name to display help for. 
+            - Call HELP >filename to generate a CSV file with help text for all commands.
+            - Call HELP PDF to view a pdf containing information on all stack commands.
+
+        To get more detailed information on a command type DOC cmd.
+    """
+
+    # Check if help is asked for a specific command
+    cmdobj = Command.cmddict.get(cmd or 'HELP')
+    if cmdobj:
+        return True, cmdobj.helptext(subcmd)
+
+    # If command is not a known Client command pass the help request on to the sim
+    forward(target_id=bs.net.act_id)
+
+
+@showhelp.subcommand
+def pdf():
+    ''' Open a pdf file with BlueSky command help text. '''
+    pdfhelp = Path("BLUESKY-COMMAND-TABLE.pdf")
+    if (Path('docs') / pdfhelp).is_file():
+        try:
+            subprocess.Popen(pdfhelp, shell=True, cwd='docs')
+        except:
+            return "Opening " + pdfhelp.as_posix() + " failed."
+    else:
+        return pdfhelp.as_posix() + "does not exist."
+
+    return "Pdf window opened"
+
+
+@command
+def makedoc():
+    ''' MAKEDOC: Make markdown templates for all stack functions
+        that don't have a doc page yet.
+    '''
+    tmp = Path('tmp')
+    if not tmp.is_dir():
+        tmp.mkdir()
+    # Get unique set of commands
+    cmdobjs = set(Command.cmddict.values())
+    for o in cmdobjs:
+        if not bs.resource(f"html/{o.name}.html").is_file():
+            with open(tmp / f"{o.name.lower()}.md", "w") as f:
+                f.write(
+                    f"# {o.name}: {o.name.capitalize()}\n"
+                    + o.help
+                    + "\n\n"
+                    + "**Usage:**\n\n"
+                    + f"    {o.brief}\n\n"
+                    + "**Arguments:**\n\n"
+                )
+                if not o.params:
+                    f.write("This command has no arguments.\n\n")
+                else:
+                    f.write(
+                        "|Name|Type|Optional|Description\n"
+                        + "|--------|------|---|---------------------------------------------------\n"
+                    )
+                    for arg in o.params:
+                        f.write(str(arg).replace(':', '|') + f" |{arg.hasdefault()}|\n")
+                f.write("\n[[Back to command reference.|Command Reference]]\n")
+    # To also get all of the sim stack commands, forward to sim
+    forward()
---
+++ packages/bluesky-simulator/bluesky_simulator-1.1.0-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/bluesky/traffic/performance/openap/coeff.py
@@ -0,0 +1,176 @@
+""" OpenAP performance library. """
+import json
+import pandas as pd
+import bluesky as bs
+from bluesky import stack
+
+from openap import prop, WRAP, drag
+import numpy as np
+
+bs.settings.set_variable_defaults(perf_path_openap="performance/OpenAP")
+
+LIFT_FIXWING = 1  # fixwing aircraft
+LIFT_ROTOR = 2  # rotor aircraft
+
+ENG_TYPE_TF = 1  # turbofan, fixwing
+ENG_TYPE_TP = 2  # turboprop, fixwing
+ENG_TYPE_TS = 3  # turboshlft, rotor
+
+
+class Coefficient:
+    def __init__(self):
+        self.actypes_fixwing = prop.available_aircraft(use_synonym=True) # fixed wing types from openap
+        self.acs_fixwing = self._load_all_fixwing_flavor()
+        self.limits_fixwing = self._load_all_fixwing_envelop()
+
+        self.acs_rotor = self._load_all_rotor_flavor()
+        self.limits_rotor = self._load_all_rotor_envelop()
+        self.actypes_rotor = list(self.acs_rotor.keys())
+
+        self.dragpolar_fixwing = self._load_fixedwing_dragpolar()
+
+    def _load_all_fixwing_flavor(self):
+        import warnings
+        warnings.simplefilter("ignore")
+
+        # load fixwing aircraft and engine from openap
+        acs = {}
+        # match acs_ with openap native data
+        for mdl in self.actypes_fixwing:
+            ac = prop.aircraft(mdl, use_synonym=True)
+            acs[mdl.upper()] = ac.copy()
+            engines = []
+            engines.append(prop.engine(ac['engine']['default']))
+            # options can have repeated strings as default or dicts (with model variant as key), do we handle this?
+            # engines.append([prop.engine(e) for e in ac_['engine']['options']])
+            acs[mdl.upper()]['engines'] = {}
+            for e in engines:
+                acs[mdl.upper()]['engines'][e['name']] = e.copy()
+        return acs
+
+    def _load_all_rotor_flavor(self):
+        # read rotor aircraft
+        acs = json.load(open(bs.resource(bs.settings.perf_path_openap) / "rotor/aircraft.json", "r"))
+        acs.pop("__comment")
+        acs_ = {}
+        for mdl, ac in acs.items():
+            acs_[mdl.upper()] = ac.copy()
+            acs_[mdl.upper()]["lifttype"] = LIFT_ROTOR
+        return acs_
+
+    def _load_all_fixwing_envelop(self):
+        """load aircraft envelop from the openap database,
+        All unit in SI"""
+        _MAX = 'maximum'
+        _MIN = 'minimum'
+        _OPT = 'default'
+        limits_fixwing = {}
+        for mdl in self.actypes_fixwing:
+            wrap = WRAP(ac=mdl)
+            mdl = mdl.upper()
+            limits_fixwing[mdl] = {}
+            limits_fixwing[mdl]["vminto"] = wrap.takeoff_speed()[_MIN]
+            limits_fixwing[mdl]["vmaxto"] = wrap.takeoff_speed()[_MAX]
+            limits_fixwing[mdl]["vminic"] = wrap.initclimb_vcas()[_MIN]
+            limits_fixwing[mdl]["vmaxic"] = wrap.initclimb_vcas()[_MAX]
+            limits_fixwing[mdl]["vminer"] = min(
+                wrap.initclimb_vcas()[_MIN],
+                wrap.climb_const_vcas()[_MIN],
+                wrap.cruise_mean_vcas()[_MIN],
+                wrap.descent_const_vcas()[_MIN],
+                wrap.finalapp_vcas()[_MIN],
+            )
+            limits_fixwing[mdl]["vmaxer"] = max(
+                wrap.initclimb_vcas()[_MAX],
+                wrap.climb_const_vcas()[_MAX],
+                wrap.cruise_mean_vcas()[_MAX],
+                wrap.descent_const_vcas()[_MAX],
+                wrap.finalapp_vcas()[_MAX],
+            )
+            limits_fixwing[mdl]["vminap"] = wrap.finalapp_vcas()[_MIN]
+            limits_fixwing[mdl]["vmaxap"] = wrap.finalapp_vcas()[_MAX]
+            limits_fixwing[mdl]["vminld"] = wrap.landing_speed()[_MIN]
+            limits_fixwing[mdl]["vmaxld"] = wrap.landing_speed()[_MAX]
+
+            limits_fixwing[mdl]["vmo"] = limits_fixwing[mdl]["vmaxer"]
+            limits_fixwing[mdl]["mmo"] = wrap.cruise_max_mach()[_OPT]
+
+            limits_fixwing[mdl]["hmax"] = wrap.cruise_max_alt()[_OPT] * 1000.0
+            limits_fixwing[mdl]["crosscl"] = wrap.climb_cross_alt_conmach()[_OPT]
+            limits_fixwing[mdl]["crossde"] = wrap.descent_const_vcas()[_OPT]
+
+            limits_fixwing[mdl]["axmax"] = wrap.takeoff_acceleration()[_MAX]
+
+            limits_fixwing[mdl]["vsmax"] = max(
+                wrap.initclimb_vs()[_MAX],
+                wrap.climb_vs_pre_concas()[_MAX],
+                wrap.climb_vs_concas()[_MAX],
+                wrap.climb_vs_conmach()[_MAX],
+            )
+
+            limits_fixwing[mdl]["vsmin"] = min(
+                wrap.initclimb_vs()[_MIN],
+                wrap.climb_vs_pre_concas()[_MIN],
+                wrap.climb_vs_concas()[_MIN],
+                wrap.climb_vs_conmach()[_MIN],
+            )
+
+        return limits_fixwing
+
+    def _load_all_rotor_envelop(self):
+        """load rotor aircraft envelop, all unit in SI"""
+        limits_rotor = {}
+        for mdl, ac in self.acs_rotor.items():
+            limits_rotor[mdl] = {}
+
+            limits_rotor[mdl]["vmin"] = ac["envelop"].get("v_min", -20)
+            limits_rotor[mdl]["vmax"] = ac["envelop"].get("v_max", 20)
+            limits_rotor[mdl]["vsmin"] = ac["envelop"].get("vs_min", -5)
+            limits_rotor[mdl]["vsmax"] = ac["envelop"].get("vs_max", 5)
+            limits_rotor[mdl]["hmax"] = ac["envelop"].get("h_max", 2500)
+
+            params = ["v_min", "v_max", "vs_min", "vs_max", "h_max"]
+            if set(params) <= set(ac["envelop"].keys()):
+                pass
+            else:
+                warn = f"Warning: Some performance parameters for {mdl} are not found, default values used."
+                stack.echo(warn)
+
+        return limits_rotor
+    
+    def _load_fixedwing_dragpolar(self):
+        dragpolar = {}
+        # openap relies on flap angles to caculate nonclean drag, BS doesn't have a flap angle concept
+        # we assume 15 degrees flap during takeoff and 40 degrees during landing
+        flap_to = 15 # degs
+        flap_ld = 40 # degs
+
+        for mdl in self.actypes_fixwing:
+            mdl = mdl.upper()
+            _polar = drag.Drag(mdl, use_synonym=True).polar
+            dragpolar[mdl] = {}
+            dragpolar[mdl]["cd0_clean"] = _polar["clean"]["cd0"]
+            dragpolar[mdl]["k_clean"] = _polar["clean"]["k"]
+            dragpolar[mdl]["e_clean"] = _polar["clean"]["e"]
+
+            lambda_f = _polar["flaps"]["lambda_f"]
+            cfc = _polar["flaps"]["cf/c"]
+            SfS = _polar["flaps"]["Sf/S"]
+            delta_cd_flap_to = lambda_f * (cfc)**1.38 * SfS * np.sin(np.deg2rad(flap_to)) ** 2
+            delta_cd_flap_ld = lambda_f * (cfc)**1.38 * SfS * np.sin(np.deg2rad(flap_ld)) ** 2
+            dragpolar[mdl]["cd0_to"] = _polar["clean"]["cd0"] + delta_cd_flap_to
+            dragpolar[mdl]["cd0_ld"] = _polar["clean"]["cd0"] + delta_cd_flap_ld
+
+            if self.acs_fixwing[mdl]['engine']['mount'] == "rear":
+                delta_e_flap_to = 0.0046 * flap_to
+                delta_e_flap_ld = 0.0046 * flap_ld
+            else:
+                delta_e_flap_to = 0.0026 * flap_to
+                delta_e_flap_ld = 0.0026 * flap_ld
+            
+            ar = self.acs_fixwing[mdl]["wing"]["span"] ** 2 / self.acs_fixwing[mdl]["wing"]["area"]
+            dragpolar[mdl]["k_to"] = 1 / (1 / _polar["clean"]["k"] + np.pi * ar * delta_e_flap_to)
+            dragpolar[mdl]["k_ld"] = 1 / (1 / _polar["clean"]["k"] + np.pi * ar * delta_e_flap_ld)
+            dragpolar[mdl]["delta_cd_gear"] = _polar["gears"]
+
+        return dragpolar
