5c992e1c798a1996d027b54f495e7d5054b480c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 17:05:08 +0000

    Add package cryptomatte-api/1.0.0/cryptomatte_api-1.0.0-cp313-cp313-win_amd64.whl

---
+++ packages/cryptomatte-api/cryptomatte_api-1.0.0-cp313-cp313-win_amd64.whl/cryptomatte_api/_stubs/__init__.pyi
@@ -0,0 +1,4 @@
+# Unsure why stub files require a from import * but when specifying the objects we want to expose those dont get shown
+from ._cryptomatte import *
+from ._metadata import *
+from ._manifest import *
