bf4fca6af953928d53c9aa4210e3306cee4e570f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 06:05:39 +0000

    Add package crypticorn-utils/2.0.0rc1/crypticorn_utils-2.0.0rc1-py3-none-any.whl

---
+++ packages/crypticorn-utils/crypticorn_utils-2.0.0rc1-py3-none-any.whl/crypticorn_utils/types.py
@@ -0,0 +1,25 @@
+import enum as _enum
+import typing as _typing
+
+ApiEnv = _typing.Literal["prod", "dev", "local", "docker"]
+
+
+class BaseUrl(_enum.StrEnum):
+    """The base URL to connect to the API."""
+
+    PROD = "https://api.crypticorn.com"
+    DEV = "https://api.crypticorn.dev"
+    LOCAL = "http://localhost"
+    DOCKER = "http://host.docker.internal"
+
+    @classmethod
+    def from_env(cls, env: ApiEnv) -> "BaseUrl":
+        """Load the base URL from the API environment."""
+        if env == "prod":
+            return cls.PROD
+        elif env == "dev":
+            return cls.DEV
+        elif env == "local":
+            return cls.LOCAL
+        elif env == "docker":
+            return cls.DOCKER
