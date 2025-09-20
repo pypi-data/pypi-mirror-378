6b2d539851073263eda779cdb542f692375d2e87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 03:12:27 +0000

    Add package mikro-next/0.14.0/mikro_next-0.14.0.tar.gz

---
+++ packages/mikro-next/mikro_next-0.14.0.tar.gz/mikro_next-0.14.0/mikro_next/widgets.py
@@ -0,0 +1,9 @@
+"""Default Widgets
+
+This Module provides default widgets that can be used with the mikro app in an arkitekt
+context.
+
+Attributes:
+    MY_TOP_REPRESENTATIONS (SearchWidget): The top representations for the currently active user
+    MY_TOP_SAMPLES (SearchWidget): The top samples for the currently active user
+"""
