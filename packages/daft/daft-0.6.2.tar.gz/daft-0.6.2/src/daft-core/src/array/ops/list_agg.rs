791d4f9f645330a2f8932bbb7ce37efa0e93edc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 07:04:28 +0000

    Add package daft/0.6.2/daft-0.6.2.tar.gz

---
+++ packages/daft/daft-0.6.2.tar.gz/daft-0.6.2/src/daft-core/src/array/ops/list_agg.rs
@@ -0,0 +1,130 @@
+use std::sync::Arc;
+
+use common_error::DaftResult;
+
+use super::{DaftListAggable, GroupIndices, as_arrow::AsArrow};
+use crate::{
+    array::{
+        DataArray, FixedSizeListArray, ListArray, StructArray,
+        growable::{Growable, GrowableArray},
+    },
+    datatypes::DaftArrowBackedType,
+    series::IntoSeries,
+};
+
+macro_rules! impl_daft_list_agg {
+    () => {
+        type Output = DaftResult<ListArray>;
+
+        fn list(&self) -> Self::Output {
+            let child_series = self.clone().into_series();
+            let offsets =
+                arrow2::offset::OffsetsBuffer::try_from(vec![0, child_series.len() as i64])?;
+            let list_field = self.field.to_list_field();
+            Ok(ListArray::new(list_field, child_series, offsets, None))
+        }
+
+        fn grouped_list(&self, groups: &GroupIndices) -> Self::Output {
+            let mut offsets = Vec::with_capacity(groups.len() + 1);
+
+            offsets.push(0);
+            for g in groups {
+                offsets.push(offsets.last().unwrap() + g.len() as i64);
+            }
+
+            let total_capacity = *offsets.last().unwrap();
+
+            let mut growable: Box<dyn Growable> = Box::new(Self::make_growable(
+                self.name(),
+                self.data_type(),
+                vec![self],
+                self.null_count() > 0,
+                total_capacity as usize,
+            ));
+
+            for g in groups {
+                for idx in g {
+                    growable.extend(0, *idx as usize, 1);
+                }
+            }
+            let list_field = self.field.to_list_field();
+
+            Ok(ListArray::new(
+                list_field,
+                growable.build()?,
+                arrow2::offset::OffsetsBuffer::try_from(offsets)?,
+                None,
+            ))
+        }
+    };
+}
+
+impl<T> DaftListAggable for DataArray<T>
+where
+    T: DaftArrowBackedType,
+    Self: IntoSeries,
+    Self: GrowableArray,
+{
+    impl_daft_list_agg!();
+}
+
+impl DaftListAggable for ListArray {
+    impl_daft_list_agg!();
+}
+
+impl DaftListAggable for FixedSizeListArray {
+    impl_daft_list_agg!();
+}
+
+impl DaftListAggable for StructArray {
+    impl_daft_list_agg!();
+}
+
+#[cfg(feature = "python")]
+impl DaftListAggable for crate::datatypes::PythonArray {
+    type Output = DaftResult<Self>;
+
+    fn list(&self) -> Self::Output {
+        use pyo3::{prelude::*, types::PyList};
+
+        use crate::array::pseudo_arrow::PseudoArrowArray;
+
+        let pyobj_vec = self.as_arrow().to_pyobj_vec();
+
+        let pylist: Py<PyList> = Python::with_gil(|py| {
+            let pyobj_vec_cloned = pyobj_vec
+                .into_iter()
+                .map(|pyobj| pyobj.clone_ref(py))
+                .collect::<Vec<_>>();
+
+            PyList::new(py, pyobj_vec_cloned).map(Into::into)
+        })?;
+
+        let arrow_array = PseudoArrowArray::from_pyobj_vec(vec![Arc::new(pylist.into())]);
+        Self::new(self.field().clone().into(), Box::new(arrow_array))
+    }
+
+    fn grouped_list(&self, groups: &GroupIndices) -> Self::Output {
+        use pyo3::{prelude::*, types::PyList};
+
+        use crate::array::pseudo_arrow::PseudoArrowArray;
+
+        let mut result_pylists: Vec<Arc<PyObject>> = Vec::with_capacity(groups.len());
+
+        Python::with_gil(|py| -> DaftResult<()> {
+            for group in groups {
+                let indices_as_array = crate::datatypes::UInt64Array::from(("", group.clone()));
+                let group_pyobjs = self.take(&indices_as_array)?.as_arrow().to_pyobj_vec();
+                let group_pyobjs_cloned = group_pyobjs
+                    .into_iter()
+                    .map(|pyobj| pyobj.clone_ref(py))
+                    .collect::<Vec<_>>();
+                result_pylists.push(Arc::new(PyList::new(py, group_pyobjs_cloned)?.into()));
+            }
+            Ok(())
+        })?;
+
+        let arrow_array = PseudoArrowArray::from_pyobj_vec(result_pylists);
+        Self::new(self.field().clone().into(), Box::new(arrow_array))
+    }
+}
---
+++ packages/daft/daft-0.6.2.tar.gz/daft-0.6.2/src/daft-core/src/series/array_impl/mod.rs
@@ -0,0 +1,12 @@
+pub mod data_array;
+pub mod logical_array;
+pub mod nested_array;
+
+use super::Series;
+
+#[derive(Debug)]
+pub struct ArrayWrapper<T>(pub T);
+
+pub trait IntoSeries {
+    fn into_series(self) -> Series;
+}
