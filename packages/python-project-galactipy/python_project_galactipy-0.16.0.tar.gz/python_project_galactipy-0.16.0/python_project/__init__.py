d09bbf702504e97828bc7bbf434474f6674cf413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 14:04:31 +0000

    Add package python-project-galactipy/0.16.0/python_project_galactipy-0.16.0.tar.gz

---
+++ packages/python-project-galactipy/python_project_galactipy-0.16.0.tar.gz/python_project_galactipy-0.16.0/python_project/__init__.py
@@ -0,0 +1,9 @@
+# type: ignore[attr-defined]
+"""Awesome `python-project` is a Python cli/package created with https://gitlab.com/galactipy/galactipy."""
+
+# Placeholder for poetry-dynamic-versioning, do not change:
+# https://github.com/mtkennerly/poetry-dynamic-versioning#installation
+__version__ = "0.16.0"
+
+# TODO Update with imported objects
+# __all__ = []
