a30f617ecd658a1bad758f4cf550ad6c992256b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 15:04:42 +0000

    Add package mpl-ascii/1.0.1/mpl_ascii-1.0.1-py3-none-any.whl

---
+++ packages/mpl-ascii/mpl_ascii-1.0.1-py3-none-any.whl/mpl_ascii/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from mpl_ascii.rendering.canvas_manager import FigureCanvasAscii, FigureManagerAscii
+
+
+FigureCanvasAscii.manager_class = FigureManagerAscii
+FigureCanvas = FigureCanvasAscii
+FigureManager = FigureManagerAscii
+
+
+
