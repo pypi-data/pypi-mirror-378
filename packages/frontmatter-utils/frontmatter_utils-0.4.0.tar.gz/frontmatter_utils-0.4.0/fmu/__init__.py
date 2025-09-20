b38c67dcf9d0fd132e57357bb6a52f7e735bfb13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 04:04:34 +0000

    Add package frontmatter-utils/0.4.0/frontmatter_utils-0.4.0.tar.gz

---
+++ packages/frontmatter-utils/frontmatter_utils-0.4.0.tar.gz/frontmatter_utils-0.4.0/fmu/__init__.py
@@ -0,0 +1,25 @@
+"""
+fmu - Front Matter Utils
+
+A Python library and CLI tool for parsing and searching front matter in files.
+"""
+
+__version__ = "0.4.0"
+__author__ = "Gerald Nguyen The Huy"
+
+from .core import parse_frontmatter, extract_content, parse_file
+from .search import search_frontmatter
+from .validation import validate_frontmatter, validate_and_output
+from .update import update_frontmatter, update_and_output
+
+__all__ = [
+    "parse_frontmatter",
+    "extract_content", 
+    "parse_file",
+    "search_frontmatter",
+    "validate_frontmatter",
+    "validate_and_output",
+    "update_frontmatter",
+    "update_and_output",
+    "__version__"
+]
