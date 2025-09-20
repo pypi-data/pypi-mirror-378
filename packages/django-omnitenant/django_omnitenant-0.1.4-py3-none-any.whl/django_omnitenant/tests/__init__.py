f716d657849e9751b7c94580292f3ad8f0f6cadc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 03:12:42 +0000

    Add package django-omnitenant/0.1.4/django_omnitenant-0.1.4-py3-none-any.whl

---
+++ packages/django-omnitenant/django_omnitenant-0.1.4-py3-none-any.whl/django_omnitenant/tests/__init__.py
@@ -0,0 +1,13 @@
+from .testcases import (
+    DBTenantTestCase,
+    DBTenantAPITestCase,
+    SchemaTenantTestCase,
+    SchemaTenantAPITestCase,
+)
+
+__all__ = [
+    "DBTenantTestCase",
+    "DBTenantAPITestCase",
+    "SchemaTenantTestCase",
+    "SchemaTenantAPITestCase",
+]
