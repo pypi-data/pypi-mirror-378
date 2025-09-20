1ab65a7df670897781af98a87268b1fe24085f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 14:04:23 +0000

    Add package pyJPI/0.1.21/pyjpi-0.1.21.tar.gz

---
+++ packages/pyJPI/pyjpi-0.1.21.tar.gz/pyjpi-0.1.21/pyjpi/library.py
@@ -0,0 +1,96 @@
+"""
+A library to interact with JPI devices.
+Rationale: according to HomeAssistant documentation, the integration MUST not interact directly with the physical devices.
+Instead, it MUST use a library published on pyPI repository.
+This file is so the first version of such the interaction library.
+"""
+
+from __future__ import annotations
+
+from datetime import datetime
+import logging
+
+import aiohttp
+
+
+class JPILibrary:
+    """Class for the pyJPI library."""
+
+    def __init__(self, session, version):
+        """Initialize a HTTP session."""
+        self._session = session
+        self._initialized = datetime.now()
+        self._log = logging.getLogger(__name__)
+        self._log.debug("JPILibrary v%s successfully instantiated", version)
+
+    def _batt_parse_text(self, text: str) -> dict:
+        """
+        Parse battery info text into a structured dictionary.
+        Input:
+            Niveau: 52%
+            En charge: NON
+            Alim. connectée: NON
+        Output:
+            {'level': 52, 'charging': False, 'power': False}
+        """
+        result = {}
+        for line in text.splitlines():
+            if not line.strip():
+                continue  # skip empty lines
+            key, value = line.split(":", 1)
+            key = key.strip()
+            value = value.strip()
+
+            if key == "Niveau":
+                result["level"] = int(value.strip("%"))
+            elif key == "En charge":
+                result["charging"] = value.upper() == "OUI"
+            elif key == "Alim. connectée":
+                result["power"] = value.upper() == "OUI"
+        return result
+
+    async def battInfo(self, url: str):
+        """
+        Returns the battery informations as a hash:
+            level: <int>
+            charging: <bool>
+            power: <bool>
+        """
+        target = f"{url}?action=battInfo"
+        resp = await self.get(target)
+        result = None
+        self._log.debug("battInfo resp=%s", resp)
+        if resp:
+            result = self._batt_parse_text(resp["text"])
+        return result
+
+    async def get(self, url: str):
+        """
+        Returns an object containing the raw HTTP response from GETting the provided url plus the got text content.
+        Uses async I/O to avoid blocking the main event loop.
+        Throw an exception in case of an error.
+        """
+        resp = None
+        result = None
+        try:
+            resp = await self._session.get(url)
+        except (aiohttp.ClientError, TimeoutError) as e:
+            self._log.error("ClientError or TimeoutError exception: %s", e)
+            return False
+        if resp:
+            text = await resp.text()
+            result = {"text": text, "resp": resp}
+        return result
+
+    async def getDeviceName(self, url: str):
+        """
+        Returns the device name as provided by the manufacturer.
+        E.g. Samsung sets that as 'Samsung SM-J320FN' for a Galaxy J3.
+        """
+        target = f"{url}?action=getDeviceName"
+        resp = await self.get(target)
+        self._log.debug("getDeviceName resp=%s", resp)
+        device_name = None
+        if resp:
+            device_name = resp["text"]
+        return device_name
---
+++ packages/pyJPI/pyjpi-0.1.21.tar.gz/pyjpi-0.1.21/tests/test_lib_battinfo.py
@@ -0,0 +1,85 @@
+"""Tests for JPILibrary methods."""
+
+from unittest.mock import AsyncMock, MagicMock
+
+import pytest
+
+from pyjpi import jpiInit
+
+from .const import URL
+
+
+@pytest.mark.asyncio
+async def test_battInfo_empty():
+    """Test for 'action=battInfo' query."""
+
+    class FakeResp:
+        """Fake response context manager."""
+
+        status = 200
+
+        def raise_for_status(self):  # pylint: disable=C0116
+            return None
+
+        async def text(self):
+            """Returns a fake empty answer."""
+            return "\n"
+
+        async def __aenter__(self):
+            return self
+
+        async def __aexit__(self, *exc):
+            return None
+
+    session = MagicMock()
+    session.get = AsyncMock(return_value=FakeResp())
+
+    lib = await jpiInit(session)
+
+    info = await lib.battInfo(URL)
+
+    assert isinstance(info, dict)
+    assert not info
+
+    # keep assertion flexible re: extra kwargs
+    assert session.get.await_args.args[0] == f"{URL}?action=battInfo"
+
+
+@pytest.mark.asyncio
+async def test_battInfo_parses_and_returns_expected_dict():
+    """Test for 'action=battInfo' query."""
+
+    class FakeResp:
+        """Fake response context manager."""
+
+        status = 200
+
+        def raise_for_status(self):  # pylint: disable=C0116
+            return None
+
+        async def text(self):
+            """Returns a fake (but with the expected format) answer."""
+            return "Niveau: 87%\nEn charge: OUI\nAlim. connectée: NON"
+
+        async def __aenter__(self):
+            return self
+
+        async def __aexit__(self, *exc):
+            return None
+
+    session = MagicMock()
+    session.get = AsyncMock(return_value=FakeResp())
+
+    lib = await jpiInit(session)
+
+    info = await lib.battInfo(URL)
+
+    assert isinstance(info, dict)
+    assert set(info.keys()) == {"level", "charging", "power"}
+    assert isinstance(info["level"], int)
+    assert isinstance(info["charging"], bool)
+    assert isinstance(info["power"], bool)
+    assert info == {"level": 87, "charging": True, "power": False}
+
+    # keep assertion flexible re: extra kwargs
+    assert session.get.await_args.args[0] == f"{URL}?action=battInfo"
