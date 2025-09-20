6b048d0893642bf8a3e2af242dfc84c53093f258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 17:05:05 +0000

    Add package github-action-toolkit/0.2.0/github_action_toolkit-0.2.0.tar.gz

---
+++ packages/github-action-toolkit/github_action_toolkit-0.2.0.tar.gz/github_action_toolkit-0.2.0/tests/test_event_payload.py
@@ -0,0 +1,17 @@
+import json
+import os
+from typing import Any
+from unittest import mock
+
+import github_action_toolkit as gat
+
+
+def test_event_payload(tmpdir: Any) -> None:
+    file = tmpdir.join("summary")
+    payload = {"test": "test"}
+    file.write(json.dumps(payload))
+
+    with mock.patch.dict(os.environ, {"GITHUB_EVENT_PATH": file.strpath}):
+        data = gat.event_payload()
+
+    assert data == payload
