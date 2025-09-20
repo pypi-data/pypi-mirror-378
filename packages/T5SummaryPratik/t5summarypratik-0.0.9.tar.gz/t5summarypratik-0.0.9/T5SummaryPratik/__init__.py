81f174eb1370fca0293e19cf7d0ff3ff073b2e58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 04:05:43 +0000

    Add package T5SummaryPratik/0.0.9/t5summarypratik-0.0.9.tar.gz

---
+++ packages/T5SummaryPratik/t5summarypratik-0.0.9.tar.gz/t5summarypratik-0.0.9/T5SummaryPratik/__init__.py
@@ -0,0 +1,10 @@
+from .pipeline import CompleteSummarizationPipeline
+from .utils import create_pipeline, quick_summarize, get, setx
+
+__all__ = [
+    "CompleteSummarizationPipeline",
+    "create_pipeline",
+    "quick_summarize",
+    "get",
+    "setx"
+]
