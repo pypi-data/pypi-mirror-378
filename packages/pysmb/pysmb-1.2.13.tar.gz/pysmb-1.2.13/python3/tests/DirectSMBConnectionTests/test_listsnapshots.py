44661243eed497947ae9c67f02799cb840ce0127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 14:05:33 +0000

    Add package pysmb/1.2.13/pysmb-1.2.13.tar.gz

---
+++ packages/pysmb/pysmb-1.2.13.tar.gz/pysmb-1.2.13/python3/tests/DirectSMBConnectionTests/test_listsnapshots.py
@@ -0,0 +1,39 @@
+
+from nose2.tools.decorators import with_setup, with_teardown
+from smb.SMBConnection import SMBConnection
+from smb import smb_structs
+from .util import getConnectionInfo
+
+conn = None
+
+def setup_func_SMB1():
+    global conn
+    smb_structs.SUPPORT_SMB2 = False
+    info = getConnectionInfo()
+    conn = SMBConnection(info['user'], info['password'], info['client_name'], info['server_name'], use_ntlm_v2 = True, is_direct_tcp = True)
+    assert conn.connect(info['server_ip'], info['server_port'])
+
+def setup_func_SMB2():
+    global conn
+    smb_structs.SUPPORT_SMB2 = True
+    info = getConnectionInfo()
+    conn = SMBConnection(info['user'], info['password'], info['client_name'], info['server_name'], use_ntlm_v2 = True, is_direct_tcp = True)
+    assert conn.connect(info['server_ip'], info['server_port'])
+
+def teardown_func():
+    global conn
+    conn.close()
+
+@with_setup(setup_func_SMB1)
+@with_teardown(teardown_func)
+def test_listsnapshots_SMB1():
+    global conn
+    results = conn.listSnapshots('smbtest', '/rfc1001.txt')
+    assert len(results) > 0
+
+@with_setup(setup_func_SMB2)
+@with_teardown(teardown_func)
+def test_listsnapshots_SMB2():
+    global conn
+    results = conn.listSnapshots('smbtest', '/rfc1001.txt')
+    assert len(results) > 0
---
+++ packages/pysmb/pysmb-1.2.13.tar.gz/pysmb-1.2.13/python3/tests/SMBConnectionTests/test_listshares.py
@@ -0,0 +1,39 @@
+
+from nose2.tools.decorators import with_setup, with_teardown
+from smb.SMBConnection import SMBConnection
+from smb import smb_structs
+from .util import getConnectionInfo
+
+conn = None
+
+def setup_func_SMB1():
+    global conn
+    smb_structs.SUPPORT_SMB2 = False
+    info = getConnectionInfo()
+    conn = SMBConnection(info['user'], info['password'], info['client_name'], info['server_name'], use_ntlm_v2 = True)
+    assert conn.connect(info['server_ip'], info['server_port'])
+
+def setup_func_SMB2():
+    global conn
+    smb_structs.SUPPORT_SMB2 = True
+    info = getConnectionInfo()
+    conn = SMBConnection(info['user'], info['password'], info['client_name'], info['server_name'], use_ntlm_v2 = True)
+    assert conn.connect(info['server_ip'], info['server_port'])
+
+def teardown_func():
+    global conn
+    conn.close()
+
+@with_setup(setup_func_SMB1)
+@with_teardown(teardown_func)
+def test_listshares_SMB1():
+    global conn
+    results = conn.listShares()
+    assert 'smbtest' in [r.name.lower() for r in results]
+
+@with_setup(setup_func_SMB2)
+@with_teardown(teardown_func)
+def test_listshares_SMB2():
+    global conn
+    results = conn.listShares()
+    assert 'smbtest' in [r.name.lower() for r in results]
