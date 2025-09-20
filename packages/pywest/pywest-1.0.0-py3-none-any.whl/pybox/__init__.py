4e24b3890e33eb4298017e80dacd84014b746673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 16:05:19 +0000

    Add package pywest/1.0.0/pywest-1.0.0-py3-none-any.whl

---
+++ packages/pywest/pywest-1.0.0-py3-none-any.whl/pybox/__init__.py
@@ -0,0 +1,12 @@
+"""
+pybox - No Code Obfuscation, No Compiled PYCs, Open Python Projects
+A tool for developing quickly shareable portable python apps based on embeddable python for Windows
+"""
+
+__version__ = "1.0.0"
+__author__ = "pybox Community"
+__email__ = "community@pybox.dev"
+
+from .pybox import main
+
+__all__ = ["main"]
