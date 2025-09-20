596358e96cf7f26515f24fbecc52f2a5ba0889ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 06:04:25 +0000

    Add package abstra/3.23.9/abstra-3.23.9.tar.gz

---
+++ packages/abstra/abstra-3.23.9.tar.gz/abstra-3.23.9/abstra/ai.py
@@ -0,0 +1,13 @@
+from abstra_internals.interface.sdk.ai import (
+    parse_boleto,
+    parse_nfse,
+    prompt,
+)
+from abstra_internals.interface.sdk.prompt_browser import experimental_prompt_browser
+
+__all__ = [
+    "prompt",
+    "parse_nfse",
+    "parse_boleto",
+    "experimental_prompt_browser",
+]
