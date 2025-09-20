78bb6fd23e3b174ccf414bb93c28751b7055a326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 03:12:44 +0000

    Add package suyo/1.3.4/suyo-1.3.4.tar.gz

---
+++ packages/suyo/suyo-1.3.4.tar.gz/suyo-1.3.4/src/suyo/__init__.py
@@ -0,0 +1,14 @@
+"""suyo
+
+Essential helper package for HamzaGSopp projects.
+"""
+
+from .core import init
+
+__all__ = ["init"]
+
+# Package-level debug flag. Set `suyo.debug = True` to enable debug prints in runtime.
+debug = False
+
+__author__ = "HamzaGSopp"
+__version__ = "1.3.4"
