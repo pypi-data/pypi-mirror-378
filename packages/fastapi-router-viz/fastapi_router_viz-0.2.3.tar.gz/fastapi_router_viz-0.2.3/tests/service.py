7a65736ff49edafaf2fffa3e085dc6030ba93afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 03:12:44 +0000

    Add package fastapi-router-viz/0.2.3/fastapi_router_viz-0.2.3.tar.gz

---
+++ packages/fastapi-router-viz/fastapi_router_viz-0.2.3.tar.gz/fastapi_router_viz-0.2.3/tests/service.py
@@ -0,0 +1,24 @@
+from fastapi_router_viz.graph import Analytics
+from pydantic import BaseModel
+from pydantic_resolve import ensure_subset
+
+class Sprint(BaseModel):
+    id: int
+    name: str
+
+class Story(BaseModel):
+    id: int
+    sprint_id: int
+    title: str
+    description: str
+
+class Task(BaseModel):
+    id: int
+    story_id: int
+    description: str
+    owner_id: int
+
+class Member(BaseModel):
+    id: int
+    username: str
+
