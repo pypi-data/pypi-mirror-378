4e9d793e6ef06be2504600901cb1956a255e58b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 06:04:30 +0000

    Add package oat-python/0.2.0/oat_python-0.2.0-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/oat-python/oat_python-0.2.0-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/oat_python/homology.py
@@ -0,0 +1,6 @@
+"""
+
+- For persistent homology of point clouds, see the :ref:`Vietoris Rips section <vietoris-rips-section>`.
+- For homology of a Dowker complexes, see the :ref:`Hypergraph section <hypergraph-section>`.
+
+"""
