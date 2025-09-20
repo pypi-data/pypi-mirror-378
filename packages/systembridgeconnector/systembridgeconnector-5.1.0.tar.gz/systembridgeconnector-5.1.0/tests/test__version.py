bc8985b8fd1d2300719eec1bde60e8a53365b3e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 04:05:36 +0000

    Add package systembridgeconnector/5.1.0/systembridgeconnector-5.1.0.tar.gz

---
+++ packages/systembridgeconnector/systembridgeconnector-5.1.0.tar.gz/systembridgeconnector-5.1.0/tests/test__version.py
@@ -0,0 +1,9 @@
+"""Test __version__ module."""
+
+from systembridgeconnector._version import __version__
+
+
+def test__version():
+    """Test the __version__ string."""
+    assert isinstance(__version__, str)
+    assert len(__version__) > 0
