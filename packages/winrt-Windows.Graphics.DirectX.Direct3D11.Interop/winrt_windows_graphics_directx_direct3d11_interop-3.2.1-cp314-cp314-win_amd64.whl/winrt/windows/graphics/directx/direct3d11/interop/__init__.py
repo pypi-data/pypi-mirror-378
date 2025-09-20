8964e4d65f6f7df21fdbdb6553027b04cf925020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 15:04:45 +0000

    Add package winrt-Windows.Graphics.DirectX.Direct3D11.Interop/3.2.1/winrt_windows_graphics_directx_direct3d11_interop-3.2.1-cp314-cp314-win_amd64.whl

---
+++ packages/winrt-Windows.Graphics.DirectX.Direct3D11.Interop/winrt_windows_graphics_directx_direct3d11_interop-3.2.1-cp314-cp314-win_amd64.whl/winrt/windows/graphics/directx/direct3d11/interop/__init__.py
@@ -0,0 +1,13 @@
+from winrt._winrt_windows_graphics_directx_direct3d11_interop import (
+    create_direct3d11_device_from_dxgi_device,
+    create_direct3d11_surface_from_dxgi_surface,
+    get_dxgi_device_from_object,
+    get_dxgi_surface_from_object,
+)
+
+__all__ = [
+    "create_direct3d11_device_from_dxgi_device",
+    "create_direct3d11_surface_from_dxgi_surface",
+    "get_dxgi_device_from_object",
+    "get_dxgi_surface_from_object",
+]
