7d75a8a47f1d50f8befee99edc4c84c4fcfc484a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 06:05:10 +0000

    Add package PyPFD/2025.0.0.7/pypfd-2025.0.0.7-py3-none-any.whl

---
+++ packages/PyPFD/pypfd-2025.0.0.7-py3-none-any.whl/PyPFD/__init__.py
@@ -0,0 +1,2 @@
+from .PyPFDAvg import testfnc, Tce,Tce_pt,Tge,Tge_pt,Tg2e,Tne,pfd_avg_1oo1,pfd_avg_1oo1_pt,pfd_avg_1oo2,pfd_avg_1oo2_pt,pfd_avg_1oo3,pfd_avg_2oo2,pfd_avg_2oo2_pt,pfd_avg_2oo3,pfd_avg_KooN
+
