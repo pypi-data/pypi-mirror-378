cf59763c4e5ef3fb0aefcd95d5551825398b2c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 16:04:47 +0000

    Add package qupepfold/0.5.0/qupepfold-0.5.0-py3-none-any.whl

---
+++ packages/qupepfold/qupepfold-0.5.0-py3-none-any.whl/qupepfold/__init__.py
@@ -0,0 +1,15 @@
+from .qupepfold import (
+    generate_turn2qubit,
+    protein_config_ansatz,
+    exact_hamiltonian,
+    build_mj_interactions,
+    protein_vqe_objective,
+)
+
+__all__ = [
+    "generate_turn2qubit",
+    "protein_config_ansatz",
+    "exact_hamiltonian",
+    "build_mj_interactions",
+    "protein_vqe_objective",
+]
