f1dccfa06892c1b281b1f69facd0f6eb787439c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 16:04:50 +0000

    Add package leksara/0.0.7/leksara-0.0.7.tar.gz

---
+++ packages/leksara/leksara-0.0.7.tar.gz/leksara-0.0.7/leksara/functions/review/__init__.py
@@ -0,0 +1,10 @@
+# review subpackage
+__all__ = [
+    "replace_rating",
+    "shorten_elongation",
+    "replace_acronym",
+    "remove_acronym",
+    "normalize_slangs",
+    "expand_contraction",
+    "word_normalization"
+]
