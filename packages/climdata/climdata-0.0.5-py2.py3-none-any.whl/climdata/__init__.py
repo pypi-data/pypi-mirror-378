384b44f0eec9a9e0544e503bd46cb855ec213db5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 03:12:31 +0000

    Add package climdata/0.0.5/climdata-0.0.5-py2.py3-none-any.whl

---
+++ packages/climdata/climdata-0.0.5-py2.py3-none-any.whl/climdata/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for climdata."""
+
+__author__ = """Kaushik Muduchuru"""
+__email__ = "kaushik.reddy.m@gmail.com"
+__version__ = "0.0.5"
+
+from .utils.utils_download import * # etc.
+from .utils.config import load_config
+from .datasets.DWD import DWDmirror as DWD
+from .datasets.MSWX import MSWXmirror as MSWX
+
