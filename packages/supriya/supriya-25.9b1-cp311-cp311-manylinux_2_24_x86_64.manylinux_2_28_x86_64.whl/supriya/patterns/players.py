a09794c68a96daaf52154d5c7a4e5ea6fb7a9ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 14:04:38 +0000

    Add package supriya/25.9b1/supriya-25.9b1-cp311-cp311-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/supriya/supriya-25.9b1-cp311-cp311-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl/supriya/patterns/players.py
@@ -0,0 +1,296 @@
+from queue import Empty, PriorityQueue, Queue
+from threading import RLock
+from typing import (
+    Callable,
+    Generator,
+    Iterable,
+    Sequence,
+    cast,
+)
+from uuid import UUID, uuid4
+from weakref import WeakSet
+
+from ..clocks import (
+    BaseClock,
+    CallbackEvent,
+    ClockContext,
+    ClockDelta,
+    Quantization,
+)
+from ..contexts import Bus, Context, ContextObject, Node
+from .events import Event, Priority, StartEvent, StopEvent
+from .patterns import Pattern
+from .structure import PinPattern
+
+
+class PatternPlayer:
+    """
+    A pattern player.
+
+    Coordinates interactions between a pattern, a clock_context, and a clock.
+    """
+
+    # TODO: Rewrite type annotation after dropping 3.8
+    _players = cast(set["PatternPlayer"], WeakSet())
+
+    def __init__(
+        self,
+        pattern: Pattern,
+        context: Context,
+        clock: BaseClock,
+        callback: (
+            Callable[["PatternPlayer", ClockContext, Event, Priority], None] | None
+        ) = None,
+        target_bus: Bus | None = None,
+        target_node: Node | None = None,
+        uuid: UUID | None = None,
+    ) -> None:
+        self._context = context
+        self._clock = clock
+        self._callback = callback
+        self._lock = RLock()
+        self._queue: Queue[
+            tuple[float, Priority, int | tuple[int, int], Event | None]
+        ] = PriorityQueue()
+        self._is_running = False
+        self._is_stopping = False
+        self._proxies_by_uuid: dict[UUID | tuple[UUID, int], ContextObject] = {}
+        self._notes_by_uuid: dict[UUID | tuple[UUID, int], float] = {}
+        self._uuid: UUID = uuid or uuid4()
+        self._target_bus = target_bus
+        self._target_node = target_node
+        self._next_delta: float | None = None
+        self._initial_seconds: float | None = None
+        self._clock_event_id = -1
+        self._clock_stop_event_id = -1
+        self._pattern = (
+            pattern
+            if (target_bus is None and target_node is None)
+            else PinPattern(pattern, target_bus=target_bus, target_node=target_node)
+        )
+        self._yielded = False
+
+    def _clock_callback(self, context: ClockContext, *args, **kwargs) -> ClockDelta:
+        for clock_context, seconds, offset, events in self._find_events(context):
+            if self._initial_seconds is None:
+                self._initial_seconds = seconds
+            with self._context.at(seconds):
+                for event, priority in events:
+                    event.perform(
+                        self._context,
+                        self._proxies_by_uuid,
+                        current_offset=offset,
+                        notes_mapping=self._notes_by_uuid,
+                        priority=priority,
+                    )
+                    if self._callback is not None:
+                        self._callback(self, clock_context, event, priority)
+        return self._next_delta
+
+    def _find_events(
+        self, clock_context: ClockContext
+    ) -> Iterable[tuple[ClockContext, float, float, Sequence[tuple[Event, Priority]]]]:
+        with self._lock:
+            current_offset = float("-inf")
+            events: list[tuple[Event, Priority]] = []
+            if (
+                not cast(CallbackEvent, clock_context.event).invocations
+                and self._callback
+            ):
+                yield (
+                    clock_context,
+                    clock_context.desired_moment.seconds,
+                    clock_context.desired_moment.offset,
+                    [(StartEvent(), Priority.START)],
+                )
+            while True:
+                try:
+                    offset, priority, index, event = self._queue.get(block=False)
+                except Empty:
+                    if events:
+                        yield (
+                            clock_context,
+                            clock_context.desired_moment.seconds,
+                            current_offset,
+                            events,
+                        )
+                    self._is_running = False
+                    if self._callback:
+                        yield (
+                            clock_context,
+                            clock_context.desired_moment.seconds,
+                            current_offset,
+                            [(StopEvent(), Priority.STOP)],
+                        )
+                    self._next_delta = None
+                    return
+                except Exception:
+                    self._next_delta = None
+                    return
+                if offset == float("-inf"):
+                    offset = clock_context.desired_moment.offset
+                delta = offset - clock_context.desired_moment.offset
+                if delta:
+                    self._queue.put((offset, priority, index, event))
+                    if events:
+                        yield (
+                            clock_context,
+                            clock_context.desired_moment.seconds,
+                            current_offset,
+                            events,
+                        )
+                    self._next_delta = delta
+                    return
+                if not isinstance(event, Event):
+                    if self._consume_iterator(offset):
+                        if self._callback:
+                            yield (
+                                clock_context,
+                                clock_context.desired_moment.seconds,
+                                current_offset,
+                                [(StopEvent(), Priority.STOP)],
+                            )
+                        self._next_delta = None
+                        return
+                elif offset != current_offset:
+                    if events:
+                        yield (
+                            clock_context,
+                            clock_context.desired_moment.seconds,
+                            current_offset,
+                            events,
+                        )
+                    current_offset = offset
+                    events = [(event, priority)]
+                else:
+                    events.append((event, priority))
+
+    def _consume_iterator(self, current_offset: float) -> bool:
+        try:
+            if self._yielded:
+                index, consumed_event = self._iterator.send(self._is_stopping)
+            else:
+                if self._is_stopping:
+                    return True
+                index, consumed_event = next(self._iterator)
+                self._yielded = True
+            for subindex, (expanded_offset, priority, expanded_event) in enumerate(
+                consumed_event.expand(current_offset)
+            ):
+                self._queue.put(
+                    (expanded_offset, priority, (index, subindex), expanded_event)
+                )
+            self._queue.put(
+                (
+                    float(current_offset + consumed_event.delta),
+                    Priority.NONE,
+                    (index, 0),
+                    None,
+                )
+            )
+        except StopIteration:
+            pass
+        return False
+
+    def _stop_callback(self, context: ClockContext, *args, **kwargs) -> ClockDelta:
+        with self._lock:
+            # Do we need to rebuild the queue? Yes.
+            # Do we need to free all playing notes? Yes.
+            # How do we handle when there are already stop events in the queue?
+            # They'll be no-ops when performed.
+            self._is_stopping = True
+            self._clock.reschedule(
+                self._clock_event_id, schedule_at=context.desired_moment.offset
+            )
+            self._reschedule_queue(context.desired_moment.offset)
+            self._free_all_notes(context.desired_moment.seconds)
+            self._clock_event_id = -1
+            self._clock_stop_event_id = -1
+        return None
+
+    def _enumerate(
+        self, iterator: Generator[Event, bool, None]
+    ) -> Generator[tuple[int, Event], bool, None]:
+        index = 0
+        should_stop = False
+        while True:
+            try:
+                if index:
+                    should_stop = (
+                        yield (index, iterator.send(should_stop)) or should_stop
+                    )
+                else:
+                    should_stop = yield (index, next(iterator))
+            except StopIteration:
+                return
+            index += 1
+
+    def _free_all_notes(self, current_seconds: float) -> None:
+        if not self._notes_by_uuid:
+            return
+        with self._context.at(current_seconds):
+            while self._notes_by_uuid:
+                uuid, _ = self._notes_by_uuid.popitem()
+                if not isinstance(node := self._proxies_by_uuid.pop(uuid), Node):
+                    raise RuntimeError
+                self._context.free_node(node)
+
+    def _reschedule_queue(self, current_offset: float) -> None:
+        events: list[tuple[float, Priority, int | tuple[int, int], Event | None]] = []
+        while not self._queue.empty():
+            events.append(self._queue.get())
+        if not events:
+            return
+        delta = events[0][0] - current_offset
+        for event in events:
+            offset, priority, index, event_ = event
+            self._queue.put((offset - delta, priority, index, event_))
+
+    def play(
+        self,
+        quantization: Quantization | None = None,
+        until: float | None = None,
+    ) -> None:
+        with self._lock:
+            if self._is_running:
+                return
+            self._iterator = self._enumerate(iter(self._pattern))
+            self._queue.put((float("-inf"), Priority.NONE, (0, 0), None))
+            self._is_running = True
+            self._is_stopping = False
+            self._yielded = False
+            self._players.add(self)
+        self._clock_event_id = self._clock.cue(
+            self._clock_callback,
+            event_type=3,
+            quantization=quantization,
+        )
+        if until:
+            self._clock_stop_event_id = self._clock.schedule(
+                self._stop_callback, event_type=2, schedule_at=until
+            )
+
+    def stop(self, quantization: Quantization | None = None) -> None:
+        with self._lock:
+            if not self._is_running or self._is_stopping:
+                return
+            # If we're stopping earlier than the until parameter, cancel the current stop callback
+            if self._clock_stop_event_id != -1:
+                self._clock.cancel(self._clock_stop_event_id)
+            self._clock.cue(
+                self._stop_callback, event_type=2, quantization=quantization
+            )
+            self._players.remove(self)
+
+    def uuid_to_note_id(self, uuid: UUID, index: int | None = None) -> float:
+        if index is not None:
+            return self._notes_by_uuid[uuid, index]
+        return self._notes_by_uuid[uuid]
+
+    @property
+    def initial_seconds(self) -> float | None:
+        return self._initial_seconds
+
+    @property
+    def uuid(self) -> UUID:
+        return self._uuid
---
+++ packages/supriya/supriya-25.9b1-cp311-cp311-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl/supriya/ugens/panning.py
@@ -0,0 +1,425 @@
+import math
+
+from ..enums import CalculationRate
+from .basic import Mix
+from .core import (
+    PseudoUGen,
+    UGen,
+    UGenOperable,
+    UGenRecursiveParams,
+    UGenVector,
+    _get_method_for_rate,
+    param,
+    ugen,
+)
+
+
+@ugen(ar=True, kr=True, channel_count=2, fixed_channel_count=True)
+class Balance2(UGen):
+    """
+    A stereo signal balancer.
+
+    ::
+
+        >>> left = supriya.ugens.WhiteNoise.ar()
+        >>> right = supriya.ugens.SinOsc.ar()
+        >>> balance_2 = supriya.ugens.Balance2.ar(
+        ...     left=left,
+        ...     level=1,
+        ...     position=0,
+        ...     right=right,
+        ... )
+        >>> balance_2
+        <Balance2.ar()>
+    """
+
+    left = param()
+    right = param()
+    position = param(0.0)
+    level = param(1.0)
+
+
+@ugen(ar=True, kr=True, channel_count=3, fixed_channel_count=True)
+class BiPanB2(UGen):
+    """
+    A 2D ambisonic b-format panner.
+
+    ::
+
+        >>> in_a = supriya.ugens.SinOsc.ar()
+        >>> in_b = supriya.ugens.WhiteNoise.ar()
+        >>> bi_pan_b_2 = supriya.ugens.BiPanB2.ar(
+        ...     azimuth=-0.5,
+        ...     gain=1,
+        ...     in_a=in_a,
+        ...     in_b=in_b,
+        ... )
+        >>> bi_pan_b_2
+        <BiPanB2.ar()>
+
+    ::
+
+        >>> w, x, y = bi_pan_b_2
+    """
+
+    in_a = param()
+    in_b = param()
+    azimuth = param()
+    gain = param(1.0)
+
+
+@ugen(ar=True, kr=True, is_multichannel=True, channel_count=4)
+class DecodeB2(UGen):
+    """
+    A 2D Ambisonic B-format decoder.
+
+    ::
+
+        >>> source = supriya.ugens.PinkNoise.ar()
+        >>> w, x, y = supriya.ugens.PanB2.ar(
+        ...     source=source,
+        ...     azimuth=supriya.ugens.SinOsc.kr(),
+        ... )
+        >>> decode_b_2 = supriya.ugens.DecodeB2.ar(
+        ...     channel_count=4,
+        ...     orientation=0.5,
+        ...     w=w,
+        ...     x=x,
+        ...     y=y,
+        ... )
+        >>> decode_b_2
+        <DecodeB2.ar()>
+    """
+
+    w = param()
+    x = param()
+    y = param()
+    orientation = param(0.5)
+
+
+@ugen(ar=True, kr=True, channel_count=2, fixed_channel_count=True)
+class Pan2(UGen):
+    """
+    A two channel equal power panner.
+
+    ::
+
+        >>> source = supriya.ugens.WhiteNoise.ar()
+        >>> pan_2 = supriya.ugens.Pan2.ar(
+        ...     source=source,
+        ... )
+        >>> pan_2
+        <Pan2.ar()>
+    """
+
+    source = param()
+    position = param(0.0)
+    level = param(1.0)
+
+
+@ugen(ar=True, kr=True, channel_count=4, fixed_channel_count=True)
+class Pan4(UGen):
+    """
+    A four-channel equal-power panner.
+
+    ::
+
+        >>> source = supriya.ugens.In.ar(bus=0)
+        >>> pan_4 = supriya.ugens.Pan4.ar(
+        ...     gain=1,
+        ...     source=source,
+        ...     x_position=0,
+        ...     y_position=0,
+        ... )
+        >>> pan_4
+        <Pan4.ar()>
+    """
+
+    source = param()
+    x_position = param(0)
+    y_position = param(0)
+    gain = param(1)
+
+
+@ugen(ar=True, kr=True, is_multichannel=True)
+class PanAz(UGen):
+    """
+    A multi-channel equal-power panner.
+
+    ::
+
+        >>> source = supriya.ugens.In.ar(bus=0)
+        >>> pan_az = supriya.ugens.PanAz.ar(
+        ...     channel_count=8,
+        ...     amplitude=1,
+        ...     orientation=0.5,
+        ...     position=0,
+        ...     source=source,
+        ...     width=2,
+        ... )
+        >>> pan_az
+        <PanAz.ar()>
+    """
+
+    source = param()
+    position = param(0)
+    amplitude = param(1)
+    width = param(2)
+    orientation = param(0.5)
+
+
+@ugen(ar=True, kr=True, channel_count=3, fixed_channel_count=True)
+class PanB(UGen):
+    """
+    A 3D ambisonic b-format panner.
+
+    ::
+
+        >>> source = supriya.ugens.In.ar(bus=0)
+        >>> pan_b = supriya.ugens.PanB.ar(
+        ...     azimuth=0,
+        ...     elevation=0,
+        ...     gain=1,
+        ...     source=source,
+        ... )
+        >>> pan_b
+        <PanB.ar()>
+    """
+
+    source = param()
+    azimuth = param(0)
+    elevation = param(0)
+    gain = param(1)
+
+
+@ugen(ar=True, kr=True, channel_count=3, fixed_channel_count=True)
+class PanB2(UGen):
+    """
+    A 2D ambisonic b-format panner.
+
+    ::
+
+        >>> source = supriya.ugens.In.ar(bus=0)
+        >>> pan_b_2 = supriya.ugens.PanB2.ar(
+        ...     azimuth=0,
+        ...     gain=1,
+        ...     source=source,
+        ... )
+        >>> pan_b_2
+        <PanB2.ar()>
+    """
+
+    source = param()
+    azimuth = param(0)
+    gain = param(1)
+
+
+@ugen(ar=True, kr=True, channel_count=2, fixed_channel_count=True)
+class Rotate2(UGen):
+    """
+    Equal-power sound-field rotator.
+
+    ::
+
+        >>> x = supriya.ugens.PinkNoise.ar() * 0.4
+        >>> y = supriya.ugens.LFTri.ar(frequency=880)
+        >>> y *= supriya.ugens.LFPulse.kr(frequency=3, width=0.1)
+        >>> position = supriya.ugens.LFSaw.kr(frequency=0.1)
+        >>> rotate_2 = supriya.ugens.Rotate2.ar(
+        ...     x=x,
+        ...     y=y,
+        ...     position=position,
+        ... )
+        >>> rotate_2
+        <Rotate2.ar()>
+
+    Returns an array of the rotator's left and right outputs.
+    """
+
+    x = param()
+    y = param()
+    position = param(0)
+
+
+class Splay(PseudoUGen):
+    """
+    A stereo signal spreader.
+
+    ::
+
+        >>> source = supriya.ugens.SinOsc.ar(frequency=[333, 444, 555, 666, 777])
+        >>> splay = supriya.ugens.Splay.ar(source=source)
+        >>> splay
+        <UGenVector([<BinaryOpUGen.ar(MULTIPLICATION)[0]>, <BinaryOpUGen.ar(MULTIPLICATION)[0]>])>
+
+    ::
+
+        >>> print(splay)
+        synthdef:
+            name: ...
+            ugens:
+            -   SinOsc.ar/0:
+                    frequency: 333.0
+                    phase: 0.0
+            -   Pan2.ar/0:
+                    source: SinOsc.ar/0[0]
+                    position: -1.0
+                    level: 1.0
+            -   SinOsc.ar/1:
+                    frequency: 444.0
+                    phase: 0.0
+            -   Pan2.ar/1:
+                    source: SinOsc.ar/1[0]
+                    position: -0.5
+                    level: 1.0
+            -   SinOsc.ar/2:
+                    frequency: 555.0
+                    phase: 0.0
+            -   Pan2.ar/2:
+                    source: SinOsc.ar/2[0]
+                    position: 0.0
+                    level: 1.0
+            -   SinOsc.ar/3:
+                    frequency: 666.0
+                    phase: 0.0
+            -   Pan2.ar/3:
+                    source: SinOsc.ar/3[0]
+                    position: 0.5
+                    level: 1.0
+            -   Sum4.ar/0:
+                    input_one: Pan2.ar/0[0]
+                    input_two: Pan2.ar/1[0]
+                    input_three: Pan2.ar/2[0]
+                    input_four: Pan2.ar/3[0]
+            -   Sum4.ar/1:
+                    input_one: Pan2.ar/0[1]
+                    input_two: Pan2.ar/1[1]
+                    input_three: Pan2.ar/2[1]
+                    input_four: Pan2.ar/3[1]
+            -   SinOsc.ar/4:
+                    frequency: 777.0
+                    phase: 0.0
+            -   Pan2.ar/4:
+                    source: SinOsc.ar/4[0]
+                    position: 1.0
+                    level: 1.0
+            -   BinaryOpUGen(ADDITION).ar/0:
+                    left: Sum4.ar/0[0]
+                    right: Pan2.ar/4[0]
+            -   BinaryOpUGen(MULTIPLICATION).ar/0:
+                    left: BinaryOpUGen(ADDITION).ar/0[0]
+                    right: 0.4472135954999579
+            -   BinaryOpUGen(ADDITION).ar/1:
+                    left: Sum4.ar/1[0]
+                    right: Pan2.ar/4[1]
+            -   BinaryOpUGen(MULTIPLICATION).ar/1:
+                    left: BinaryOpUGen(ADDITION).ar/1[0]
+                    right: 0.4472135954999579
+    """
+
+    ### CLASS VARIABLES ###
+
+    _ordered_keys = (
+        "spread",
+        "level",
+        "center",
+        "normalize",
+        "source",
+    )
+    _unexpanded_keys = ("source",)
+
+    @classmethod
+    def _new_expanded(cls, calculation_rate=None, **kwargs):
+        def recurse(
+            all_expanded_params: UGenRecursiveParams,
+        ) -> UGenOperable:
+            if (
+                not isinstance(all_expanded_params, dict)
+                and len(all_expanded_params) == 1
+            ):
+                all_expanded_params = all_expanded_params[0]
+            if isinstance(all_expanded_params, dict):
+                return cls._new_single(
+                    calculation_rate=calculation_rate,
+                    special_index=0,
+                    **all_expanded_params,
+                )
+            return UGenVector(
+                *(recurse(expanded_params) for expanded_params in all_expanded_params)
+            )
+
+        return Mix.multichannel(
+            recurse(UGen._expand_params(kwargs, unexpanded_keys=cls._unexpanded_keys)),
+            2,
+        )
+
+    @classmethod
+    def _new_single(
+        cls,
+        *,
+        calculation_rate=None,
+        center=0,
+        level=1,
+        normalize=True,
+        source=None,
+        spread=1,
+        **kwargs,
+    ):
+        positions = [
+            (i * (2 / (len(source) - 1)) - 1) * spread + center
+            for i in range(len(source))
+        ]
+        if normalize:
+            if calculation_rate == CalculationRate.AUDIO:
+                level = level * math.sqrt(1 / len(source))
+            else:
+                level = level / len(source)
+        panners = _get_method_for_rate(Pan2, calculation_rate)(
+            source=source, position=positions
+        )
+        return Mix.multichannel(panners, 2) * level
+
+    @classmethod
+    def ar(cls, *, source, center=0, level=1, normalize=True, spread=1):
+        return cls._new_expanded(
+            calculation_rate=CalculationRate.AUDIO,
+            center=center,
+            level=level,
+            normalize=normalize,
+            source=source,
+            spread=spread,
+        )
+
+    @classmethod
+    def kr(cls, *, source, center=0, level=1, normalize=True, spread=1):
+        return cls._new_expanded(
+            calculation_rate=CalculationRate.CONTROL,
+            center=center,
+            level=level,
+            normalize=normalize,
+            source=source,
+            spread=spread,
+        )
+
+
+@ugen(ar=True, kr=True)
+class XFade2(UGen):
+    """
+    Two channel equal power crossfader.
+
+    ::
+
+        >>> xfade_3 = supriya.ugens.XFade2.ar(
+        ...     in_a=supriya.ugens.Saw.ar(),
+        ...     in_b=supriya.ugens.SinOsc.ar(),
+        ...     level=1,
+        ...     pan=supriya.ugens.LFTri.kr(frequency=0.1),
+        ... )
+        >>> xfade_3
+        <XFade2.ar()[0]>
+    """
+
+    in_a = param()
+    in_b = param(0)
+    pan = param(0)
+    level = param(1)
