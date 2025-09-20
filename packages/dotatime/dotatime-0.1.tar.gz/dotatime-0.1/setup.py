8bddbd09ee7996308e2d2fbb8ef8e03f109557ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 07:04:16 +0000

    Add package dotatime/0.1/dotatime-0.1.tar.gz

---
+++ packages/dotatime/dotatime-0.1.tar.gz/dotatime-0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name='dotatime',
+ version='0.1',
+ author="justin",
+ author_email="justin@dev.org",
+ description="<Basic desc>",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
