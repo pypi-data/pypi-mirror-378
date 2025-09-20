1d478a6478a6571dd2ab2f479fea18a6c1e34128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 04:04:53 +0000

    Add package skore-hub-project/0.0.11/skore_hub_project-0.0.11-py3-none-any.whl

---
+++ packages/skore-hub-project/skore_hub_project-0.0.11-py3-none-any.whl/skore_hub_project/authentication/login.py
@@ -0,0 +1,42 @@
+"""Login to ``skore hub``."""
+
+from __future__ import annotations
+
+from contextlib import suppress
+from webbrowser import open as open_webbrowser
+
+from httpx import HTTPError
+from rich.align import Align
+from rich.panel import Panel
+
+from .. import console
+from ..client import api
+from . import token as Token
+
+
+def login(*, timeout=600):
+    """Login to ``skore hub``."""
+    with suppress(HTTPError):
+        if Token.exists() and (Token.access() is not None):
+            return
+
+    url, device_code, user_code = api.get_oauth_device_login()
+
+    if not open_webbrowser(url):
+        console.print(
+            Panel(
+                Align(
+                    f"Please visit this [link={url}]page[/link] to login.",
+                    align="center",
+                ),
+                title="[cyan]Skore HUB[/cyan]",
+                border_style="orange1",
+                expand=False,
+                padding=1,
+                title_align="center",
+            )
+        )
+
+    api.get_oauth_device_code_probe(device_code, timeout=timeout)
+    api.post_oauth_device_callback(device_code, user_code)
+    Token.save(*api.get_oauth_device_token(device_code))
---
+++ packages/skore-hub-project/skore_hub_project-0.0.11-py3-none-any.whl/skore_hub_project/authentication/token.py
@@ -0,0 +1,59 @@
+"""Token used for ``skore hub`` authentication."""
+
+from __future__ import annotations
+
+import json
+import pathlib
+import tempfile
+from datetime import datetime, timezone
+
+from ..client.api import post_oauth_refresh_token
+
+
+def filepath():
+    """Filepath used to save the tokens on disk."""
+    return pathlib.Path(tempfile.gettempdir(), "skore.token")
+
+
+def save(access: str, refreshment: str, expires_at: str):
+    """
+    Save the token.
+
+    Save the tokens to the disk to prevent user to login more than once, as long as
+    the access token is valid or can be refreshed.
+    """
+    filepath().write_text(
+        json.dumps(
+            (
+                access,
+                refreshment,
+                expires_at,
+            )
+        )
+    )
+
+
+def exists() -> bool:
+    """Existence of the token."""
+    return filepath().exists()
+
+
+def access(*, refresh=True) -> str:
+    """
+    Access token.
+
+    Parameters
+    ----------
+    refresh : bool, optional
+        Refresh the token on-the-fly if necessary, default True.
+    """
+    access, refreshment, expiration = json.loads(filepath().read_text())
+
+    if refresh and datetime.fromisoformat(expiration) <= datetime.now(timezone.utc):
+        # Retrieve freshly updated tokens
+        access, refreshment, expiration = post_oauth_refresh_token(refreshment)
+
+        # Re-save the refreshed tokens
+        save(access, refreshment, expiration)
+
+    return access
