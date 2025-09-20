f163ec432bd278814e392805bd0f9dccbe0932da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 05:04:11 +0000

    Add package aiogram-renderer/1.3.8b24/aiogram_renderer-1.3.8b24-py3-none-any.whl

---
+++ packages/aiogram-renderer/aiogram_renderer-1.3.8b24-py3-none-any.whl/aiogram_renderer/widgets/inline/__init__.py
@@ -0,0 +1,15 @@
+__all__ = [
+    "Button",
+    "Mode",
+    "Delete",
+    "Disable",
+    "ComeTo",
+    "Panel",
+    "DynamicPanel",
+    "Row",
+    "Column"
+]
+
+from .button import Button, Mode, Delete, Disable, ComeTo
+from .panel import Panel, DynamicPanel, Row, Column
+
