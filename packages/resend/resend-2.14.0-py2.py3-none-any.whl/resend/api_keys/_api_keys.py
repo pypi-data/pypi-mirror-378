49c7190069871749cda42615f7fbcefec9089008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 14:04:42 +0000

    Add package resend/2.14.0/resend-2.14.0-py2.py3-none-any.whl

---
+++ packages/resend/resend-2.14.0-py2.py3-none-any.whl/resend/api_keys/_api_keys.py
@@ -0,0 +1,108 @@
+from typing import Any, Dict, List, cast
+
+from typing_extensions import NotRequired, TypedDict
+
+from resend import request
+from resend.api_keys._api_key import ApiKey
+
+
+class ApiKeys:
+
+    class ListResponse(TypedDict):
+        """
+        ListResponse type that wraps a list of API key objects
+
+        Attributes:
+            data (List[ApiKey]): A list of API key objects
+        """
+
+        data: List[ApiKey]
+        """
+        A list of API key objects
+        """
+
+    class CreateApiKeyResponse(TypedDict):
+        """
+        CreateApiKeyResponse is the type that wraps the response of the API key that was created
+
+        Attributes:
+            id (str): The ID of the created API key
+            token (str): The token of the created API key
+        """
+
+        id: str
+        """
+        The ID of the created API key
+        """
+        token: str
+        """
+        The token of the created API key
+        """
+
+    class CreateParams(TypedDict):
+        name: str
+        """
+        The API key name.
+        """
+        permission: NotRequired[str]
+        """
+        The API key can have full access to Resend's API or be only
+        restricted to send emails. * full_access: Can create, delete, get,
+        and update any resource. * sending_access: Can only send emails.
+        """
+        domain_id: NotRequired[str]
+        """
+        Restrict an API key to send emails only from a specific domain.
+        This is only used when the permission is set to sending_access.
+        """
+
+    @classmethod
+    def create(cls, params: CreateParams) -> CreateApiKeyResponse:
+        """
+        Add a new API key to authenticate communications with Resend.
+        see more: https://resend.com/docs/api-reference/api-keys/create-api-key
+
+        Args:
+            params (CreateParams): The API key creation parameters
+
+        Returns:
+            CreateApiKeyResponse: The created API key response with id and token
+        """
+        path = "/api-keys"
+        resp = request.Request[ApiKeys.CreateApiKeyResponse](
+            path=path, params=cast(Dict[Any, Any], params), verb="post"
+        ).perform_with_content()
+        return resp
+
+    @classmethod
+    def list(cls) -> ListResponse:
+        """
+        Retrieve a list of API keys for the authenticated user.
+        see more: https://resend.com/docs/api-reference/api-keys/list-api-keys
+
+        Returns:
+            ListResponse: A list of API key objects
+        """
+        path = "/api-keys"
+        resp = request.Request[ApiKeys.ListResponse](
+            path=path, params={}, verb="get"
+        ).perform_with_content()
+        return resp
+
+    @classmethod
+    def remove(cls, api_key_id: str) -> None:
+        """
+        Remove an existing API key.
+        see more: https://resend.com/docs/api-reference/api-keys/delete-api-key
+
+        Args:
+            api_key_id (str): The ID of the API key to remove
+
+        Returns:
+            None
+        """
+        path = f"/api-keys/{api_key_id}"
+
+        # This would raise if failed
+        request.Request[None](path=path, params={}, verb="delete").perform()
+        return None
---
+++ packages/resend/resend-2.14.0-py2.py3-none-any.whl/resend/contacts/_contacts.py
@@ -0,0 +1,247 @@
+from typing import Any, Dict, List, Optional, cast
+
+from typing_extensions import NotRequired, TypedDict
+
+from resend import request
+
+from ._contact import Contact
+
+
+class Contacts:
+
+    class RemoveContactResponse(TypedDict):
+        """
+        RemoveContactResponse is the type that wraps the response of the contact that was removed
+
+        Attributes:
+            object (str): 'contact'
+            contact (str): The ID of the removed contact
+            deleted (bool): Whether the contact was deleted
+        """
+
+        object: str
+        """
+        The object type: contact
+        """
+        contact: str
+        """
+        The ID of the removed contact.
+        """
+        deleted: bool
+        """
+        Whether the contact was deleted.
+        """
+
+    class ListResponse(TypedDict):
+        """
+        ListResponse type that wraps a list of contact objects
+
+        Attributes:
+            object (str): The object type: list
+            data (List[Contact]): A list of contact objects
+        """
+
+        object: str
+        """
+        The object type: list
+        """
+        data: List[Contact]
+        """
+        A list of contact objects
+        """
+
+    class CreateContactResponse(TypedDict):
+        """
+        CreateContactResponse is the type that wraps the response of the contact that was created
+
+        Attributes:
+            object (str): The ID of the created contact
+            id (str): The ID of the created contact
+        """
+
+        object: str
+        """
+        The object type: email
+        """
+        id: str
+        """
+        The ID of the scheduled email that was canceled.
+        """
+
+    class UpdateContactResponse(TypedDict):
+        """
+        UpdateContactResponse is the type that wraps the response of the contact that was updated
+
+        Attributes:
+            object (str): The ID of the updated contact
+            id (str): The ID of the updated contact
+        """
+
+        object: str
+        """
+        The object type: email
+        """
+        id: str
+        """
+        The ID of the updated contact.
+        """
+
+    class CreateParams(TypedDict):
+        audience_id: str
+        """
+        The audience id.
+        """
+        email: str
+        """
+        The email of the contact.
+        """
+        first_name: NotRequired[str]
+        """
+        The first name of the contact.
+        """
+        last_name: NotRequired[str]
+        """
+        The last name of the contact.
+        """
+        unsubscribed: NotRequired[bool]
+        """
+        The unsubscribed status of the contact.
+        """
+
+    class UpdateParams(TypedDict):
+        audience_id: str
+        """
+        The audience id.
+        """
+        id: NotRequired[str]
+        """
+        The contact id.
+        """
+        email: NotRequired[str]
+        """
+        The email of the contact.
+        """
+        first_name: NotRequired[str]
+        """
+        The first name of the contact.
+        """
+        last_name: NotRequired[str]
+        """
+        The last name of the contact.
+        """
+        unsubscribed: NotRequired[bool]
+        """
+        The unsubscribed status of the contact.
+        """
+
+    @classmethod
+    def create(cls, params: CreateParams) -> CreateContactResponse:
+        """
+        Create a new contact.
+        see more: https://resend.com/docs/api-reference/contacts/create-contact
+
+        Args:
+            params (CreateParams): The contact creation parameters
+
+        Returns:
+            CreateContactResponse: The created contact response
+        """
+
+        path = f"/audiences/{params['audience_id']}/contacts"
+        resp = request.Request[Contacts.CreateContactResponse](
+            path=path, params=cast(Dict[Any, Any], params), verb="post"
+        ).perform_with_content()
+        return resp
+
+    @classmethod
+    def update(cls, params: UpdateParams) -> UpdateContactResponse:
+        """
+        Update an existing contact.
+        see more: https://resend.com/docs/api-reference/contacts/update-contact
+
+        Args:
+            params (UpdateParams): The contact update parameters
+
+        Returns:
+            UpdateContactResponse: The updated contact response.
+        """
+        if params.get("id") is None and params.get("email") is None:
+            raise ValueError("id or email must be provided")
+
+        val = params.get("id") if params.get("id") is not None else params.get("email")
+
+        path = f"/audiences/{params['audience_id']}/contacts/{val}"
+        resp = request.Request[Contacts.UpdateContactResponse](
+            path=path, params=cast(Dict[Any, Any], params), verb="patch"
+        ).perform_with_content()
+        return resp
+
+    @classmethod
+    def list(cls, audience_id: str) -> ListResponse:
+        """
+        List all contacts for the provided audience.
+        see more: https://resend.com/docs/api-reference/contacts/list-contacts
+
+        Args:
+            audience_id (str): The audience ID
+
+        Returns:
+            ListResponse: A list of contact objects
+        """
+        path = f"/audiences/{audience_id}/contacts"
+        resp = request.Request[Contacts.ListResponse](
+            path=path, params={}, verb="get"
+        ).perform_with_content()
+        return resp
+
+    @classmethod
+    def get(
+        cls, audience_id: str, id: Optional[str] = None, email: Optional[str] = None
+    ) -> Contact:
+        """
+        Get a contact.
+        see more: https://resend.com/docs/api-reference/contacts/get-contact
+
+        Args:
+            id (str): The contact ID
+            audience_id (str): The audience ID
+            email (Optional[str]): The contact email
+
+        Returns:
+            Contact: The contact object
+        """
+        contact = email if id is None else id
+        if contact is None:
+            raise ValueError("id or email must be provided")
+
+        path = f"/audiences/{audience_id}/contacts/{contact}"
+        resp = request.Request[Contact](
+            path=path, params={}, verb="get"
+        ).perform_with_content()
+        return resp
+
+    @classmethod
+    def remove(
+        cls, audience_id: str, id: Optional[str] = None, email: Optional[str] = None
+    ) -> RemoveContactResponse:
+        """
+        Remove a contact by ID or by Email
+        see more: https://resend.com/docs/api-reference/contacts/delete-contact
+
+        Args:
+            audience_id (str): The audience ID
+            id (str): The contact ID
+            email (str): The contact email
+
+        Returns:
+            RemoveContactResponse: The removed contact response object
+        """
+        contact = email if id is None else id
+        if contact is None:
+            raise ValueError("id or email must be provided")
+        path = f"/audiences/{audience_id}/contacts/{contact}"
+
+        resp = request.Request[Contacts.RemoveContactResponse](
+            path=path, params={}, verb="delete"
+        ).perform_with_content()
+        return resp
