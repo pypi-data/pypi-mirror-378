d5a129d88060dd8dcabed6cadb5a037bc11ada22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 14:04:25 +0000

    Add package rcs/2.0.0rc8/rcs-2.0.0rc8.tar.gz

---
+++ packages/rcs/rcs-2.0.0rc8.tar.gz/rcs-2.0.0rc8/src/rcs/client.py
@@ -0,0 +1,13 @@
+import httpx
+import mimetypes
+
+import os
+from .base_client import PinnacleBase, AsyncPinnacleBase
+
+
+class Pinnacle(PinnacleBase):
+    pass
+
+
+class AsyncPinnacle(AsyncPinnacleBase):
+    pass
