53f26bf4aad6a38daf02c6d35817e0908b02eacf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 03:12:31 +0000

    Add package qing-client/0.1.11/qing_client-0.1.11.tar.gz

---
+++ packages/qing-client/qing_client-0.1.11.tar.gz/qing_client-0.1.11/qingclient/types/__init__.py
@@ -0,0 +1,53 @@
+from .base import (
+    ApiResponse,
+    Pagination,
+    PaginatedResponse,
+    ClientConfig,
+    UserContext,
+    RequestOptions
+)
+from .auth import LoginResponse, LoginApiResponse
+from .wx import (
+    WxMiniProgramTokenResponse,
+    WxOfficialAccountTokenResponse,
+    WxJsapiTicketResponse,
+    WxMiniProgramLoginRequest,
+    WxMiniProgramLoginResponse,
+    WxSignatureRequest,
+    WxSignatureResponse
+)
+from .msg import MailRequest, FeishuMessage
+from .user import (
+    UserCreateRequest,
+    UserUpdateRequest,
+    PasswordChangeRequest,
+    UserResponse,
+    UserListResponse,
+    UserRole
+)
+
+__all__ = [
+    'ApiResponse',
+    'Pagination',
+    'PaginatedResponse',
+    'ClientConfig',
+    'UserContext',
+    'RequestOptions',
+    'LoginResponse',
+    'LoginApiResponse',
+    'WxMiniProgramTokenResponse',
+    'WxOfficialAccountTokenResponse',
+    'WxJsapiTicketResponse',
+    'WxMiniProgramLoginRequest',
+    'WxMiniProgramLoginResponse',
+    'WxSignatureRequest',
+    'WxSignatureResponse',
+    'MailRequest',
+    'FeishuMessage',
+    'UserCreateRequest',
+    'UserUpdateRequest',
+    'PasswordChangeRequest',
+    'UserResponse',
+    'UserListResponse',
+    'UserRole'
+]
