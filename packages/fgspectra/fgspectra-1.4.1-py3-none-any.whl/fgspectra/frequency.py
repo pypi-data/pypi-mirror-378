b3b7dd898135c59959d4527a2620072f6af1ae0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 03:12:28 +0000

    Add package fgspectra/1.4.1/fgspectra-1.4.1-py3-none-any.whl

---
+++ packages/fgspectra/fgspectra-1.4.1-py3-none-any.whl/fgspectra/frequency.py
@@ -0,0 +1,384 @@
+# -*- coding: utf-8 -*-
+r"""
+Frequency-dependent foreground components.
+
+This module implements the frequency-dependent component of common foreground
+contaminants.
+
+This package draws inspiration from FGBuster (Davide Poletti and Josquin Errard)
+and BeFoRe (David Alonso and Ben Thorne).
+"""
+
+import numpy as np
+from scipy import constants
+from abc import abstractmethod
+from .model import Model
+
+try:
+    from numpy import trapezoid
+except ImportError:
+    from numpy import trapz as trapezoid
+
+T_CMB = 2.72548
+H_OVER_KT_CMB = constants.h * 1e9 / constants.k / T_CMB
+
+
+def _flux2cmb(nu):
+    """Converts flux to thermodynamics units"""
+    x = H_OVER_KT_CMB * nu
+    g2_min1 = (
+        2.0
+        * constants.k**3
+        * T_CMB**2
+        * x**4
+        * np.exp(x)
+        / (constants.h * constants.c * np.expm1(x)) ** 2
+    )
+    return 1.0 / g2_min1
+
+
+def _rj2cmb(nu):
+    x = H_OVER_KT_CMB * nu
+    return (np.expm1(x) / x) ** 2 / np.exp(x)
+
+
+class FreqModel(Model):
+    def eval_bandpass(self, **kw):
+        """Bandpass integrated version of eval()
+
+        The inheriting class's eval() function should have
+
+            if isinstance(nu, list):
+                return self.eval_bandpass(**kw)
+
+        - to pass integrated list frequencies back to eval() one by one
+
+        * iterates over the ``nu`` argument of the caller
+          (while keeping all the other arguments fixed)
+        * splits each element of the iteration in ``nu_band, transmittance``
+        * integrates the caller function over the bandpass.
+          ``np.trapezoid(self.eval(nu_band) * transmittance, nu_band)``
+          Note that no normalization nor unit conversion is done to the
+          transmittance
+        * stacks the output of the iteration (the frequency dimension is the last)
+          and returns it
+        """
+        # This piece of code is fairly complicated, we did because:
+        # 1) We want to call eval on each element of the nu list (i.e. we iterate
+        #    over the bandpasses) but we don't want to define a new eval_bandpass
+        #    function for every class
+        # 2) We don't want to use a decorator because it breaks the signature
+        #    handling of eval and the modification of its defaults.
+
+        # You are here because this function was called inside eval before any other
+        # variable was defined.
+
+        # Store the nu-transmittance list because the nu keyword argument has to be
+        # modified with the frequencies of each bandpass
+        nus_transmittances = kw.pop("nu")
+        res = None
+        # Fill the entries by iterating over the bandpasses
+        for i_band, (nu, transmittance) in enumerate(nus_transmittances):
+            # Allowing dimension of transmittance to be [freq, ell] instead of just [freq]
+            if len(transmittance.shape) == 1:
+                integral = trapezoid(self.eval(nu=nu, **kw) * transmittance, nu)
+            else:
+                # In case transmittance has shape [freq, ell], the SED f has to be trasposed
+                # to perform the integration in frequency
+                integral = trapezoid(
+                    self.eval(nu=nu, **kw)[..., np.newaxis] * transmittance, nu, axis=0
+                )
+
+            if res is None:
+                res = np.empty(integral.shape + (len(nus_transmittances),))
+
+            res[..., i_band] = integral
+
+        return res
+
+    @abstractmethod
+    def eval(self, **kwargs):
+        pass
+
+
+class PowerLaw(FreqModel):
+    r"""Power Law
+
+    .. math:: f(\nu) = (\nu / \nu_0)^{\beta}
+    """
+
+    def eval(self, nu=None, beta=None, nu_0=None):
+        """Evaluation of the SED
+
+        Parameters
+        ----------
+        nu: float or array
+            Frequency in the same units as `nu_0`. If array, the shape is
+            ``(freq)``.
+        beta: float or array
+            Spectral index. If array, the shape is ``(...)``.
+        nu_0: float or array
+            Reference frequency in the same units as `nu`. If array, the shape
+            is ``(...)``.
+
+        Returns
+        -------
+        sed: ndarray
+            If `nu` is an array, the shape is ``(..., freq)``.
+            If `nu` is scalar, the shape is ``(..., 1)``.
+            Note that the last dimension is guaranteed to be the frequency.
+
+        Note
+        ----
+        The extra dimensions ``...`` in the output are the broadcast of the
+        ``...`` in the input (which are required to be broadcast-compatible).
+
+        Examples
+        --------
+
+        - T, E and B synchrotron SEDs with the same reference frequency but
+          different spectral indices. `beta` is an array with shape ``(3)``,
+          `nu_0` is a scalar.
+
+        - SEDs of synchrotron and dust (approximated as power law). Both `beta`
+          and `nu_0` are arrays with shape ``(2)``
+
+        """
+        if isinstance(nu, list):
+            return self.eval_bandpass(nu=nu, beta=beta, nu_0=nu_0)
+
+        beta = np.array(beta)[..., np.newaxis]
+        nu_0 = np.array(nu_0)[..., np.newaxis]
+        return (nu / nu_0) ** beta * (_rj2cmb(nu) / _rj2cmb(nu_0))
+
+
+class Synchrotron(PowerLaw):
+    """Alias of :class:`PowerLaw`"""
+
+    pass
+
+
+class ModifiedBlackBody(FreqModel):
+    r"""Modified black body in K_RJ
+
+    .. math:: f(\nu) = (\nu / \nu_0)^{\beta + 1} / (e^x - 1)
+
+    where :math:`x = h \nu / k_B T_d`
+    """
+
+    def eval(self, nu=None, nu_0=None, temp=None, beta=None):
+        """Evaluation of the SED
+
+        Parameters
+        ----------
+        nu: float or array
+            Frequency in GHz.
+        beta: float or array
+            Spectral index.
+        temp: float or array
+            Dust temperature.
+        nu_0: float
+            Reference frequency in Hz.
+
+        Returns
+        -------
+        sed: ndarray
+            The last dimension is the frequency dependence.
+            The leading dimensions are the broadcast between the hypothetic
+            dimensions of `beta` and `temp`.
+        """
+        if isinstance(nu, list):
+            return self.eval_bandpass(nu=nu, nu_0=nu_0, temp=temp, beta=beta)
+
+        beta = np.array(beta)[..., np.newaxis]
+        temp = np.array(temp)[..., np.newaxis]
+        x = 1e9 * constants.h * nu / (constants.k * temp)
+        x_0 = 1e9 * constants.h * nu_0 / (constants.k * temp)
+        res = (nu / nu_0) ** (beta + 1.0) * np.expm1(x_0) / np.expm1(x)
+        return res * (_rj2cmb(nu) / _rj2cmb(nu_0))
+
+
+class CIB(ModifiedBlackBody):
+    """Alias of :class:`ModifiedBlackBody`"""
+
+    pass
+
+
+class ThermalSZ(FreqModel):
+    r"""Thermal Sunyaev-Zel'dovich in K_CMB
+
+    This class implements the
+
+    .. math:: f(\nu) = x \coth(x/2) - 4
+
+    where :math:`x = h \nu / k_B T_CMB`
+    """
+
+    @staticmethod
+    def f(nu):
+        x = constants.h * (nu * 1e9) / (constants.k * T_CMB)
+        return x / np.tanh(x / 2.0) - 4.0
+
+    def eval(self, nu=None, nu_0=None):
+        """Compute the SED with the given frequency and parameters.
+
+        nu : float
+            Frequency in GHz.
+        T_CMB (optional) : float
+        """
+        if isinstance(nu, list):
+            return self.eval_bandpass(nu=nu, nu_0=nu_0)
+
+        return ThermalSZ.f(nu) / ThermalSZ.f(nu_0)
+
+
+class FreeFree(FreqModel):
+    r"""Free-free
+
+    .. math:: f(\nu) = EM * ( 1 + log( 1 + (\nu_{ff} / \nu)^{3/\pi} ) )
+    .. math:: \nu_{ff} = 255.33e9 * (Te / 1000)^{3/2}
+    """
+
+    def eval(self, nu=None, EM=None, Te=None):
+        """Evaluation of the SED
+
+        Parameters
+        ----------
+        nu: float or array
+            Frequency in the same units as `nu_0`. If array, the shape is
+            ``(freq)``.
+        EM: float or array
+            Emission measure in cm^-6 pc (usually around 300). If array, the shape is ``(...)``.
+        Te: float or array
+            Electron temperature (typically around 7000). If array, the shape is ``(...)``.
+
+        Returns
+        -------
+        sed: ndarray
+            If `nu` is an array, the shape is ``(..., freq)``.
+            If `nu` is scalar, the shape is ``(..., 1)``.
+            Note that the last dimension is guaranteed to be the frequency.
+
+        Note
+        ----
+        The extra dimensions ``...`` in the output are the broadcast of the
+        ``...`` in the input (which are required to be broadcast-compatible).
+
+        Examples
+        --------
+
+        - Free-free emission in temperature.
+
+        """
+        if isinstance(nu, list):
+            return self.eval_bandpass(nu=nu, EM=EM, Te=Te)
+
+        EM = np.array(EM)[..., np.newaxis]
+        Te = np.array(Te)[..., np.newaxis]
+        Teff = (Te / 1.0e3) ** 1.5
+        nuff = 255.33e9 * Teff
+        gff = 1.0 + np.log(1.0 + (nuff / nu) ** (np.sqrt(3) / np.pi))
+        print("warning: I need to check the units on this")
+        return EM * gff
+
+
+class ConstantSED(FreqModel):
+    """Frequency-independent component."""
+
+    def eval(self, nu=None, amp=1.0):
+        """Evaluation of the SED
+
+        Parameters
+        ----------
+        nu: float or array
+            It just determines the shape of the output.
+        amp: float or array
+            Amplitude (or set of amplitudes) of the constant SED.
+
+        Returns
+        -------
+        sed: ndarray
+            If `nu` is an array, the shape is ``amp.shape + (freq)``.
+            If `nu` is scalar, the shape is ``amp.shape + (1)``.
+            Note that the last dimension is guaranteed to be the frequency.
+        """
+        if isinstance(nu, list):
+            return self.eval_bandpass(nu=nu, amp=amp)
+
+        amp = np.array(amp)[..., np.newaxis]
+        return amp * np.ones_like(np.array(nu))
+
+
+class FreeSED(FreqModel):
+    """Frequency-dependent component for which every entry of the SED is specified."""
+
+    def eval(self, nu=None, sed=None):
+        """Evaluation of the SED
+
+        Parameters
+        ----------
+        nu: float or array
+            It just determines the shape of the output.
+        sed: float or array
+            Values of the SED. Must be the same shape as nu.
+            There is no normalisation, entries are used as is.
+
+        Returns
+        -------
+        sed: ndarray
+            If `nu` is an array, the shape is ``amp.shape + (freq)``.
+            If `nu` is scalar, the shape is ``amp.shape + (1)``.
+            Note that the last dimension is guaranteed to be the frequency.
+        """
+        if isinstance(nu, (list, np.ndarray)):
+            try:
+                assert len(nu) == len(sed[..., :])
+            except:
+                print("SED and nu must have the same shape.")
+        if isinstance(nu, list):
+            return self.eval_bandpass(nu=nu, sed=sed)
+        return np.asarray(sed)
+
+
+class Join(FreqModel):
+    """Join several SED models together"""
+
+    def __init__(self, *seds, **kwargs):
+        """Join several SED models together
+
+        Parameters
+        ----------
+        *sed:
+            Sequence of SED models to be joined together
+        """
+        self._seds = seds
+        self.set_defaults(**kwargs)
+
+    def set_defaults(self, **kwargs):
+        if "kwseq" in kwargs:
+            for sed, sed_kwargs in zip(self._seds, kwargs["kwseq"]):
+                sed.set_defaults(**sed_kwargs)
+
+    def _get_repr(self):
+        return {type(self).__name__: [sed._get_repr() for sed in self._seds]}
+
+    @property
+    def defaults(self):
+        return {"kwseq": [sed.defaults for sed in self._seds]}
+
+    def eval(self, kwseq=None):
+        """Compute the SED with the given frequency and parameters.
+
+        *kwseq
+            The length of ``kwseq`` has to be equal to the number of SEDs
+            joined. ``kwseq[i]`` is a dictionary containing the keyword
+            arguments of the ``i``-th SED.
+        """
+        if kwseq:
+            seds = [sed(**kwargs) for sed, kwargs in zip(self._seds, kwseq)]
+        else:  # Handles the case in which no parameter has to be passed
+            seds = [sed() for sed in self._seds]
+        res = np.empty((len(seds),) + np.broadcast(*seds).shape)
+        for i in range(len(seds)):
+            res[i] = seds[i]
+        return res
---
+++ packages/fgspectra/fgspectra-1.4.1-py3-none-any.whl/fgspectra/power.py
@@ -0,0 +1,387 @@
+r"""
+Power spectrum
+
+This module implements the ell-dependent component of common foreground
+contaminants.
+
+This module draws inspiration from FGBuster (Davide Poletti and Josquin Errard)
+and BeFoRe (David Alonso and Ben Thorne).
+"""
+
+import os
+import importlib.resources as ilr
+
+import numpy as np
+from .model import Model
+
+
+def _get_power_file(model):
+    """File path for the named model"""
+    data_path = ilr.files("fgspectra") / "data"
+    filename = os.path.join(data_path, "cl_%s.dat" % model)
+    if os.path.exists(filename):
+        return filename
+    raise ValueError("No template for model " + model)
+
+
+class PowerSpectrumFromFile(Model):
+    """Power spectrum loaded from file(s)
+
+    Parameters
+    ----------
+    filenames: array_like of strings
+        File(s) to load. It can be a string or any (nested) sequence of strings
+
+    Examples
+    --------
+
+    >>> ell = range(5)
+
+    Power spectrum of a single file
+
+    >>> my_file = 'cl.dat'
+    >>> ps = PowerSpectrumFromFile(my_file)
+    >>> ps(ell).shape
+    (5)
+    >>> ps = PowerSpectrumFromFile([my_file])  # List
+    >>> ps(ell).shape
+    (1, 5)
+
+    Two correlated components
+
+    >>> my_files = [['cl_comp1.dat', 'cl_comp1xcomp2.dat'],
+    ...             ['cl_comp1xcomp2.dat', 'cl_comp2.dat']]
+    >>> ps = PowerSpectrumFromFile(my_files)
+    >>> ps(ell).shape
+    (2, 2, 5)
+
+    """
+
+    def __init__(self, filenames, **kwargs):
+        """
+
+        The file format should be two columns, ell and the spectrum.
+        """
+        filenames = np.array(filenames)
+        self._cl = np.empty(filenames.shape + (0,))
+
+        for i, filename in np.ndenumerate(filenames):
+            ell, spec = np.genfromtxt(filename, unpack=True)
+            ell = ell.astype(int)
+            # Make sure that the new spectrum fits self._cl
+            n_missing_ells = ell.max() + 1 - self._cl.shape[-1]
+            if n_missing_ells > 0:
+                pad_width = [(0, 0)] * self._cl.ndim
+                pad_width[-1] = (0, n_missing_ells)
+                self._cl = np.pad(
+                    self._cl, pad_width, mode="constant", constant_values=0
+                )
+
+            self._cl[i + (ell,)] = spec
+        self.set_defaults(**kwargs)
+
+    def eval(self, ell=None, ell_0=None, amp=1.0):
+        """Compute the power spectrum with the given ell and parameters."""
+        #### TEMPORARY ####
+        if ell_0 is None:
+            print("NO NORMALISATION")
+            return amp * self._cl[..., ell]
+        else:
+            return amp * self._cl[..., ell] / self._cl[..., ell_0, np.newaxis]
+
+
+class PowerLawExtendedTemplate(PowerSpectrumFromFile):
+    """ " Power Spectrum from a file, extended by a power law on small scales."""
+
+    def eval(self, ell=None, ell_0=None, alpha=None, amp=1.0):
+        """Compute the power spectrum with the given ell and parameters."""
+        # TODO : the switch from template to power law is at ell_0 for now !
+        res = amp * self._cl[..., ell] / self._cl[..., ell_0, np.newaxis]
+        res[..., ell > ell_0] = amp * (ell[ell > ell_0] / ell_0) ** alpha
+        return res
+
+
+class PowerLawRescaledTemplate(PowerSpectrumFromFile):
+    """Power Spectrum from a file, rescaled by a power law."""
+
+    def eval(self, ell=None, ell_0=None, alpha=None, amp=1.0):
+        """Compute the power spectrum with the given ell and parameters."""
+        return (
+            amp
+            * self._cl[..., ell]
+            / self._cl[..., ell_0, np.newaxis]
+            * (ell / ell_0) ** alpha
+        )
+
+
+class tSZ_150_bat(PowerSpectrumFromFile):
+    """PowerSpectrum for Thermal Sunyaev-Zel'dovich (Dunkley et al. 2013)."""
+
+    def __init__(self, **kwargs):
+        """Intialize object with parameters."""
+        super().__init__(_get_power_file("tsz_150_bat"), **kwargs)
+
+
+class kSZ_bat_full(PowerSpectrumFromFile):
+    """
+    PowerSpectrum for Kinematic Sunyaev-Zel'dovich (Dunkley et al. 2013).
+    Contains both contributions from late-time kSZ and reionization kSZ.
+    """
+
+    def __init__(self, **kwargs):
+        """Intialize object with parameters."""
+        super().__init__(_get_power_file("ksz_bat_full"), **kwargs)
+
+
+class kSZ_bat(PowerSpectrumFromFile):
+    """PowerSpectrum for Kinematic Sunyaev-Zel'dovich (Dunkley et al. 2013)."""
+
+    def __init__(self, **kwargs):
+        """Intialize object with parameters."""
+        super().__init__(_get_power_file("ksz_bat"), **kwargs)
+
+
+class PowerLaw(Model):
+    r"""Power law
+
+    .. math:: C_\ell = (\ell / \ell_0)^\alpha
+    """
+
+    def eval(self, ell=None, alpha=None, ell_0=None, amp=1.0):
+        """
+
+        Parameters
+        ----------
+        ell: float or array
+            Multipole
+        alpha: float or array
+            Spectral index.
+        ell_0: float
+            Reference ell
+        amp: float or array
+            Amplitude, shape must be compatible with `alpha`.
+
+        Returns
+        -------
+        cl: ndarray
+            The last dimension is ell.
+            The leading dimensions are the hypothetic dimensions of `alpha`
+        """
+        alpha = np.array(alpha)[..., np.newaxis]
+        amp = np.array(amp)[..., np.newaxis]
+        return amp * (ell / ell_0) ** alpha
+
+
+class CorrelatedPowerLaws(PowerLaw):
+    """As PowerLaw, but requires only the diagonal of the component-component
+    dimensions and the correlation coefficient between TWO PL
+    """
+
+    def eval(self, ell=None, alpha=None, ell_0=None, amp=None, rho=None):
+        """
+        Parameters
+        ----------
+        ell: float or array
+            Multipole
+        amp: array
+            amplitude of the auto-spectra. Shape must be ``(..., 2)``, which
+            means that for time being we support only two correlated components
+        alpha: float or array
+            Spectral index. Shape must be broadcast-compatible with `amp`
+        ell_0: float
+            Reference ell
+
+        Returns
+        -------
+        cl: ndarray
+            The dimensions are ``(..., comp1, comp2, ell)``.
+            The leading dimensions are the hypothetic dimensions of `alpha` and
+            `amp`.
+        """
+        alpha = np.array(alpha)
+        amp = np.array(amp)
+
+        alpha = (alpha[..., np.newaxis, :] + alpha[..., np.newaxis]) / 2
+        amp = (amp[..., np.newaxis, :] * amp[..., np.newaxis]) ** 0.5
+        amp[..., 1, 0] *= rho
+        amp[..., 0, 1] *= rho
+
+        return super().eval(ell=ell, alpha=alpha, ell_0=ell_0, amp=amp)
+
+
+class PowerSpectraAndCorrelation(Model):
+    r"""Components' spectra and their correlation
+
+    Spectrum of correlated components defined by the spectrum of each component
+    and their correlation
+
+    Parameters
+    ----------
+    *power_spectra : series of `PowerSpectrum`
+        The series has lenght :math:`N (N + 1) / 2`, where :math:`N` is the
+        number of components. They specify the upper (or lower) triangle of the
+        component-component cross spectra, which is symmetric. The series stores
+        a `PowerSpectrum` for each component-component combination.
+        The main diagonal (i.e. the autospectra) goes first, the second diagonal
+        of the correlation matrix follows, then the third, etc.
+        The ordering is similar to the one returned by `healpy.anafast`.
+    """
+
+    def __init__(self, *power_spectra, **kwargs):
+        self._power_spectra = power_spectra
+        self.n_comp = np.rint(-1 + np.sqrt(1 + 8 * len(power_spectra))) // 2
+        self.n_comp = int(self.n_comp)
+        assert (self.n_comp + 1) * self.n_comp // 2 == len(power_spectra)
+        self.set_defaults(**kwargs)
+
+    def set_defaults(self, **kwargs):
+        if "kwseq" in kwargs:
+            for i, cl_kwargs in enumerate(kwargs["kwseq"]):
+                self._power_spectra[i].set_defaults(**cl_kwargs)
+
+    @property
+    def defaults(self):
+        return {"kwseq": [ps.defaults for ps in self.power_spectra]}
+
+    def _get_repr(self):
+        return {type(self).__name__: [ps._get_repr() for ps in self.power_spectra]}
+
+    def eval(self, kwseq=None):
+        """Compute the SED with the given frequency and parameters.
+
+        Parameters
+        ----------
+        kwseq
+            The length of `kwseq` has to be equal to the number of ps joined.
+            ``kwseq[i]`` is the argument list of the ``i``-th ps.
+
+        """
+        spectra = np.array(
+            [ps(**kwargs) for ps, kwargs in zip(self._power_spectra, kwseq)]
+        )
+        corrs = spectra[self.n_comp :]
+        cls = spectra[: self.n_comp]
+        sqrt_cls = [np.sqrt(cl) for cl in cls]
+        cls_shape = np.broadcast(*cls).shape
+
+        res = np.empty(  # Shape is (..., comp, comp, ell)
+            cls_shape[1:-1] + (self.n_comp, self.n_comp) + cls_shape[-1:]
+        )
+
+        for i in range(self.n_comp):
+            res[..., i, i, :] = cls[i]
+
+        i_corr = 0
+        for k_off_diag in range(1, self.n_comp):
+            for el_off_diag in range(self.n_comp - k_off_diag):
+                i = el_off_diag
+                j = el_off_diag + k_off_diag
+                res[..., i, j, :] = sqrt_cls[i] * sqrt_cls[j] * corrs[i_corr]
+                res[..., j, i, :] = res[..., i, j, :]
+                i_corr += 1
+
+        """
+        i_corr = 0
+        for i in range(self.n_comp):
+            res[..., i, i, :] = cls[i]
+            for j in range(i + 1, self.n_comp):
+                res[..., i, j, :] = sqrt_cls[i] * sqrt_cls[j] * corrs[i_corr]
+                res[..., j, i, :] = res[..., i, j, :]
+                i_corr += 1
+        """
+
+        assert i_corr == len(corrs)
+        return res
+
+
+class PowerSpectraAndCovariance(Model):
+    r"""Components' spectra and their covariance
+
+    Spectrum of correlated components defined by the spectrum of each component
+    and their correlation
+
+    Parameters
+    ----------
+    *power_spectra : series of `PowerSpectrum`
+        The series has length :math:`N (N + 1) / 2`, where :math:`N` is the
+        number of components. They specify the upper (or lower) triangle of the
+        component-component cross spectra, which is symmetric. The series stores
+        a `PowerSpectrum` for each component-component combination.
+        The main diagonal (i.e. the autospectra) goes first, the second diagonal
+        of the covariance matrix follows, then the third, etc.
+        The ordering is similar to the one returned by `healpy.anafast`.
+    """
+
+    def __init__(self, *power_spectra, **kwargs):
+        self._power_spectra = power_spectra
+        self.n_comp = np.rint(-1 + np.sqrt(1 + 8 * len(power_spectra))) // 2
+        self.n_comp = int(self.n_comp)
+        assert (self.n_comp + 1) * self.n_comp // 2 == len(power_spectra)
+        self.set_defaults(**kwargs)
+
+    def set_defaults(self, **kwargs):
+        if "kwseq" in kwargs:
+            for i, cl_kwargs in enumerate(kwargs["kwseq"]):
+                self._power_spectra[i].set_defaults(**cl_kwargs)
+
+    @property
+    def defaults(self):
+        return {"kwseq": [ps.defaults for ps in self._power_spectra]}
+
+    def _get_repr(self):
+        return {type(self).__name__: [ps._get_repr() for ps in self._power_spectra]}
+
+    def eval(self, kwseq=None):
+        """Compute the Cl with the given frequency and parameters.
+
+        kwseq
+            The length of `argss` has to be equal to the number of SEDs joined.
+            ``kwseq[i]`` is the argument list of the ``i``-th SED.
+        """
+        spectra = np.array(
+            [ps(**kwargs) for ps, kwargs in zip(self._power_spectra, kwseq)]
+        )
+        res = np.empty(  # Shape is (..., comp, comp, ell)
+            spectra.shape[1:-1] + (self.n_comp, self.n_comp) + spectra.shape[-1:]
+        )
+
+        i_corr = 0
+        for k_off_diag in range(0, self.n_comp):
+            for el_off_diag in range(self.n_comp - k_off_diag):
+                i = el_off_diag
+                j = el_off_diag + k_off_diag
+                res[..., i, j, :] = spectra[i_corr]
+                res[..., j, i, :] = res[..., i, j, :]
+                i_corr += 1
+
+        assert i_corr == len(spectra)
+        return res
+
+
+class SZxCIB_Reichardt2012(PowerSpectraAndCorrelation):
+    """PowerSpectrum for SZxCIB (Dunkley et al. 2013)."""
+
+    def __init__(self, **kwargs):
+        """Initialize object with parameters."""
+        power_spectra = [
+            PowerSpectrumFromFile(_get_power_file("tsz_150_bat")),
+            PowerLaw(),
+            lambda xi: xi,
+        ]
+        super().__init__(*power_spectra)
+
+
+class SZxCIB_Addison2012(PowerSpectraAndCovariance):
+    """PowerSpectrum for SZxCIB (Dunkley et al. 2013)."""
+
+    def __init__(self, **kwargs):
+        """Intialize object with parameters."""
+        power_spectra = [
+            PowerSpectrumFromFile(_get_power_file("tsz_150_bat")),
+            PowerLaw(),
+            PowerSpectrumFromFile(_get_power_file("sz_x_cib")),
+        ]
+        super().__init__(*power_spectra, **kwargs)
+
+
+# CMB template cls?
