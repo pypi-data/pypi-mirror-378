6d9f9eb4275e0b9433bfe88c63e043241076abf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 07:04:19 +0000

    Add package wofrysrw/1.1.32/wofrysrw-1.1.32.tar.gz

---
+++ packages/wofrysrw/wofrysrw-1.1.32.tar.gz/wofrysrw-1.1.32/wofrysrw/srw/__init__.py
@@ -0,0 +1,12 @@
+try:
+    try:
+        from srwpy.srwlib import *   # OASYS 2.0 - SRW 4
+        from srwpy.uti_plot import *
+    except:
+        from srwlib import *     # SRW 3
+        from uti_plot import *
+except:
+    try:
+        from oasys_srw.srwlib import *   # OASYS 1.X
+        from oasys_srw.uti_plot import *
+    except: raise ImportError("SRW is not available")
