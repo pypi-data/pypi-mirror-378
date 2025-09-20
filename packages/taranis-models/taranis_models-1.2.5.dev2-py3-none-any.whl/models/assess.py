b08c96eeb328e2fefa40104d268d78ce085f92e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 03:12:15 +0000

    Add package taranis-models/1.2.5.dev2/taranis_models-1.2.5.dev2-py3-none-any.whl

---
+++ packages/taranis-models/taranis_models-1.2.5.dev2-py3-none-any.whl/models/assess.py
@@ -0,0 +1,67 @@
+from datetime import datetime
+from pydantic import field_validator, model_validator, field_serializer
+import langcodes
+
+from models.base import TaranisBaseModel
+
+
+class NewsItem(TaranisBaseModel):
+    _core_endpoint = "/assess/newsitems"
+
+    osint_source_id: str
+    hash: str = ""
+    author: str = ""
+    title: str = ""
+    review: str | None = None
+    content: str = ""
+    web_url: str = ""
+    source: str = ""
+    published_date: datetime | None = None
+    collected_date: datetime | None = None
+    attributes: list[str] | None = None
+    language: str | None = None
+
+    @field_validator("language", mode="before")
+    def normalize_language_code(cls, v):
+        if not v:
+            return None
+        try:
+            lang = langcodes.find(v)
+            return lang.language
+        except LookupError:
+            return None
+
+    @model_validator(mode="after")
+    def set_default_dates(self):
+        now = datetime.now()
+        if not self.collected_date:
+            self.collected_date = now
+        if not self.published_date:
+            self.published_date = self.collected_date
+        return self
+
+    @field_serializer("published_date", "collected_date", when_used="always")
+    def serialize_datetime(self, value: datetime | None) -> str | None:
+        return value.isoformat() if value else None
+
+
+class StoryTag(TaranisBaseModel):
+    name: str
+    size: int
+    type: str | None = None
+
+
+class Story(TaranisBaseModel):
+    _core_endpoint = "/assess/stories"
+    _model_name = "story"
+    _search_fields = ["title", "description"]
+    _pretty_name = "Story"
+
+    id: str | None = None
+    title: str | None = None
+    description: str | None = None
+    created: datetime | None = None
+    last_updated: datetime | None = None
+    news_items: list[NewsItem] = []
+    tags: list[dict] = []
+    attributes: list[dict] = []
