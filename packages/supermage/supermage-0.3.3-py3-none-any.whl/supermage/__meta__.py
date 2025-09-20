504ebc25b17856e50dc718b74c333f6f8eb82913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 05:04:51 +0000

    Add package supermage/0.3.3/supermage-0.3.3-py3-none-any.whl

---
+++ packages/supermage/supermage-0.3.3-py3-none-any.whl/supermage/__meta__.py
@@ -0,0 +1,12 @@
+# `name` is the name of the package as used for `pip install package`
+name = "supermage"
+# `path` is the name of the package for `import package`
+path = name.lower().replace("-", "_").replace(" ", "_")
+# Your version number should follow https://python.org/dev/peps/pep-0440 and
+# https://semver.org
+version = "0.3.3"
+author = "Michael James Yantovski Barth"
+author_email = "mjb299@pitt.edu"
+description = "A gas dynamics simulator for galaxies, and so much more."  # One-liner
+url = ""  # your project homepage
+license = "Unlicense"  # See https://choosealicense.com
