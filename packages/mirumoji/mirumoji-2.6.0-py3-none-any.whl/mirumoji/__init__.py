638f9736c2dff7a6b4986c8a0ee3fdb6506b4de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 05:05:35 +0000

    Add package mirumoji/2.6.0/mirumoji-2.6.0-py3-none-any.whl

---
+++ packages/mirumoji/mirumoji-2.6.0-py3-none-any.whl/mirumoji/__init__.py
@@ -0,0 +1,16 @@
+"""
+CLI and GUI launcher for the [Mirumoji](https://github.com/svdC1/mirumoji)
+project, an open-source, self-hostable Japanese language immersion tool.
+"""
+
+from . import (
+    gui,
+    launcher,
+    gui_launcher
+)
+
+__all__ = [
+    gui,
+    launcher,
+    gui_launcher
+]
