1690570a604b7c28ce997c2bf41907e781a6cb9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 06:05:02 +0000

    Add package sp-obs/0.4.0/sp_obs-0.4.0-py3-none-any.whl

---
+++ packages/sp-obs/sp_obs-0.4.0-py3-none-any.whl/sp_obs/_internal/core/recognised_integrations.py
@@ -0,0 +1,15 @@
+GEN_AI_INTEGRATION = {
+    "api.openai.com": "openai",
+    "api.anthropic.com": "anthropic",
+}
+
+TOOLS_INTEGRATION = {
+    "api.elevenlabs.io": "elevenlabs",
+    "serpapi.com": "serpapi",
+    "api.firecrawl.dev": "firecrawl",
+    "app.scrapingbee.com": "scrapingbee",
+    "api.deepgram.com": "deepgram",
+    "api.perplexity.ai": "perplexity",
+}
+
+INTEGRATIONS = GEN_AI_INTEGRATION | TOOLS_INTEGRATION
