c668e9870dd4658e082da97350fbf1b43bed43c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 04:05:45 +0000

    Add package oipd/1.0.2/oipd-1.0.2.tar.gz

---
+++ packages/oipd/oipd-1.0.2.tar.gz/oipd-1.0.2/oipd/core/__init__.py
@@ -0,0 +1,27 @@
+from oipd.core.pdf import (
+    calculate_pdf,
+    calculate_cdf,
+    calculate_quartiles,
+    # Export custom exceptions
+    OIPDError,
+    InvalidInputError,
+    CalculationError,
+)
+from oipd.core.parity import (
+    # Internal parity functions - not part of public API
+    preprocess_with_parity,
+    infer_forward_from_atm,
+    apply_put_call_parity,
+    detect_parity_opportunity,
+)
+
+__all__ = [
+    "calculate_pdf",
+    "calculate_cdf",
+    "calculate_quartiles",
+    "OIPDError",
+    "InvalidInputError",
+    "CalculationError",
+    # Parity functions are internal - not exported in public __all__
+    # but available for internal use by estimator.py
+]
