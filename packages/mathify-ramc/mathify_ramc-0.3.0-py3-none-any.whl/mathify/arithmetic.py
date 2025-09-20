f39d96f8cfe38d9ba88b400209f2b588135f5655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 14:05:24 +0000

    Add package mathify-ramc/0.3.0/mathify_ramc-0.3.0-py3-none-any.whl

---
+++ packages/mathify-ramc/mathify_ramc-0.3.0-py3-none-any.whl/mathify/arithmetic.py
@@ -0,0 +1,11 @@
+def add(a,b):
+    '''This is a arthimetic addition operation'''
+    return a+b
+
+def subtract(a,b):
+    """This is a arthimetic subtraction operation"""
+    return a-b
+
+def main():
+    # Your logic for algebra
+    print("This is the arithmetic entry point")
