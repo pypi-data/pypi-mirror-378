1ee800713380bef00355e0aeca16df8371eac89f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 05:04:17 +0000

    Add package edrn.auth/2.2.1/edrn_auth-2.2.1-py3-none-any.whl

---
+++ packages/edrn.auth/edrn_auth-2.2.1-py3-none-any.whl/edrn/auth/__init__.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+'''🔐 EDRN Auth.'''
+
+import importlib.metadata
+
+
+PACKAGE_NAME = __name__
+__version__ = VERSION = importlib.metadata.version(__name__)
---
+++ packages/edrn.auth/edrn_auth-2.2.1-py3-none-any.whl/edrn/auth/defaults.py
@@ -0,0 +1,3 @@
+# encoding: utf-8
+
+'''🔐 EDRN auth: defaults.'''
