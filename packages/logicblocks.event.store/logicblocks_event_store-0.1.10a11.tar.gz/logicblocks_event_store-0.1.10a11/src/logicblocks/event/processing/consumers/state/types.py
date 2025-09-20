586d07ba2b49077344c11b2e8b3da7eb09de13c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 04:04:44 +0000

    Add package logicblocks.event.store/0.1.10a11/logicblocks_event_store-0.1.10a11.tar.gz

---
+++ packages/logicblocks.event.store/logicblocks_event_store-0.1.10a11.tar.gz/logicblocks_event_store-0.1.10a11/src/logicblocks/event/processing/consumers/state/types.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+from logicblocks.event.sources import constraints
+from logicblocks.event.types import Event, JsonObject
+
+
+class EventConsumerStateConverter[E: Event](ABC):
+    @abstractmethod
+    def event_to_state(self, event: E) -> JsonObject:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def state_to_query_constraint(
+        self, state: JsonObject
+    ) -> constraints.QueryConstraint | None:
+        raise NotImplementedError()
