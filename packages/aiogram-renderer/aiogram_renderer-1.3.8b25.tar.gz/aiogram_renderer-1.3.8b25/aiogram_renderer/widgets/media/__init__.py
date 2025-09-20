c444880b12a471a572ac0e6052464f562ba247f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 05:04:07 +0000

    Add package aiogram-renderer/1.3.8b25/aiogram_renderer-1.3.8b25.tar.gz

---
+++ packages/aiogram-renderer/aiogram_renderer-1.3.8b25.tar.gz/aiogram_renderer-1.3.8b25/aiogram_renderer/widgets/media/__init__.py
@@ -0,0 +1,14 @@
+__all__ = [
+    "FileBytes",
+    "VideoBytes",
+    "PhotoBytes",
+    "AudioBytes",
+    "File",
+    "Video",
+    "Photo",
+    "Audio",
+]
+
+from .bytes import FileBytes, VideoBytes, PhotoBytes, AudioBytes
+from .path import File, Video, Photo, Audio
+
