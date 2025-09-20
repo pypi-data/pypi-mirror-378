0fb577254d8a4e93d07fa1c3ff2cba00725155a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 03:12:28 +0000

    Add package odoo-addon-otrs-somconnexio/16.0.1.1.1/odoo_addon_otrs_somconnexio-16.0.1.1.1.tar.gz

---
+++ packages/odoo-addon-otrs-somconnexio/odoo_addon_otrs_somconnexio-16.0.1.1.1.tar.gz/odoo_addon_otrs_somconnexio-16.0.1.1.1/odoo/addons/otrs_somconnexio/otrs_factories/sb_header_data_from_crm_lead.py
@@ -0,0 +1,19 @@
+from otrs_somconnexio.otrs_models.switchboard_header_data import SwitchboardHeaderData
+from .base_data import BaseDataFromOdoo
+
+
+class SBHeaderDataFromCRMLead(BaseDataFromOdoo):
+    DataModel = SwitchboardHeaderData
+
+    def __init__(self, crm_lead):
+        self.crm_lead = crm_lead
+
+    def _get_data(self):
+        return {
+            "contact_phone": self.crm_lead.phone,
+            "notes": self.crm_lead.description,
+            "email": self.crm_lead.email_from,
+            "sales_team": self.crm_lead.team_id.code,
+            "order_id": self.crm_lead.id,
+            "technology": "Switchboard",
+        }
