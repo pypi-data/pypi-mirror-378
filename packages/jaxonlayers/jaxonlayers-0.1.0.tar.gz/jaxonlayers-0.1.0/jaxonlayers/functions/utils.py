dd5a6a65d5357d6bd526d90ccffc9b8853e6f732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 16:05:05 +0000

    Add package jaxonlayers/0.1.0/jaxonlayers-0.1.0.tar.gz

---
+++ packages/jaxonlayers/jaxonlayers-0.1.0.tar.gz/jaxonlayers-0.1.0/jaxonlayers/functions/utils.py
@@ -0,0 +1,9 @@
+import jax
+import jax.numpy as jnp
+
+
+def default_floating_dtype():
+    if jax.config.jax_enable_x64:  # pyright: ignore
+        return jnp.float64
+    else:
+        return jnp.float32
