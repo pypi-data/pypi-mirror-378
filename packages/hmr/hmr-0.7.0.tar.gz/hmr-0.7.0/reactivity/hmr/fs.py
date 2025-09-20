b17b18118c738021747dc02173eb20b365091bc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 07:04:39 +0000

    Add package hmr/0.7.0/hmr-0.7.0.tar.gz

---
+++ packages/hmr/hmr-0.7.0.tar.gz/hmr-0.7.0/reactivity/hmr/fs.py
@@ -0,0 +1,36 @@
+import sys
+from collections import defaultdict
+from functools import cache
+from pathlib import Path
+
+from ..primitives import Subscribable
+from ._common import HMR_CONTEXT
+
+
+@defaultdict
+def fs_signals():
+    return Subscribable(context=HMR_CONTEXT)
+
+
+@cache
+def setup_fs_audithook():
+    current_computations = HMR_CONTEXT.current_computations
+
+    @sys.addaudithook
+    def _(event: str, args: tuple):
+        if event == "open":
+            file, _, flags = args
+
+            if (flags % 2 == 0) and current_computations and isinstance(file, str):
+                track(file)
+
+
+def track(file: str | Path):
+    fs_signals[Path(file).resolve()].track()
+
+
+def notify(file: Path):
+    fs_signals[file].notify()
+
+
+__all__ = "notify", "setup_fs_audithook", "track"
