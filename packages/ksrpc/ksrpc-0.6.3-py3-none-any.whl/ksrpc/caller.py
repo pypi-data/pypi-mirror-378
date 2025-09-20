ee3bf967d04de59e1374ad94dbf30bdf30ccec7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 15:05:11 +0000

    Add package ksrpc/0.6.3/ksrpc-0.6.3-py3-none-any.whl

---
+++ packages/ksrpc/ksrpc-0.6.3-py3-none-any.whl/ksrpc/caller.py
@@ -0,0 +1,89 @@
+import hashlib
+import inspect
+import sys
+from datetime import datetime
+from importlib import import_module
+
+from loguru import logger
+
+from ksrpc.serializer.pkl_gzip import serialize
+
+logger.remove()
+logger.add(sys.stderr,
+           format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level:<8}</level> | PID:{process.id} | TID:{thread.id} | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+           level="INFO", colorize=True)
+
+
+def make_key(module, name, args, kwargs):
+    """生成缓存key"""
+    key = f'{module}::{name}_{repr(args)}_{repr(kwargs)}'
+    return hashlib.md5(key.encode('utf-8')).hexdigest()
+
+
+def get_func(module, name):
+    # ksrpc.server.demo.CLASS.B.__getitem__(3)，这种格式只能确信第一个是模块，最后一个是方法，其他都不确定
+    m = import_module(module)
+    for n in name.split('.'):
+        if len(n) == 0:
+            continue
+        if hasattr(m, n):
+            m = getattr(m, n)
+        else:
+            m = import_module(f"{m.__name__}.{n}")
+    return m
+
+
+async def async_call(module, name, args, kwargs):
+    """简版API调用。没有各种额外功能"""
+    key = make_key(module, name, args, kwargs)
+
+    # 返回的数据包
+    d = dict(status=200,  # status.HTTP_200_OK,
+             datetime=datetime.now().isoformat(),  # 加查询时间，缓存中也许可以判断是否过期
+             module=module,
+             name=name,
+             args=args,
+             kwargs=kwargs)
+    try:
+        # 转成字符串，后面可能于做cache的key
+        logger.info(f'{module}::{name}\t{args}\t{kwargs}'[:200])
+
+        func = get_func(module, name)
+
+        # 可以调用的属性
+        if callable(func):
+            # 例如os.remove
+            type_ = 'function'
+            if inspect.iscoroutinefunction(func):
+                output = await func(*args, **kwargs)
+            else:
+                output = func(*args, **kwargs)
+        else:
+            output = func
+
+        # 结果的类型名
+        class_name = output.__class__.__name__
+
+        # 不可调用的直接返回
+        if class_name == 'module':
+            # 例如os.path，但用法为os.path()
+            data = repr(output)
+        elif class_name in ('dict_keys', 'dict_values'):
+            # 无法序列化，只能强行转换
+            # 例如：sys.modules.keys()
+            data = list(output)
+        else:
+            # 例如math.pi，但得加()才能触发，math.pi()
+            data = output
+
+        d['type'] = type(output).__name__
+        d['data'] = data
+    except Exception as e:
+        d['status'] = 500  # status.HTTP_500_INTERNAL_SERVER_ERROR
+        d['type'] = type(e).__name__
+        d['data'] = repr(e)
+
+    # 由于错误信息也想缓存，所以这里进行编码
+    buf = serialize(d).read()
+
+    return key, buf, d
---
+++ packages/ksrpc/ksrpc-0.6.3-py3-none-any.whl/ksrpc/client.py
@@ -0,0 +1,106 @@
+# 客户端，用于处理请求
+# 需要选择到底是http还是ws，还是nng一类的
+import asyncio
+import threading
+
+from loguru import logger
+
+from ksrpc.connections import BaseConnection
+
+
+class RpcClient:
+    """根据调用生成网络请求
+
+    Warnings
+    --------
+    不能在`asyncio.gather`中使用
+    """
+
+    def __init__(self,
+                 module: str,
+                 connection: BaseConnection,
+                 ):
+        """初始化
+
+        Parameters
+        ----------
+        module: str
+            顶层模块名
+        connection: Connection
+            连接对象
+
+        """
+        self._module = module
+        self._connection = connection
+        self._names = []
+        self._lock = threading.Lock()
+
+    def __getattr__(self, name):
+        with self._lock:
+            # 同一对象在asyncio.gather中使用会混乱，请用独立对象
+            self._names.append(name)
+            return self
+
+    # def __len__(self):
+    #     # 不知怎么回事，被主动调用了
+    #     return 0
+
+    def __del__(self):
+        self._connection = None
+
+    async def __call__(self, *args, **kwargs):
+        with self._lock:
+            name = '.'.join(self._names)
+            # 用完后得重置，否则第二次用时不正确了
+            self._names = []
+
+        # 排序，参数顺序统一后，排序生成key便不会浪费了
+        kwargs = dict(sorted(kwargs.items()))
+        try:
+            return await self._connection.call(self._module, name, args, kwargs)
+        except asyncio.TimeoutError:
+            await self._connection.reset()  # 重置
+            logger.warning(f'{self._module}::{name} timeout')
+            raise
+        except Exception as e:
+            await self._connection.reset()  # 重置
+            logger.warning(f'{self._module}::{name} error, {e}')
+            raise
+
+
+class RpcProxy:
+    """每次调用都生成一个RpcClient对象，占用资源略多于RpcClient
+
+    Notes
+    --------
+    可以在`asyncio.gather`中并行使用
+
+    """
+
+    def __init__(self,
+                 top_module: str,
+                 connection: BaseConnection,
+                 ):
+        """初始化
+
+        Parameters
+        ----------
+        top_module: str
+            顶层模块名
+        connection: Connection
+            连接对象
+        """
+        self._top_module = top_module
+        self._connection = connection
+
+    def __getattr__(self, name):
+        # 第一个方法调用用来生成新RpcClient对象，用来解决不能并发的问题
+        return RpcClient(self._top_module, self._connection).__getattr__(name)
+
+    async def __call__(self, *args, **kwargs):
+        return await RpcClient(self._top_module, self._connection)()
+    def __del__(self):
+        self._connection = None
+
+
+
