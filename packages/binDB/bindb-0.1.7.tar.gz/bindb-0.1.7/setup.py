caf61fab4977333b79b3aa15a196ecadcdc1f6dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 16:05:21 +0000

    Add package binDB/0.1.7/bindb-0.1.7.tar.gz

---
+++ packages/binDB/bindb-0.1.7.tar.gz/bindb-0.1.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='binDB',
+    version='0.1.7',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'pycountry',
+        'pycountry-convert',
+        'fuzzywuzzy',
+    ],
+    author='islamraisul796',
+    author_email='islamraisul796@gmail.com', # Using a placeholder email based on username
+    description='A Python library for retrieving BIN (Bank Identification Number) information.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
