6b3efaaec9b30dcaa33841778fd95fa8627e8e12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 03:12:46 +0000

    Add package osbot-utils/3.6.0/osbot_utils-3.6.0.tar.gz

---
+++ packages/osbot-utils/osbot_utils-3.6.0.tar.gz/osbot_utils-3.6.0/osbot_utils/helpers/flows/schemas/Schema__Flow__Log.py
@@ -0,0 +1,10 @@
+from typing                             import Optional
+from osbot_utils.type_safe.primitives.domains.identifiers.Timestamp_Now  import Timestamp_Now
+from osbot_utils.type_safe.Type_Safe    import Type_Safe
+
+
+class Schema__Flow__Log(Type_Safe):                         # Represents a log entry from the flow execution
+    timestamp   : Timestamp_Now
+    level       : int
+    message     : str
+    task_id     : Optional[str]
