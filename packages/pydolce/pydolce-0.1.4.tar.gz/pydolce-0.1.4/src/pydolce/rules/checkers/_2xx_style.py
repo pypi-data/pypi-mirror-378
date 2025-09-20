90fdf8e5a86bbffe0ca5af479ca795f18b406525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-20 03:12:41 +0000

    Add package pydolce/0.1.4/pydolce-0.1.4.tar.gz

---
+++ packages/pydolce/pydolce-0.1.4.tar.gz/pydolce-0.1.4/src/pydolce/rules/checkers/_2xx_style.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+from pydolce.parser import CodeSegment
+from pydolce.rules.rules import Rule, RuleContext, RuleResult
+
+_INDEX = int(Path(__file__).stem[1]) * 100
+
+
+def _id(n: int) -> int:
+    return _INDEX + n
+
+
+@Rule.register(_id(1), "Docstring has invalid style.")
+def invalid_docstring_style(
+    segment: CodeSegment, ctx: RuleContext
+) -> RuleResult | None:
+    if not segment.doc.strip() or segment.parsed_doc is None:
+        return None
+
+    if ctx.config.ensure_style is not None:
+        used_style = segment.parsed_doc.style
+        if used_style is None:
+            return RuleResult.bad(
+                [
+                    f"Docstring style could not be determined, but should be '{ctx.config.ensure_style}'."
+                ]
+            )
+        used_style_name = used_style.name.lower()
+        if used_style_name != ctx.config.ensure_style:
+            return RuleResult.bad(
+                [
+                    f"Docstring style is '{used_style_name}', "
+                    f"but should be '{ctx.config.ensure_style}'."
+                ]
+            )
+
+    return RuleResult.good()
---
+++ packages/pydolce/pydolce-0.1.4.tar.gz/pydolce-0.1.4/tests/test_all_rules_registered.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+import pydolce.rules.checkers
+
+
+def test_al_rules_registered() -> None:
+    registered_rules = set(pydolce.rules.rules.Rule.all_rules.keys())
+
+    checkers_path = Path(pydolce.rules.checkers.__path__[0])
+
+    # Get all rule codes from the checkers modules
+    expected_rules = set()
+    for checker_file in checkers_path.glob("*.py"):
+        if checker_file.name == "__init__.py":
+            continue
+        module = checker_file.stem
+        # count all functions in module that has Rule.register or Rule.llm_register decorator
+        checker_module = __import__(f"pydolce.rules.checkers.{module}", fromlist=[""])
+        for attr_name in dir(checker_module):
+            attr = getattr(checker_module, attr_name)
+            if callable(attr) and hasattr(attr, "rule_ref"):
+                expected_rules.add(attr.rule_ref)
+
+    assert len(expected_rules) > 0, "No rules found in checkers."
+
+    outside_checkers = registered_rules - expected_rules
+    assert len(outside_checkers) == 0, (
+        f"Rules {outside_checkers} seem to be registered outside checkers submodule."
+    )
+
+    not_registered = expected_rules - set(registered_rules)
+    assert len(not_registered) == 0, f"Rules {not_registered} are not registered."
