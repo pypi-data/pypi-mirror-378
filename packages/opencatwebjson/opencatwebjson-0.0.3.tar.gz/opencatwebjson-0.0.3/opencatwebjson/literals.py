7c1da4c0603d4652729948e3f5ee3f3eaa58e417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 05:06:06 +0000

    Add package opencatwebjson/0.0.3/opencatwebjson-0.0.3.tar.gz

---
+++ packages/opencatwebjson/opencatwebjson-0.0.3.tar.gz/opencatwebjson-0.0.3/opencatwebjson/literals.py
@@ -0,0 +1,29 @@
+from typing import Literal, Union, NamedTuple
+from .classes import Size2
+
+Number = Union[int, float]
+
+# Tuples
+class Vec2(NamedTuple):
+    x: float
+    y: float
+
+Tuple2 = tuple[Number, Number]
+
+# Literals
+FontStyle = Literal["normal", "italic"]
+FontWeight = Literal["thin", "extralight", "light", "regular", "medium", "semibold", "bold", "extrabold", "heavy"]
+HorizontalAlignment = Literal["center", "left", "right"]
+VerticalAlignment = Literal["bottom", "center", "top"]
+Truncate = Literal["atend", "none", "splitword"]
+ScaleType = Literal["crop", "fit", "slice", "stretch", "tile"]
+ResampleMode = Literal["default", "pixelated"]
+ProductType = Literal["asset", "gamepass", "product"]
+OutlineMode = Literal["border", "contextual"] # Contextual only works when text has rich markup disabled
+OutlineType = Literal["bevel", "miter", "round"]
+ListDirection = Literal["horizontal", "vertical"]
+
+# Unions
+TextSize = Union[int, float, Literal["scaled"]]
+CanvasSize = Union[Size2, Literal["auto", "auto_x", "auto_y"]]
+MaxSize = Union[Tuple2, Literal["inf"]]
