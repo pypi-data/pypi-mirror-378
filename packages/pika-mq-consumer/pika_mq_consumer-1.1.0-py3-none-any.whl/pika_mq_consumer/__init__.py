2a14f6d8b399c9d40b80d3d4ba7d3513914adbce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 15:05:26 +0000

    Add package pika-mq-consumer/1.1.0/pika_mq_consumer-1.1.0-py3-none-any.whl

---
+++ packages/pika-mq-consumer/pika_mq_consumer-1.1.0-py3-none-any.whl/pika_mq_consumer/__init__.py
@@ -0,0 +1,38 @@
+"""
+Pika MQ Consumer - 简化的RabbitMQ消费者包装器
+
+这个包提供了一个简洁的接口来消费RabbitMQ消息，基于pika库构建。
+让您可以像使用npm包一样轻松集成MQ消费功能。
+现在还包含统一的文件上传服务和HTTP客户端。
+
+作者: pika-mq-consumer
+版本: 1.1.0
+许可: MIT
+"""
+
+from .consumer import MQConsumer
+from .exceptions import MQConsumerError, ConnectionError, ConsumerError
+from .uploader import FileUploader, upload_image, upload_video, upload_file
+from .http_client import http_post
+
+__version__ = "1.1.0"
+__author__ = "pika-mq-consumer"
+__email__ = "iyahe29@gmail.com"
+
+# 导出主要类和异常
+__all__ = [
+    # MQ消费者
+    'MQConsumer',
+    'MQConsumerError',
+    'ConnectionError',
+    'ConsumerError',
+
+    # 文件上传
+    'FileUploader',
+    'upload_image',
+    'upload_video',
+    'upload_file',
+
+    # HTTP客户端
+    'http_post'
+]
