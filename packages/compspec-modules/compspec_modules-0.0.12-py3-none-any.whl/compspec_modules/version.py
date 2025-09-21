5841e144280a42b12b00c649132002cff3956e92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 14:05:07 +0000

    Add package compspec-modules/0.0.12/compspec_modules-0.0.12-py3-none-any.whl

---
+++ packages/compspec-modules/compspec_modules-0.0.12-py3-none-any.whl/compspec_modules/version.py
@@ -0,0 +1,20 @@
+__author__ = "Vanessa Sochat"
+__copyright__ = "Copyright 2024, Vanessa Sochat"
+__license__ = "MIT"
+
+__version__ = "0.0.12"
+AUTHOR = "Vanessa Sochat"
+AUTHOR_EMAIL = "vsoch@users.noreply.github.com"
+NAME = "compspec-modules"
+PACKAGE_URL = "https://github.com/compspec/compspec-modules"
+KEYWORDS = "compatibility, compspec, modules, subsystem, scheduling, hpc"
+DESCRIPTION = "Subsystem graph generation for environment modules"
+LICENSE = "LICENSE"
+
+################################################################################
+# Global requirements
+
+INSTALL_REQUIRES = (("compspec", {"min_version": "0.1.0"}),)
+
+TESTS_REQUIRES = (("pytest", {"min_version": "4.6.2"}),)
+INSTALL_REQUIRES_ALL = INSTALL_REQUIRES + TESTS_REQUIRES
