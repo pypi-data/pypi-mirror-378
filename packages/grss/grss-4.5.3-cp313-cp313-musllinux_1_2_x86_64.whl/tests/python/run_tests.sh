4468f0c15dcd3d3b314eebd7e4902c5b573d59f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 05:05:17 +0000

    Add package grss/4.5.3/grss-4.5.3-cp313-cp313-musllinux_1_2_x86_64.whl

---
+++ packages/grss/grss-4.5.3-cp313-cp313-musllinux_1_2_x86_64.whl/tests/python/run_tests.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# raise error on failure
+set -e
+
+# raise error if no arguments are provided
+if [ $# -eq 0 ]; then
+    echo "No arguments provided. Please provide the name of the directory containing the test files."
+    exit 1
+fi
+
+# first argument is the path to the directory containing the test files
+cd $1
+
+# for each file in the directory, run the test
+jupyter nbconvert --to script *.ipynb
+for file in *.py
+do
+    echo "Running $file..."
+    python $file
+    echo "$file completed."
+    rm $file
+done
+
+# return to the original directory
+cd ..
