2ddeccb07d44a32f224643ded0a191480f6532d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 03:20:53 +0000

    Add package pyscratch-lobotomy2012/0.1.5/pyscratch_lobotomy2012-0.1.5-py3-none-any.whl

---
+++ packages/pyscratch-lobotomy2012/pyscratch_lobotomy2012-0.1.5-py3-none-any.whl/pyscratch/pyscratch.py
@@ -0,0 +1,12 @@
+from .run import run
+from .Sprite.sprite import Sprite
+from .Sprite.code import Code
+
+def init():
+    import pygame
+
+    pygame.init()
+
+    print("\033[96m==== Welcome to PyScratch ====")
+    print("\033[94mPyScratch 0.1.5]")
+    print("\033[92mhttps://github.com/Lobotomy2012/PyScratch/tree/main\033[0m")
