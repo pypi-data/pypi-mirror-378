ea760d8532df7352486f7bf10d666381e1e25e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 14:04:53 +0000

    Add package mineru-vl-utils/0.1.9/mineru_vl_utils-0.1.9.tar.gz

---
+++ packages/mineru-vl-utils/mineru_vl_utils-0.1.9.tar.gz/mineru_vl_utils-0.1.9/mineru_vl_utils/vlm_client/__init__.py
@@ -0,0 +1,15 @@
+from .base_client import (
+    DEFAULT_SYSTEM_PROMPT,
+    DEFAULT_USER_PROMPT,
+    SamplingParams,
+    VlmClient,
+    new_vlm_client,
+)
+
+__all__ = [
+    "DEFAULT_SYSTEM_PROMPT",
+    "DEFAULT_USER_PROMPT",
+    "SamplingParams",
+    "VlmClient",
+    "new_vlm_client",
+]
