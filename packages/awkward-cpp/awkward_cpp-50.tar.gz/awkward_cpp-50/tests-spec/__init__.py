12b3891df526af61b282f5910b261ea7e815390a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 04:05:26 +0000

    Add package awkward-cpp/50/awkward_cpp-50.tar.gz

---
+++ packages/awkward-cpp/awkward_cpp-50.tar.gz/awkward_cpp-50/tests-spec/__init__.py
@@ -0,0 +1,10 @@
+# AUTO GENERATED ON 2025-09-20 AT 15:57:04
+# DO NOT EDIT BY HAND!
+#
+# To regenerate file, run
+#
+#     python dev/generate-tests.py
+#
+
+# fmt: off
+
---
+++ packages/awkward-cpp/awkward_cpp-50.tar.gz/awkward_cpp-50/tests-spec/test_pyawkward_NumpyArray_subrange_equal_float64.py
@@ -0,0 +1,17 @@
+# AUTO GENERATED ON 2025-09-20 AT 15:57:04
+# DO NOT EDIT BY HAND!
+#
+# To regenerate file, run
+#
+#     python dev/generate-tests.py
+#
+
+# fmt: off
+
+import pytest
+import numpy as np
+import kernels
+
+@pytest.mark.skip(reason='Unable to generate any tests for kernel')
+def test_pyawkward_NumpyArray_subrange_equal_float64_1():
+    raise NotImplementedError('Unable to generate any tests for kernel')
