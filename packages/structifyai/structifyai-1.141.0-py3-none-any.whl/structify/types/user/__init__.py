ee713d1802dd980dbabecd11d045ff6615373b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 06:04:37 +0000

    Add package structifyai/1.141.0/structifyai-1.141.0-py3-none-any.whl

---
+++ packages/structifyai/structifyai-1.141.0-py3-none-any.whl/structify/types/user/__init__.py
@@ -0,0 +1,8 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .subscription_plan import SubscriptionPlan as SubscriptionPlan
+from .create_session_response import CreateSessionResponse as CreateSessionResponse
+from .stripe_create_session_params import StripeCreateSessionParams as StripeCreateSessionParams
+from .stripe_create_subscription_params import StripeCreateSubscriptionParams as StripeCreateSubscriptionParams
