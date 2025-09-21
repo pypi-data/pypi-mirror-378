18e44a6f2c3a6b20c007678a4172fd5c6186c6d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 05:05:21 +0000

    Add package html-compose/0.9.1/html_compose-0.9.1-py3-none-any.whl

---
+++ packages/html-compose/html_compose-0.9.1-py3-none-any.whl/html_compose/attributes/slot_attrs.py
@@ -0,0 +1,20 @@
+from . import BaseAttribute
+
+
+class SlotAttrs:
+    """
+    This module contains functions for attributes in the 'slot' element.
+    Which is inherited by a class so we can generate type hints
+    """
+
+    @staticmethod
+    def name(value: str) -> BaseAttribute:
+        """
+        "slot" attribute: name  
+        Name of shadow tree slot  
+
+        :param value: Text  
+        :return: An name attribute to be added to your element
+        """  # fmt: skip
+
+        return BaseAttribute("name", value)
