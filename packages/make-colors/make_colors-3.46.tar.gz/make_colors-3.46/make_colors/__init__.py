2588ba448fed1f34c66efd27495bd09cd3099e9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 15:06:19 +0000

    Add package make-colors/3.46/make_colors-3.46.tar.gz

---
+++ packages/make-colors/make_colors-3.46.tar.gz/make_colors-3.46/make_colors/__init__.py
@@ -0,0 +1,6 @@
+from .make_colors import make_colors, MakeColors, make_color
+
+from . import __version__ as version
+__version__ 	= version.version
+__email__		= "cumulus13@gmail.com"
+__author__		= "Hadi Cahyadi"
