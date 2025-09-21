27f9fcae85a077790546761fead8d94363b73e02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 05:05:15 +0000

    Add package pytelecomm/1.1.11/pytelecomm-1.1.11.tar.gz

---
+++ packages/pytelecomm/pytelecomm-1.1.11.tar.gz/pytelecomm-1.1.11/src/pytelecomm/__init__.py
@@ -0,0 +1,10 @@
+import sys
+from pathlib import Path
+from jupyterlab.labapp import main
+# from . import run_notebook
+
+def run_notebook():
+    main([Path(sys.prefix, "share/notebooks", "pytelecomm.ipynb").resolve()])
+
+if __name__ == "__main__":
+	 run_notebook()
