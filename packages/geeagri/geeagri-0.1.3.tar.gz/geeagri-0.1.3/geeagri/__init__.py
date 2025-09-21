c373e3ceb036867c9376c7ced7f2e4d6961369aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 06:04:37 +0000

    Add package geeagri/0.1.3/geeagri-0.1.3.tar.gz

---
+++ packages/geeagri/geeagri-0.1.3.tar.gz/geeagri-0.1.3/geeagri/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for geeagri."""
+
+__author__ = """Krishnagopal Halder"""
+__email__ = "geonextgis@gmail.com"
+__version__ = "0.1.3"
+
+from . import analysis
+from . import extract
+from . import preprocessing
