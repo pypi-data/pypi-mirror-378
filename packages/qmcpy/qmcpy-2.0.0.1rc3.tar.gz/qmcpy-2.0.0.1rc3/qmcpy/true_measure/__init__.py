39f62eb78ff685c955707d5b01630586b6be5d42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 05:05:21 +0000

    Add package qmcpy/2.0.0.1rc3/qmcpy-2.0.0.1rc3.tar.gz

---
+++ packages/qmcpy/qmcpy-2.0.0.1rc3.tar.gz/qmcpy-2.0.0.1rc3/qmcpy/true_measure/__init__.py
@@ -0,0 +1,16 @@
+from .abstract_true_measure import AbstractTrueMeasure
+from .brownian_motion import BrownianMotion
+from .geometric_brownian_motion import GeometricBrownianMotion
+from .gaussian import Gaussian
+from .lebesgue import Lebesgue
+from .uniform import Uniform
+from .kumaraswamy import Kumaraswamy
+from .bernoulli_cont import BernoulliCont
+from .johnsons_su import JohnsonsSU
+from .scipy_wrapper import SciPyWrapper
+from .matern_gp import MaternGP
+
+TrueMeasure = AbstractTrueMeasure
+_TrueMeasure = AbstractTrueMeasure
+Normal = Gaussian
+Matern = MaternGP
