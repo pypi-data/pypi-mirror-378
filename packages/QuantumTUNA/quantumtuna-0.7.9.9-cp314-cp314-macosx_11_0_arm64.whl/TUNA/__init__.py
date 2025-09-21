2d5a31c055c18983be40115b609b5fbabde154ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 14:04:40 +0000

    Add package QuantumTUNA/0.7.9.9/quantumtuna-0.7.9.9-cp314-cp314-macosx_11_0_arm64.whl

---
+++ packages/QuantumTUNA/quantumtuna-0.7.9.9-cp314-cp314-macosx_11_0_arm64.whl/TUNA/__init__.py
@@ -0,0 +1,12 @@
+"""
+
+TUNA
+
+~~~~ Theoretical Unification of Nuclear Arrangements ~~~~
+
+A user-friendly quantum chemistry program for diatomics.
+
+"""
+
+__version__ = "0.7.9.9"
+__author__ = "Harry Brough"
