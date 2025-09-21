622f76ca4fd549f8d44e20428cd312cf83e06b44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 14:04:59 +0000

    Add package qmcpy/2.0.0.1rc4/qmcpy-2.0.0.1rc4.tar.gz

---
+++ packages/qmcpy/qmcpy-2.0.0.1rc4.tar.gz/qmcpy-2.0.0.1rc4/qmcpy/discrete_distribution/__init__.py
@@ -0,0 +1,12 @@
+from .abstract_discrete_distribution import AbstractDiscreteDistribution
+from .iid_std_uniform import IIDStdUniform
+from .lattice import Lattice
+from .digital_net_b2 import DigitalNetB2
+from .halton import Halton
+
+DiscreteDistribution = AbstractDiscreteDistribution
+_DiscreteDistribution = AbstractDiscreteDistribution
+Sobol = DigitalNetB2
+DigitalNet = DigitalNetB2
+Net = DigitalNetB2
+
