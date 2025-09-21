f518e65d6a13803c44fa38fd2dbeead471093e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 14:04:51 +0000

    Add package ephem-cli/2.0.3/ephem_cli-2.0.3-py3-none-any.whl

---
+++ packages/ephem-cli/ephem_cli-2.0.3-py3-none-any.whl/ephem/commands/cal.py
@@ -0,0 +1,11 @@
+from ephem.display.month import format_calendar
+
+
+def run(args):
+    """Main entry point for calendar command."""
+
+    output = format_calendar(args)
+
+    if output:  # Plain text mode
+        for line in output:
+            print(line)
