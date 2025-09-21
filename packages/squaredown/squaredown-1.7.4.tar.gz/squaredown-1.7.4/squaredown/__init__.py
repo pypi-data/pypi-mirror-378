653f9edac524cf42320e72d79220e29ac7080f6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 03:20:44 +0000

    Add package squaredown/1.7.4/squaredown-1.7.4.tar.gz

---
+++ packages/squaredown/squaredown-1.7.4.tar.gz/squaredown-1.7.4/squaredown/__init__.py
@@ -0,0 +1,10 @@
+"""A set of functions to retrieve and save Square data into MongoDB.
+"""
+from squaredown.catalog import Catalog
+from squaredown.connector import Connector
+from squaredown.locations import Locations
+from squaredown.orders import Orders
+from squaredown.report_data import ReportData
+
+
+__version__ = "1.7.4"
