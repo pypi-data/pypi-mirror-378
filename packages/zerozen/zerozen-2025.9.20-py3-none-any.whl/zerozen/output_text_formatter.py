53a6a20b1d5b8377428622b60fc7ce3e29f0ca1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 04:05:35 +0000

    Add package zerozen/2025.9.20/zerozen-2025.9.20-py3-none-any.whl

---
+++ packages/zerozen/zerozen-2025.9.20-py3-none-any.whl/zerozen/output_text_formatter.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+from xml.etree.ElementTree import Element, tostring, SubElement
+from typing import Any
+
+
+def pydantic_to_xml(obj: BaseModel) -> str:
+    def value_to_xml(parent: Element, key: str, value: Any):
+        if isinstance(value, BaseModel):
+            child = SubElement(parent, key)
+            model_to_xml(child, value)
+        elif isinstance(value, dict):
+            child = SubElement(parent, key)
+            for k, v in value.items():
+                value_to_xml(child, k, v)
+        elif isinstance(value, list):
+            for item in value:
+                value_to_xml(parent, key, item)
+        else:
+            child = SubElement(parent, key)
+            child.text = str(value)
+
+    def model_to_xml(parent: Element, model: BaseModel):
+        model_dict = model.model_dump()
+        for key, value in model_dict.items():
+            value_to_xml(parent, key, value)
+
+    root = Element(obj.__class__.__name__)
+    model_to_xml(root, obj)
+    return tostring(root, "utf-8").decode()
