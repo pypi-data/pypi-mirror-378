561af0cc444e82ea41be0a4382fb7a16691f0023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 05:06:18 +0000

    Add package transcribe-with-whisper/0.1.1/transcribe_with_whisper-0.1.1.tar.gz

---
+++ packages/transcribe-with-whisper/transcribe_with_whisper-0.1.1.tar.gz/transcribe_with_whisper-0.1.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="transcribe-with-whisper",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        "pydub",
+        "webvtt-py",
+        "pyannote.audio",
+        "huggingface_hub",
+        "torch",
+        "faster-whisper",
+    ],
+    entry_points={
+        "console_scripts": [
+            "transcribe-with-whisper=transcribe_with_whisper.main:main",
+        ],
+    },
+    python_requires=">=3.8",
+    include_package_data=True,
+    description="Video transcription with speaker diarization and HTML output",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Jay Pfaffman",
+    url="https://github.com/literatecomputing/transcribe-with-whisper",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
