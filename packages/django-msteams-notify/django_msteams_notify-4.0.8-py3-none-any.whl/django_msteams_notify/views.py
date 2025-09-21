78c7cf7643e41cd6b0ab462d9b2a64070bf656dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 14:05:04 +0000

    Add package django-msteams-notify/4.0.8/django_msteams_notify-4.0.8-py3-none-any.whl

---
+++ packages/django-msteams-notify/django_msteams_notify-4.0.8-py3-none-any.whl/django_msteams_notify/views.py
@@ -0,0 +1,22 @@
+from rest_framework.generics import ListAPIView, CreateAPIView
+from django_msteams_notify.models import TeamsNotification
+from django_msteams_notify.serializers import TeamsNotificationSerializer
+from django_msteams_notify.tasks import send_teams_notification_task
+from django.db import transaction
+
+class TeamsNotificationListAPIView(ListAPIView):
+    queryset = TeamsNotification.objects.all().order_by('-sent_at')
+    serializer_class = TeamsNotificationSerializer
+
+class TeamsNotificationCreateAPIView(CreateAPIView):
+    serializer_class = TeamsNotificationSerializer
+
+    def perform_create(self, serializer):
+        # Save notification to DB
+        notification = serializer.save()
+        # Schedule Celery task **after the DB transaction is committed**
+        transaction.on_commit(
+            lambda: send_teams_notification_task.delay(str(notification.id))
+        )
+
+
