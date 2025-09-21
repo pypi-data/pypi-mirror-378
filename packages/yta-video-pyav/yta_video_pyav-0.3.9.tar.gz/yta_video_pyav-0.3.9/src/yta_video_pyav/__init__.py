210c33095840e77efa772420891f0d69b4c83ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 05:06:27 +0000

    Add package yta-video-pyav/0.3.9/yta_video_pyav-0.3.9.tar.gz

---
+++ packages/yta-video-pyav/yta_video_pyav-0.3.9.tar.gz/yta_video_pyav-0.3.9/src/yta_video_pyav/__init__.py
@@ -0,0 +1,16 @@
+"""
+Module to include video handling by 
+using pyav and also an awesome video
+editor built-in.
+
+Interesting information:
+| Abrev.  | Nombre completo            | Uso principal                          |
+| ------- | -------------------------- | -------------------------------------- |
+| VAO     | Vertex Array Object        | Esquema de datos de vértices           |
+| VBO     | Vertex Buffer Object       | Datos crudos de vértices en GPU        |
+| FBO     | Frame Buffer Object        | Renderizar fuera de pantalla           |
+| UBO     | Uniform Buffer Object      | Variables `uniform` compartidas        |
+| EBO/IBO | Element / Index Buffer Obj | Índices para reutilizar vértices       |
+| PBO     | Pixel Buffer Object        | Transferencia rápida de imágenes       |
+| RBO     | Render Buffer Object       | Almacén intermedio (profundidad, etc.) |
+"""
