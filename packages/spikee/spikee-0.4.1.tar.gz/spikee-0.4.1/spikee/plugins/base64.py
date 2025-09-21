7d6cb9533f55f13c7cb4df71699a73d60f41cac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 03:20:44 +0000

    Add package spikee/0.4.1/spikee-0.4.1.tar.gz

---
+++ packages/spikee/spikee-0.4.1.tar.gz/spikee-0.4.1/spikee/plugins/base64.py
@@ -0,0 +1,28 @@
+"""
+Base64 Encoding Plugin
+
+This plugin transforms the input text into Base64 encoding.
+
+Usage:
+    spikee generate --plugins base64
+
+Parameters:
+    text (str): The input text to be transformed.
+
+Returns:
+    str: The transformed text in Base64 encoding.
+"""
+
+import base64
+
+def transform(text: str, exclude_patterns: List[str] = None) -> str:
+    """
+    Transforms the input text into Base64 encoding.
+
+    Args:
+        text (str): The input text.
+
+    Returns:
+        str: The transformed text in Base64 encoding.
+    """
+    return base64.b64encode(text.encode()).decode()
