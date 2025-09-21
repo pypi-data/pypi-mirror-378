4d42cc860414f0bf70978c5e26a47fe39b624ba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 05:05:13 +0000

    Add package mkdocs-mcp/0.1.3/mkdocs_mcp-0.1.3-py3-none-any.whl

---
+++ packages/mkdocs-mcp/mkdocs_mcp-0.1.3-py3-none-any.whl/mkdocs_mcp/__main__.py
@@ -0,0 +1,83 @@
+import os
+import logging
+
+import click
+from mkdocs.commands import build
+from mkdocs.__main__ import (
+    common_config_options,
+    common_options,
+    cli,
+    clean_help,
+    site_dir_help,
+)
+from mkdocs import config
+
+from mcp.server.fastmcp import FastMCP
+from mcp.server.fastmcp.resources import FunctionResource
+from pydantic.networks import AnyUrl
+
+from mkdocs_mcp.plugin import PageInfo
+
+
+log = logging.getLogger(__name__)
+mcp = FastMCP()
+
+
+def add_resource(mcp: FastMCP, page: PageInfo) -> None:
+    # Register as regular resource
+    resource = FunctionResource(
+        uri=AnyUrl(f"docs://{page.path_md}"),
+        name=page.title,
+        description=f"The {page.title} documentation",
+        mime_type="text/markdown",
+        fn=lambda: page.content,
+    )
+    mcp.add_resource(resource=resource)
+
+
+@cli.command(name="build")
+@click.option(
+    "-mt",
+    "--mcp-transport",
+    type=click.Choice(["stdio", "sse"]),
+    help="MCP mode",
+    default="stdio",
+)
+@click.option(
+    "--mkdocs-project-path",
+    type=click.Path(exists=True),
+    help="Path to the MkDocs project",
+    envvar="MKDOCS_PROJECT_PATH",
+    default=None,
+)
+@click.option("-c", "--clean/--dirty", is_flag=True, default=True, help=clean_help)
+@common_config_options
+@click.option("-d", "--site-dir", type=click.Path(), help=site_dir_help)
+@common_options
+def build_and_mcp(mcp_transport, mkdocs_project_path, clean, **kwargs):
+    """Build the MkDocs documentation and start MCP Server."""
+
+    if mkdocs_project_path:
+        os.chdir(mkdocs_project_path)
+
+    cfg = config.load_config(**kwargs)
+    cfg.plugins.on_startup(command="build", dirty=not clean)
+    try:
+        build.build(cfg)
+    finally:
+        cfg.plugins.on_shutdown()
+
+    mcp_plugin = cfg.plugins.get("mcp")
+
+    mcp._mcp_server.name = cfg.site_name
+    log.info("Starting adding resources to MCP")
+    for name, page in mcp_plugin.md_pages.items():
+        add_resource(mcp, page)
+        log.debug(f"Adding resource {name} to MCP")
+
+    log.info("Starting MCP server")
+    mcp.run(mcp_transport)
+
+
+if __name__ == "__main__":
+    build_and_mcp()
---
+++ packages/mkdocs-mcp/mkdocs_mcp-0.1.3-py3-none-any.whl/mkdocs_mcp/plugin.py
@@ -0,0 +1,116 @@
+from typing import TYPE_CHECKING
+import logging
+from dataclasses import dataclass
+from pathlib import Path
+
+from html2text import html2text
+
+from mkdocs.plugins import BasePlugin
+from mkdocs.config import config_options
+from mkdocs.config.base import Config
+
+if TYPE_CHECKING:
+    from mkdocs.config.defaults import MkDocsConfig
+    from mkdocs.structure.pages import Page
+
+
+log = logging.getLogger(__name__)
+
+
+@dataclass
+class PageInfo:
+    """Class to store page information"""
+
+    title: str
+    path_md: Path
+    content: str
+
+
+class MkDocsMCPConfig(Config):
+    naming_style = config_options.Choice(
+        (
+            "src_file",
+            "dst_url",
+            "title",
+        ),
+        default="src_file",
+    )
+    prefer_markdown = config_options.Type(bool, default=True)
+    combine_all_pages = config_options.Type(bool, default=False)
+    combine_by_folder = config_options.Type(bool, default=False)
+
+
+class MkDocsMCP(BasePlugin[MkDocsMCPConfig]):
+    """The MkDocs plugin keeps track of pages,
+    allowing to use the mapping to serve resources with a MCP server.
+
+    This plugin defines the following event hooks:
+
+    - `on_post_page`
+
+
+    """
+
+    md_pages: dict[str, PageInfo] = {}
+    """Dictionary mapping section names to a list of page infos."""
+
+    def on_post_page(
+        self, output: str, /, *, page: "Page", config: "MkDocsConfig"
+    ) -> str | None:
+        """
+        The `post_page` event is called after the template is rendered, but
+        before it is written to disc and can be used to alter the output of the
+        page. If an empty string is returned, the page is skipped and nothing is
+        written to disc.
+
+        Args:
+            output: output of rendered template as string
+            page: `mkdocs.structure.pages.Page` instance
+            config: global configuration object
+
+        Returns:
+            output of rendered template as string
+        """
+        markdown = page.markdown
+        file_path = page.file.src_uri
+
+        match self.config.get("naming_style"):
+            case "src_file":
+                title = file_path
+            case "dst_url":
+                title = page.abs_url or ""
+            case "title":
+                title = page.title
+
+        if self.config.get("prefer_markdown") and markdown:
+            content = markdown
+        else:
+            content = html2text(output)
+
+        if self.config.get("combine_all_pages"):
+            aggregated_page = self.md_pages.get(
+                "all",
+                PageInfo(title="all (aggregated)", path_md=Path("."), content=""),
+            )
+            aggregated_page.content = aggregated_page.content + content + "\n"
+            self.md_pages["all"] = aggregated_page
+
+        if self.config.get("combine_by_folder"):
+            parent = Path(file_path).parent
+            while len(parent.parts):
+                aggregated_page = self.md_pages.get(
+                    str(parent),
+                    PageInfo(
+                        title=f"{parent} (aggregated)", path_md=parent, content=""
+                    ),
+                )
+                aggregated_page.content = aggregated_page.content + content + "\n"
+                self.md_pages[str(parent)] = aggregated_page
+                parent = parent.parent
+
+        self.md_pages[file_path] = PageInfo(
+            title=title,
+            path_md=Path(file_path),
+            content=content,
+        )
+        return output
