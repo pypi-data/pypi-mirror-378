906e6292d6ce64a956a355c0f2794d543e9f83c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 14:04:54 +0000

    Add package xsget/0.1.56/xsget-0.1.56.tar.gz

---
+++ packages/xsget/xsget-0.1.56.tar.gz/xsget-0.1.56/src/xsget/chapter.py
@@ -0,0 +1,39 @@
+# Copyright (C) 2021,2022,2023,2024,2025 Kian-Meng Ang
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# pylint: disable=consider-using-f-string
+
+"""Data model for an extracted Chapter."""
+
+from dataclasses import dataclass, field
+
+
+@dataclass(repr=False)
+class Chapter:
+    """A Chapter model class."""
+
+    title: str = field(default="")
+    url: str = field(default="")
+    content_path: str = field(default="")
+    filename: str = field(default="")
+
+    def __repr__(self) -> str:
+        return (
+            f"Chapter(url='{self.url}', title='{self.title}', "
+            f"filename='{self.filename}', content_path='{self.content_path}')"
+        )
+
+    def __str__(self) -> str:
+        return self.title
