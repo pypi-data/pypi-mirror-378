ac51842510b5f55ef675c126844120036450bd56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 14:04:38 +0000

    Add package hiinsta/0.1.1/hiinsta-0.1.1-py3-none-any.whl

---
+++ packages/hiinsta/hiinsta-0.1.1-py3-none-any.whl/hiinsta/__init__.py
@@ -0,0 +1,11 @@
+"""
+hiinsta - A Python package for hiinsta functionality.
+"""
+
+__version__ = "0.1.1"
+__author__ = "Tomas Santana"
+__email__ = "tomas@cervant.chat"
+
+from .InstagramMessenger import InstagramMessenger
+
+__all__ = ["InstagramMessenger"]
