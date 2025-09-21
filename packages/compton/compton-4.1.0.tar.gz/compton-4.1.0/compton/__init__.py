33afd77047f33ff9862d05da9dde63550f9a0775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 14:04:53 +0000

    Add package compton/4.1.0/compton-4.1.0.tar.gz

---
+++ packages/compton/compton-4.1.0.tar.gz/compton-4.1.0/compton/__init__.py
@@ -0,0 +1,15 @@
+from .orchestrator import Orchestrator
+from .provider import (
+    Provider,
+    Dispatcher
+)
+from .reducer import Reducer
+from .consumer import Consumer
+from .common import (
+    Vector,
+    Symbol,
+    Payload
+)
+
+
+__version__ = '4.1.0'
