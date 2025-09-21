9a2138dce38fc519352c57b4ea273707bb5ab4c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 14:05:00 +0000

    Add package flujograma/1.0.3/flujograma-1.0.3.tar.gz

---
+++ packages/flujograma/flujograma-1.0.3.tar.gz/flujograma-1.0.3/examples/ejemplos-seb-service.py
@@ -0,0 +1,6 @@
+from flujograma.diagrams_api import Diagram, ELB, EC2, RDS, S3
+
+with Diagram("Web Services", show=False, filename="web_services_final.png"):
+    ELB("lb") >> EC2("web") >> RDS("userdb") >> S3("store")
+    ELB("lb") >> EC2("web") >> RDS("userdb") << EC2("stat")
+    (ELB("lb") >> EC2("web")) >> EC2("web") >> RDS("userdb")
