90859d58de82c14955b41d39409aa6ac25cbed4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 14:04:43 +0000

    Add package kitstructlog/0.1.0/kitstructlog-0.1.0-py3-none-any.whl

---
+++ packages/kitstructlog/kitstructlog-0.1.0-py3-none-any.whl/kitstructlog/__init__.py
@@ -0,0 +1,15 @@
+from kitstructlog.main import (
+    InitLoggers,
+    LoggerReg,
+    LoggerError,
+    LoggerNotFoundError,
+    SetupLogger,
+)
+
+__all__ = [
+    "InitLoggers",
+    "LoggerError",
+    "LoggerNotFoundError",
+    "LoggerReg",
+    "SetupLogger",
+]
