0b65bf018bedb88abc994566c9e28adf942dd0a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 06:04:37 +0000

    Add package jupyter-kernel-handler/0.1.3/jupyter_kernel_handler-0.1.3.tar.gz

---
+++ packages/jupyter-kernel-handler/jupyter_kernel_handler-0.1.3.tar.gz/jupyter_kernel_handler-0.1.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='jupyter_kernel_handler',
+    version='0.1.3',
+    description='Jupyter Kernel Handler: Cell Execution Monitor with colored output and custom hooks',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Dataxcelerator',
+    author_email='opensource@dataxcelerator.com',
+    url='https://github.com/Dataxcelerator/jupyter_kernel_handler',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'ipython',
+        'jupyter',
+    ],
+    python_requires='>=3.7',
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
