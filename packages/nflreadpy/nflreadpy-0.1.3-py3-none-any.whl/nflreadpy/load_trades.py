035468fb38b87a3b1811b4d2b334e7887dfa4850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 05:06:20 +0000

    Add package nflreadpy/0.1.3/nflreadpy-0.1.3-py3-none-any.whl

---
+++ packages/nflreadpy/nflreadpy-0.1.3-py3-none-any.whl/nflreadpy/load_trades.py
@@ -0,0 +1,28 @@
+"""Load NFL trades data."""
+
+import polars as pl
+
+from .config import DataFormat
+from .downloader import get_downloader
+
+
+def load_trades() -> pl.DataFrame:
+    """
+    Load NFL trades data.
+
+    Returns:
+        Polars DataFrame with NFL trade information including players,
+        teams, draft picks, and trade details.
+
+    See Also:
+        <https://nflreadr.nflverse.com/reference/load_trades.html>
+
+    Data Dictionary:
+        <https://nflreadr.nflverse.com/articles/dictionary_trades.html>
+    """
+    downloader = get_downloader()
+
+    # Load trades data from nfldata repository (CSV format since RDS isn't readable)
+    df = downloader.download("nfldata", "trades", format=DataFormat.CSV)
+
+    return df
