99bc2ca1c7e5c09703fcb2b3c01ac74a8f036ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 15:05:43 +0000

    Add package rushclis/0.1.0/rushclis-0.1.0.tar.gz

---
+++ packages/rushclis/rushclis-0.1.0.tar.gz/rushclis-0.1.0/rushclis/color.py
@@ -0,0 +1,21 @@
+﻿from colorama import Fore, Style
+
+
+def print_color(color,
+                *values,
+                sep: str | None = " ",
+                end: str | None = "\n",
+                file=None,
+                flush=False):
+    print(f"{getattr(Fore, color, Fore.WHITE)}", end="")
+    print(*values, sep=sep, end=end, file=file, flush=flush)
+    print(f"{Style.RESET_ALL}", end="")
+
+
+def print_red(
+        *values,
+        sep: str | None = " ",
+        end: str | None = "\n",
+        file=None,
+        flush=False):
+    print_color("RED", *values, sep=sep, end=end, file=file, flush=flush)
