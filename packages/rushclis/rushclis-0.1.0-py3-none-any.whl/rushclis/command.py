eef911e068acc4680170a915080290f35b22ab75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 15:05:43 +0000

    Add package rushclis/0.1.0/rushclis-0.1.0-py3-none-any.whl

---
+++ packages/rushclis/rushclis-0.1.0-py3-none-any.whl/rushclis/command.py
@@ -0,0 +1,29 @@
+﻿from argparse import ArgumentParser, HelpFormatter
+
+from rushclis.color import print_red
+
+
+class Command(ArgumentParser):
+    def __init__(self,
+                 prog=None,
+                 usage=None,
+                 description=None,
+                 epilog=None,
+                 parents=[],
+                 formatter_class=HelpFormatter,
+                 prefix_chars='-',
+                 fromfile_prefix_chars=None,
+                 argument_default=None,
+                 conflict_handler='error',
+                 add_help=True,
+                 allow_abbrev=True,
+                 exit_on_error=True,
+                 print_error=False):
+        super().__init__(prog, usage, description, epilog, parents, formatter_class, prefix_chars,
+                         fromfile_prefix_chars, argument_default, conflict_handler, add_help, allow_abbrev,
+                         exit_on_error)
+        self.print_error = print_error
+
+    def error(self, message):
+        if self.print_error:
+            print_red(f"{message}")
