499a650cf6312cea0a2c342ed5f88e7da6daa4f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 03:21:38 +0000

    Add package brujula/0.1.0/brujula-0.1.0-py3-none-any.whl

---
+++ packages/brujula/brujula-0.1.0-py3-none-any.whl/brujula/__init__.py
@@ -0,0 +1,6 @@
+"""Brujula - A simple hello world Python package."""
+
+from .main import hello_world
+
+__version__ = "0.1.0"
+__all__ = ["hello_world"]
---
+++ packages/brujula/brujula-0.1.0-py3-none-any.whl/brujula/main.py
@@ -0,0 +1,12 @@
+def hello_world():
+    """A simple hello world function."""
+    return "Hello, world!"
+
+
+def main():
+    """Main entry point for the brujula package."""
+    print(hello_world())
+
+
+if __name__ == "__main__":
+    main()
