71e8a63b3b4d8238b45b9227c90b10739fbacce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 15:05:47 +0000

    Add package cugraph-service-server/0.0.1/cugraph_service_server-0.0.1.tar.gz

---
+++ packages/cugraph-service-server/cugraph_service_server-0.0.1.tar.gz/cugraph_service_server-0.0.1/README.md
@@ -0,0 +1,1 @@
+# cugraph-service-server\nA dummy Python package version of cugraph-service-server.
---
+++ packages/cugraph-service-server/cugraph_service_server-0.0.1.tar.gz/cugraph_service_server-0.0.1/src/ca_certificates/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.0.1"
+
+def info():
+    return "This is a dummy cugraph-service-server package for PyPI."
