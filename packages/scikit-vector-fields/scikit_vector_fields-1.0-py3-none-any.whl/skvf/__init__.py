4d3857d550df5f691fb7a61e32ba9b33a08b0eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 03:20:44 +0000

    Add package scikit-vector-fields/1.0/scikit_vector_fields-1.0-py3-none-any.whl

---
+++ packages/scikit-vector-fields/scikit_vector_fields-1.0-py3-none-any.whl/skvf/__init__.py
@@ -0,0 +1,41 @@
+import skvf.entities
+import skvf.operations
+import skvf.plot
+import skvf.data
+
+
+__version__ = '1.0'
+
+
+EPSILON_0 = 8.8541878128e-12 # F/m
+MU_0 = 1.25663706212e-6 # H/m
+VELOCITY_OF_LIGHT = 299792458.0
+
+
+
+MILLI = 1e-3
+MICRO = 1e-6
+NANO = 1e-9
+PICO = 1e-12
+
+GIGA = 1e9
+MEGA = 1e6
+KILO = 1e3
+
+
+
+
+
+# class vector():
+	# def __init__(self):
+		# print('This is vector')
+	
+# class space():
+	# def __init__(self):
+		# print('This is space')
+		
+# class field():
+	# def __init__(self):
+		# print('This is field')
+		
+		
