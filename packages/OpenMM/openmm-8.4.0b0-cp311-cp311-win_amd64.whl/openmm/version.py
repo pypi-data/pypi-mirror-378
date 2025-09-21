61e0e66241168f48e424da096adfee476c7842e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 05:05:50 +0000

    Add package OpenMM/8.4.0b0/openmm-8.4.0b0-cp311-cp311-win_amd64.whl

---
+++ packages/OpenMM/openmm-8.4.0b0-cp311-cp311-win_amd64.whl/openmm/version.py
@@ -0,0 +1,12 @@
+
+# THIS FILE IS GENERATED FROM OPENMM SETUP.PY
+import os
+short_version = '8.4.0'
+version = '8.4.0'
+full_version = '8.4.0.dev-5b07f0d'
+git_revision = '5b07f0d1e8d6c8d3dc0bd5dc691b36e34a353030'
+release = False
+openmm_library_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "OpenMM.libs", "lib"))
+
+if not release:
+    version = full_version
