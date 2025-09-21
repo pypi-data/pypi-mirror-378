30fb0b700f3296681e727b22ad6a5578e44db670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 05:06:20 +0000

    Add package Qmorse/1.0.0/qmorse-1.0.0.tar.gz

---
+++ packages/Qmorse/qmorse-1.0.0.tar.gz/qmorse-1.0.0/README.md
@@ -0,0 +1,17 @@
+# Qmorse
+
+Qmorse is a small Python library for **encoding and decoding Binary Morse code**. 
+It allows you to quickly convert text to Morse code and vice versa.
+
+## Features
+
+- Convert text to Morse code.
+- Convert Morse code back to text.
+- Simple and lightweight.
+
+## Installation
+
+You can install Qmorse via pip:
+
+```bash
+pip install Qmorse
---
+++ packages/Qmorse/qmorse-1.0.0.tar.gz/qmorse-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="Qmorse",
+    version="1.0.0",
+    author="Hobab",
+    author_email="b66669420@gmail.com",
+    description="A mini library for encoding and decoding Binary Morse code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/amiralihabibzadeh/Qmorse",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
