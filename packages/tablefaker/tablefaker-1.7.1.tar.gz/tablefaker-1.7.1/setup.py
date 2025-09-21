b362cb48de52cf238f03b2d53b4a1c4301e75f72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-21 04:05:43 +0000

    Add package tablefaker/1.7.1/tablefaker-1.7.1.tar.gz

---
+++ packages/tablefaker/tablefaker-1.7.1.tar.gz/tablefaker-1.7.1/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as file:
+    description = file.read()
+
+setup(
+    name='tablefaker',
+    version='1.7.1',
+    description='A Python package to generate fake tabular data. Get data in pandas dataframe or export to Parquet, DeltaLake, Csv, Json, Excel or Sql',
+    long_description = description,
+    long_description_content_type = "text/markdown",
+    author='Necati Arslan',
+    author_email='necatia@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'faker',
+        'pyyaml',
+        'pandas',
+        'openpyxl',
+        'fastparquet',
+        'psutil'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Topic :: Software Development :: Libraries',
+    ],
+    project_urls={
+        "Documentation": "https://github.com/necatiarslan/table-faker/blob/main/README.md",
+        "Source": "https://github.com/necatiarslan/table-faker",
+    },
+    entry_points={
+        'console_scripts': [
+            'tablefaker = tablefaker.cli:main',
+        ],
+    },
+)
