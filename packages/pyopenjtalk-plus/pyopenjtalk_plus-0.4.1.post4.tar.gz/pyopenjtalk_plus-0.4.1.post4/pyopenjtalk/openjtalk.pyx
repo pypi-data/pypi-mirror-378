32a70ccb18fa0f7d3a0ebb23971d1091af6604e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 04:09:52 +0000

    Add package pyopenjtalk-plus/0.4.1.post4/pyopenjtalk_plus-0.4.1.post4.tar.gz

---
+++ packages/pyopenjtalk-plus/pyopenjtalk_plus-0.4.1.post4.tar.gz/pyopenjtalk_plus-0.4.1.post4/pyopenjtalk/openjtalk.pyx
@@ -0,0 +1,442 @@
+# coding: utf-8
+# cython: boundscheck=True, wraparound=True
+# cython: c_string_type=unicode, c_string_encoding=ascii
+# cython: language_level=3
+# pyright: reportGeneralTypeIssues=false
+# pyright: reportMissingParameterType=false
+# pyright: reportUnknownLambdaType=false
+# pyright: reportUnknownParameterType=false
+# pyright: reportWildcardImportFromLibrary=false
+
+import errno
+import numpy as np
+from contextlib import contextmanager
+from threading import Lock
+
+cimport numpy as np
+np.import_array()
+
+from libc.stdlib cimport calloc
+from libc.string cimport strlen
+from libc.stdint cimport *
+
+from .openjtalk.mecab cimport Mecab, Mecab_initialize, Mecab_load, Mecab_analysis
+from .openjtalk.mecab cimport Mecab_get_feature, Mecab_get_size, Mecab_refresh, Mecab_clear
+from .openjtalk.mecab cimport createModel, Model, Tagger, Lattice
+from .openjtalk.mecab cimport mecab_dict_index as _mecab_dict_index
+from .openjtalk.njd cimport NJD, NJD_initialize, NJD_refresh, NJD_clear
+from .openjtalk cimport njd as _njd
+from .openjtalk.jpcommon cimport JPCommon, JPCommon_initialize,JPCommon_make_label
+from .openjtalk.jpcommon cimport JPCommon_get_label_size, JPCommon_get_label_feature
+from .openjtalk.jpcommon cimport JPCommon_refresh, JPCommon_clear
+from .openjtalk.text2mecab cimport text2mecab
+from .openjtalk.mecab2njd cimport mecab2njd
+from .openjtalk.njd2jpcommon cimport njd2jpcommon
+
+cdef njd_node_get_string(_njd.NJDNode* node):
+    return (<bytes>(_njd.NJDNode_get_string(node))).decode("utf-8")
+
+cdef njd_node_get_pos(_njd.NJDNode* node):
+    return (<bytes>(_njd.NJDNode_get_pos(node))).decode("utf-8")
+
+cdef njd_node_get_pos_group1(_njd.NJDNode* node):
+    return (<bytes>(_njd.NJDNode_get_pos_group1(node))).decode("utf-8")
+
+cdef njd_node_get_pos_group2(_njd.NJDNode* node):
+    return (<bytes>(_njd.NJDNode_get_pos_group2(node))).decode("utf-8")
+
+cdef njd_node_get_pos_group3(_njd.NJDNode* node):
+    return (<bytes>(_njd.NJDNode_get_pos_group3(node))).decode("utf-8")
+
+cdef njd_node_get_ctype(_njd.NJDNode* node):
+    return (<bytes>(_njd.NJDNode_get_ctype(node))).decode("utf-8")
+
+cdef njd_node_get_cform(_njd.NJDNode* node):
+    return (<bytes>(_njd.NJDNode_get_cform(node))).decode("utf-8")
+
+cdef njd_node_get_orig(_njd.NJDNode* node):
+    return (<bytes>(_njd.NJDNode_get_orig(node))).decode("utf-8")
+
+cdef njd_node_get_read(_njd.NJDNode* node):
+    return (<bytes>(_njd.NJDNode_get_read(node))).decode("utf-8")
+
+cdef njd_node_get_pron(_njd.NJDNode* node):
+    return (<bytes>(_njd.NJDNode_get_pron(node))).decode("utf-8")
+
+cdef int njd_node_get_acc(_njd.NJDNode* node) noexcept:
+    return _njd.NJDNode_get_acc(node)
+
+cdef int njd_node_get_mora_size(_njd.NJDNode* node) noexcept:
+    return _njd.NJDNode_get_mora_size(node)
+
+cdef njd_node_get_chain_rule(_njd.NJDNode* node):
+    return (<bytes>(_njd.NJDNode_get_chain_rule(node))).decode("utf-8")
+
+cdef int njd_node_get_chain_flag(_njd.NJDNode* node) noexcept:
+    return _njd.NJDNode_get_chain_flag(node)
+
+
+cdef node2feature(_njd.NJDNode* node):
+    return {
+        "string": njd_node_get_string(node),
+        "pos": njd_node_get_pos(node),
+        "pos_group1": njd_node_get_pos_group1(node),
+        "pos_group2": njd_node_get_pos_group2(node),
+        "pos_group3": njd_node_get_pos_group3(node),
+        "ctype": njd_node_get_ctype(node),
+        "cform": njd_node_get_cform(node),
+        "orig": njd_node_get_orig(node),
+        "read": njd_node_get_read(node),
+        "pron": njd_node_get_pron(node),
+        "acc": njd_node_get_acc(node),
+        "mora_size": njd_node_get_mora_size(node),
+        "chain_rule": njd_node_get_chain_rule(node),
+        "chain_flag": njd_node_get_chain_flag(node),
+    }
+
+
+cdef njd2feature(_njd.NJD* njd):
+    cdef _njd.NJDNode* node = njd.head
+    features = []
+    while node is not NULL:
+        features.append(node2feature(node))
+        node = node.next
+    return features
+
+
+cdef void feature2njd(_njd.NJD* njd, features):
+    cdef _njd.NJDNode* node
+
+    for feature_node in features:
+        node = <_njd.NJDNode *> calloc(1, sizeof(_njd.NJDNode))
+        _njd.NJDNode_initialize(node)
+        # set values
+        _njd.NJDNode_set_string(node, feature_node["string"].encode("utf-8"))
+        _njd.NJDNode_set_pos(node, feature_node["pos"].encode("utf-8"))
+        _njd.NJDNode_set_pos_group1(node, feature_node["pos_group1"].encode("utf-8"))
+        _njd.NJDNode_set_pos_group2(node, feature_node["pos_group2"].encode("utf-8"))
+        _njd.NJDNode_set_pos_group3(node, feature_node["pos_group3"].encode("utf-8"))
+        _njd.NJDNode_set_ctype(node, feature_node["ctype"].encode("utf-8"))
+        _njd.NJDNode_set_cform(node, feature_node["cform"].encode("utf-8"))
+        _njd.NJDNode_set_orig(node, feature_node["orig"].encode("utf-8"))
+        _njd.NJDNode_set_read(node, feature_node["read"].encode("utf-8"))
+        _njd.NJDNode_set_pron(node, feature_node["pron"].encode("utf-8"))
+        _njd.NJDNode_set_acc(node, feature_node["acc"])
+        _njd.NJDNode_set_mora_size(node, feature_node["mora_size"])
+        _njd.NJDNode_set_chain_rule(node, feature_node["chain_rule"].encode("utf-8"))
+        _njd.NJDNode_set_chain_flag(node, feature_node["chain_flag"])
+        _njd.NJD_push_node(njd, node)
+
+# based on Mecab_load in impl. from mecab.cpp
+cdef inline int Mecab_load_with_userdic(Mecab *m, char* dicdir, char* userdic) noexcept nogil:
+    if userdic == NULL or strlen(userdic) == 0:
+        return Mecab_load(m, dicdir)
+
+    if m == NULL or dicdir == NULL or strlen(dicdir) == 0:
+        return 0
+
+    Mecab_clear(m)
+
+    cdef char* argv[5]
+    argv[0] = "mecab"
+    argv[1] = "-d"
+    argv[2] = dicdir
+    argv[3] = "-u"
+    argv[4] = userdic
+    cdef Model *model = createModel(5, argv)
+
+    if model == NULL:
+        return 0
+    m.model = model
+
+    cdef Tagger *tagger = model.createTagger()
+    if tagger == NULL:
+        Mecab_clear(m)
+        return 0
+    m.tagger = tagger
+
+    cdef Lattice *lattice = model.createLattice()
+    if lattice == NULL:
+        Mecab_clear(m)
+        return 0
+    m.lattice = lattice
+
+    return 1
+
+def _generate_lock_manager():
+    lock = Lock()
+
+    @contextmanager
+    def f():
+        with lock:
+            yield
+
+    return f
+
+
+cdef class OpenJTalk:
+    """OpenJTalk
+
+    Args:
+        dn_mecab (bytes): Dictionary path for MeCab.
+        userdic (bytes): Dictionary path for MeCab userdic.
+            This option is ignored when empty bytestring is given.
+            Default is empty.
+    """
+    cdef Mecab* mecab
+    cdef NJD* njd
+    cdef JPCommon* jpcommon
+    _lock_manager = _generate_lock_manager()
+
+    def __cinit__(self, bytes dn_mecab=b"/usr/local/dic", bytes userdic=b""):
+        cdef char* _dn_mecab = dn_mecab
+        cdef char* _userdic = userdic
+
+        self.mecab = new Mecab()
+        self.njd = new NJD()
+        self.jpcommon = new JPCommon()
+
+        with nogil:
+            Mecab_initialize(self.mecab)
+            NJD_initialize(self.njd)
+            JPCommon_initialize(self.jpcommon)
+
+            r = self._load(_dn_mecab, _userdic)
+            if r != 1:
+                self._clear()
+                raise RuntimeError("Failed to initialize Mecab")
+
+    cdef void _clear(self) noexcept nogil:
+        Mecab_clear(self.mecab)
+        NJD_clear(self.njd)
+        JPCommon_clear(self.jpcommon)
+
+    cdef int _load(self, char* dn_mecab, char* userdic) noexcept nogil:
+        return Mecab_load_with_userdic(self.mecab, dn_mecab, userdic)
+
+    def _run_mecab(self, text):
+        cdef char buff[8192]
+        if isinstance(text, str):
+            text = text.encode("utf-8")
+
+        cdef const char* _text = text
+        cdef int result
+        with nogil:
+            result = text2mecab(buff, 8192, _text)
+        if result != 0:
+            if result == errno.ERANGE:
+                raise RuntimeError("Text is too long")
+            if result == errno.EINVAL:
+                raise RuntimeError("Invalid input for text2mecab")
+            raise RuntimeError("Unknown error: " + str(result))
+
+        cdef int morph_size
+        cdef char** mecab_morphs
+        with nogil:
+            Mecab_analysis(self.mecab, buff)
+
+            morph_size = Mecab_get_size(self.mecab)
+            mecab_morphs = Mecab_get_feature(self.mecab)
+
+        # seperating word with space
+        morphs = []
+        for i in range(morph_size):
+            m = (<bytes>(mecab_morphs[i])).decode('utf-8')
+            if '記号,空白' not in m:
+                morphs.append(m)
+
+        Mecab_refresh(self.mecab)
+
+        return morphs
+
+    @_lock_manager()
+    def run_mecab(self, text):
+        """Run MeCab analysis and return features
+
+        Args:
+            text: Input text
+
+        Returns:
+            list[str]: List of MeCab features
+        """
+        return self._run_mecab(text)
+
+    def _run_njd_from_mecab(self, mecab_features):
+        # if empty list, return empty list
+        new_size = len(mecab_features)
+        if new_size == 0:
+            return []
+
+        byte_morphs = [m.encode('utf-8')+b'\x00' for m in mecab_features]
+        int_morphs = np.zeros(len(byte_morphs), dtype=np.uint64)
+        for i in range(new_size):
+            int_morphs[i] = <uint64_t>(<char *>byte_morphs[i])
+
+        cdef uint64_t[:] cint_morphs = int_morphs
+        cdef char** new_mecab_morphs = <char**>&cint_morphs[0]
+        with nogil:
+            mecab2njd(self.njd, new_mecab_morphs, new_size)
+
+            _njd.njd_set_pronunciation(self.njd)
+
+        feature = njd2feature(self.njd)
+        feature = apply_original_rule_before_chaining(feature)
+        NJD_refresh(self.njd)
+        feature2njd(self.njd, feature)
+
+        with nogil:
+            _njd.njd_set_digit(self.njd)
+            _njd.njd_set_accent_phrase(self.njd)
+            _njd.njd_set_accent_type(self.njd)
+            _njd.njd_set_unvoiced_vowel(self.njd)
+            _njd.njd_set_long_vowel(self.njd)
+        feature = njd2feature(self.njd)
+
+        # Note that this will release memory for njd feature
+        NJD_refresh(self.njd)
+
+        return feature
+
+    @_lock_manager()
+    def run_njd_from_mecab(self, mecab_features):
+        """Run NJD processing from MeCab features
+
+        Args:
+            mecab_features: List of MeCab features (strings)
+
+        Returns:
+            list[dict]: List of NJD features
+        """
+        return self._run_njd_from_mecab(mecab_features)
+
+    @_lock_manager()
+    def run_frontend(self, text):
+        """Run OpenJTalk's text processing frontend
+
+        Args:
+            text: Input text
+
+        Returns:
+            List[dict]: List of NJD features
+        """
+        mecab_features = self._run_mecab(text)
+        return self._run_njd_from_mecab(mecab_features)
+
+    @_lock_manager()
+    def make_label(self, features):
+        """Make full-context label
+        """
+        feature2njd(self.njd, features)
+        with nogil:
+            njd2jpcommon(self.jpcommon, self.njd)
+
+            JPCommon_make_label(self.jpcommon)
+
+            label_size = JPCommon_get_label_size(self.jpcommon)
+            label_feature = JPCommon_get_label_feature(self.jpcommon)
+
+        labels = []
+        for i in range(label_size):
+            # This will create a copy of c string
+            # http://cython.readthedocs.io/en/latest/src/tutorial/strings.html
+            labels.append(<unicode>label_feature[i])
+
+        # Note that this will release memory for label feature
+        JPCommon_refresh(self.jpcommon)
+        NJD_refresh(self.njd)
+
+        return labels
+
+    def g2p(self, text, kana=False, join=True):
+        """Grapheme-to-phoeneme (G2P) conversion (Cython implementation)
+        """
+        njd_features = self.run_frontend(text)
+
+        if not kana:
+            labels = self.make_label(njd_features)
+            prons = list(map(lambda s: s.split("-")[1].split("+")[0], labels[1:-1]))
+            if join:
+                prons = " ".join(prons)
+            return prons
+
+        # kana
+        prons = []
+        for n in njd_features:
+            if n["pos"] == "記号":
+                p = n["string"]
+            else:
+                p = n["pron"]
+            # remove special chars
+            for c in "’":
+                p = p.replace(c,"")
+            prons.append(p)
+        if join:
+            prons = "".join(prons)
+        return prons
+
+    def __dealloc__(self):
+        self._clear()
+        del self.mecab
+        del self.njd
+        del self.jpcommon
+
+def mecab_dict_index(bytes dn_mecab, bytes path, bytes out_path):
+    cdef char* argv[10]
+    argv[0] = "mecab-dict-index"
+    argv[1] = "-d"
+    argv[2] = dn_mecab
+    argv[3] = "-u"
+    argv[4] = out_path
+    argv[5] = "-f"
+    argv[6] = "utf-8"
+    argv[7] = "-t"
+    argv[8] = "utf-8"
+    argv[9] = path
+    cdef int ret
+    with nogil:
+        ret = _mecab_dict_index(10, argv)
+    return ret
+
+def build_mecab_dictionary(bytes dn_mecab):
+    cdef char* argv[9]
+    argv[0] = "mecab-dict-index"
+    argv[1] = "-d"
+    argv[2] = dn_mecab
+    argv[3] = "-o"
+    argv[4] = dn_mecab
+    argv[5] = "-f"
+    argv[6] = "utf-8"
+    argv[7] = "-t"
+    argv[8] = "utf-8"
+    cdef int ret
+    with nogil:
+        ret = _mecab_dict_index(9, argv)
+    return ret
+
+def apply_original_rule_before_chaining(njd_features):
+    for i, njd in enumerate(njd_features[:-1]):
+        # サ変動詞(スル)の前にサ変接続や名詞が来た場合は、一つのアクセント句に纏める
+        if (njd["pos_group1"] in ["サ変接続", "格助詞", "接続助詞"] or (njd["pos"] == "名詞" and njd["pos_group1"] == "一般") or njd["pos"] == "副詞" ) and njd_features[i+1]["ctype"] == "サ変・スル":
+            njd_features[i+1]["chain_flag"] = 1
+        # ご遠慮、ご配慮のような接頭語がつく場合にその後に続く単語の結合則を変更する
+        if (njd["string"] in ["お","御","ご"] and njd["chain_rule"] == "P1"):
+            if njd_features[i+1]["acc"] == 0 or njd_features[i+1]["acc"] == njd_features[i+1]["mora_size"]:
+                njd_features[i+1]['chain_rule'] = "C4"
+                njd_features[i+1]["acc"] = 0
+            else:
+                njd_features[i+1]['chain_rule'] = "C1"
+        # 動詞(自立)が連続する場合(ex 推し量る、刺し貫く)、後ろの動詞のアクセント核が採用される
+        if njd["pos"] == "動詞"  and njd_features[i+1]["pos"] == "動詞" :
+            njd_features[i+1]["chain_rule"] = "C1" if njd_features[i+1]["acc"] != 0 else "C4"
+        # 連用形のアクセント核の登録を修正する
+        if njd["cform"] in ["連用形","連用タ接続","連用ゴザイ接続","連用テ接続"] and njd["acc"] == njd["mora_size"] > 1 :
+            njd["acc"] -= 1
+        # 「らる、られる」＋「た」の組み合わせで「た」の助動詞/F2@0を上書きしてアクセントを下げないようにする
+        if njd["orig"] in ["れる", "られる","せる", "させる","ちゃう"]  and njd_features[i+1]["string"] in ["た"] :
+            njd_features[i+1]["chain_rule"] = "F2@1"
+
+        # 形容詞＋「なる、する」は一つのアクセント句に纏める
+        if njd["pos"] == "形容詞" and njd_features[i+1]["orig"] in ["なる", "する"]:
+            njd_features[i+1]["chain_flag"] = 1
+
+    return njd_features
---
+++ packages/pyopenjtalk-plus/pyopenjtalk_plus-0.4.1.post4.tar.gz/pyopenjtalk_plus-0.4.1.post4/setup.py
@@ -0,0 +1,287 @@
+import os
+import subprocess
+import sys
+from glob import glob
+from itertools import chain
+from os.path import exists, join
+
+import numpy as np
+import setuptools.command.build_ext
+import setuptools.command.build_py
+import setuptools.command.develop
+from setuptools import Extension, find_packages, setup
+
+
+platform_is_windows = sys.platform == "win32"
+
+version = "0.4.1-post4"
+
+msvc_extra_compile_args_config = [
+    "/source-charset:utf-8",
+    "/execution-charset:utf-8",
+]
+
+
+def msvc_extra_compile_args(compile_args):
+    cas = set(compile_args)
+    xs = filter(lambda x: x not in cas, msvc_extra_compile_args_config)
+    return list(chain(compile_args, xs))
+
+
+msvc_define_macros_config = [
+    ("_CRT_NONSTDC_NO_WARNINGS", None),
+    ("_CRT_SECURE_NO_WARNINGS", None),
+]
+
+
+def msvc_define_macros(macros):
+    mns = set([i[0] for i in macros])
+    xs = filter(lambda x: x[0] not in mns, msvc_define_macros_config)
+    return list(chain(macros, xs))
+
+
+class custom_build_ext(setuptools.command.build_ext.build_ext):
+    def build_extensions(self):
+        compiler_type_is_msvc = self.compiler.compiler_type == "msvc"
+        for entry in self.extensions:
+            if compiler_type_is_msvc:
+                entry.extra_compile_args = msvc_extra_compile_args(
+                    entry.extra_compile_args if hasattr(entry, "extra_compile_args") else []
+                )
+                entry.define_macros = msvc_define_macros(
+                    entry.define_macros if hasattr(entry, "define_macros") else []
+                )
+
+        setuptools.command.build_ext.build_ext.build_extensions(self)
+
+
+def check_cmake_in_path():
+    try:
+        result = subprocess.run(
+            ["cmake", "--version"],
+            capture_output=True,
+            text=True,
+        )
+        if result.returncode == 0:
+            # CMake is in the system path
+            return True, result.stdout.strip()
+        else:
+            # CMake is not in the system path
+            return False, None
+    except FileNotFoundError:
+        # CMake command not found
+        return False, None
+
+
+if os.name == "nt":  # Check if the OS is Windows
+    # Check if CMake is in the system path
+    cmake_found, cmake_version = check_cmake_in_path()
+
+    if cmake_found:
+        print(
+            f"CMake is in the system path. Version: \
+              {cmake_version}"
+        )
+    else:
+        raise SystemError(
+            "CMake is not found in the \
+                          system path. Make sure CMake \
+                          is installed and in the system \
+                          path."
+        )
+
+# open_jtalk sources
+src_top = join("lib", "open_jtalk", "src")
+
+# generate config.h for mecab
+# NOTE: need to run cmake to generate config.h
+# we could do it on python side but it would be very tricky,
+# so far let's use cmake tool
+if not exists(join(src_top, "mecab", "src", "config.h")):
+    cwd = os.getcwd()
+    build_dir = join(src_top, "build")
+    os.makedirs(build_dir, exist_ok=True)
+    os.chdir(build_dir)
+
+    r = subprocess.run(["cmake", ".."])
+    r.check_returncode()
+    os.chdir(cwd)
+
+all_src = []
+include_dirs = []
+for s in [
+    "jpcommon",
+    "mecab/src",
+    "mecab2njd",
+    "njd",
+    "njd2jpcommon",
+    "njd_set_accent_phrase",
+    "njd_set_accent_type",
+    "njd_set_digit",
+    "njd_set_long_vowel",
+    "njd_set_pronunciation",
+    "njd_set_unvoiced_vowel",
+    "text2mecab",
+]:
+    all_src += glob(join(src_top, s, "*.c"))
+    all_src += glob(join(src_top, s, "*.cpp"))
+    include_dirs.append(join(os.getcwd(), src_top, s))
+
+# Extension for OpenJTalk frontend
+ext_modules = [
+    Extension(
+        name="pyopenjtalk.openjtalk",
+        sources=[join("pyopenjtalk", "openjtalk.pyx"), *all_src],
+        include_dirs=[np.get_include(), *include_dirs],
+        extra_compile_args=[],
+        extra_link_args=[],
+        language="c++",
+        define_macros=[
+            ("HAVE_CONFIG_H", None),
+            ("DIC_VERSION", "102"),
+            ("MECAB_DEFAULT_RC", '"dummy"'),
+            ("PACKAGE", '"open_jtalk"'),
+            ("VERSION", '"1.11"'),
+            ("CHARSET_UTF_8", None),
+            ("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION"),
+        ],
+    )
+]
+
+# Extension for HTSEngine backend
+htsengine_src_top = join("lib", "hts_engine_API", "src")
+all_htsengine_src = glob(join(htsengine_src_top, "lib", "*.c"))
+ext_modules += [
+    Extension(
+        name="pyopenjtalk.htsengine",
+        sources=[join("pyopenjtalk", "htsengine.pyx"), *all_htsengine_src],
+        include_dirs=[np.get_include(), join(htsengine_src_top, "include")],
+        extra_compile_args=[],
+        extra_link_args=[],
+        libraries=["winmm"] if platform_is_windows else [],
+        language="c++",
+        define_macros=[
+            ("AUDIO_PLAY_NONE", None),
+            ("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION"),
+        ],
+    )
+]
+
+# Adapted from https://github.com/pytorch/pytorch
+cwd = os.path.dirname(os.path.abspath(__file__))
+if os.getenv("PYOPENJTALK_BUILD_VERSION"):
+    version = os.getenv("PYOPENJTALK_BUILD_VERSION")
+else:
+    try:
+        sha = subprocess.check_output(["git", "rev-parse", "HEAD"], cwd=cwd).decode("ascii").strip()
+        # version += "+" + sha[:7]
+    except subprocess.CalledProcessError:
+        pass
+    except OSError:  # FileNotFoundError for python 3
+        pass
+
+
+class build_py(setuptools.command.build_py.build_py):
+    def run(self):
+        self.create_version_file()
+        setuptools.command.build_py.build_py.run(self)
+
+    @staticmethod
+    def create_version_file():
+        global version, cwd
+        print("-- Building version " + version)
+        version_path = os.path.join(cwd, "pyopenjtalk", "version.py")
+        with open(version_path, "w") as f:
+            f.write(f"__version__ = '{version}'\n")
+
+
+class develop(setuptools.command.develop.develop):
+    def run(self):
+        build_py.create_version_file()
+        setuptools.command.develop.develop.run(self)
+
+
+with open("README.md", encoding="utf8") as fd:
+    long_description = fd.read()
+
+setup(
+    name="pyopenjtalk-plus",
+    version=version,
+    description="A Python wrapper for OpenJTalk with additional improvements",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Ryuichi Yamamoto",
+    author_email="zryuichi@gmail.com",
+    url="https://github.com/tsukumijima/pyopenjtalk-plus",
+    license="MIT",
+    packages=find_packages(),
+    package_data={
+        "": [
+            "dictionary/COPYING",
+            "dictionary/*.bin",
+            "dictionary/*.dic",
+            "dictionary/left-id.def",
+            "dictionary/pos-id.def",
+            "dictionary/rewrite.def",
+            "dictionary/right-id.def",
+            "htsvoice/*.htsvoice",
+            # オリジナルの Pickle 形式のモデルファイルは参照用に残してあるもの
+            # ビルド後の wheel には ONNX 形式のモデルファイルのみを含める
+            "yomi_model/*.onnx",
+            "yomi_model/*.py",
+        ],
+        "pyopenjtalk": ["py.typed", "*.pyi"],
+    },
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": custom_build_ext, "build_py": build_py, "develop": develop},
+    install_requires=[
+        "importlib_resources; python_version<'3.9'",
+        "numpy>=1.24.0",
+        "sudachipy; python_version>='3.9'",
+        "sudachipy<=0.6.8; python_version<'3.9'",
+        "sudachidict_core",
+    ],
+    tests_require=["nose", "coverage"],
+    extras_require={
+        "docs": [
+            "sphinx_rtd_theme",
+            "nbsphinx>=0.8.6",
+            "Jinja2>=3.0.1",
+            "pandoc",
+            "ipython",
+            "jupyter",
+        ],
+        "dev": [
+            "cython>=3.0",
+            "ruff<0.14.0",
+            "taskipy",
+            "types-setuptools",
+            "click",
+            "types-decorator",
+            "importlib-metadata<5.0",
+        ],
+        "test": ["pytest"],
+        "onnxruntime": ["onnxruntime"],
+        "marine": ["marine-plus"],
+    },
+    classifiers=[
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Cython",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+    ],
+    keywords=["OpenJTalk", "Research"],
+)
