ee8b8ba4669a2649ac9f5aa7221ce613c53db206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 05:06:42 +0000

    Add package dircomply/0.8.0/dircomply-0.8.0-py3-none-any.whl

---
+++ packages/dircomply/dircomply-0.8.0-py3-none-any.whl/dircomply/version.py
@@ -0,0 +1,13 @@
+"""
+version.py
+
+Author: Benevant Mathew
+Date: 2025-09-21
+"""
+__version__ = "0.8.0"
+__author__ = "Benevant Mathew"
+__email__ = "benevantmathewv@gmail.com"
+__release_date__="21-09-2025"
+
+if __name__ == "__main__":
+    print(__version__)
