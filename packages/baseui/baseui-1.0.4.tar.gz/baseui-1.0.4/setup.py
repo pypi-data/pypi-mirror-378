2d75360be2b4f35a38e6442af91b4ae34df535bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 05:09:34 +0000

    Add package baseui/1.0.4/baseui-1.0.4.tar.gz

---
+++ packages/baseui/baseui-1.0.4.tar.gz/baseui-1.0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name                          = "baseui",
+    version                       = "1.0.4",
+    packages                      = find_packages(),
+    install_requires              = ["requests"],  # Dependencies
+    author                        = "Abdelmathin Habachi",
+    author_email                  = "contact@abdelmathin.com" ,
+    description                   = "baseui",
+    long_description              = "# baseui",
+    long_description_content_type = "text/markdown",
+    url                           = "https://github.com/Abdelmathin/baseui",
+    classifiers                   = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
