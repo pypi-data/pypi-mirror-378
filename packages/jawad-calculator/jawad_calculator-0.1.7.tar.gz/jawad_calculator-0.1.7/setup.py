6766ae6350bcc76be7b375a6429d985a5850496a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 15:06:23 +0000

    Add package jawad-calculator/0.1.7/jawad_calculator-0.1.7.tar.gz

---
+++ packages/jawad-calculator/jawad_calculator-0.1.7.tar.gz/jawad_calculator-0.1.7/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jawad_calculator',
+    version='0.1.7',
+    packages=find_packages(),
+    description='A simple calculator with addition and subtraction',
+    author='Jawad Iqbal',
+    author_email='jawad.iqbal@somersetft.nhs.uk',
+    url='https://github.com/jawad-iqbal99/calculator',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
