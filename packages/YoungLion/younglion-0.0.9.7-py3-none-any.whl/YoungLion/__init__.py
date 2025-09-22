cc2483ea2e638a8372d34723e5a4a97d3e5577e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 04:06:52 +0000

    Add package YoungLion/0.0.9.7/younglion-0.0.9.7-py3-none-any.whl

---
+++ packages/YoungLion/younglion-0.0.9.7-py3-none-any.whl/YoungLion/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+__version__ = "0.0.9.7"
+author={
+    'name':"Cavanşir","surname":"Qurbanzadə","username":"cavanshirpro","email":"cavanshirpro@gmail.com"
+}
+from .function import *
+from .search import *
