981487eb052dafb37e6bd5b72039cb7a22c0737c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 04:07:16 +0000

    Add package xwarning/0.18/xwarning-0.18-py3-none-any.whl

---
+++ packages/xwarning/xwarning-0.18-py3-none-any.whl/xwarning/__init__.py
@@ -0,0 +1,42 @@
+# from .xwarning import warn, warning, configure  # optional shortcut exports
+# xwarning/__init__.py
+
+from .xwarning import (
+    warn,
+    warning,
+    configure,
+    WarningPrinter,
+    xwarnings,
+    xwarning,
+)
+
+# Export warning classes for direct use
+# from warnings import (
+#     Warning,
+#     UserWarning,
+#     DeprecationWarning,
+#     FutureWarning,
+#     RuntimeWarning,
+#     SyntaxWarning,
+#     ImportWarning,
+#     UnicodeWarning,
+# )
+
+# Make it available at package level
+__all__ = [
+    'warn',
+    'warning', 
+    'configure',
+    'WarningPrinter',
+    'xwarnings',
+    'xwarning',
+    # Warning classes
+    'Warning',
+    'UserWarning',
+    'DeprecationWarning',
+    'FutureWarning',
+    'RuntimeWarning',
+    'SyntaxWarning',
+    'ImportWarning',
+    'UnicodeWarning',
+]
