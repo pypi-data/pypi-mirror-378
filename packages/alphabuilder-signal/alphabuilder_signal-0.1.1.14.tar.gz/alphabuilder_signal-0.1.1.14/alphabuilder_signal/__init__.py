3839304a6de5c0e9997e1e40dce9f271bf9d5bee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 04:06:51 +0000

    Add package alphabuilder-signal/0.1.1.14/alphabuilder_signal-0.1.1.14.tar.gz

---
+++ packages/alphabuilder-signal/alphabuilder_signal-0.1.1.14.tar.gz/alphabuilder_signal-0.1.1.14/alphabuilder_signal/__init__.py
@@ -0,0 +1,5 @@
+from .fetch import Fetch
+from .target import TargetClassifier, TargetRegressor
+from .indicators import LaggedFeatures, MovingAverage, TechnicalIndicators
+
+__all__ = ["Fetch", "TargetClassifier", "LaggedFeatures", "MovingAverage", "TechnicalIndicators", "TargetRegressor"]
