62aa248fbfdaad50d5868f78d2a60811c0d89d5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 03:24:22 +0000

    Add package VY4E-OptModel/1.0.14/vy4e_optmodel-1.0.14.tar.gz

---
+++ packages/VY4E-OptModel/vy4e_optmodel-1.0.14.tar.gz/vy4e_optmodel-1.0.14/src/vy4e_optmodel/__init__.py
@@ -0,0 +1,19 @@
+"""
+VY4E-OptModel: An open-source optimization model for the design and operation of hybrid renewable energy systems.
+
+
+    Args:
+        case:   Name of the folder where the CSV files of the case are found
+        dir:    Main path where the case folder can be found
+        solver: Name of the solver
+
+    Returns:
+        Output results in CSV files that are found in the case folder.
+
+    Examples:
+        >>> import VY4E-OptModel as oM
+        >>> oM.routine("VPP1", "C:\\Users\\UserName\\Documents\\GitHub\\VY4E-OptModel", "glpk")
+"""
+__version__ = "1.0.14"
+
+from .oM_Main import main
