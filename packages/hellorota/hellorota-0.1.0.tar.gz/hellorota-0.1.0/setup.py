d8bcf8041cc8b11b66c4dcc8346d2c95882163ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 06:06:01 +0000

    Add package hellorota/0.1.0/hellorota-0.1.0.tar.gz

---
+++ packages/hellorota/hellorota-0.1.0.tar.gz/hellorota-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hellorota",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    description="Funzione HelloWorld con effetto typing",
+    author="Tuo Nome",
+    author_email="tuo@email.com",
+    url="https://github.com/tuoaccount/helloworld",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
