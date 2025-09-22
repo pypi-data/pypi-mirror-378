e76230db8e47b897518f149539e3e4bee138ad18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 15:06:26 +0000

    Add package fundrive-ossutil/0.1.3/fundrive_ossutil-0.1.3-py3-none-any.whl

---
+++ packages/fundrive-ossutil/fundrive_ossutil-0.1.3-py3-none-any.whl/fundrives/ossutil/__init__.py
@@ -0,0 +1,13 @@
+"""
+fundrive-ossutil: 阿里云OSS命令行工具ossutil的Python包装器
+
+提供更加友好的Python接口来使用阿里云OSS服务。
+"""
+
+from .install import install_ossutil, get_ossutil_path, is_ossutil_installed
+
+__version__ = "0.1.1"
+__author__ = "farfarfun"
+__email__ = "farfarfun@gmail.com"
+
+__all__ = ["install_ossutil", "get_ossutil_path", "is_ossutil_installed"]
