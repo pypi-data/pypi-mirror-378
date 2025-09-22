289b3289a4880947723a30034c9990c71fa98779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 06:05:55 +0000

    Add package ozgursozluk/0.9.6/ozgursozluk-0.9.6.tar.gz

---
+++ packages/ozgursozluk/ozgursozluk-0.9.6.tar.gz/ozgursozluk-0.9.6/src/ozgursozluk/main.py
@@ -0,0 +1,32 @@
+from datetime import datetime, UTC
+
+import flask
+
+from . import __version__, configs
+
+
+app = flask.Flask(__name__)
+app.secret_key = configs.SECRET_KEY
+
+
+@app.route("/robots.txt")
+def robots():
+    return flask.send_from_directory(app.static_folder, "robots.txt")
+
+
+@app.route("/external/privacy-policy")
+def privacy_policy() -> str:
+    return flask.render_template("privacy-policy.html")
+
+
+@app.route("/external/terms-of-service")
+def terms_of_service() -> str:
+    return flask.render_template("terms-of-service.html")
+
+
+@app.route("/api/healtcheck")
+def healtcheck() -> flask.Response:
+    return flask.jsonify(status="healthy", version=__version__, timestamp=datetime.now(UTC))
+
+
+from . import views
