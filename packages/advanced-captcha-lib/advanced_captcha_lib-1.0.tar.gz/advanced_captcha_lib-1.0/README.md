7440679a553af8dc850d743e3b85ec313bfa24ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 04:06:11 +0000

    Add package advanced-captcha-lib/1.0/advanced_captcha_lib-1.0.tar.gz

---
+++ packages/advanced-captcha-lib/advanced_captcha_lib-1.0.tar.gz/advanced_captcha_lib-1.0/README.md
@@ -0,0 +1,13 @@
+# advanced_captcha_lib
+
+Advanced CAPTCHA library supporting multiple GUI frameworks (PySide6, PyQt5, PyQt6, Tkinter, wxPython).
+
+## Features
+
+- 3 types of CAPTCHA:
+  1. Optical illusion
+  2. Numeric simple
+  3. Deformed text
+- Language auto-detection (English/Spanish)
+- One attempt per CAPTCHA
+- Easy to call from any GUI button
---
+++ packages/advanced-captcha-lib/advanced_captcha_lib-1.0.tar.gz/advanced_captcha_lib-1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="advanced_captcha_lib",
+    version="1.0",
+    description="Advanced CAPTCHA library with language detection",
+    author="TuFueguito",
+    packages=find_packages(),
+    install_requires=[
+        "PySide6",
+        "customtkinter",
+        "PyQt5",
+        "PyQt6",
+        "wxPython"
+    ],
+    python_requires=">=3.9",
+)
