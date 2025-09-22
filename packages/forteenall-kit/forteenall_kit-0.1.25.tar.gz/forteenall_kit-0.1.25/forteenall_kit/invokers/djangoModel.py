438e9a02d221a06a3596de1c7de746cdb6bc5707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 04:05:51 +0000

    Add package forteenall-kit/0.1.25/forteenall_kit-0.1.25.tar.gz

---
+++ packages/forteenall-kit/forteenall_kit-0.1.25.tar.gz/forteenall_kit-0.1.25/forteenall_kit/invokers/djangoModel.py
@@ -0,0 +1,13 @@
+class DjangoModelField:
+    pass
+
+class DjangoModelData:
+    pass
+
+class Feature:
+    def execute(self):
+        pass
+    def _generate_model_code(self, model_name, fields, app_name):
+        pass
+    def _write_model_file(self, file_path, content):
+        pass
