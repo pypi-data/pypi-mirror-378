6a6a461775dd5daff03d6e8a60ab68d1ec3508ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 06:06:05 +0000

    Add package pianosdk/16.582.1/pianosdk-16.582.1.tar.gz

---
+++ packages/pianosdk/pianosdk-16.582.1.tar.gz/pianosdk-16.582.1/pianosdk/publisher/models/upgrade_option.py
@@ -0,0 +1,18 @@
+from datetime import date, datetime
+from pydantic.main import BaseModel
+from typing import Optional
+from pianosdk.publisher.models.term_info import TermInfo
+from typing import List
+
+
+class UpgradeOption(BaseModel):
+    id: Optional[str] = None
+    term_from: Optional['TermInfo'] = None
+    term_to: Optional['TermInfo'] = None
+    bill_timing: Optional[str] = None
+    prorate_access: Optional[bool] = None
+    immediate_access: Optional[bool] = None
+    show_option_in_channels: Optional[List[str]] = None
+
+
+UpgradeOption.model_rebuild()
