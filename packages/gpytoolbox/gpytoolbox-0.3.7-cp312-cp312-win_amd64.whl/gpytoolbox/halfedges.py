7e9372cbe74cd87277b1d0a8e23175005ea7abf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 05:07:23 +0000

    Add package gpytoolbox/0.3.7/gpytoolbox-0.3.7-cp312-cp312-win_amd64.whl

---
+++ packages/gpytoolbox/gpytoolbox-0.3.7-cp312-cp312-win_amd64.whl/gpytoolbox/halfedges.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+def halfedges(F):
+    """Given a triangle mesh with face indices F, returns all oriented halfedges
+    as indices into the vertex array.
+
+    The ordering convention for halfedges is the following:
+    [halfedge opposite vertex 0,
+     halfedge opposite vertex 1,
+     halfedge opposite vertex 2]
+
+    Parameters
+    ----------
+    F : (m,3) numpy int array
+        face index list of a triangle mesh
+
+    Returns
+    -------
+    he : (m,3,2) numpy int array
+        halfedge list as per above conventions
+
+    Examples
+    --------
+    ```python
+    # Sample mesh
+    v = np.array([[0.0,0.0],[1.0,0.0],[0.0,1.0]])
+    f = np.array([[0,1,2]],dtype=int)
+    # Call to halfedges
+    from gpytoolbox import halfedges
+    # (Thank you to Lukas Hermann for finding and fixing a typo in this next line)
+    he = halfedges(f)
+    ```
+    
+    """
+    
+    assert F.shape[0] > 0
+    assert F.shape[1] == 3
+
+    he = np.block([[[F[:,1][:,None], F[:,2][:,None]]],
+        [[F[:,2][:,None], F[:,0][:,None]]],
+        [[F[:,0][:,None], F[:,1][:,None]]]]).transpose((1,0,2))
+    return he
+
