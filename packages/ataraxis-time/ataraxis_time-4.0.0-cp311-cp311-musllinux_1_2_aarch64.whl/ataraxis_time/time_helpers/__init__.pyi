d995864f664dc9ad58995938709af8b1e017c600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 14:06:05 +0000

    Add package ataraxis-time/4.0.0/ataraxis_time-4.0.0-cp311-cp311-musllinux_1_2_aarch64.whl

---
+++ packages/ataraxis-time/ataraxis_time-4.0.0-cp311-cp311-musllinux_1_2_aarch64.whl/ataraxis_time/time_helpers/__init__.pyi
@@ -0,0 +1,9 @@
+from .helper_functions import (
+    TimeUnits as TimeUnits,
+    TimestampFormats as TimestampFormats,
+    convert_time as convert_time,
+    get_timestamp as get_timestamp,
+    convert_timestamp as convert_timestamp,
+)
+
+__all__ = ["TimeUnits", "TimestampFormats", "convert_time", "convert_timestamp", "get_timestamp"]
