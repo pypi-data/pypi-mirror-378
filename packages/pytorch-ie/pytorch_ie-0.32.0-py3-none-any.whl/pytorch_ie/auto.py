6da341dd8a3b9f207a539bed6e1db55493dc4d05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 06:05:52 +0000

    Add package pytorch-ie/0.32.0/pytorch_ie-0.32.0-py3-none-any.whl

---
+++ packages/pytorch-ie/pytorch_ie-0.32.0-py3-none-any.whl/pytorch_ie/auto.py
@@ -0,0 +1,6 @@
+# kept for backward compatibility
+from pie_core import AutoTaskModule
+
+# kept for backward compatibility
+from pytorch_ie.model import AutoPyTorchIEModel as AutoModel
+from pytorch_ie.pipeline import PyTorchIEPipeline as AutoPipeline
