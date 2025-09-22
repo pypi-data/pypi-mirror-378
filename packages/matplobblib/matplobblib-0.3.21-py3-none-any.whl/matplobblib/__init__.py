b1ac6251e915e46e699c824005654ea3de69e81d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 03:24:19 +0000

    Add package matplobblib/0.3.21/matplobblib-0.3.21-py3-none-any.whl

---
+++ packages/matplobblib/matplobblib-0.3.21-py3-none-any.whl/matplobblib/__init__.py
@@ -0,0 +1,18 @@
+__version__ = "0.3.21"
+
+import importlib as _importlib
+from .forall import *
+
+submodules = [
+    'tvims',
+    'aisd',
+    'ml',
+    'nm',
+    'tod'
+    ]
+def __dir__():
+    return submodules
+
+def __getattr__(name):
+    if name in submodules:
+        return _importlib.import_module(f'matplobblib.{name}')
