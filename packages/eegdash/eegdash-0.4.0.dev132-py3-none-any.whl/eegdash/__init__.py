98d4c46c76a7310c6a57655d9f0b213965b27470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 04:06:21 +0000

    Add package eegdash/0.4.0.dev132/eegdash-0.4.0.dev132-py3-none-any.whl

---
+++ packages/eegdash/eegdash-0.4.0.dev132-py3-none-any.whl/eegdash/__init__.py
@@ -0,0 +1,10 @@
+from .api import EEGDash, EEGDashDataset
+from .dataset import EEGChallengeDataset
+from .hbn import preprocessing
+from .utils import _init_mongo_client
+
+_init_mongo_client()
+
+__all__ = ["EEGDash", "EEGDashDataset", "EEGChallengeDataset", "preprocessing"]
+
+__version__ = "0.4.0.dev132"
