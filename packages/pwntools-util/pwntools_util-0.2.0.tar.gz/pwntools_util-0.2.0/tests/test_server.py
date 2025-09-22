3a97936d9e0075170fbe9b19b663abd4aefb215d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 15:06:35 +0000

    Add package pwntools-util/0.2.0/pwntools_util-0.2.0.tar.gz

---
+++ packages/pwntools-util/pwntools_util-0.2.0.tar.gz/pwntools_util-0.2.0/tests/test_server.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    print("Hello world")
+    print("Hello 123")
+    print("Hello -1337")
+    print("Pi is 3.14159")
+    print("There are +365 days in -1 year")
+    print("List 1: [1, 2, 3, 4, 5]")
+    print("List 2: [1, -3., .5, -7.0, 9e9], hurray!")
+    inp = input("Enter anything -> ")
+    print(f"Your entered \"{inp}\"!")
