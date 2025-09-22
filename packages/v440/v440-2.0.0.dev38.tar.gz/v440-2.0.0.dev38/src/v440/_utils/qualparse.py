3a13ae4718621a031858be1b30bfa5941c0e8610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 14:06:50 +0000

    Add package v440/2.0.0.dev38/v440-2.0.0.dev38.tar.gz

---
+++ packages/v440/v440-2.0.0.dev38.tar.gz/v440-2.0.0.dev38/src/v440/_utils/qualparse.py
@@ -0,0 +1,181 @@
+from __future__ import annotations
+
+from typing import *
+
+from v440._utils import utils
+from v440._utils.Cfg import Cfg
+from v440._utils.Digest import Digest
+from v440._utils.Pattern import Pattern
+
+parse_leg: Digest = Digest("parse_leg")
+
+
+@parse_leg.overload()
+def parse_leg() -> tuple:
+    return None, None, None
+
+
+@parse_leg.overload(int)
+def parse_leg(value: int) -> tuple:
+    return None, abs(value), None
+
+
+@parse_leg.overload(list)
+def parse_leg(value: list) -> tuple:
+    return tuple([value[:2]] + value[2:])
+
+
+@parse_leg.overload(str)
+def parse_leg(value: str) -> list:
+    v = value
+    prephase: Any = None
+    presubphase: Any = None
+    post: Any = None
+    dev: Any = None
+    m: Any
+    x: Any
+    y: Any
+    while v:
+        m = Pattern.QUALIFIERS.leftbound.search(v)
+        v = v[m.end() :]
+        if m.group("N"):
+            post = m.group("N")
+            continue
+        x = m.group("l")
+        y = m.group("n")
+        if x == "dev":
+            dev = y
+            continue
+        if x in ("post", "r", "rev"):
+            post = y
+            continue
+        prephase = x
+        presubphase = y
+    return [[prephase, presubphase], post, dev]
+
+
+parse_pre: Digest = Digest("parse_pre")
+
+
+@parse_pre.overload()
+def parse_pre() -> list:
+    return [None, None]
+
+
+@parse_pre.overload(list)
+def parse_pre(value: list) -> Any:
+    l: Any
+    n: Any
+    l, n = list(map(utils.segment, value))
+    if [l, n] == [None, None]:
+        return [None, None]
+    l = Cfg.cfg.data["phases"][l]
+    if not isinstance(n, int):
+        raise TypeError
+    return [l, n]
+
+
+@parse_pre.overload(str)
+def parse_pre(value: str) -> list:
+    if value == "":
+        return [None, None]
+    v: str = value
+    v = v.replace("_", ".")
+    v = v.replace("-", ".")
+    m: Any = Pattern.PARSER.bound.search(v)
+    l: Any
+    n: Any
+    l, n = m.groups()
+    l = Cfg.cfg.data["phases"][l]
+    n = 0 if (n is None) else int(n)
+    return [l, n]
+
+
+parse_dev: Digest = Digest("parse_dev")
+
+
+@parse_dev.overload()
+def parse_dev() -> None:
+    return
+
+
+@parse_dev.overload(int)
+def parse_dev(value: int) -> int:
+    if value < 0:
+        raise ValueError
+    return value
+
+
+@parse_dev.overload(list)
+def parse_dev(value: list) -> Optional[int]:
+    v: list = list(map(utils.segment, value))
+    if len(v) != 2:
+        raise ValueError
+    if v[0] != "dev":
+        raise ValueError
+    if isinstance(v[1], str):
+        raise ValueError
+    return v[1]
+
+
+@parse_dev.overload(str)
+def parse_dev(value: str) -> Optional[int]:
+    v: str = value
+    v = v.replace("_", ".")
+    v = v.replace("-", ".")
+    m: Any = Pattern.PARSER.bound.search(v)
+    x: Any
+    y: Any
+    x, y = m.groups()
+    if x not in (None, "dev"):
+        raise ValueError
+    if y is not None:
+        return int(y)
+
+
+parse_post: Digest = Digest("parse_post")
+
+
+@parse_post.overload()
+def parse_post() -> None:
+    return
+
+
+@parse_post.overload(int)
+def parse_post(value: int) -> int:
+    if value < 0:
+        raise ValueError
+    return value
+
+
+@parse_post.overload(list)
+def parse_post(value: list) -> Optional[int]:
+    v: list = list(map(utils.segment, value))
+    if len(v) == 0:
+        raise ValueError
+    if len(v) > 2:
+        raise ValueError
+    if len(v) == 1:
+        v.insert(0, "")
+    if v[0] not in ("post", "rev", "r", ""):
+        raise ValueError
+    if isinstance(v[1], str):
+        raise TypeError
+    return v[1]
+
+
+@parse_post.overload(str)
+def parse_post(value: str) -> Optional[int]:
+    v: str = value
+    v = v.replace("_", ".")
+    v = v.replace("-", ".")
+    m: Any = Pattern.PARSER.bound.search(v)
+    x: Any
+    y: Any
+    x, y = m.groups()
+    if x not in (None, "post", "rev", "r"):
+        raise ValueError
+    if y is None:
+        return None
+    else:
+        return int(y)
