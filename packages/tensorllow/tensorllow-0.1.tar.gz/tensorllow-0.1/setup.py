156001439f914cf4740d71aae9188fed5a4a4c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 04:06:57 +0000

    Add package tensorllow/0.1/tensorllow-0.1.tar.gz

---
+++ packages/tensorllow/tensorllow-0.1.tar.gz/tensorllow-0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name='tensorllow',
+ version='0.1',
+ author="mark",
+ author_email="tensorllow@dev.org",
+ description="Tensorllow is an open source machine learning framework for everyone.",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
