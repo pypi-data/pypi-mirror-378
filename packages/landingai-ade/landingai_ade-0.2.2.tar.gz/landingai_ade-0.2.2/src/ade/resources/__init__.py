a6d830ecdd7e31bd65a2f9cc76e7e97f28341449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 15:06:37 +0000

    Add package landingai-ade/0.2.2/landingai_ade-0.2.2.tar.gz

---
+++ packages/landingai-ade/landingai_ade-0.2.2.tar.gz/landingai_ade-0.2.2/src/ade/resources/__init__.py
@@ -0,0 +1,19 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .ade import (
+    AdeResource,
+    AsyncAdeResource,
+    AdeResourceWithRawResponse,
+    AsyncAdeResourceWithRawResponse,
+    AdeResourceWithStreamingResponse,
+    AsyncAdeResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "AdeResource",
+    "AsyncAdeResource",
+    "AdeResourceWithRawResponse",
+    "AsyncAdeResourceWithRawResponse",
+    "AdeResourceWithStreamingResponse",
+    "AsyncAdeResourceWithStreamingResponse",
+]
