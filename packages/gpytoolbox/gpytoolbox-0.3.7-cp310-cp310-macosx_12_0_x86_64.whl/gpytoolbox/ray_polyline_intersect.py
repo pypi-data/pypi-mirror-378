4c49852b63c3e101ace22b26d1cde80e5b2b95b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 05:07:18 +0000

    Add package gpytoolbox/0.3.7/gpytoolbox-0.3.7-cp310-cp310-macosx_12_0_x86_64.whl

---
+++ packages/gpytoolbox/gpytoolbox-0.3.7-cp310-cp310-macosx_12_0_x86_64.whl/gpytoolbox/ray_polyline_intersect.py
@@ -0,0 +1,112 @@
+import numpy as np
+from numpy.linalg import solve
+from .edge_indices import edge_indices
+
+def ray_polyline_intersect(position, direction, polyline_vertices, max_distance = 100.0, EC=None):
+    """Shoot a ray in 2D from a position and see where it crashes into a given polyline
+
+    Calculates the intersection point between a ray and a given polyline in 2D
+
+    Parameters
+    ----------
+    position : numpy double array
+        Vector of camera position coordinates
+    direction : numpy double array
+        Vector of camera direction 
+    polyline_vertices : numpy double array
+        Matrix of polyline vertex coordinates
+    max_distance : double, optional (default 100.0)
+        Maximum ray distance to consider
+    EC : numpy int array, optional (default None)
+        Matrix of edge indices into polyline_vertices
+
+    Returns
+    -------
+    x : numpy double array
+        Vector of intersection point coordinates (np.inf if no intersection)
+    n : numpy double array
+        Vector of polyline normal at intersection (zeros if no intersection)
+    ind : int 
+        Index into EC of intersection edge (-1 if no intersection)
+
+    See Also
+    --------
+    test_ray_mesh_intersect.
+
+    Notes
+    -----
+    This does a for loop on all the edges of the polyline, suffering performance.
+
+    Examples
+    --------
+    ```python
+    # Build a polyline; for example, a square
+    V = np.array([ [-1.0, -1.0], [-1.0, 1.0], [1.0, 1.0], [1.0, -1.0] ])
+    # Camera position and direction
+    cam_pos = np.array([-0.5,-1.5])
+    cam_dir = np.array([0.0,1.0])
+    # Looking upwards: intersection should be [-0.5,-1.0], normal downwards
+    x, n, ind = gpytoolbox.ray_polyline_intersect(cam_pos,cam_dir,V)
+    ```
+    """
+
+    if (EC is None):
+        EC = edge_indices(polyline_vertices.shape[0],closed=True)
+    ind = -1
+    x = np.array([np.inf, np.inf])
+    n = np.array([0.0, 0.0])
+    distance_to_hit = max_distance 
+    for i in range(0,EC.shape[0]):
+        # Two endpoints of segment in polyline
+        a0 = polyline_vertices[EC[i,0],:]
+        a1 = polyline_vertices[EC[i,1],:]
+        # Linear system will have this determinant
+        det = direction[0]*(a1[1]- a0[1]) - direction[1]*(a1[0]- a0[0])
+        if np.abs(det)<1e-8:
+            # Parallel. Find out if intersecting (coincident) or not
+            # Plug one of the segment endpoints into our ray equation
+            eq0 = direction[1]*(a0[0] - position[0]) - direction[0]*(a0[1] - position[1])
+            if np.abs(eq0)<1e-8: # Coincident
+                # Is it behind us?
+                sign0 = np.dot(a0 - position,direction)
+                sign1 = np.dot(a1 - position,direction)
+                # How far are they?
+                t0 = np.linalg.norm(a0 - position)/np.linalg.norm(direction)
+                t1 = np.linalg.norm(a0 - position)/np.linalg.norm(direction)
+                hit = -1
+                if sign0>0 and sign1>0:
+                    if t0<t1:
+                        hit = 0
+                    else:
+                        hit = 1
+                elif sign0>0:
+                    hit = 0
+                elif sign1>0:
+                    hit = 1
+                # Assign
+                if hit==0:
+                    distance_to_hit = t0
+                    x = position + t0*direction
+                    ind = i
+                    n = np.array([-(a1[1] - a0[1]),a1[0] - a0[0]])
+                    # This isn't robust but honestly just don't repeat points in the polyline!
+                    n = n/np.linalg.norm(n)
+                elif hit==1:
+                    distance_to_hit = t1
+                    x = position + t1*direction
+                    ind = i
+                    n = np.array([-(a1[1] - a0[1]),a1[0] - a0[0]])
+                    # This isn't robust but honestly just don't repeat points in the polyline!
+                    n = n/np.linalg.norm(n)
+        else: # Not parallel, there must be an intersection (even if outside segment bounds)
+            A = np.array( [ [ direction[0] , a1[0] - a0[0] ] , [direction[1] , a1[1]- a0[1] ] ])
+            b = np.reshape(a1 - position,(2,1))
+            t = solve(A,b)
+            if t[1]>0.0 and t[1]<1.0 and t[0]>0 and t[0]<distance_to_hit:
+                distance_to_hit = t[0]
+                x = position + t[0]*direction
+                ind = i
+                n = np.array([-(a1[1] - a0[1]),a1[0] - a0[0]])
+                # This isn't robust but honestly just don't repeat points in the polyline!
+                n = n/np.linalg.norm(n)
+    return x, n, ind
---
+++ packages/gpytoolbox/gpytoolbox-0.3.7-cp310-cp310-macosx_12_0_x86_64.whl/gpytoolbox/triangle_triangle_adjacency.py
@@ -0,0 +1,53 @@
+import numpy as np
+from gpytoolbox.halfedges import halfedges
+from gpytoolbox.array_correspondence import array_correspondence
+
+def triangle_triangle_adjacency(F):
+    """Given a manifold triangle mesh with face indices F, computes adjacency
+    info between triangles
+    
+    The ordering convention for halfedges is the following:
+    [halfedge opposite vertex 0,
+     halfedge opposite vertex 1,
+     halfedge opposite vertex 2]
+
+    Parameters
+    ----------
+    F : (m,3) numpy int array
+        face index list of a triangle mesh
+
+    Returns
+    -------
+    TT : (m,3) numpy int array
+        Index list specifying which face j is adjacent to which face i across
+        the respective halfedge in position (i,j).
+        If there is no adjacent face (boundary halfedge), the entry is -1.
+    TTi : (m,3) numpy int array
+        Index list specifying which halfedge of face j (0,1,2) is adjacent to i
+        in position (i,j).
+
+    Examples
+    --------
+    ```python
+    from gpytoolbox import regular_square_mesh, triangle_triangle_adjacency
+    v, f = regular_square_mesh(10)
+    TT, TTi = triangle_triangle_adjacency(f)
+    ```
+    
+    """
+    
+    m = F.shape[0]
+    assert m > 0
+    assert F.shape[1] == 3
+
+    he = halfedges(F)
+    he_flat = np.concatenate((he[:,0,:], he[:,1,:], he[:,2,:]), axis=0)
+    he_flip_flat = np.flip(he_flat, axis=-1)
+
+    map_to_flip = array_correspondence(he_flat,he_flip_flat,axis=0)
+    TT = np.reshape(np.where(map_to_flip<0, -1, map_to_flip % m), F.shape, order='F')
+    TTi = np.reshape(np.where(map_to_flip<0, -1, map_to_flip // m), F.shape, order='F')
+
+    return TT, TTi
+
+
