f12c550b83a26b2ef1f541375ce69f3815894468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 15:06:46 +0000

    Add package digiflow/6.4.16/digiflow-6.4.16-py3-none-any.whl

---
+++ packages/digiflow/digiflow-6.4.16-py3-none-any.whl/digiflow/validate/common.py
@@ -0,0 +1,43 @@
+"""Common Validation API"""
+
+from abc import (
+    abstractmethod,
+)
+from dataclasses import (
+    dataclass,
+)
+from typing import (
+    List,
+)
+
+
+UNSET_NUMBR = -1
+INVALID_LABEL_UNSET = 'INVALID_UNSET'
+INVALID_LABEL_RANGE = 'INVALID_RANGE'
+INVALID_LABEL_TYPE = 'INVALID_TYPE'
+
+
+@dataclass
+class Invalid:
+    """Container for invalid data"""
+
+    label: str
+    location: str
+    info: str
+
+
+class Validator:
+    """Common Base Interface"""
+
+    def __init__(self, label: str, input_data):
+        self.label = label
+        self.input_data = input_data
+        self.invalids: List[Invalid] = []
+
+    @abstractmethod
+    def valid(self) -> bool:
+        """Specific implementation is subject 
+        of concrete Validator Implementation
+        """
+
+        return len(self.invalids) == 0
