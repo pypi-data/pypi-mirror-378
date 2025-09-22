666cc4a0e8c8a20aca657adca287031d87f1b6f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 08:05:27 +0000

    Add package techinrealestate/1.1.2/techinrealestate-1.1.2.tar.gz

---
+++ packages/techinrealestate/techinrealestate-1.1.2.tar.gz/techinrealestate-1.1.2/techinrealestate/apimaker/__init__.py
@@ -0,0 +1,11 @@
+"""
+Apimaker API Services
+
+Future integration for Apimaker API services.
+This module will contain Apimaker-specific implementations.
+"""
+
+# Future imports will go here
+# from .zillow import get_zillow_listings, get_property_details
+
+__all__ = []
