ba77cd82275a68237f6e0b26bf16b99cc7ecb748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 14:05:56 +0000

    Add package netbox-validity/3.3.0/netbox_validity-3.3.0-py3-none-any.whl

---
+++ packages/netbox-validity/netbox_validity-3.3.0-py3-none-any.whl/validity/static/validity/connection-type-select.js
@@ -0,0 +1,24 @@
+function fillTextArea(public_creds, private_creds) {
+    document.getElementById('id_public_credentials').value = JSON.stringify(public_creds, null, 2);
+    document.getElementById('id_private_credentials').value = JSON.stringify(private_creds, null, 2);
+}
+
+function fillCredentials(valueExtracter, connectionTypeInfo) {
+    try {
+        const connectionType = valueExtracter(connectionTypeInfo)
+        const defaultCredentials = JSON.parse(document.getElementById('default_credentials').textContent)[connectionType];
+        if (defaultCredentials !== undefined) {
+            fillTextArea(defaultCredentials.public, defaultCredentials.private);
+        }
+    } catch(e) {
+        console.log(e.name, e.message)
+    }
+}
+
+window.onload = () => {
+    const select = document.getElementById('connection_type_select');
+    if ("tomselect" in select) // NetBox 4.x
+        select.tomselect.on("change", fillCredentials.bind(undefined, (value) => value))
+    else // NetBox 3.x
+        select.slim.onChange = fillCredentials.bind(undefined, (value) => value.value)
+}
