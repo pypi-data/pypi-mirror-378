fef45758d15553b0cdc8936800672ae7bd5df668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 04:06:54 +0000

    Add package pysnowball/0.1.8/pysnowball-0.1.8-py3-none-any.whl

---
+++ packages/pysnowball/pysnowball-0.1.8-py3-none-any.whl/pysnowball/cube.py
@@ -0,0 +1,22 @@
+from pysnowball import api_ref
+from pysnowball import utls
+
+host = "xueqiu.com"
+
+def nav_daily(symbol):
+    url = api_ref.nav_daily + symbol
+    return utls.fetch(url, host)
+
+def rebalancing_history(symbol, count=20, page=1):
+    url = api_ref.rebalancing_history + symbol
+    url = url + "&count=" + str(count)
+    url = url + "&page=" + str(page)
+    return utls.fetch(url, host)
+
+def rebalancing_current(symbol):
+    url = api_ref.rebalancing_current + symbol
+    return utls.fetch(url, host)
+
+def quote_current(symbol):
+    url = api_ref.quote_current + symbol
+    return utls.fetch(url, host)
