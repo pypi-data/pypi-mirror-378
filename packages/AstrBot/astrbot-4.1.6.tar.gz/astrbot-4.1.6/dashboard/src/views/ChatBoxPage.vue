cc744e01e0626dc623cbe63ead5656c685025308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 16:05:35 +0000

    Add package AstrBot/4.1.6/astrbot-4.1.6.tar.gz

---
+++ packages/AstrBot/astrbot-4.1.6.tar.gz/astrbot-4.1.6/dashboard/src/views/ChatBoxPage.vue
@@ -0,0 +1,41 @@
+<script setup>
+import ChatPage from './ChatPage.vue';
+import { useCustomizerStore } from '@/stores/customizer';
+const customizer = useCustomizerStore();
+</script>
+
+<template>
+    <v-app :theme="customizer.uiTheme" style="height: 100%; width: 100%;">
+        <div
+            style="height: 100%; width: 100%; display: flex; flex-direction: column; align-items: center; justify-content: center;">
+            <div id="container">
+                <ChatPage :chatbox-mode="true"></ChatPage>
+            </div>
+        </div>
+    </v-app>
+</template>
+
+<style scoped>
+#container {
+    width: 100%;
+    height: 100%;
+}
+
+@media (min-width: 768px) {
+    #container {
+        min-width: 600px;
+        min-height: 370px;
+        max-width: 1100px;
+        max-height: 860px;
+        padding: 36px;
+    }
+}
+
+@media (max-width: 767px) {
+    #container {
+        width: 100%;
+        height: 100%;
+        padding: 0;
+    }
+}
+</style>
