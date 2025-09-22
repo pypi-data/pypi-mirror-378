683109fa00746432fdd122340946a367a3c00370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 05:07:02 +0000

    Add package django-admin-reset/0.7.2/django_admin_reset-0.7.2-py3-none-any.whl

---
+++ packages/django-admin-reset/django_admin_reset-0.7.2-py3-none-any.whl/django_admin_reset/tests/settings.py
@@ -0,0 +1,45 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3'
+    }
+}
+INSTALLED_APPS = [
+    'django_admin_reset',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles'
+]
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+# Fastest password hasher (to speed up tests) from Django 5.1 onward
+PASSWORD_HASHERS = ['django.contrib.auth.hashers.MD5PasswordHasher']
+SECRET_KEY = '!!!INSECURE!!! 2OQcxcgohdo= !!!INSECURE!!!'
+ROOT_URLCONF = 'django_admin_reset.tests.urls'
+STATIC_URL = '/static/'
+USE_TZ = True
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
