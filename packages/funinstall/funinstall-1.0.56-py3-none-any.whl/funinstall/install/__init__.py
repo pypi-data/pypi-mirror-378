17fe0ea432bcea1b7472c5ff29e3916dd731fc5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 15:06:51 +0000

    Add package funinstall/1.0.56/funinstall-1.0.56-py3-none-any.whl

---
+++ packages/funinstall/funinstall-1.0.56-py3-none-any.whl/funinstall/install/__init__.py
@@ -0,0 +1,29 @@
+from .ossutil import OSSUtilInstall
+from .command import app as install_app
+from .command import (
+    install_brew,
+    install_code_server,
+    install_frpc,
+    install_go,
+    install_newapi,
+    install_nodejs,
+    install_onehub,
+    install_ossutil,
+    install_uif,
+    install_v2rayA,
+)
+
+__all__ = [
+    "OSSUtilInstall",
+    "install_app",
+    "install_brew",
+    "install_code_server",
+    "install_frpc",
+    "install_go",
+    "install_newapi",
+    "install_nodejs",
+    "install_onehub",
+    "install_ossutil",
+    "install_uif",
+    "install_v2rayA",
+]
