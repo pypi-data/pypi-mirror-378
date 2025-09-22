aa0021b2d12e521802d423e7254ce2cccb19fea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-22 16:05:39 +0000

    Add package pathling/8.1.0/pathling-8.1.0-py2.py3-none-any.whl

---
+++ packages/pathling/pathling-8.1.0-py2.py3-none-any.whl/pathling/_version.py
@@ -0,0 +1,9 @@
+#
+# Auto generated from POM project version.
+# Please do not modify.
+#
+__version__="8.1.0"
+__java_version__="8.1.0"
+__scala_version__="2.12"
+__delta_version__="3.3.2"
+__hadoop_version__="3.3.4"
