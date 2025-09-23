758e9a3e0c113f94e28f6c20f40da7739ceef43f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 07:06:35 +0000

    Add package tap-google-play/0.6.1/tap_google_play-0.6.1.tar.gz

---
+++ packages/tap-google-play/tap_google_play-0.6.1.tar.gz/tap_google_play-0.6.1/noxfile.py
@@ -0,0 +1,37 @@
+"""Nox configuration."""
+
+from __future__ import annotations
+
+import nox
+
+python_versions = [
+    "3.14",
+    "3.13",
+    "3.12",
+    "3.11",
+    "3.10",
+]
+nox.needs_version = ">=2024.4.15"
+nox.options.default_venv_backend = "uv"
+nox.options.sessions = ("tests",)
+
+
+@nox.session(python=python_versions)
+def tests(session: nox.Session) -> None:
+    """Execute pytest tests."""
+    session.run(
+        "uv",
+        "run",
+        "--verbose",
+        "--python",
+        f"python{session.python}",
+        "pytest",
+        *session.posargs,
+    )
+
+
+@nox.session
+def mypy(session: nox.Session) -> None:
+    """Check types."""
+    args = session.posargs or ("tap_google_play",)
+    session.run("uv", "run", "mypy", *args)
