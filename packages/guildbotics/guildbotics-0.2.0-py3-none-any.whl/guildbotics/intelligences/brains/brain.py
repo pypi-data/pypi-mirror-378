5301918569ac2da0a17b68ae321ce764ab7b800c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 04:05:27 +0000

    Add package guildbotics/0.2.0/guildbotics-0.2.0-py3-none-any.whl

---
+++ packages/guildbotics/guildbotics-0.2.0-py3-none-any.whl/guildbotics/intelligences/brains/brain.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+from logging import Logger
+from typing import Type
+
+from pydantic import BaseModel
+
+
+class Brain(ABC):
+
+    def __init__(
+        self,
+        person_id: str,
+        name: str,
+        logger: Logger,
+        description: str = "",
+        response_class: Type[BaseModel] | None = None,
+    ):
+        """
+        Initialize the Intelligence.
+        Args:
+            person_id (str): ID of the person using the intelligence.
+            name (str): Name of the intelligence.
+            logger (Logger): Logger instance for logging.
+            description (str): Description of the intelligence.
+            response_class (Type[BaseModel] | None): Class for the response model.
+        """
+        self.person_id = person_id
+        self.name = name
+        self.logger = logger
+        self.description = description
+        self.response_class = response_class
+
+    @abstractmethod
+    async def run(self, message: str, **kwargs):
+        pass
