72751a73a874f2d32b83e28b25235a4d65240874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 06:06:24 +0000

    Add package piper/0.14.5/piper-0.14.5.tar.gz

---
+++ packages/piper/piper-0.14.5.tar.gz/piper-0.14.5/tests/test_packaging.py
@@ -0,0 +1,29 @@
+"""Validate what's available directly on the top-level import."""
+
+from inspect import isfunction
+
+import pytest
+
+__author__ = "Vince Reuter"
+__email__ = "vreuter@virginia.edu"
+
+
+@pytest.mark.parametrize(
+    ["obj_name", "typecheck"],
+    [
+        ("add_logging_options", isfunction),
+        ("check_all_commands", isfunction),
+        ("determine_uncallable", isfunction),
+        ("logger_via_cli", isfunction),
+    ],
+)
+def test_top_level_exports(obj_name, typecheck):
+    """At package level, validate object availability and type."""
+    import pypiper
+
+    try:
+        obj = getattr(pypiper, obj_name)
+    except AttributeError:
+        pytest.fail("Unavailable on {}: {}".format(pypiper.__name__, obj_name))
+    else:
+        assert typecheck(obj)
