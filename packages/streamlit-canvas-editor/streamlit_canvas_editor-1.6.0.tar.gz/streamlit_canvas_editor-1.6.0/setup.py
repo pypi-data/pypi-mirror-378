0ab91d56f1c0757bb46b0d6ccc32041b8c0793a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 16:06:28 +0000

    Add package streamlit-canvas-editor/1.6.0/streamlit_canvas_editor-1.6.0.tar.gz

---
+++ packages/streamlit-canvas-editor/streamlit_canvas_editor-1.6.0.tar.gz/streamlit_canvas_editor-1.6.0/setup.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import setuptools
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="streamlit_canvas_editor",
+    version="1.6.0",
+    author="Adrien Lefèvre",
+    author_email="adrien.lefevre@etas.com",
+    description="Streamlit component that allows you to do X",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.7",
+    install_requires=["streamlit>=1.2", "jinja2"],
+)
