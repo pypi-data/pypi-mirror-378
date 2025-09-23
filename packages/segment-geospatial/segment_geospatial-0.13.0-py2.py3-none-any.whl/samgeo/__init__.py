af458a6f520396b3ad74766384b418a76c653853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 06:06:40 +0000

    Add package segment-geospatial/0.13.0/segment_geospatial-0.13.0-py2.py3-none-any.whl

---
+++ packages/segment-geospatial/segment_geospatial-0.13.0-py2.py3-none-any.whl/samgeo/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for segment-geospatial."""
+
+__author__ = """Qiusheng Wu"""
+__email__ = "giswqs@gmail.com"
+__version__ = "0.13.0"
+
+
+from .samgeo import *  # noqa: F403
+from .samgeo2 import *  # noqa: F403
