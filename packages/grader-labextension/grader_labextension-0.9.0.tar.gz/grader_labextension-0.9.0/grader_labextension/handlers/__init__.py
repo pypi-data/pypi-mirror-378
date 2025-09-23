37187d0b8b2d7124b7d8fd3d55ad72f75470f2c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 03:16:51 +0000

    Add package grader-labextension/0.9.0/grader_labextension-0.9.0.tar.gz

---
+++ packages/grader-labextension/grader_labextension-0.9.0.tar.gz/grader_labextension-0.9.0/grader_labextension/handlers/__init__.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2022, TU Wien
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from grader_labextension.handlers import (
+    assignment,
+    base_handler,
+    grading,
+    lectures,
+    permission,
+    submissions,
+    version_control,
+)
+
+__all__ = [
+    "assignment",
+    "grading",
+    "lectures",
+    "submissions",
+    "permission",
+    "version_control",
+    "base_handler",
+]
