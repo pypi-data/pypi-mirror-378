dc79e58969453f68a6d467c4aec6b6b3d7d50f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 15:05:44 +0000

    Add package abhi77/0.5/abhi77-0.5.tar.gz

---
+++ packages/abhi77/abhi77-0.5.tar.gz/abhi77-0.5/abhi/__init__.py
@@ -0,0 +1,4 @@
+from .flutter_topics import topics
+from .flutter_codes import get_code
+
+__all__ = ["topics", "get_code"]
---
+++ packages/abhi77/abhi77-0.5.tar.gz/abhi77-0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="abhi77",  
+    version="0.5",  
+    packages=find_packages(),
+    install_requires=[],
+    description="Flutter syllabus and example codes package",
+    author="Abhishek Yadav",
+    license="MIT",
+)
