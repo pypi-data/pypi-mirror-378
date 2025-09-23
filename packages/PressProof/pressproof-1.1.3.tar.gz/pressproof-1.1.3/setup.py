23e1e90763ebef3784e4d05c26e62ff1884661a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 07:07:27 +0000

    Add package PressProof/1.1.3/pressproof-1.1.3.tar.gz

---
+++ packages/PressProof/pressproof-1.1.3.tar.gz/pressproof-1.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f: 
+    description = f.read()
+    description.replace("assets/example.png", "https://github.com/Slynyr/PressProof/raw/main/assets/example.png")
+
+setup(
+    name="PressProof",
+    version="1.1.3", 
+    packages=find_packages(),
+    install_requires=["colorama", "openai", "beautifulsoup4", "requests"],
+    entry_points={
+        "console_scripts": [
+            "pressproof = pressproof.__main__:mainEntryPoint"
+        ]
+    }, 
+    long_description=description,
+    long_description_content_type="text/markdown"
+)
