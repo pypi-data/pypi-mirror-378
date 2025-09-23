743ab787322fe73cc900e666753fdbad9fa5372d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 14:06:22 +0000

    Add package je-auto-control/0.0.172/je_auto_control-0.0.172.tar.gz

---
+++ packages/je-auto-control/je_auto_control-0.0.172.tar.gz/je_auto_control-0.0.172/je_auto_control/windows/core/utils/win32_keypress_check.py
@@ -0,0 +1,19 @@
+import sys
+
+from je_auto_control.utils.exception.exception_tags import windows_import_error
+from je_auto_control.utils.exception.exceptions import AutoControlException
+
+if sys.platform not in ["win32", "cygwin", "msys"]:
+    raise AutoControlException(windows_import_error)
+
+import ctypes
+
+
+def check_key_is_press(keycode: [int, str]) -> bool:
+    if isinstance(keycode, int):
+        temp: int = ctypes.windll.user32.GetAsyncKeyState(keycode)
+    else:
+        temp = ctypes.windll.user32.GetAsyncKeyState(ord(keycode))
+    if temp > 1:
+        return True
+    return False
