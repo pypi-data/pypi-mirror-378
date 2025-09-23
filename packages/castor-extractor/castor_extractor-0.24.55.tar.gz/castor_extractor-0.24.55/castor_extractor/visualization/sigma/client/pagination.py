e1a1771524fd423136bd3498ac1f157a2e2726c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 03:18:04 +0000

    Add package castor-extractor/0.24.55/castor_extractor-0.24.55.tar.gz

---
+++ packages/castor-extractor/castor_extractor-0.24.55.tar.gz/castor_extractor-0.24.55/castor_extractor/visualization/sigma/client/pagination.py
@@ -0,0 +1,29 @@
+from typing import Optional
+
+from pydantic import ConfigDict, Field
+from pydantic.alias_generators import to_camel
+
+from ....utils import PaginationModel
+
+SIGMA_API_LIMIT = 1_000  # default number of records per page
+SIGMA_QUERIES_PAGINATION_LIMIT = 50
+
+
+class SigmaPagination(PaginationModel):
+    next_page: Optional[str] = "0"
+    entries: list = Field(default_factory=list)
+
+    model_config = ConfigDict(
+        alias_generator=to_camel,
+        populate_by_name=True,
+        from_attributes=True,
+    )
+
+    def is_last(self) -> bool:
+        return self.next_page is None
+
+    def next_page_payload(self) -> dict:
+        return {"page": self.next_page}
+
+    def page_results(self) -> list:
+        return self.entries
