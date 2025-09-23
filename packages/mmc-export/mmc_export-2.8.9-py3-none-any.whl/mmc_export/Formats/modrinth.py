57ec99ad5b106b925c585aa999e451237916516b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 16:06:00 +0000

    Add package mmc-export/2.8.9/mmc_export-2.8.9-py3-none-any.whl

---
+++ packages/mmc-export/mmc_export-2.8.9-py3-none-any.whl/mmc_export/Formats/modrinth.py
@@ -0,0 +1,120 @@
+from pathlib import Path
+
+from ..Helpers.structures import File, Intermediate, Resource, Writer
+from ..Helpers.utils import get_name_from_scheme
+
+
+class Modrinth(Writer):
+
+    def __init__(self, path: Path, intermediate: Intermediate) -> None:
+
+        self.index = dict()
+
+        super().__init__(path, intermediate)
+
+    def add_resource(self, resource: Resource) -> None:
+        
+        relative_path = Path(resource.file.relativePath) / resource.file.name
+
+        data = {
+            "path": relative_path.as_posix(),
+            "hashes": {"sha1": resource.file.hash.sha1, "sha512": resource.file.hash.sha512},
+            "downloads": sorted(provider.url for provider in resource.providers.values()),
+            "fileSize": resource.file.size
+        }
+
+        if resource.optional: data['env'] = {"client": "optional", "server": "optional"}
+
+        self.index['files'].append(data)
+
+    def add_override(self, file: File) -> None:
+
+        overrides_dir = self.temp_dir / "overrides"
+        overrides_dir.mkdir(parents=True, exist_ok=True)
+
+        file_path = overrides_dir / file.relativePath
+        file_path.mkdir(parents=True, exist_ok=True)
+        if file.disabled: file_path = file_path / (file.name + ".disabled")
+
+        from shutil import copy2 as copy_file
+        copy_file(file.path, file_path)
+
+    def write_index(self) -> None:
+        
+        match self.intermediate.modloader.type:
+            case "fabric": modloader = "fabric-loader"
+            case "quilt": modloader = "quilt-loader"
+            case _: modloader = self.intermediate.modloader.type
+        
+        self.index = {
+            "formatVersion": 1,
+            "game": "minecraft",
+
+            "versionId": self.intermediate.version,
+            "name": self.intermediate.name,
+            "summary": self.intermediate.description,
+
+            "files": [],
+
+            "dependencies": {
+                "minecraft": self.intermediate.minecraft_version,
+                modloader: self.intermediate.modloader.version
+            }
+        }
+
+    def print_bundled(self, bundled_files: list[Resource]) -> None: 
+
+        gh_links = []
+        other_links = []
+
+        for resource in bundled_files:
+
+            if resource.links:
+                link = sorted(set(resource.links), reverse=True)[-1]
+                md_link = f"* [{resource.name}]({link})"
+            else: md_link = f"* {resource.name} (link unknown)"; link = ""
+
+            if link.startswith("https://github.com"): 
+                gh_links.append(md_link)
+            else: other_links.append(md_link)
+
+        newline = '\n'
+        message = "Sources for bundled mods:\n\n"
+        if gh_links: message +=f"GitHub links\n{newline.join(gh_links)}\n"
+        if other_links: message +=f"\nOther links\n{newline.join(other_links)}\n"
+
+        message +="\nAlways check the licenses to see if they allow distribution!"
+
+        md_file = self.modpack_path / "bundled_links.md"
+        if gh_links or other_links: md_file.write_text(message)
+
+    def write(self) -> None:
+
+        self.write_index()
+
+        for override in self.intermediate.overrides:
+            self.add_override(override)
+
+        bundled_files: list[Resource] = list()
+        from copy import deepcopy as copy_object
+
+        for resource in self.intermediate.resources:
+            resource_copy = copy_object(resource)
+            resource_copy.providers.pop('CurseForge', None)
+
+            if not resource_copy.providers:
+                self.add_override(resource.file)
+                bundled_files.append(resource)
+            else: self.add_resource(resource_copy)
+
+        self.print_bundled(bundled_files)
+
+        from json import dump as write_json
+        with open(self.temp_dir / "modrinth.index.json", 'w') as file:
+            write_json(self.index, file, indent=4)
+
+        from shutil import make_archive
+        name = get_name_from_scheme("MR", "Modrinth", self.intermediate)
+        archive_path = self.modpack_path.joinpath("__tmp__." + name).as_posix()
+        archive = Path(make_archive(archive_path, 'zip', self.temp_dir, '.'))
+        archive.replace(archive.with_name(name + ".mrpack"))
---
+++ packages/mmc-export/mmc_export-2.8.9-py3-none-any.whl/mmc_export/Helpers/structures.py
@@ -0,0 +1,93 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Literal
+
+
+@dataclass
+class File:
+
+    name: str = field(default_factory=str)
+
+    @dataclass
+    class Hash:
+        sha1: str = field(default_factory=str)
+        sha256: str = field(default_factory=str)
+        sha512: str = field(default_factory=str)
+        murmur2: str = field(default_factory=str)
+
+    hash: Hash = field(default_factory=Hash)
+    size: int = field(default_factory=int)
+
+    path: Path = field(default_factory=Path)
+    relativePath: str = field(default_factory=str)
+    disabled: bool = False
+
+
+@dataclass
+class Resource:
+
+    "Represents downloadable item i.e. mod, resourcepack, shaderpack etc."
+
+    name: str = field(default_factory=str)
+    links: list[str] = field(default_factory=list)
+    optional: bool = False
+
+    @dataclass
+    class Provider:
+
+        ID: str | int | None = None
+        fileID: str | int | None = None
+        url: str = field(default_factory=str)
+
+        slug: str = field(default_factory=str)
+        author: str = field(default_factory=str)
+
+    file: File = field(default_factory=File)
+    providers: dict[Literal["Modrinth", "CurseForge", "Other"], Provider] = field(default_factory=dict)
+
+
+@dataclass
+class Intermediate:
+
+    name: str = field(default_factory=str)
+    author: str = field(default_factory=str)
+    version: str = field(default_factory=str)
+    description: str = field(default_factory=str)
+
+    @dataclass
+    class ModLoader:
+        type: str = field(default_factory=str)
+        version: str = field(default_factory=str)
+
+    minecraft_version: str = field(default_factory=str)
+    modloader: ModLoader = field(default_factory=ModLoader)
+
+    resources: list[Resource] = field(default_factory=list)
+    overrides: list[File] = field(default_factory=list)
+
+
+class Format(ABC):
+    def __init__(self, path: Path) -> None:
+
+        from tempfile import TemporaryDirectory
+
+        self._temp_dir = TemporaryDirectory()
+        self.temp_dir = Path(self._temp_dir.name)
+
+        self.modpack_path = path
+
+    def __del__(self) -> None:
+        self._temp_dir.cleanup()
+
+
+class Writer(Format):
+    def __init__(self, path: Path, intermediate: Intermediate) -> None:
+
+        self.intermediate = intermediate
+
+        super().__init__(path)
+
+    @abstractmethod
+    def write(self) -> None:
+        raise NotImplementedError()
