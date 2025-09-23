7da1cbc5e8ea20eca6cf9bfc28e356bb7baef68d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 05:05:31 +0000

    Add package sch-datascience-toolkit/0.0.1/sch_datascience_toolkit-0.0.1.tar.gz

---
+++ packages/sch-datascience-toolkit/sch_datascience_toolkit-0.0.1.tar.gz/sch_datascience_toolkit-0.0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='sch-datascience-toolkit',
+    version='0.0.1',
+    description='PyPi package created by Schibsted\'s Product & Application Security team.',
+    long_description='',
+    url='https://schibsted.com',
+    author='Alexander Kjäll',
+    author_email='alexander.kjaell@schibsted.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
