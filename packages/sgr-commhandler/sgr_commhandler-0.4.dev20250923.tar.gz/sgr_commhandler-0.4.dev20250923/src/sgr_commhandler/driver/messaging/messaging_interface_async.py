ef11afddaf6fc615008b1d6e3a79b0362f0c1c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 16:06:06 +0000

    Add package sgr-commhandler/0.4.dev20250923/sgr_commhandler-0.4.dev20250923.tar.gz

---
+++ packages/sgr-commhandler/sgr_commhandler-0.4.dev20250923.tar.gz/sgr_commhandler-0.4.dev20250923/src/sgr_commhandler/driver/messaging/messaging_interface_async.py
@@ -0,0 +1,206 @@
+import logging
+from collections.abc import Callable
+from typing import Any, Dict, Optional
+
+from sgr_specification.v0.generic import DataDirectionProduct, Units
+from sgr_specification.v0.product import (
+    DeviceFrame,
+)
+from sgr_specification.v0.product import (
+    MessagingDataPoint as MessagingDataPointSpec,
+)
+from sgr_specification.v0.product import (
+    MessagingFunctionalProfile as MessagingFunctionalProfileSpec,
+)
+from sgr_commhandler.api.data_point_api import (
+    DataPoint,
+    DataPointProtocol,
+)
+from sgr_commhandler.api.functional_profile_api import (
+    FunctionalProfile
+)
+from sgr_commhandler.api.device_api import (
+    SGrBaseInterface
+)
+from sgr_commhandler.validators import build_validator
+
+logger = logging.getLogger(__name__)
+
+
+def build_messaging_data_point(
+    data_point: MessagingDataPointSpec,
+    functional_profile: MessagingFunctionalProfileSpec,
+    interface: 'SGrMessagingInterface',
+) -> DataPoint:
+    """
+    Builds a data point of a messaging interface.
+    """
+    protocol = MessagingDataPoint(data_point, functional_profile, interface)
+    data_type = None
+    if data_point.data_point and data_point.data_point.data_type:
+        data_type = data_point.data_point.data_type
+    validator = build_validator(data_type)
+    return DataPoint(protocol, validator)
+
+
+class MessagingDataPoint(DataPointProtocol[MessagingDataPointSpec]):
+    """
+    Implements a data point of a messaging interface.
+    """
+
+    def __init__(
+        self,
+        dp_spec: MessagingDataPointSpec,
+        fp_spec: MessagingFunctionalProfileSpec,
+        interface: 'SGrMessagingInterface',
+    ):
+        self._dp_spec = dp_spec
+        self._fp_spec = fp_spec
+
+        dp_config = self._dp_spec.messaging_data_point_configuration
+        if not dp_config:
+            raise Exception('Messaging data point configuration missing')
+
+        self._fp_name = ''
+        if (
+            fp_spec.functional_profile is not None
+            and fp_spec.functional_profile.functional_profile_name is not None
+        ):
+            self._fp_name = fp_spec.functional_profile.functional_profile_name
+
+        self._dp_name = ''
+        if (
+            dp_spec.data_point is not None
+            and dp_spec.data_point.data_point_name is not None
+        ):
+            self._dp_name = dp_spec.data_point.data_point_name
+
+        self._interface = interface
+
+    def name(self) -> tuple[str, str]:
+        return self._fp_name, self._dp_name
+
+    def get_specification(self) -> MessagingDataPointSpec:
+        return self._dp_spec
+
+    async def get_val(self, parameters: Optional[dict[str, str]] = None, skip_cache: bool = False):
+        raise Exception('Not implemented')
+
+    async def set_val(self, value: Any):
+        raise Exception('Not implemented')
+
+    def direction(self) -> DataDirectionProduct:
+        if (
+            self._dp_spec.data_point is None
+            or self._dp_spec.data_point.data_direction is None
+        ):
+            raise Exception('missing data direction')
+        return self._dp_spec.data_point.data_direction
+
+    def unit(self) -> Units:
+        if (
+            self._dp_spec.data_point is None
+            or self._dp_spec.data_point.unit is None
+        ):
+            return Units.NONE
+        return self._dp_spec.data_point.unit
+
+    def can_subscribe(self) -> bool:
+        return True
+
+    def subscribe(self, fn: Callable[[Any], None]):
+        # TODO implement
+        raise Exception('not implemented yet')
+
+    def unsubscribe(self):
+        # TODO implement
+        raise Exception('not implemented yet')
+
+
+class MessagingFunctionalProfile(FunctionalProfile[MessagingFunctionalProfileSpec]):
+    """
+    Implements a functional profile of a messaging interface.
+    """
+
+    def __init__(
+        self,
+        fp_spec: MessagingFunctionalProfileSpec,
+        interface: 'SGrMessagingInterface',
+    ):
+        self._fp_spec = fp_spec
+        self._interface = interface
+
+        raw_dps = []
+        if (
+            self._fp_spec.data_point_list
+            and self._fp_spec.data_point_list.data_point_list_element
+        ):
+            raw_dps = self._fp_spec.data_point_list.data_point_list_element
+
+        dps = [
+            build_messaging_data_point(dp, self._fp_spec, self._interface)
+            for dp in raw_dps
+        ]
+
+        self._data_points = {dp.name(): dp for dp in dps}
+
+    def name(self) -> str:
+        if (
+            self._fp_spec.functional_profile
+            and self._fp_spec.functional_profile.functional_profile_name
+        ):
+            return self._fp_spec.functional_profile.functional_profile_name
+        return ''
+
+    def get_data_points(self) -> dict[tuple[str, str], DataPoint]:
+        return self._data_points
+
+    def get_specification(self) -> MessagingFunctionalProfileSpec:
+        return self._fp_spec
+
+
+class SGrMessagingInterface(SGrBaseInterface):
+    """
+    SmartGridready External Interface Class for Messaging Protocols
+    """
+
+    def __init__(
+        self, frame: DeviceFrame
+    ):
+        self._initialize_device(frame)
+
+        if (
+            self.device_frame.interface_list
+            and self.device_frame.interface_list
+            and self.device_frame.interface_list.messaging_interface
+        ):
+            self._raw_interface = (
+                self.device_frame.interface_list.messaging_interface
+            )
+        else:
+            raise Exception('No messaging interface')
+        desc = self._raw_interface.messaging_interface_description
+        if desc is None:
+            raise Exception('No messaging interface description')
+
+        # TODO configure interface
+
+        raw_fps = []
+        if (
+            self._raw_interface.functional_profile_list
+            and self._raw_interface.functional_profile_list.functional_profile_list_element
+        ):
+            raw_fps = self._raw_interface.functional_profile_list.functional_profile_list_element
+        fps = [MessagingFunctionalProfile(profile, self) for profile in raw_fps]
+        self.functional_profiles = {fp.name(): fp for fp in fps}
+
+    def is_connected(self):
+        return False
+
+    async def disconnect_async(self):
+        # TODO implement
+        pass
+
+    async def connect_async(self):
+        # TODO implement
+        pass
---
+++ packages/sgr-commhandler/sgr_commhandler-0.4.dev20250923.tar.gz/sgr_commhandler-0.4.dev20250923/src/sgr_commhandler/driver/rest/authentication.py
@@ -0,0 +1,205 @@
+import base64
+import json
+import logging
+from typing import Awaitable, Callable, TypeAlias
+
+import aiohttp
+import jmespath
+from aiohttp.client import ClientSession
+from jmespath.exceptions import JMESPathError
+from multidict import CIMultiDict
+from sgr_specification.v0.product import RestApiInterface
+from sgr_specification.v0.product.rest_api_types import (
+    RestApiAuthenticationMethod,
+)
+
+logger = logging.getLogger(__name__)
+
+Authenticator: TypeAlias = Callable[
+    [RestApiInterface, ClientSession], Awaitable[bool]
+]
+
+
+async def authenticate_not(
+    interface: RestApiInterface, session: ClientSession
+) -> bool:
+    """
+    Skips authentication.
+
+    Parameters
+    ----------
+    interface : RestApiInterface
+        the device interface
+    session : ClientSession
+        the REST client session
+    
+    Returns
+    -------
+    bool
+        True if authenticated, False otherwise
+    """
+
+    return True
+
+
+async def authenticate_with_bearer_token(
+    interface: RestApiInterface, session: ClientSession
+) -> bool:
+    """
+    Authenticates using Bearer token (JWT).
+
+    Parameters
+    ----------
+    interface : RestApiInterface
+        the device interface
+    session : ClientSession
+        the REST client session
+    
+    Returns
+    -------
+    bool
+        True if authenticated, False otherwise
+    """
+
+    try:
+        description = interface.rest_api_interface_description
+        if description is None:
+            raise Exception("no interface description")
+        base_url = description.rest_api_uri
+        if base_url is None:
+            raise Exception("no base URL")
+        bearer_option = description.rest_api_bearer
+        if bearer_option is None:
+            raise Exception("no Bearer option")
+        service_call = bearer_option.rest_api_service_call
+        if service_call is None:
+            raise Exception("no REST service call for authentication")
+        request_path = service_call.request_path
+        if request_path is None:
+            raise Exception("no request path")
+
+        authentication_url = f"{base_url}{request_path}"
+        logger.debug("auth URL = " + authentication_url)
+
+        # All headers into dictionary
+        request_headers: CIMultiDict[str] = CIMultiDict()
+        if service_call.request_header:
+            for header_entry in service_call.request_header.header:
+                if header_entry.header_name:
+                    request_headers.add(header_entry.header_name, header_entry.value)
+
+        request_body = service_call.request_body
+        if request_body is None:
+            raise Exception("no request body")
+
+        data = json.loads(request_body)
+        async with session.post(
+            url=authentication_url,
+            headers=request_headers,
+            json=data
+        ) as res:
+            if 200 <= res.status < 300:
+                logger.info(f"Authentication successful: Status {res.status}")
+                try:
+                    response = await res.text()
+                    token = jmespath.search("accessToken", json.loads(response))
+                    if token:
+                        session.headers.update(
+                            {"Authorization": f"Bearer {token}"}
+                        )
+
+                        logger.info("Token retrieved successfully")
+                        return True
+                    else:
+                        logger.warning("Token not found in the response")
+                        return False
+                except json.JSONDecodeError:
+                    logger.error("Failed to decode JSON response")
+                except JMESPathError:
+                    logger.error("Failed to search JSON data using JMESPath")
+            else:
+                logger.warning(f"Authentication failed: Status {res.status}")
+                logger.info(f"Response: {await res.text()}")
+
+    except aiohttp.ClientError as e:
+        logger.error(f"Network error occurred: {e}")
+    except Exception as e:
+        logger.error(f"An unexpected error occurred: {e}")
+    finally:
+        return False
+
+
+async def authenticate_with_basic_auth(
+    interface: RestApiInterface, session: ClientSession
+) -> bool:
+    """
+    Authenticates using Basic Authentication.
+
+    Parameters
+    ----------
+    interface : RestApiInterface
+        the device interface
+    session : ClientSession
+        the REST client session
+    
+    Returns
+    -------
+    bool
+        True if authenticated, False otherwise
+    """
+
+    description = interface.rest_api_interface_description
+    if description is None:
+        raise Exception("no interface description")
+    basic_option = description.rest_api_basic
+    if basic_option is None:
+        raise Exception("no Basic option")
+    username = basic_option.rest_basic_username if basic_option.rest_basic_username is not None else ''
+    password = basic_option.rest_basic_password if basic_option.rest_basic_password is not None else ''
+
+    # cannot use aiohttp.BasicAuth here, must set headers
+    credentials = f'{username}:{password}'
+    session.headers.update(
+        {"Authorization": f"Basic {base64.urlsafe_b64encode(bytes(credentials, 'utf-8')).decode('utf-8')}"}
+    )
+
+    return True
+
+
+supported_authentication_methods: dict[
+    RestApiAuthenticationMethod, Authenticator
+] = {
+    RestApiAuthenticationMethod.NO_SECURITY_SCHEME: authenticate_not,
+    RestApiAuthenticationMethod.BEARER_SECURITY_SCHEME: authenticate_with_bearer_token,
+    RestApiAuthenticationMethod.BASIC_SECURITY_SCHEME: authenticate_with_basic_auth
+}
+
+
+async def setup_authentication(
+    rest_api_interface: RestApiInterface, session: ClientSession
+) -> bool:
+    """
+    Performs authentication asynchronously, depending on the configured method.
+
+    Parameters
+    ----------
+    interface : RestApiInterface
+        the device interface
+    session : ClientSession
+        the REST client session
+    
+    Returns
+    -------
+    bool
+        True if authenticated, False otherwise
+    """
+    descr = rest_api_interface.rest_api_interface_description
+    if descr is None:
+        raise Exception("no REST interface description")
+    method = descr.rest_api_authentication_method
+    if method is None:
+        raise Exception("no authentication method")
+    auth_fn = supported_authentication_methods.get(method)
+    if auth_fn is None:
+        raise Exception("unsupported authentication method")
+    return await auth_fn(rest_api_interface, session)
