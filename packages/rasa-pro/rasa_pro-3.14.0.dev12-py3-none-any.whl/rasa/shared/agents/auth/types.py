e7e99da64831cfc1ecf48eafb653dbb2deaa8bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 04:05:58 +0000

    Add package rasa-pro/3.14.0.dev12/rasa_pro-3.14.0.dev12-py3-none-any.whl

---
+++ packages/rasa-pro/rasa_pro-3.14.0.dev12-py3-none-any.whl/rasa/shared/agents/auth/types.py
@@ -0,0 +1,11 @@
+"""Fundamental types for authentication protocols."""
+
+from enum import Enum
+
+
+class AgentAuthType(Enum):
+    """An Enum class that represents the supported authentication protocol types."""
+
+    API_KEY = "api_key"
+    OAUTH2 = "oauth2"
+    BEARER_TOKEN = "bearer_token"
