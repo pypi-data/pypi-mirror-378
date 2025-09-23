3b8586e7346ae06013e102afd6a0ec11fe74a6ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 03:17:02 +0000

    Add package pymagento/2.6.0/pymagento-2.6.0-py3-none-any.whl

---
+++ packages/pymagento/pymagento-2.6.0-py3-none-any.whl/magento/types.py
@@ -0,0 +1,114 @@
+import sys
+from typing import Union, TypedDict, Literal, List, Any, Dict
+
+from api_session import JSONDict
+
+if sys.version_info >= (3, 11):
+    from typing import NotRequired
+else:
+    # noinspection PyUnreachableCode
+    from typing_extensions import NotRequired
+
+__all__ = (
+    'AttributeOption',
+    'BasePrice',
+    'Category',
+    'Customer',
+    'DeleteCouponsResponseDict',
+    'MagentoEntity',
+    'MediaEntry',
+    'Order',
+    'PathId',
+    'ErrorDict',
+    'PriceUpdateResultDict',
+    'Product',
+    'ShippingAddress',
+    'Sku',
+    'SourceItem',
+    'SourceItemIn',
+)
+
+MagentoEntity = JSONDict
+
+PathId = Union[int, str]
+Sku = str
+
+# TODO: proper types
+AttributeOption = MagentoEntity
+Category = MagentoEntity
+Customer = MagentoEntity
+MediaEntry = MagentoEntity
+Order = MagentoEntity
+Product = MagentoEntity
+
+
+# Source items
+# ============
+# See https://developer.adobe.com/commerce/webapi/rest/inventory/manage-source-items/
+
+class SourceItemIn(TypedDict):
+    """Input source item."""
+    sku: Sku
+    source_code: str
+    quantity: NotRequired[int]
+    status: NotRequired[int]
+
+
+class SourceItem(TypedDict):
+    """Source item as returned by Magento."""
+    sku: Sku
+    source_code: str
+    quantity: int
+    status: int
+
+
+# Prices
+# ======
+
+class BasePrice(TypedDict):
+    """Base price dict."""
+    price: Union[int, float]
+    store_id: int
+    sku: Sku
+
+
+# Orders
+# ======
+
+class ShippingAddress(TypedDict):
+    """Shipping address dict."""
+    address_type: Literal["shipping"]
+    entity_id: int
+    parent_id: int
+    firstname: str
+    lastname: str
+    email: str
+    street: List[str]
+    city: str
+    postcode: str
+    region: NotRequired[str]
+    region_code: str
+    region_id: int
+    country_id: str
+    telephone: str
+    company: NotRequired[str]
+
+
+# Other types
+# ===========
+
+class DeleteCouponsResponseDict(TypedDict):
+    """Response from the `coupons/deleteByIds` endpoint."""
+    failed_items: List[Any]
+    missing_items: List[Any]
+
+
+class ErrorDict(TypedDict):
+    """Error dict."""
+    message: str
+    parameters: List[str]
+
+
+class PriceUpdateResultDict(ErrorDict):
+    """Response from the `products/base-prices` endpoint."""
+    extension_attributes: Dict[str, Any]
