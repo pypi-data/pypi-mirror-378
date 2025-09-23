ad74d493a2f6bc6a2ba992a123ad02191677f015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 03:18:10 +0000

    Add package yta-constants/0.0.52/yta_constants-0.0.52-py3-none-any.whl

---
+++ packages/yta-constants/yta_constants-0.0.52-py3-none-any.whl/yta_constants/color.py
@@ -0,0 +1,14 @@
+from yta_constants.enum import YTAEnum as Enum
+
+
+class ColorString(Enum):
+    """
+    The hexadecimal string that corresponds to the color.
+    """
+    
+    WHITE = '#FFFFFF'
+    BLACK = '#000000'
+    RED = '#FF000000'
+    GREEN = '#00FF00'
+    BLUE = '#0000FF'
+    # TODO: Add colors from Manim or other libraries
