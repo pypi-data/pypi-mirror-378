07d79004be34dda01172d2747cc5d205aac1a831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 15:05:52 +0000

    Add package moreblocks/0.0.1/moreblocks-0.0.1-py3-none-any.whl

---
+++ packages/moreblocks/moreblocks-0.0.1-py3-none-any.whl/test/integration/test_integration.py
@@ -0,0 +1,46 @@
+# TODO(nick): enable in GH actions
+
+from os import environ
+
+from slack_sdk import WebClient
+
+from moreblocks import Attachment, Color, ImageBlock, Message, SectionBlock
+
+
+def test_basic_attachment_message() -> None:
+    block = SectionBlock("Hello, world!", block_id="block1")
+    attachment = Attachment(blocks=block, color=Color.BLACK)
+    message = Message(
+        channel="#moreblocks",
+        attachments=[
+            attachment,
+        ],
+    )
+    client = WebClient(token=environ["SLACK_BOT_TOKEN"])
+    response = client.chat_postMessage(**message)
+    assert response.status_code == 200
+    with open("test/samples/message_basic_attachment.json", "r") as expected:
+        assert repr(message) == expected.read()
+
+
+def test_compound_message() -> None:
+    block1 = SectionBlock("Block, One", block_id="fake_block1")
+    block2 = SectionBlock("Block, Two", block_id="fake_block2")
+    block3 = ImageBlock(
+        title="Crash",
+        image_url="http://bit.ly/slack-block-test-image",
+        alt_text="crash",
+        block_id="fake_block3",
+    )
+    attachment1 = Attachment(blocks=block1, color=Color.PURPLE)
+    attachment2 = Attachment(blocks=[block2, block3], color=Color.YELLOW)
+    message = Message(
+        channel="#moreblocks",
+        blocks=[block1, block3],
+        attachments=[attachment1, attachment2],
+    )
+    client = WebClient(token=environ["SLACK_BOT_TOKEN"])
+    response = client.chat_postMessage(**message)
+    assert response.status_code == 200
+    with open("test/samples/message_compound.json", "r") as expected:
+        assert repr(message) == expected.read()
---
+++ packages/moreblocks/moreblocks-0.0.1-py3-none-any.whl/test/unit/test_rich_text.py
@@ -0,0 +1,200 @@
+from moreblocks.rich_text import (
+    ListType,
+    RichText,
+    RichTextChannel,
+    RichTextCodeBlock,
+    RichTextEmoji,
+    RichTextLink,
+    RichTextList,
+    RichTextQuote,
+    RichTextSection,
+    RichTextUser,
+    RichTextUserGroup,
+)
+
+from .utils import fetch_sample
+
+
+def test_rich_text_channel_basic() -> None:
+    assert fetch_sample(
+        path="test/samples/rich_text/rich_text_channel_basic.json"
+    ) == repr(
+        RichTextChannel(
+            channel_id="C0261C65XNY",
+            bold=True,
+            italic=False,
+            strike=True,
+            highlight=True,
+            client_highlight=True,
+            unlink=False,
+        )
+    )
+
+
+def test_rich_text_emoji_basic() -> None:
+    assert fetch_sample(
+        path="test/samples/rich_text/rich_text_emoji_basic.json"
+    ) == repr(
+        RichTextEmoji(
+            name="wave",
+        )
+    )
+
+
+def test_rich_text_link_basic() -> None:
+    assert fetch_sample(
+        path="test/samples/rich_text/rich_text_link_basic.json"
+    ) == repr(
+        RichTextLink(
+            url="https://google.com/",
+            text="Google",
+            unsafe=False,
+            bold=True,
+            italic=False,
+            strike=True,
+            code=True,
+        )
+    )
+
+
+def test_rich_text_basic() -> None:
+    assert fetch_sample(path="test/samples/rich_text/rich_text_basic.json") == repr(
+        RichText(
+            text="I am a bold rich text block!",
+            bold=True,
+            italic=True,
+            strike=False,
+        )
+    )
+
+
+def test_rich_text_user_basic() -> None:
+    assert fetch_sample(
+        path="test/samples/rich_text/rich_text_user_basic.json"
+    ) == repr(
+        RichTextUser(
+            user_id="DR36TNNLA",
+            bold=True,
+            italic=False,
+            strike=True,
+            highlight=True,
+            client_highlight=True,
+            unlink=False,
+        )
+    )
+
+
+def test_rich_text_user_group_basic() -> None:
+    assert fetch_sample(
+        path="test/samples/rich_text/rich_text_user_group_basic.json"
+    ) == repr(
+        RichTextUserGroup(
+            user_group_id="C01RGRU0RUK",
+            bold=True,
+            italic=False,
+            strike=True,
+            highlight=True,
+            client_highlight=True,
+            unlink=False,
+        )
+    )
+
+
+CHANNEL = "channel"
+EMOJI = "emoji"
+LINK = "link"
+TEXT = "text"
+USER = "user"
+USER_GROUP = "user_group"
+
+
+def test_rich_text_list_basic() -> None:
+    assert fetch_sample(
+        path="test/samples/rich_text/rich_text_list_basic.json"
+    ) == repr(
+        RichTextList(
+            elements=[
+                RichTextSection(
+                    elements=[
+                        RichText(
+                            text="Oh",
+                        )
+                    ]
+                ),
+                RichTextSection(
+                    elements=[
+                        RichText(
+                            text="Hi",
+                        )
+                    ]
+                ),
+                RichTextSection(elements=[RichText(text="Mark")]),
+            ],
+            style=ListType.BULLET,
+            indent=0,
+            offset=0,
+            border=1,
+        )
+    )
+
+
+def test_rich_text_list_ordered() -> None:
+    assert fetch_sample(
+        path="test/samples/rich_text/rich_text_list_ordered.json"
+    ) == repr(
+        RichTextList(
+            elements=[
+                RichTextSection(
+                    elements=[
+                        RichText(
+                            text="Oh",
+                        )
+                    ]
+                ),
+                RichTextSection(
+                    elements=[
+                        RichText(
+                            text="Hi",
+                        )
+                    ]
+                ),
+            ],
+            style=ListType.ORDERED,
+            indent=1,
+            offset=2,
+            border=3,
+        )
+    )
+
+
+def test_rich_tex_code_block_basic() -> None:
+    assert fetch_sample(
+        path="test/samples/rich_text/rich_text_code_block_basic.json"
+    ) == repr(
+        RichTextCodeBlock(
+            elements=[RichText(text="\ndef hello_world():\n    print('hello, world')")],
+            border=0,
+        )
+    )
+
+
+def test_rich_text_quote_basic() -> None:
+    assert fetch_sample(
+        path="test/samples/rich_text/rich_text_quote_basic.json"
+    ) == repr(
+        RichTextQuote(
+            elements=[RichText(text="Great and good are seldom the same man")], border=1
+        ),
+    )
+
+
+def test_rich_text_section() -> None:
+    assert fetch_sample(
+        path="test/samples/rich_text/rich_text_section_basic.json"
+    ) == repr(
+        RichTextSection(
+            elements=[
+                RichText(text="The only true wisdom is in knowing you know nothing")
+            ]
+        )
+    )
