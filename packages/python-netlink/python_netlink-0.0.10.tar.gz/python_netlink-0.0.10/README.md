63b9fcfdff9ae9391d34a1ae29f73127b2babc73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 04:06:00 +0000

    Add package python-netlink/0.0.10/python_netlink-0.0.10.tar.gz

---
+++ packages/python-netlink/python_netlink-0.0.10.tar.gz/python_netlink-0.0.10/README.md
@@ -0,0 +1,8 @@
+# Netlink
+[![Netlink](https://github.com/kinnay/netlink/workflows/Release/badge.svg)](https://github.com/kinnay/netlink/actions/workflows/main.yaml) 
+
+This package provides an asynchronous implementation of the Linux netlink protocol, including basic support for nl80211.
+
+This package can be installed using `pip3 install python-netlink`.
+
+Documentation is available [here](https://netlink.readthedocs.io).
---
+++ packages/python-netlink/python_netlink-0.0.10.tar.gz/python_netlink-0.0.10/setup.py
@@ -0,0 +1,23 @@
+
+import setuptools
+
+description = \
+	"Asynchronous netlink library for Python"
+
+long_description = \
+	"This is an asynchronous netlink library for Python, " \
+	"including basic support for nl80211."
+
+setuptools.setup(
+	name = "python-netlink",
+	version = "0.0.10",
+	description = description,
+	long_description = long_description,
+	author = "Yannik Marchand",
+	author_email = "ymarchand@me.com",
+	url = "https://github.com/kinnay/netlink",
+	license = "GPLv3",
+	platforms = ["Linux"],
+	packages = ["netlink"],
+	install_requires = ["trio"]
+)
