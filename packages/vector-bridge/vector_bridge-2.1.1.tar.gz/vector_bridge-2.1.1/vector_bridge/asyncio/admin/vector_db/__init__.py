474789978edfa18d1256fcda363d8e48d30fd990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 07:07:03 +0000

    Add package vector-bridge/2.1.1/vector_bridge-2.1.1.tar.gz

---
+++ packages/vector-bridge/vector_bridge-2.1.1.tar.gz/vector_bridge-2.1.1/vector_bridge/asyncio/admin/vector_db/__init__.py
@@ -0,0 +1,13 @@
+from vector_bridge import AsyncVectorBridgeClient
+from vector_bridge.asyncio.admin.vector_db.changeset import \
+    AsyncVectorDBChangesetAdmin
+from vector_bridge.asyncio.admin.vector_db.state import AsyncVectorDBStateAdmin
+
+
+class AsyncVectorDBAdmin:
+    """Async admin client for VectorDB management endpoints."""
+
+    def __init__(self, client: AsyncVectorBridgeClient):
+        self.client = client
+        self.changeset = AsyncVectorDBChangesetAdmin(client)
+        self.state = AsyncVectorDBStateAdmin(client)
