a112b9c00d38962c6e760dd34dcdf4cceaf628a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 14:06:22 +0000

    Add package ob-metaflow-stubs/6.0.10.10/ob-metaflow-stubs-6.0.10.10.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.10.10.tar.gz/ob-metaflow-stubs-6.0.10.10/metaflow-stubs/metadata_provider/__init__.pyi
@@ -0,0 +1,16 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.18.7.5+obcheckpoint(0.2.7);ob(v1)                                                    #
+# Generated on 2025-09-23T01:34:30.666685                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import metadata as metadata
+from .metadata import DataArtifact as DataArtifact
+from .metadata import MetadataProvider as MetadataProvider
+from .metadata import MetaDatum as MetaDatum
+from . import util as util
+from . import heartbeat as heartbeat
+
---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.10.10.tar.gz/ob-metaflow-stubs-6.0.10.10/metaflow-stubs/plugins/kubernetes/spot_monitor_sidecar.pyi
@@ -0,0 +1,20 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.18.7.5+obcheckpoint(0.2.7);ob(v1)                                                    #
+# Generated on 2025-09-23T01:34:30.745250                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+
+class SpotTerminationMonitorSidecar(object, metaclass=type):
+    def __init__(self):
+        ...
+    def process_message(self, msg):
+        ...
+    @classmethod
+    def get_worker(cls):
+        ...
+    ...
+
