59edb093127c2cf59f50f57c0ade7fedf734a3dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 09:06:21 +0000

    Add package rusticlone/1.6.0/rusticlone-1.6.0-py3-none-any.whl

---
+++ packages/rusticlone/rusticlone-1.6.0-py3-none-any.whl/rusticlone/helpers/custom.py
@@ -0,0 +1,282 @@
+"""
+Load customizations by interpreting passed arguments
+"""
+
+# ┌───────────────────────────────────────────────────────────────┐
+# │ Contents of custom.py                                         │
+# ├───────────────────────────────────────────────────────────────┘
+# │
+# ├── IMPORTS
+# ├── CLASSES
+# ├── FUNCTIONS
+# │
+# └───────────────────────────────────────────────────────────────
+
+# ################################################################ IMPORTS
+
+# file locations
+from pathlib import Path
+
+# args type
+from configargparse import Namespace
+
+# os and hostname
+import platform
+
+# exit
+import sys
+
+# toml parsing
+import tomllib
+
+# rusticlone
+from rusticlone.helpers.action import Action
+from rusticlone.helpers.rustic import Rustic
+from rusticlone.helpers.notification import notify_user
+from rusticlone.processing.parallel import (
+    system_backup_parallel,
+    system_archive_parallel,
+    system_upload_parallel,
+    system_restore_parallel,
+    system_download_parallel,
+    system_extract_parallel,
+)
+from rusticlone.processing.sequential import (
+    system_backup_sequential,
+    system_archive_sequential,
+    system_upload_sequential,
+    system_restore_sequential,
+    system_download_sequential,
+    system_extract_sequential,
+)
+from rusticlone.processing.profile import parse_repo, parse_sources
+
+# ################################################################ CLASSES
+
+
+class Custom:
+    """
+    Set variables to args flags, or use hardcoded values
+    """
+
+    def __init__(self, args) -> None:
+        """
+        Initialize variables
+        """
+        self.hostname = platform.node()
+        self.parallel = args.parallel
+        self.command = args.command[0]
+        self.operating_system = platform.system()
+        self.default_log_file = Path("rusticlone.log")
+        self.apprise_url = ""
+        # log file
+        # rustic use log file from config, while from rclone it is passed from either cli or here
+        if args.log_file is not None:
+            self.log_file = Path(args.log_file)
+        else:
+            self.log_file = self.default_log_file
+        # profiles dirs, the same where rustic reads profiles
+        if self.operating_system == "Windows":
+            self.profiles_dirs = [
+                Path.home() / "AppData/Roaming/rustic/config",
+                Path("C:/ProgramData/rustic/config"),
+            ]
+        elif self.operating_system == "Darwin":
+            self.profiles_dirs = [
+                Path.home() / "Library/Application Support/rustic",
+                Path("/etc/rustic"),
+            ]
+        else:
+            self.profiles_dirs = [Path.home() / ".config/rustic", Path("/etc/rustic")]
+        # remote prefix: rclone remote + subdirectory without trailing slash
+        if args.remote is not None:
+            self.remote_prefix = args.remote.rstrip("/")
+        else:
+            self.remote_prefix = "None:/"
+        # test profile
+        if args.profile:
+            self.provided_profile = args.profile
+        else:
+            self.provided_profile = ""
+        if args.apprise_url:
+            self.apprise_url = args.apprise_url
+
+    def check_log_file(self) -> None:
+        """
+        Create log file parent folders if missing, delete old log file
+        """
+        action = Action("Checking log file")
+        if self.log_file != self.default_log_file:
+            used_log = str(self.log_file)
+            self.log_file.parents[0].mkdir(parents=True, exist_ok=True)
+            self.log_file.unlink(missing_ok=True)
+            self.log_file.touch()
+        else:
+            # if not defined in Rustic profiles, "./rusticlone.log" is used
+            used_log = "defined in Rustic profiles"
+        action.stop(f"Log file: {used_log}", "")
+
+    def check_profiles_dirs(self) -> None:
+        """
+        Check that at least one profiles directory exists
+        """
+        action = Action("Checking profiles directory")
+        existing_dirs = []
+        old_profiles_dirs = self.profiles_dirs
+        for profile_dir in self.profiles_dirs:
+            if profile_dir.exists() and profile_dir.is_dir():
+                existing_dirs.append(profile_dir)
+        if existing_dirs:
+            self.profiles_dirs = existing_dirs
+            pretty_dirs = str(
+                [str(profiles_dir) for profiles_dir in self.profiles_dirs]
+            )
+            action.stop(f"Profiles directories: {pretty_dirs}", "")
+        else:
+            action.abort(
+                f"Could not find any profiles directory among {old_profiles_dirs}"
+            )
+
+
+# ################################################################ FUNCTIONS
+
+
+def has_needed_config_components(profile_path: Path) -> bool:
+    """
+    Check if a profile should be processed by running rustic show-config
+    """
+    try:
+        profile_name = profile_path.stem
+        rustic = Rustic(profile_name, "show-config")
+        config = tomllib.loads(rustic.stdout)
+        has_repo = parse_repo(config) is not None
+        has_sources = parse_sources(config) is not None
+        return has_repo and has_sources
+    except (AttributeError, tomllib.TOMLDecodeError, KeyError, IndexError):
+        return False
+
+
+def list_profiles(profiles_dirs: list, provided_profile: str = "*") -> list:
+    """
+    Scan profiles from directories if none have been provided explicitely
+    Don't scan from /etc/rustic if ~/.config/rustic has some profiles'
+    """
+    action = Action("Reading profiles")
+    profiles: list[str] = []
+    opened_files = 0
+    if not provided_profile:
+        provided_profile = "*"
+    for profiles_dir in profiles_dirs:
+        if not profiles:
+            action.stop(f'Scanning "{profiles_dir}"', "")
+            files = sorted(list(profiles_dir.glob(f"{provided_profile}.toml")))
+            for file in files:
+                opened_files += 1
+                if (
+                    file.is_file()
+                    and file.stem not in profiles
+                    and has_needed_config_components(file)
+                ):
+                    profiles.append(file.stem)
+    # remove duplicates
+    profiles = list(set(profiles))
+    if profiles:
+        action.stop(f"Profiles: {str(profiles)}", "")
+        return profiles
+    else:
+        print("")
+        print(f"Glob pattern: {provided_profile}")
+        print(f"Files tried: {opened_files}")
+        action.abort("Could not find any valid profile")
+        sys.exit(1)
+
+
+def process_profiles(
+    profiles: list,
+    parallel: bool,
+    command: str,
+    log_file: Path,
+    remote_prefix: str,
+    apprise_url: str,
+) -> None:
+    """
+    Process all profiles according to the command specified and parallel flag
+    """
+    results = {}
+    if parallel:
+        match command:
+            case "backup":
+                system_backup_parallel(
+                    profiles=profiles, log_file=log_file, remote_prefix=remote_prefix
+                )
+            case "archive":
+                system_archive_parallel(profiles=profiles, log_file=log_file)
+            case "upload":
+                system_upload_parallel(
+                    profiles=profiles, log_file=log_file, remote_prefix=remote_prefix
+                )
+            case "restore":
+                system_restore_parallel(
+                    profiles=profiles,
+                    log_file=log_file,
+                    remote_prefix=remote_prefix,
+                )
+            case "download":
+                system_download_parallel(
+                    profiles=profiles, log_file=log_file, remote_prefix=remote_prefix
+                )
+            case "extract":
+                system_extract_parallel(profiles=profiles, log_file=log_file)
+            case _:
+                print(f"Invalid command '{command}'")
+    else:
+        match command:
+            case "backup":
+                results = system_backup_sequential(
+                    profiles=profiles, log_file=log_file, remote_prefix=remote_prefix
+                )
+            case "archive":
+                results = system_archive_sequential(
+                    profiles=profiles, log_file=log_file
+                )
+            case "upload":
+                results = system_upload_sequential(
+                    profiles=profiles, log_file=log_file, remote_prefix=remote_prefix
+                )
+            case "restore":
+                results = system_restore_sequential(
+                    profiles=profiles,
+                    log_file=log_file,
+                    remote_prefix=remote_prefix,
+                )
+            case "download":
+                results = system_download_sequential(
+                    profiles=profiles, log_file=log_file, remote_prefix=remote_prefix
+                )
+            case "extract":
+                results = system_extract_sequential(
+                    profiles=profiles, log_file=log_file
+                )
+            case _:
+                print(f"Invalid command '{command}'")
+                sys.exit(1)
+    if apprise_url and results:
+        notify_user(results, apprise_url)
+
+
+def load_customizations(args: Namespace):
+    """
+    Interpret arguments, make a list of profiles to process, and process them
+    """
+    custom = Custom(args)
+    custom.check_log_file()
+    custom.check_profiles_dirs()
+    profiles = list_profiles(custom.profiles_dirs, custom.provided_profile)
+    process_profiles(
+        profiles,
+        custom.parallel,
+        custom.command,
+        custom.log_file,
+        custom.remote_prefix,
+        custom.apprise_url,
+    )
---
+++ packages/rusticlone/rusticlone-1.6.0-py3-none-any.whl/rusticlone/processing/sequential.py
@@ -0,0 +1,227 @@
+"""
+Process all the Rustic profiles in the system one at the time
+"""
+
+# ┌───────────────────────────────────────────────────────────────┐
+# │ Contents of sequential.py                                     │
+# ├───────────────────────────────────────────────────────────────┘
+# │
+# ├── IMPORTS
+# ├──┐FUNCTIONS
+# │  ├── BACKUP
+# │  └── RESTORE
+# │
+# └───────────────────────────────────────────────────────────────
+
+# ################################################################ IMPORTS
+
+# file locations
+from pathlib import Path
+
+# rusticlone
+from rusticlone.helpers.action import Action
+from rusticlone.helpers.timer import Timer
+from rusticlone.helpers.notification import Result
+from rusticlone.helpers.formatting import print_stats
+from rusticlone.processing.atomic import (
+    profile_archive,
+    profile_upload,
+    profile_download,
+    profile_extract,
+)
+
+# ################################################################ FUNCTIONS
+# ################################ BACKUP
+
+
+def system_backup_sequential(
+    profiles: list, log_file: Path, remote_prefix: str
+) -> dict[str, Result]:
+    """
+    Launch system_archive() and system_upload()
+    """
+    # action = Action("Backing up system", status="")
+    print("========================================")
+    Action("System", status="[backup]")
+    timer = Timer()
+    archive_results = system_archive_sequential(profiles=profiles, log_file=log_file)
+    upload_results = system_upload_sequential(
+        profiles=profiles,
+        log_file=log_file,
+        remote_prefix=remote_prefix,
+        archive_results=archive_results,
+    )
+    timer.stop("System backup duration")
+    return {**archive_results, **upload_results}
+
+
+def system_archive_sequential(profiles: list, log_file: Path) -> dict[str, Result]:
+    """
+    For each profile, archive it
+
+    Args:
+        profiles (list): List of profiles to be archived
+
+    Returns:
+        None
+    """
+    # print_stats("Creating local snapshots", "")
+    print("________________________________________")
+    print_stats("System", "[archive]")
+    print_stats("", "")
+    archive_results = {}
+    for name in profiles:
+        archive_success, duration = profile_archive(name=name, log_file=log_file)
+        if archive_success:
+            print_stats("", "")
+        else:
+            print_stats(f"Error archiving {name}", "")
+            print_stats("", "")
+        archive_results[name + "_archive"] = Result(
+            name, "archive", archive_success, duration
+        )
+    return archive_results
+
+
+def system_upload_sequential(
+    profiles: list,
+    log_file: Path,
+    remote_prefix: str,
+    archive_results: dict[str, Result] | None = None,
+) -> dict[str, Result]:
+    """
+    For each profile, upload it
+
+    Args:
+        profiles (list): List of profiles to be uploaded
+        log_file (Path): log file for Rclone
+        remote_prefix (str): prefix of Rclone remote
+
+    Returns:
+        None
+    """
+    # print_stats("Uploading local snapshots", "")
+    print("________________________________________")
+    print_stats("System", "[upload]")
+    print_stats("", "")
+    upload_results = {}
+    if archive_results is None:
+        archive_results = {}
+    for name in profiles:
+        archive_success = True
+        archive_result = archive_results.get(name + "_archive", None)
+        if archive_result is not None:
+            archive_success = archive_result.success
+        if archive_success:
+            upload_success, duration = profile_upload(
+                name=name, log_file=log_file, remote_prefix=remote_prefix
+            )
+            if upload_success:
+                print_stats("", "")
+            else:
+                print_stats(f"Error uploading {name}", "")
+            upload_results[name + "_upload"] = Result(
+                name, "upload", upload_success, duration
+            )
+        else:
+            print_stats(f"Not uploading {name} as archiving failed", "")
+            print_stats("", "")
+            upload_results[name + "_upload"] = Result(name, "upload", False, "skipped")
+    return upload_results
+
+
+# ################################ RESTORE
+
+
+def system_restore_sequential(
+    profiles: list, log_file: Path, remote_prefix: str
+) -> dict[str, Result]:
+    """
+    Launch system_download() and system_extract()
+    """
+    # action = Action("Restoring system", status="")
+    print("========================================")
+    Action("System", False, "[restore]")
+    timer = Timer()
+    download_results = system_download_sequential(
+        profiles, log_file=log_file, remote_prefix=remote_prefix
+    )
+    extract_results = system_extract_sequential(
+        profiles=profiles,
+        log_file=log_file,
+        download_results=download_results,
+    )
+    timer.stop("System restore duration")
+    return {**download_results, **extract_results}
+
+
+def system_download_sequential(
+    profiles: list, log_file: Path, remote_prefix: str
+) -> dict[str, Result]:
+    """
+    For each profile, download it to the repo location
+
+    Args:
+        profiles (list): List of profiles to be uploaded
+        log_file (Path): log file for Rclone
+        remote_prefix (str): prefix of Rclone remote
+
+    Returns:
+        None
+    """
+    # print_stats("Downloading remote snapshots", "")
+    print("________________________________________")
+    print_stats("System", "[download]")
+    print_stats("", "")
+    download_results = {}
+    for name in profiles:
+        download_success, duration = profile_download(
+            name=name, log_file=log_file, remote_prefix=remote_prefix
+        )
+        if download_success:
+            print_stats("", "")
+        else:
+            print_stats(f"Error downloading {name}", "")
+        download_results[name + "_download"] = Result(
+            name, "download", download_success, duration
+        )
+    return download_results
+
+
+def system_extract_sequential(
+    profiles: list,
+    log_file: Path,
+    download_results: dict[str, Result] | None = None,
+) -> dict[str, Result]:
+    """
+    For each profile,
+    extract it to a specific folder,
+    move the extracted snapshot to root location
+    """
+    # print_stats("Extracting local snapshots", "")
+    print("________________________________________")
+    print_stats("System", "[extract]")
+    print_stats("", "")
+    if download_results is None:
+        download_results = {}
+    extract_results = {}
+    for name in profiles:
+        download_success = True
+        download_result = download_results.get(name + "_download", None)
+        if download_result is not None:
+            download_success = download_result.success
+        if download_success:
+            extract_success, duration = profile_extract(name=name, log_file=log_file)
+            if extract_success:
+                print_stats("", "")
+            else:
+                print_stats(f"Error extracting {name}", "")
+            extract_results[name + "_extract"] = Result(
+                name, "extract", extract_success, duration
+            )
+        else:
+            print_stats(f"Not extracting {name} as download failed", "")
+            extract_results[name + "_extract"] = Result(
+                name, "extract", False, "skipped"
+            )
+    return extract_results
