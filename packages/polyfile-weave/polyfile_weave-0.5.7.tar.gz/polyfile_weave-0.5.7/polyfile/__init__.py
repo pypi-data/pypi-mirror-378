2f8666cb55ff44187fc40deb03a4c3e21111c977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 06:07:01 +0000

    Add package polyfile-weave/0.5.7/polyfile_weave-0.5.7.tar.gz

---
+++ packages/polyfile-weave/polyfile_weave-0.5.7.tar.gz/polyfile_weave-0.5.7/polyfile/__init__.py
@@ -0,0 +1,15 @@
+from . import (
+    nes,
+    pdf,
+    jpeg,
+    zipmatcher,
+    nitf,
+    http,
+    kaitaimatcher,
+    languagematcher,
+    pickles,
+    polyfile
+)
+
+from .__main__ import main
+from .polyfile import __version__, InvalidMatch, Match, Matcher, Parser, PARSERS, register_parser, Submatch
