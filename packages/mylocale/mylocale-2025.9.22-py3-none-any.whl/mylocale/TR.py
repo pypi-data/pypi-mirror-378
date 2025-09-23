b4020444f212f93da17bba86b20bedfee16b152c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 04:05:25 +0000

    Add package mylocale/2025.9.22/mylocale-2025.9.22-py3-none-any.whl

---
+++ packages/mylocale/mylocale-2025.9.22-py3-none-any.whl/mylocale/TR.py
@@ -0,0 +1,33 @@
+import csv
+import locale
+
+
+class TR:
+    def __init__(self, langcode: str, csv_file: str):
+        self.langcode = langcode
+        self.csv_file = csv_file
+        with open(csv_file, newline="", mode="r") as f:
+            self.locale_csv = csv.DictReader(f=f, delimiter=",")
+        self.csv_langcodes = self.locale_csv.fieldnames
+
+    def check_rtl(langcode: str):
+        # print(self.locale_csv)
+        rtl_langcodes = ["ar", "he", "fa", "ur", "ps", "ku", "dv", "yi", "sd", "kmr"]
+        if langcode in rtl_langcodes and langcode in self.locale_csv.fieldnames:
+            return True
+        else:
+            return False
+
+    def tr(self, target_key, langcode: str):
+        for item in self.locale_csv:
+            if item["stringname"] == target_key:
+                try:
+                    if item[langcode] == "":
+                        self.f.close()
+                        return item["en"]
+                    else:
+                        self.f.close()
+                        return item[langcode]
+                except:
+                    self.f.close()
+                    return item["en"]
