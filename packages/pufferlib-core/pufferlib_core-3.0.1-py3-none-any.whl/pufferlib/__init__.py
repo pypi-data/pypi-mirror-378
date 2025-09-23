2fa52a22f6396c4b031a3677a6204eccbf6536d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 15:05:43 +0000

    Add package pufferlib-core/3.0.1/pufferlib_core-3.0.1-py3-none-any.whl

---
+++ packages/pufferlib-core/pufferlib_core-3.0.1-py3-none-any.whl/pufferlib/__init__.py
@@ -0,0 +1,10 @@
+"""
+PufferLib Core - Minimal vectorized environment functionality
+"""
+
+from . import spaces
+from . import emulation
+from . import vector
+
+__version__ = "2.0.0"
+__all__ = ["spaces", "emulation", "vector"]
