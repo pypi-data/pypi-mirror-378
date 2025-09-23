b5fe35140e72b66b4a1362b5b3829b52c608e8d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 14:06:21 +0000

    Add package rfd-protocol/2.1.0/rfd_protocol-2.1.0.tar.gz

---
+++ packages/rfd-protocol/rfd_protocol-2.1.0.tar.gz/rfd_protocol-2.1.0/src/rfd/__init__.py
@@ -0,0 +1,25 @@
+"""
+RFD Protocol - Reality-First Development
+
+A protocol that prevents AI hallucination in software development by enforcing
+concrete reality checkpoints.
+"""
+
+__version__ = "2.1.0"
+__author__ = "RFD Team"
+__email__ = "team@rfd-protocol.dev"
+__description__ = "Reality-First Development Protocol"
+
+from .rfd import RFD
+from .build import BuildEngine
+from .validation import ValidationEngine  
+from .spec import SpecEngine
+from .session import SessionManager
+
+__all__ = [
+    "RFD",
+    "BuildEngine", 
+    "ValidationEngine",
+    "SpecEngine",
+    "SessionManager"
+]
