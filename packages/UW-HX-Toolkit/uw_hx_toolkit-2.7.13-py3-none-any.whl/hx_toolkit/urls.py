4b59e965186bc40d136b2abedb1d001b2a181ae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 07:06:26 +0000

    Add package UW-HX-Toolkit/2.7.13/uw_hx_toolkit-2.7.13-py3-none-any.whl

---
+++ packages/UW-HX-Toolkit/uw_hx_toolkit-2.7.13-py3-none-any.whl/hx_toolkit/urls.py
@@ -0,0 +1,9 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from django.conf.urls import url
+from django.contrib import admin
+
+urlpatterns = [
+    url('admin/', admin.site.urls),
+]
