e09dfde8ed738e87e67b4e7e7b7879e458b877e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 16:07:11 +0000

    Add package runkmc/0.1.0a6/runkmc-0.1.0a6-pp38-pypy38_pp73-macosx_11_0_arm64.whl

---
+++ packages/runkmc/runkmc-0.1.0a6-pp38-pypy38_pp73-macosx_11_0_arm64.whl/cpp/include/runkmc/common.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <assert.h>
+#include <chrono>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <memory>
+
+#include "version.h"
+
+#include "utils/console.h"
+#include "utils/random.h"
+
+#include "core/types.h"
+#include "core/registry.h"
