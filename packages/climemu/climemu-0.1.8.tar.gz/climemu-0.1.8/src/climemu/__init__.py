c8237484049f391e1ec0a3759811cf1232147540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 14:06:28 +0000

    Add package climemu/0.1.8/climemu-0.1.8.tar.gz

---
+++ packages/climemu/climemu-0.1.8.tar.gz/climemu-0.1.8/src/climemu/__init__.py
@@ -0,0 +1,17 @@
+from climemu.utils import Registry
+
+__version__ = "0.1.8"
+
+"""
+Registery of pretrained emulators for usage
+"""
+EMULATORS = Registry()
+
+
+def build_emulator(name):
+    model = EMULATORS[name]()
+    return model
+
+
+from .emulators import Bouabid2025Emulator
+__all__ = ['build_emulator', 'Bouabid2025Emulator']
