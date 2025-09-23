a61801567cc090f4e97143d27115f671a5838055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 05:05:49 +0000

    Add package explainable/1.2.2/explainable-1.2.2-py3-none-any.whl

---
+++ packages/explainable/explainable-1.2.2-py3-none-any.whl/explainable/entities.py
@@ -0,0 +1,118 @@
+from dataclasses import dataclass, field
+from typing import Any, TypedDict
+try:
+    from typing import NotRequired
+except ImportError:
+    from typing_extensions import NotRequired
+
+
+@dataclass
+class Node:
+    data: Any
+    object_id: str = ""
+    widget: str = ""
+    default_x: float = 100.0
+    default_y: float = 100.0
+    is_draggable: bool = True
+
+
+class TextNodeData(TypedDict):
+    text: str
+    width: NotRequired[int]
+    height: NotRequired[int]
+    background: NotRequired[str]
+    foreground: NotRequired[str]
+
+
+@dataclass
+class TextNode(Node):
+    data: TextNodeData = field(default_factory=lambda: TextNodeData(text=""))
+    widget: str = "text"
+
+
+class NumberNodeData(TypedDict):
+    value: float
+
+
+@dataclass
+class NumberNode(Node):
+    data: NumberNodeData = field(default_factory=lambda: NumberNodeData(value=0.0))
+    widget: str = "number"
+
+
+class RowNodeData(TypedDict):
+    children: list[Node]
+
+
+@dataclass
+class RowNode(Node):
+    data: RowNodeData = field(default_factory=lambda: RowNodeData(children=[]))
+    widget: str = "row"
+
+
+class ColumnNodeData(TypedDict):
+    children: list[Node]
+
+
+@dataclass
+class ColumnNode(Node):
+    data: ColumnNodeData = field(default_factory=lambda: ColumnNodeData(children=[]))
+    widget: str = "column"
+
+
+class PixelNodeData(TypedDict):
+    color: str
+    size: NotRequired[int]
+    width: NotRequired[int]
+    height: NotRequired[int]
+
+
+@dataclass
+class PixelNode(Node):
+    data: PixelNodeData = field(default_factory=lambda: PixelNodeData(color=""))
+    widget: str = "pixel"
+
+
+class LineChartNodeData(TypedDict):
+    data: list[tuple[float, float]]
+
+
+@dataclass
+class LineChartNode(Node):
+    data: LineChartNodeData = field(default_factory=lambda: LineChartNodeData(data=[]))
+    widget: str = "linechart"
+
+
+class ClickableExclusiveNodeData(TypedDict):
+    group: str
+    widget: Node
+
+
+@dataclass
+class ClickableExclusiveNode(Node):
+    data: ClickableExclusiveNodeData = field(default_factory=lambda: ClickableExclusiveNodeData(group="", widget=TextNode()))
+    widget: str = "clickable_exclusive"
+
+
+@dataclass
+class Edge:
+    edge_id: str
+    node_start_id: str
+    node_end_id: str
+    data: Any = None
+    line_width: float = 2.0
+    line_color: str = '#fff'
+    label_color: str = '#fff'
+    widget: str = "edge"
+
+
+@dataclass
+class ClickableExclusiveEdge(Edge):
+    edge_id: str
+    node_start_id: str
+    node_end_id: str
+    data: Any = None
+    line_width: float = 2.0
+    line_color: str = '#fff'
+    label_color: str = '#fff'
+    widget: str = "clickable_exclusive_edge"
