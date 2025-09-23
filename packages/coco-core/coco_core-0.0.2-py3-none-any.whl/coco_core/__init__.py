49b06f72555023bfdb21af4e9d70ff204104c4d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 15:05:50 +0000

    Add package coco-core/0.0.2/coco_core-0.0.2-py3-none-any.whl

---
+++ packages/coco-core/coco_core-0.0.2-py3-none-any.whl/coco_core/__init__.py
@@ -0,0 +1,10 @@
+"""coco_core
+
+A middleware rich backend framework for building APIs.
+"""
+
+__version__ = "0.0.1"
+
+from .app import CocoApplication
+
+__all__ = ["__version__", "CocoApplication"]
