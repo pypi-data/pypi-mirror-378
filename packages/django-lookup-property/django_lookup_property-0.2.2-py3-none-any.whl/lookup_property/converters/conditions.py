2738ea5086a3927bcb0df7a6c88f3e9366afc749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 04:05:58 +0000

    Add package django-lookup-property/0.2.2/django_lookup_property-0.2.2-py3-none-any.whl

---
+++ packages/django-lookup-property/django_lookup_property-0.2.2-py3-none-any.whl/lookup_property/converters/conditions.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+import ast
+
+from django.db import models
+from django.db.models import functions
+
+from lookup_property.typing import Expr, State
+
+from .expressions import expression_to_ast
+
+
+@expression_to_ast.register
+def _(expression: models.Case, state: State) -> ast.IfExp:
+    """
+    Case(When(condition=Q(fizz=True), then=Value("foo")), default=Value("bar")))
+    -> if "foo" self.fizz == True else "bar"
+    """
+    cases: list[models.When] = expression.cases.copy()
+    default = expression_to_ast(expression.default, state=state)
+    return cases_to_if_expression(cases, default, state=state)
+
+
+def cases_to_if_expression(cases: list[models.When], default: ast.AST, state: State) -> ast.IfExp:
+    """Recursively convert the When expressions of a Case expression to an if expression."""
+    case = cases.pop(0)
+    statement = cases_to_if_expression(cases, default, state=state) if cases else default
+    return ast.IfExp(
+        test=expression_to_ast(case.condition, state=state),
+        body=expression_to_ast(case.result, state=state),
+        orelse=statement,
+    )
+
+
+@expression_to_ast.register
+def _(expression: functions.Coalesce, state: State) -> ast.IfExp:
+    """
+    Coalesce("foo", "bar)
+    -> self.foo if self.foo is not None else self.bar if self.bar is not None else None
+    """
+    arguments: list[Expr] = expression.get_source_expressions().copy()
+    return args_to_if_expression(arguments, state=state)
+
+
+def args_to_if_expression(args: list[Expr], state: State) -> ast.IfExp:
+    arg = args.pop(0)
+    statement = args_to_if_expression(args, state=state) if args else ast.Constant(value=None)
+    value = expression_to_ast(arg, state=state)
+    return ast.IfExp(
+        test=ast.Compare(
+            left=value,
+            ops=[ast.NotEq() if isinstance(value, ast.Constant) else ast.IsNot()],
+            comparators=[ast.Constant(value=None)],
+        ),
+        body=value,
+        orelse=statement,
+    )
