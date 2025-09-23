85a5d374f8887ccdf8fe989290c465977b723c8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 15:05:47 +0000

    Add package abhi77/0.6/abhi77-0.6.tar.gz

---
+++ packages/abhi77/abhi77-0.6.tar.gz/abhi77-0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="abhi77",
+    version="0.6",   # bump version each time
+    packages=find_packages(),
+    install_requires=[],
+    description="Flutter syllabus and example codes package",
+    author="Abhishek Yadav",
+    license="MIT",
+)
