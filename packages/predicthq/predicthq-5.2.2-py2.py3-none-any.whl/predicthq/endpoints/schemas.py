5670cb550a8661ba6e630a187edba2eed98971a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 08:09:41 +0000

    Add package predicthq/5.2.2/predicthq-5.2.2-py2.py3-none-any.whl

---
+++ packages/predicthq/predicthq-5.2.2-py2.py3-none-any.whl/predicthq/endpoints/schemas.py
@@ -0,0 +1,65 @@
+from typing import Callable, Optional
+
+from pydantic import BaseModel, HttpUrl
+
+
+class _Config(BaseModel):
+    verify_ssl: Optional[bool] = False
+
+
+class ConfigMixin(BaseModel):
+    config: _Config
+
+
+class ResultSet(BaseModel):
+    _endpoint: Optional[Callable] = None
+    _more: Optional[Callable] = None
+
+    count: Optional[int] = None
+    previous: Optional[HttpUrl] = None
+    next: Optional[HttpUrl] = None
+
+    def _parse_params(self, url):
+        return dict(url.query_params())
+
+    def has_previous(self):
+        return self.previous is not None
+
+    def has_next(self):
+        return self.next is not None
+
+    def get_next(self):
+        if not self.has_next() or not hasattr(self, "_more"):
+            return
+        params = self._parse_params(self.next)
+        return self._more(**params)
+
+    def get_previous(self):
+        if not self.has_previous() or not hasattr(self, "_more"):
+            return
+        params = self._parse_params(self.previous)
+        return self._more(**params)
+
+    def iter_pages(self):
+        next_page = self
+        while next_page.has_next():
+            next_page = next_page.get_next()
+            yield next_page
+
+    def iter_items(self):
+        return iter(self.results)
+
+    def iter_all(self):
+        for item in self.iter_items():
+            yield item
+        for page in self.iter_pages():
+            for item in page.iter_items():
+                yield item
+
+    def __iter__(self):
+        return self.iter_items()
+
+
+class ArgKwargResultSet(ResultSet):
+    _args: Optional[dict] = None
+    _kwargs: Optional[dict] = None
---
+++ packages/predicthq/predicthq-5.2.2-py2.py3-none-any.whl/predicthq/endpoints/v1/broadcasts/schemas.py
@@ -0,0 +1,94 @@
+from datetime import datetime
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+from predicthq.endpoints.schemas import ResultSet
+
+
+class GeoPoint(BaseModel):
+    lat: float
+    lon: float
+
+
+class BroadcastEventEntities(BaseModel):
+    entity_id: str
+    type: str
+    name: str
+    formatted_address: Optional[str] = None
+
+
+class BroadcastEventLocation(BaseModel):
+    geopoint: GeoPoint
+    place_hierarchies: List[List[str]]
+    country: str
+
+
+class BroadcastEventDates(BaseModel):
+    start: datetime
+    end: Optional[datetime] = None
+    start_local: datetime
+    end_local: Optional[datetime] = None
+    timezone: str
+
+    # predicted_end_local is a paid feature.
+    # It will only show up in your response body if you
+    # have subscribed to it.
+    predicted_end_local: Optional[datetime] = None
+
+
+class BroadcastEvent(BaseModel):
+    event_id: str
+    title: str
+    category: str
+    labels: Optional[List[str]] = None
+    dates: BroadcastEventDates
+    location: BroadcastEventLocation
+    entities: List[BroadcastEventEntities]
+
+    # The following fields are paid features.
+    # They will only show up in your response body if you
+    # have subscribed to them.
+    phq_attendance: Optional[int] = None
+    phq_rank: Optional[int] = None
+    local_rank: Optional[int] = None
+    aviation_rank: Optional[int] = None
+
+
+class Place(BaseModel):
+    place_id: str
+    type: str
+    name: str
+    county: str
+    region: str
+    country: str
+
+
+class BroadcastLocation(BaseModel):
+    geopoint: GeoPoint
+    place_hierarchies: List[List[str]]
+    places: List[Place]
+    country: str
+
+
+class BroadcastDates(BaseModel):
+    start: datetime
+    start_local: datetime
+    timezone: str
+
+
+class Broadcast(BaseModel):
+    broadcast_id: str
+    updated: datetime
+    first_seen: datetime
+    dates: Optional[BroadcastDates] = None
+    location: Optional[BroadcastLocation] = None
+    phq_viewership: Optional[int] = None
+    record_status: str
+    broadcast_status: Optional[str] = None
+    event: Optional[BroadcastEvent] = None
+
+
+class BroadcastResultSet(ResultSet):
+    overflow: bool
+    results: List[Optional[Broadcast]]
