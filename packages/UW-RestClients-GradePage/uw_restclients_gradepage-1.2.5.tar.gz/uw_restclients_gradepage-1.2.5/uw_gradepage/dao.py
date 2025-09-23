73ac8ffc821f54df9088688fcdfdd10c3923137b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 07:06:46 +0000

    Add package UW-RestClients-GradePage/1.2.5/uw_restclients_gradepage-1.2.5.tar.gz

---
+++ packages/UW-RestClients-GradePage/uw_restclients_gradepage-1.2.5.tar.gz/uw_restclients_gradepage-1.2.5/uw_gradepage/dao.py
@@ -0,0 +1,18 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Contains UW GradePage DAO implementations.
+"""
+from restclients_core.dao import DAO
+from os.path import abspath, dirname
+import os
+
+
+class GradePage_DAO(DAO):
+    def service_name(self):
+        return 'gradepage'
+
+    def service_mock_paths(self):
+        path = [abspath(os.path.join(dirname(__file__), "resources"))]
+        return path
