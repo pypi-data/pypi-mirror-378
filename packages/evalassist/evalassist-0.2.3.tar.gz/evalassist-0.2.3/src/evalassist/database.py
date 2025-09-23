cce8a3fcd22f4f3e3e90dab133df42c51ed2ffe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 05:06:05 +0000

    Add package evalassist/0.2.3/evalassist-0.2.3.tar.gz

---
+++ packages/evalassist/evalassist-0.2.3.tar.gz/evalassist-0.2.3/src/evalassist/database.py
@@ -0,0 +1,15 @@
+import alembic.command
+import alembic.config
+from sqlmodel import create_engine
+
+from .const import DATABASE_URL, EVAL_ASSIST_DIR, STORAGE_ENABLED
+from .model import AppUser, LogRecord, StoredTestCase  # noqa: F401
+
+engine = None
+if STORAGE_ENABLED:
+    engine = create_engine(DATABASE_URL)
+
+    alembic_cfg = alembic.config.Config(
+        EVAL_ASSIST_DIR / "alembic.ini", config_args={"sqlalchemy.url": DATABASE_URL}
+    )
+    alembic.command.upgrade(alembic_cfg, "head")
