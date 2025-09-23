b6a12dfdbfbc22884c8e093768ce84f3f98d4f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 03:17:58 +0000

    Add package yoyodyne-pretrained/0.1.4/yoyodyne_pretrained-0.1.4-py3-none-any.whl

---
+++ packages/yoyodyne-pretrained/yoyodyne_pretrained-0.1.4-py3-none-any.whl/yoyodyne_pretrained/cli.py
@@ -0,0 +1,56 @@
+"""Command-line interface."""
+
+import logging
+
+from lightning.pytorch import callbacks as pytorch_callbacks, cli
+from yoyodyne import trainers
+
+from . import callbacks, data, models
+
+
+class YoyodynePretrainedCLI(cli.LightningCLI):
+    """The Yoyodyne Pretrained CLI interface.
+
+    Use with `--help` to see the full list of options.
+    """
+
+    def add_arguments_to_parser(
+        self, parser: cli.LightningArgumentParser
+    ) -> None:
+        parser.add_lightning_class_args(
+            pytorch_callbacks.ModelCheckpoint,
+            "checkpoint",
+            required=False,
+        )
+        parser.add_lightning_class_args(
+            callbacks.PredictionWriter,
+            "prediction",
+            required=False,
+        )
+        parser.link_arguments("model.init_args.model_name", "data.model_name")
+
+
+def main() -> None:
+    logging.basicConfig(
+        format="%(filename)s %(levelname)s: %(asctime)s - %(message)s",
+        datefmt="%d-%b-%y %H:%M:%S",
+        level="INFO",
+    )
+    # Select the model.
+    YoyodynePretrainedCLI(
+        model_class=models.BaseModel,
+        datamodule_class=data.DataModule,
+        subclass_mode_model=True,
+        trainer_class=trainers.Trainer,
+    )
+
+
+def python_interface(args: cli.ArgsType = None) -> None:
+    """Interface to use models through Python."""
+    YoyodynePretrainedCLI(
+        models.BaseModel,
+        data.DataModule,
+        subclass_mode_model=True,
+        trainer_class=trainers.Trainer,
+        args=args,
+    )
