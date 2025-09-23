60fb412f86cd8ee9e8283c0db5edd28d4ea3f0ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 16:06:06 +0000

    Add package modoboa/2.6.0/modoboa-2.6.0.tar.gz

---
+++ packages/modoboa/modoboa-2.6.0.tar.gz/modoboa-2.6.0/modoboa/frontend_dist/assets/VSpacer-BWZq1PhD.js
@@ -0,0 +1,1 @@
+/* empty css              */import{f as r}from"./VAvatar-J563boKh.js";const p=r("v-spacer","div","VSpacer");export{p as V};
---
+++ packages/modoboa/modoboa-2.6.0.tar.gz/modoboa-2.6.0/modoboa/frontend_dist/assets/transports-B9mWU1W8.js
@@ -0,0 +1,1 @@
+import{C as t}from"./index-YIGRKgNA.js";const r="transports",s={getAll(){return t.get(`/${r}/`)}};export{s as t};
