71aaf59c8c3edd6f98fe0fe6beaf2ffa575945f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 03:16:34 +0000

    Add package rafay-workflow-sdk/0.0.31rc14/rafay_workflow_sdk-0.0.31rc14-py3-none-any.whl

---
+++ packages/rafay-workflow-sdk/rafay_workflow_sdk-0.0.31rc14-py3-none-any.whl/python_sdk_rafay_workflow/const.py
@@ -0,0 +1,6 @@
+ActivityIDHeader         = "X-Activity-ID"
+EnvironmentIDHeader      = "X-Environment-ID"
+EnvironmentNameHeader    = "X-Environment-Name"
+WorkflowTokenHeader      = "X-Workflow-Token"
+EngineAPIEndpointHeader  = "X-Engine-Endpoint"
+ActivityFileUploadHeader = "X-Activity-File-Upload"
