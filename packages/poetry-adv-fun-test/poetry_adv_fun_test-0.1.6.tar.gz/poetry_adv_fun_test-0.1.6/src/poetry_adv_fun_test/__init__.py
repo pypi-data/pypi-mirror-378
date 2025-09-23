c7a9d92b4c7f8b0abfe8b25e33ba05d0419f5fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 15:06:37 +0000

    Add package poetry-adv-fun-test/0.1.6/poetry_adv_fun_test-0.1.6.tar.gz

---
+++ packages/poetry-adv-fun-test/poetry_adv_fun_test-0.1.6.tar.gz/poetry_adv_fun_test-0.1.6/src/poetry_adv_fun_test/__init__.py
@@ -0,0 +1,17 @@
+"""
+poetry_adv_fun_test
+A Python library for modeling active and passive electronic components.
+"""
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:  # For Python < 3.8
+    from importlib_metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("poetry-adv-fun-test")
+except PackageNotFoundError:
+    __version__ = "unknown"  # fallback if metadata is missing
+
+from .active import Diode, BJT, MOSFET, JFET, OpAmp
+from .passive import Resistor, Capacitor, Inductor
