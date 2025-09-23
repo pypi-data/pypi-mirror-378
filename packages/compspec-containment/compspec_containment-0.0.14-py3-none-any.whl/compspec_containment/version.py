0a68c07b431a69c8cc0f986512664eb55dae9717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 15:06:32 +0000

    Add package compspec-containment/0.0.14/compspec_containment-0.0.14-py3-none-any.whl

---
+++ packages/compspec-containment/compspec_containment-0.0.14-py3-none-any.whl/compspec_containment/version.py
@@ -0,0 +1,20 @@
+__author__ = "Vanessa Sochat"
+__copyright__ = "Copyright 2024-2026, Vanessa Sochat"
+__license__ = "MIT"
+
+__version__ = "0.0.14"
+AUTHOR = "Vanessa Sochat"
+AUTHOR_EMAIL = "vsoch@users.noreply.github.com"
+NAME = "compspec-containment"
+PACKAGE_URL = "https://github.com/compspec/compspec-containment"
+KEYWORDS = "compatibility, compspec, containment, flux, subsystem, scheduling, hpc"
+DESCRIPTION = "Subsystem graph generation for the Flux containment subsystem"
+LICENSE = "LICENSE"
+
+################################################################################
+# Global requirements
+
+INSTALL_REQUIRES = (("compspec", {"min_version": "0.1.0"}),)
+
+TESTS_REQUIRES = (("pytest", {"min_version": "4.6.2"}),)
+INSTALL_REQUIRES_ALL = INSTALL_REQUIRES + TESTS_REQUIRES
