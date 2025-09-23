f0a23422d923ea3ce5527a18c643bc7a0333ba3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 05:05:42 +0000

    Add package SidraDataProductSDK/2025.8.1.post180/SidraDataProductSDK-2025.8.1.post180-py3-none-any.whl

---
+++ packages/SidraDataProductSDK/SidraDataProductSDK-2025.8.1.post180-py3-none-any.whl/SidraDataProductSDK/api/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from SidraDataProductSDK.api.asset_api import AssetApi
+from SidraDataProductSDK.api.azure_search_api import AzureSearchApi
+from SidraDataProductSDK.api.configuration_api import ConfigurationApi
+from SidraDataProductSDK.api.data_source_api import DataSourceApi
+from SidraDataProductSDK.api.group_api import GroupApi
+from SidraDataProductSDK.api.model_api import ModelApi
+from SidraDataProductSDK.api.pipeline_api import PipelineApi
+from SidraDataProductSDK.api.refresh_api import RefreshApi
+from SidraDataProductSDK.api.role_member_api import RoleMemberApi
+from SidraDataProductSDK.api.staging_configuration_api import StagingConfigurationApi
+from SidraDataProductSDK.api.status_api import StatusApi
+from SidraDataProductSDK.api.sync_api import SyncApi
+from SidraDataProductSDK.api.validation_api import ValidationApi
