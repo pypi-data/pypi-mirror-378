8897080b523f1b604361ae0705fab0b1d4ad35ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 05:05:50 +0000

    Add package ome-zarr-models/1.1/ome_zarr_models-1.1.tar.gz

---
+++ packages/ome-zarr-models/ome_zarr_models-1.1.tar.gz/ome_zarr_models-1.1/src/ome_zarr_models/_v06/multiscales.py
@@ -0,0 +1,9 @@
+from ome_zarr_models.common.multiscales import Dataset, MultiscaleBase
+
+__all__ = ["Dataset", "Multiscale"]
+
+
+class Multiscale(MultiscaleBase):
+    """
+    An element of multiscales metadata.
+    """
