068f3b59807ea4cf210de8a7ff5c32b816dd5a39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 15:05:59 +0000

    Add package rubka-webapi/3.7.6/rubka_webapi-3.7.6.tar.gz

---
+++ packages/rubka-webapi/rubka_webapi-3.7.6.tar.gz/rubka_webapi-3.7.6/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "rubka_webapi",
+    version = "3.7.6",
+    author="httex",
+    author_email = "wanwsnys69@gmail.com",
+    description="یک کتابخانه قدرتمند و آسان برای ساخت ربات‌های خودکار در روبیکا.",
+    keywords = ["rubika", "rubino", "pyrubi", "pyrubika", "bot", "chat"],
+    long_description = open("README.md", encoding="utf-8").read(),
+    python_requires="~=3.7",
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/xetay12/rubka_webapi",
+    packages = find_packages(),
+    install_requires = ["urllib3", "tqdm", "websocket-client", "pycryptodome", "mutagen", "filetype"],
+    classifiers = [
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Topic :: Internet",
+        "Topic :: Communications",
+        "Topic :: Communications :: Chat",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Libraries :: Application Frameworks"
+    ]
+)
