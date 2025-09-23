9abf04e518ab93b6144418e445e53efd71c08fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 14:06:22 +0000

    Add package zenml-nightly/0.85.0.dev20250923/zenml_nightly-0.85.0.dev20250923.tar.gz

---
+++ packages/zenml-nightly/zenml_nightly-0.85.0.dev20250923.tar.gz/zenml_nightly-0.85.0.dev20250923/src/zenml/zen_server/dashboard/assets/Tick-B5JTYJPV.js
@@ -0,0 +1,1 @@
+import{j as s}from"./@radix-CliVkFaa.js";import{bo as c,c as i}from"./index-DNZysKmJ.js";import{S as e}from"./check-kZOtsBOj.js";function n({tickClasses:r,...o}){return s.jsx(c,{...o,children:s.jsx(e,{className:i("h-4 w-4 fill-success-300",r)})})}export{n as T};
