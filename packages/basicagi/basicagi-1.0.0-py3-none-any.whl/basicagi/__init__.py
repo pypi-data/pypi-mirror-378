267bb13a34ff48b14f303635d1d50eb3e313261c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 05:05:46 +0000

    Add package basicagi/1.0.0/basicagi-1.0.0-py3-none-any.whl

---
+++ packages/basicagi/basicagi-1.0.0-py3-none-any.whl/basicagi/__init__.py
@@ -0,0 +1,24 @@
+"""
+BasicAGI - Asterisk AGI Python Library
+
+A Python library for creating Asterisk Gateway Interface (AGI) applications.
+This library provides a simple and intuitive interface for interacting with
+Asterisk PBX through the AGI protocol.
+
+Basic usage:
+    from basicagi import BasicAGI
+
+    agi = BasicAGI()
+    agi.answer()
+    agi.stream_file("welcome")
+    agi.hangup()
+"""
+
+from .basic_agi import BasicAGI
+
+__version__ = "1.0.0"
+__author__ = "Andrius Kairiukstis"
+__email__ = "k@andrius.mobi"
+__description__ = "A Python library for Asterisk Gateway Interface (AGI) applications"
+
+__all__ = ["BasicAGI"]
