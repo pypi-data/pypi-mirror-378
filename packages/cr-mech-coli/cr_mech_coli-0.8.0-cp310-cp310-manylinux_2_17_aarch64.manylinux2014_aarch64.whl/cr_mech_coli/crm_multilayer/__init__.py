1f8f666523efa6432d97bc74e950f4d867f704ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 07:07:33 +0000

    Add package cr-mech-coli/0.8.0/cr_mech_coli-0.8.0-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/cr-mech-coli/cr_mech_coli-0.8.0-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/cr_mech_coli/crm_multilayer/__init__.py
@@ -0,0 +1,8 @@
+"""
+.. code-block:: text
+    :caption: Usage of the `crm_multilayer` script
+"""
+
+from cr_mech_coli.crm_multilayer.crm_multilayer_rs import *
+
+from .main import crm_multilayer_main
