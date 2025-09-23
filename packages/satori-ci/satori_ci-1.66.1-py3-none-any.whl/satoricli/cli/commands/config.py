d36dc052e9c5cc8e379cf7c08e7e101f6f27c6e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 07:06:51 +0000

    Add package satori-ci/1.66.1/satori_ci-1.66.1-py3-none-any.whl

---
+++ packages/satori-ci/satori_ci-1.66.1-py3-none-any.whl/satoricli/cli/commands/config.py
@@ -0,0 +1,53 @@
+from argparse import ArgumentParser
+from typing import Optional
+
+from satoricli.cli.utils import console, error_console
+from satoricli.utils import load_config, save_config
+
+from .base import BaseCommand
+
+
+class ConfigCommand(BaseCommand):
+    name = "config"
+
+    def register_args(self, parser: ArgumentParser):
+        parser.add_argument(
+            "key",
+            metavar="KEY",
+            choices=("token", "host", "timeout", "default_team"),
+            nargs="?",
+        )
+        parser.add_argument("value", metavar="VALUE", nargs="?")
+        parser.add_argument(
+            "-d", "--delete", action="store_true", help="delete config key"
+        )
+
+    def __call__(
+        self, key: Optional[str], value: Optional[str], delete: bool, **kwargs
+    ):
+        config = load_config()
+
+        if delete and key:
+            del config[kwargs["profile"]][key]
+            save_config(config)
+            return
+        elif delete and key is None:
+            console.print("Must provide a key to delete")
+            return 1
+
+        if key is None:
+            console.print(config[kwargs["profile"]])
+            return
+
+        if value is None:
+            try:
+                console.print(config[kwargs["profile"]][key])
+            except KeyError:
+                error_console.print(
+                    f"ERROR: {key} missing in profile {kwargs['profile']}"
+                )
+            return 1
+
+        config.setdefault(kwargs["profile"], {})[key] = value
+        console.print(f"{key} updated for profile {kwargs['profile']}")
+        save_config(config)
