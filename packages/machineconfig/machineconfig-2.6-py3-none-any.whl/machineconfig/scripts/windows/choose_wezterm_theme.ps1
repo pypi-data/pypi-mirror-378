7fa769e86140f59f6cf0e4f2e5464abd88a72f59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 14:06:28 +0000

    Add package machineconfig/2.6/machineconfig-2.6-py3-none-any.whl

---
+++ packages/machineconfig/machineconfig-2.6-py3-none-any.whl/machineconfig/scripts/windows/choose_wezterm_theme.ps1
@@ -0,0 +1,1 @@
+uv run --python 3.13 --with machineconfig python -m fire machineconfig.scripts.python.choose_wezterm_theme main2 $args[0]
---
+++ packages/machineconfig/machineconfig-2.6-py3-none-any.whl/machineconfig/scripts/windows/cloud_repo_sync.ps1
@@ -0,0 +1,1 @@
+uv run --python 3.13 --with machineconfig python -m machineconfig.scripts.python.cloud_repo_sync $args
