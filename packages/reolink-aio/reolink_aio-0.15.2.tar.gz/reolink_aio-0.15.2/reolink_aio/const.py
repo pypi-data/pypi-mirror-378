45b5a80c7e0928bdc08421b40e926716ee7d368d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 07:06:24 +0000

    Add package reolink-aio/0.15.2/reolink_aio-0.15.2.tar.gz

---
+++ packages/reolink-aio/reolink_aio-0.15.2.tar.gz/reolink_aio-0.15.2/reolink_aio/const.py
@@ -0,0 +1,62 @@
+"""Constants for Reolink"""
+
+WAKING_COMMANDS = {
+    "GetWhiteLed",
+    "GetZoomFocus",
+    "GetAudioCfg",
+    "GetPtzGuard",
+    "GetAutoReply",
+    "GetPtzTraceSection",
+    "GetAiCfg",
+    "GetAiAlarm",
+    "GetPtzCurPos",
+    "GetAudioAlarm",
+    "GetDingDongList",
+    "GetDingDongCfg",
+    "DingDongOpt",
+    "GetPerformance",
+    "GetMask",
+    "296",
+    "483",
+}
+
+NONE_WAKING_COMMANDS = {
+    "GetEnc",
+    "GetIsp",
+    "GetEvents",
+    "GetMdState",
+    "GetAiState",
+    "GetIrLights",
+    "GetBatteryInfo",
+    "GetPirInfo",
+    "GetPowerLed",
+    "GetAutoFocus",
+    "GetDeviceAudioCfg",
+    "GetManualRec",
+    "GetImage",
+    "GetBuzzerAlarmV20",
+    "GetEmailV20",
+    "GetEmail",
+    "GetPushV20",
+    "GetPush",
+    "GetFtpV20",
+    "GetFtp",
+    "GetRecV20",
+    "GetRec",
+    "GetAudioAlarmV20",
+    "GetMdAlarm",
+    "GetAlarm",
+    "GetStateLight",
+    "GetHddInfo",
+    "GetChannelstatus",
+    "GetPushCfg",
+    "GetScene",
+    "115",
+    "208",
+    "594",
+}
+
+UNKNOWN = "Unknown"
+
+MIN_COLOR_TEMP = 3000
+MAX_COLOR_TEMP = 6000
