c0742d8f8d26e1e482e34958cc3ef42a388838d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 07:06:26 +0000

    Add package marimo/0.16.1/marimo-0.16.1.tar.gz

---
+++ packages/marimo/marimo-0.16.1.tar.gz/marimo-0.16.1/marimo/_static/assets/_baseMap-CtlwA90f.js
@@ -0,0 +1,1 @@
+import{b as m}from"./_baseEach-CvTX9w0Y.js";import{A as s}from"./index-C7CoaNFb.js";function e(r,o){var a=-1,t=s(r)?Array(r.length):[];return m(r,function(n,f,i){t[++a]=o(n,f,i)}),t}export{e as b};
