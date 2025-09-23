ae65ff01b8ded140f5a5d3487d824dd55d2ca186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 16:06:27 +0000

    Add package zope.security/8.1/zope_security-8.1.tar.gz

---
+++ packages/zope.security/zope_security-8.1.tar.gz/zope_security-8.1/README.rst
@@ -0,0 +1,28 @@
+===================
+ ``zope.security``
+===================
+
+.. image:: https://github.com/zopefoundation/zope.security/actions/workflows/tests.yml/badge.svg
+        :target: https://github.com/zopefoundation/zope.security/actions/workflows/tests.yml
+
+.. image:: https://coveralls.io/repos/github/zopefoundation/zope.security/badge.svg?branch=master
+        :target: https://coveralls.io/github/zopefoundation/zope.security?branch=master
+
+.. image:: https://readthedocs.org/projects/zopesecurity/badge/?version=latest
+        :target: https://zopesecurity.readthedocs.io/en/latest/
+        :alt: Documentation Status
+
+.. image:: https://img.shields.io/pypi/v/zope.security.svg
+        :target: https://pypi.python.org/pypi/zope.security/
+        :alt: Latest release
+
+.. image:: https://img.shields.io/pypi/pyversions/zope.security.svg
+        :target: https://pypi.org/project/zope.security/
+        :alt: Supported Python versions
+
+
+The Security framework provides a generic mechanism to implement security
+policies on Python objects.
+
+Documentation is available at https://zopesecurity.readthedocs.io/
+
---
+++ packages/zope.security/zope_security-8.1.tar.gz/zope_security-8.1/setup.py
@@ -0,0 +1,185 @@
+#############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.security package
+"""
+import os
+import platform
+import sys
+from distutils.errors import CCompilerError
+from distutils.errors import DistutilsExecError
+from distutils.errors import DistutilsPlatformError
+
+from setuptools import Extension
+from setuptools import setup
+from setuptools.command.build_ext import build_ext
+
+
+version = '8.1'
+
+
+class optional_build_ext(build_ext):
+    """This class subclasses build_ext and allows
+       the building of C extensions to fail.
+    """
+
+    def run(self):
+        try:
+            build_ext.run(self)
+        except DistutilsPlatformError as e:
+            self._unavailable(e)
+
+    def build_extension(self, ext):
+        try:
+            build_ext.build_extension(self, ext)
+        except (CCompilerError, DistutilsExecError, OSError) as e:
+            self._unavailable(e)
+
+    def _unavailable(self, e):
+        print('*' * 80)
+        print("""WARNING:
+        An optional code optimization (C extension) could not be compiled.
+        Optimizations for this package will not be available!""")
+        print()
+        print(e)
+        print('*' * 80)
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+
+codeoptimization = [
+    Extension(
+        "zope.security._proxy",
+        include_dirs=[os.path.join('include', 'zope.proxy')],
+        sources=[os.path.join('src', 'zope', 'security', "_proxy.c")]
+    ),
+    Extension(
+        "zope.security._zope_security_checker",
+        [os.path.join('src', 'zope', 'security',
+                      "_zope_security_checker.c")]
+    ),
+]
+
+
+# Jython cannot build the C optimizations, while on PyPy they are
+# anti-optimizations (the C extension compatibility layer is known-slow,
+# and defeats JIT opportunities).
+py_impl = getattr(platform, 'python_implementation', lambda: None)
+is_pypy = py_impl() == 'PyPy'
+is_jython = 'java' in sys.platform
+
+if is_pypy or is_jython:
+    ext_modules = []
+else:
+    ext_modules = codeoptimization
+
+
+TESTS_REQUIRE = [
+    'BTrees',
+    'zope.component',
+    'zope.configuration',
+    'zope.location',
+    'zope.testing',
+    'zope.testrunner >= 6.4',
+]
+
+
+setup(name='zope.security',
+      version=version,
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.dev',
+      description='Zope Security Framework',
+      long_description=(
+          read('README.rst')
+          + '\n\n' +
+          read('CHANGES.rst')
+      ),
+      keywords="zope security policy principal permission",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
+          'Programming Language :: Python :: 3.12',
+          'Programming Language :: Python :: 3.13',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope :: 3',
+      ],
+      url='http://github.com/zopefoundation/zope.security',
+      project_urls={
+          'Documentation': 'https://zopesecurity.readthedocs.io',
+          'Issue Tracker': ('https://github.com/zopefoundation'
+                            '/zope.security/issues'),
+          'Sources': 'https://github.com/zopefoundation/zope.security',
+      },
+      license='ZPL-2.1',
+      # we need the following two parameters because we compile C code,
+      # otherwise only the shared library is installed:
+      package_dir={'': 'src'},
+      packages=['zope.security',],
+      cmdclass={
+          'build_ext': optional_build_ext,
+      },
+      ext_modules=ext_modules,
+      python_requires='>=3.9',
+      install_requires=[
+          'setuptools',
+          'zope.component',
+          'zope.i18nmessageid',
+          'zope.interface',
+          'zope.location',
+          'zope.proxy >= 6.1',
+          'zope.schema >= 4.2.0',
+      ],
+      extras_require={
+          'pytz': [
+              "pytz"
+          ],
+          'untrustedpython': [
+              'zope.untrustedpython >= 5.0.dev0',
+          ],
+          'zcml': [
+              'zope.configuration'
+          ],
+          'test': TESTS_REQUIRE,
+          'docs': [
+              'Sphinx',
+              'repoze.sphinx.autointerface',
+              'sphinx_rtd_theme',
+              'zope.configuration',
+              'zope.testing',
+          ],
+      },
+      include_package_data=True,
+      zip_safe=False,
+      )
