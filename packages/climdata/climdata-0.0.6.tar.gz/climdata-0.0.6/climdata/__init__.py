569bf9b32d8ee633d5c14de2736e77f998309adc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 04:05:30 +0000

    Add package climdata/0.0.6/climdata-0.0.6.tar.gz

---
+++ packages/climdata/climdata-0.0.6.tar.gz/climdata-0.0.6/climdata/__init__.py
@@ -0,0 +1,12 @@
+"""Top-level package for climdata."""
+
+__author__ = """Kaushik Muduchuru"""
+__email__ = "kaushik.reddy.m@gmail.com"
+__version__ = "0.0.6"
+
+from .utils.utils_download import * # etc.
+from .utils.config import load_config
+from .datasets.DWD import DWDmirror as DWD
+from .datasets.MSWX import MSWXmirror as MSWX
+from .datasets.ERA5 import ERA5Mirror as ERA5
+
