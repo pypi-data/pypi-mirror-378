3a2e056f0bbe76134b29a6cf364173d7edeff764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 17:06:11 +0000

    Add package TyphonBreaker/1.0.7.2/typhonbreaker-1.0.7.2-py3-none-any.whl

---
+++ packages/TyphonBreaker/typhonbreaker-1.0.7.2-py3-none-any.whl/Typhon/__init__.py
@@ -0,0 +1,12 @@
+# Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
+
+__version__ = "1.0.7.2"
+
+from .Typhon import bypassRCE, bypassREAD, bypassMAIN
+
+
+__all__ = [
+    "bypassMAIN",
+    "bypassRCE",
+    "bypassREAD",
+]
