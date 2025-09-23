b2d3b7f91c3bdf82f3c99ac28867d6e9e0af1946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 05:06:18 +0000

    Add package kof-parser/0.1.4/kof_parser-0.1.4-py3-none-any.whl

---
+++ packages/kof-parser/kof_parser-0.1.4-py3-none-any.whl/kof_parser/model.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+
+
+class Location(BaseModel):
+    name: str = ""
+    methods: list[str] = Field(default_factory=list)
+    point_easting: float | None = None
+    point_northing: float | None = None
+    point_z: float | None = Field(None, ge=-10000, le=10000)
+    srid: int | None = None
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__} {self.name} ({self.point_easting}, {self.point_northing}, {self.point_z})>"
