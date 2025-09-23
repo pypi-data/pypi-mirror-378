bc1dca1d72a60b71acb487ae81ce6dd4a66daf6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 07:06:54 +0000

    Add package sl-transit-repl/0.1.0/sl_transit_repl-0.1.0-py3-none-any.whl

---
+++ packages/sl-transit-repl/sl_transit_repl-0.1.0-py3-none-any.whl/sl_transit_repl/__init__.py
@@ -0,0 +1,6 @@
+"""SL Transit REPL - A command-line tool for Stockholm public transit departures."""
+
+from .main import SLTransitREPL, main
+
+__version__ = "0.1.0"
+__all__ = ["SLTransitREPL", "main"]
