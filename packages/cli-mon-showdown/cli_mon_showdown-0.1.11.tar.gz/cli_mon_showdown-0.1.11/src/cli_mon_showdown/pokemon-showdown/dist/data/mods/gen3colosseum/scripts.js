44253271cbb790b428d19a76bd828aa03275c963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 06:05:49 +0000

    Add package cli-mon-showdown/0.1.11/cli_mon_showdown-0.1.11.tar.gz

---
+++ packages/cli-mon-showdown/cli_mon_showdown-0.1.11.tar.gz/cli_mon_showdown-0.1.11/src/cli_mon_showdown/pokemon-showdown/dist/data/mods/gen3colosseum/scripts.js
@@ -0,0 +1,40 @@
+"use strict";
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+var scripts_exports = {};
+__export(scripts_exports, {
+  Scripts: () => Scripts
+});
+module.exports = __toCommonJS(scripts_exports);
+const Scripts = {
+  inherit: "gen3",
+  gen: 3,
+  checkWin(faintData) {
+    if (this.sides.every((side) => !side.pokemonLeft)) {
+      this.win(faintData ? faintData.target.side : null);
+      return true;
+    }
+    for (const side of this.sides) {
+      if (!side.foePokemonLeft()) {
+        this.win(side);
+        return true;
+      }
+    }
+  }
+};
+//# sourceMappingURL=scripts.js.map
