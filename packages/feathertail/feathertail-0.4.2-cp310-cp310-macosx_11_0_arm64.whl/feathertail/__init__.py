8fc23d250efd92ac011cb69c0666a36f0ed11d26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 05:06:09 +0000

    Add package feathertail/0.4.2/feathertail-0.4.2-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/feathertail/feathertail-0.4.2-cp310-cp310-macosx_11_0_arm64.whl/feathertail/__init__.py
@@ -0,0 +1,10 @@
+"""
+Feathertail - A tiny, fast, Rust-backed transformation core for Python table data
+"""
+
+# Import the compiled module
+from .feathertail import *
+
+__version__ = "0.4.2"
+__author__ = "Odos Matthews"
+__email__ = "odosmatthews@gmail.com"
