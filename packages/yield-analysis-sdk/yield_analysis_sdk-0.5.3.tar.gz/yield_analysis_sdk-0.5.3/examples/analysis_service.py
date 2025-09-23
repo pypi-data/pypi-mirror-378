4124cbe5804fe053c52a9657dc6cd0801e033135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 03:17:23 +0000

    Add package yield-analysis-sdk/0.5.3/yield_analysis_sdk-0.5.3.tar.gz

---
+++ packages/yield-analysis-sdk/yield_analysis_sdk-0.5.3.tar.gz/yield_analysis_sdk-0.5.3/examples/analysis_service.py
@@ -0,0 +1,135 @@
+import json
+import threading
+import time
+from typing import Any, Dict, Optional
+
+from dotenv import load_dotenv
+from pydantic import BaseModel, Field, ValidationError
+from virtuals_acp import ACPJob, ACPJobPhase, ACPMemo, VirtualsACP
+
+load_dotenv(override=True)
+
+from virtuals_acp.env import EnvSettings
+
+from yield_analysis_sdk import (
+    AnalysisRequest,
+    AnalysisResponse,
+    AnalysisResult,
+    Chain,
+    SharePriceHistory,
+    VaultInfo,
+    analyze_yield_with_daily_share_price,
+    get_daily_share_price_history_from_subgraph,
+    normalize_address,
+)
+
+
+class CustomEnvSettings(EnvSettings):
+    SUBGRAPH_API_KEY: Optional[str] = None
+
+
+USDC_TOKEN_ADDRESS = {
+    Chain.BASE: "0x833589fcd6edb6e08f4c7c32d4f71b54bda02913",
+}
+
+USDC_VAULT_ADDRESSES = {
+    Chain.BASE: [
+        "0xc1256Ae5FF1cf2719D4937adb3bbCCab2E00A2Ca",
+        "0x616a4E1db48e22028f6bbf20444Cd3b8e3273738",
+    ]
+}
+
+ID_TO_CHAIN = {
+    1: Chain.ETHEREUM,
+    8453: Chain.BASE,
+    42161: Chain.ARBITRUM,
+    10: Chain.OPTIMISM,
+    137: Chain.POLYGON,
+    56: Chain.BSC,
+    33139: Chain.GNOS,
+}
+
+
+def seller():
+    env = CustomEnvSettings()
+
+    def on_new_task(job: ACPJob):
+        # Convert job.phase to ACPJobPhase enum if it's an integer
+        if job.phase == ACPJobPhase.REQUEST:
+            # Check if there's a memo that indicates next phase is NEGOTIATION
+            for memo in job.memos:
+                if memo.next_phase == ACPJobPhase.NEGOTIATION:
+                    try:
+                        AnalysisRequest.model_validate_json(memo.content)
+                        job.respond(True)
+                    except ValidationError as e:
+                        job.respond(False, "Invalid request format")
+                    finally:
+                        break
+        elif job.phase == ACPJobPhase.TRANSACTION:
+
+            for memo in job.memos:
+                if memo.next_phase == ACPJobPhase.EVALUATION:
+                    # Check if there's a memo that indicates next phase is EVALUATION
+                    analysis_request = AnalysisRequest.model_validate_json(
+                        job.service_requirement
+                    )
+
+                    # for test purpose, only support strategies with same chainId
+                    # fetch price history
+                    price_histories = get_daily_share_price_history_from_subgraph(
+                        ID_TO_CHAIN[analysis_request.strategies[0].chainId],
+                        [
+                            strategy.address.lower()
+                            for strategy in analysis_request.strategies
+                        ],
+                        6,
+                        90,
+                        env.SUBGRAPH_API_KEY,
+                    )
+
+                    # analyze yield
+                    result: AnalysisResponse = AnalysisResponse(analyses=[])
+                    for price_history in price_histories:
+                        result.analyses.append(
+                            AnalysisResult(
+                                vault_info=VaultInfo(
+                                    chain=analysis_request.chain,
+                                    address=price_history.address,
+                                    name=price_history.name,
+                                    protocol="Morpho Meta Vault",
+                                    last_updated_timestamp=int(time.time()),
+                                ),
+                                performance=analyze_yield_with_daily_share_price(
+                                    price_history,
+                                ),
+                            )
+                        )
+
+                    print(f"Delivering analysis result: {result.model_dump_json()}")
+                    delivery_data = {"type": "text", "value": result.model_dump_json()}
+
+                    # deliver job
+                    job.deliver(json.dumps(delivery_data))
+                    break
+
+    if env.WHITELISTED_WALLET_PRIVATE_KEY is None:
+        raise ValueError("WHITELISTED_WALLET_PRIVATE_KEY is not set")
+    if env.SELLER_ENTITY_ID is None:
+        raise ValueError("SELLER_ENTITY_ID is not set")
+
+    # Initialize the ACP client
+    VirtualsACP(
+        wallet_private_key=env.WHITELISTED_WALLET_PRIVATE_KEY,
+        agent_wallet_address=env.SELLER_AGENT_WALLET_ADDRESS,
+        on_new_task=on_new_task,
+        entity_id=env.SELLER_ENTITY_ID,
+    )
+
+    print("Waiting for new task...")
+    # Keep the script running to listen for new tasks
+    threading.Event().wait()
+
+
+if __name__ == "__main__":
+    seller()
---
+++ packages/yield-analysis-sdk/yield_analysis_sdk-0.5.3.tar.gz/yield_analysis_sdk-0.5.3/examples/registration.py
@@ -0,0 +1,111 @@
+import time
+from datetime import datetime, timedelta
+
+from dotenv import load_dotenv
+from virtuals_acp.client import VirtualsACP
+from virtuals_acp.env import EnvSettings
+from virtuals_acp.job import ACPJob
+from virtuals_acp.models import ACPAgentSort, ACPJobPhase
+
+from yield_analysis_sdk import Chain, Contract, RegistrationRequest
+
+load_dotenv(override=True)
+
+VAULT_USDC_MORPHO_SPARK = "0x236919F11ff9eA9550A4287696C2FC9e18E6e890"
+BIOS_AGENT_ADDRESS = "0xA908B2A981ae1106e5B047ef44604CCaA1E1c7F9"
+
+# --- Configuration for the job polling interval ---
+POLL_INTERVAL_SECONDS = 20
+
+
+# --------------------------------------------------
+
+
+def buyer():
+    env = EnvSettings()
+    acp = VirtualsACP(
+        wallet_private_key=env.WHITELISTED_WALLET_PRIVATE_KEY,
+        agent_wallet_address=env.BUYER_AGENT_WALLET_ADDRESS,
+        entity_id=env.BUYER_ENTITY_ID,
+    )
+    print(f"Buyer ACP Initialized. Agent: {acp.agent_address}")
+
+    # # Browse available agents based on a keyword and cluster name
+    # relevant_agents = acp.browse_agents(
+    #     keyword="<your_filter_agent_keyword>",
+    #     cluster="<your_cluster_name>",
+    #     graduated=False,
+    # )
+    # print(f"Relevant agents: {relevant_agents}")
+
+    # # Pick one of the agents based on your criteria (in this example we just pick the first one)
+    # chosen_agent = relevant_agents[0]
+
+    chosen_agent = acp.get_agent(wallet_address=BIOS_AGENT_ADDRESS)
+
+    # Pick one of the service offerings based on your criteria (in this example we just pick the first one)
+    chosen_job_offering = chosen_agent.offerings[0]
+
+    print()
+
+    # 1. Initiate Job
+    print(
+        f"\nInitiating job with Seller: {chosen_agent.wallet_address}, Evaluator: {env.EVALUATOR_AGENT_WALLET_ADDRESS}"
+    )
+
+    job_id = chosen_job_offering.initiate_job(
+        # <your_schema_field> can be found in your ACP Visualiser's "Edit Service" pop-up.
+        # Reference: (./images/specify_requirement_toggle_switch.png)
+        service_requirement=RegistrationRequest(
+            vault=Contract(
+                chain=Chain.BASE,
+                address=VAULT_USDC_MORPHO_SPARK,
+            ),
+            contracts=[
+                Contract(
+                    chain=Chain.BASE,
+                    address="0x239A8F7778E5C57B4237733E4448f915C8112b58",
+                )
+            ],
+            github_repo_url="https://github.com/yield-analysis/yield-analysis-sdk",
+        ).model_dump_json(),
+        evaluator_address=env.BUYER_AGENT_WALLET_ADDRESS,
+        expired_at=datetime.now() + timedelta(days=1),
+    )
+
+    print(f"Job {job_id} initiated")
+    # 2. Wait for Seller's acceptance memo (which sets next_phase to TRANSACTION)
+    print(f"\nWaiting for Seller to accept job {job_id}...")
+
+    while True:
+        # wait for some time before checking job again
+        time.sleep(POLL_INTERVAL_SECONDS)
+        job: ACPJob = acp.get_job_by_onchain_id(job_id)
+        print(f"Polling Job {job_id}: Current Phase: {job.phase.name}")
+
+        if job.phase == ACPJobPhase.NEGOTIATION:
+            # Check if there's a memo that indicates next phase is TRANSACTION
+            for memo in job.memos:
+                if memo.next_phase == ACPJobPhase.TRANSACTION:
+                    print("Paying job", job_id)
+                    job.pay(job.price)
+        elif job.phase == ACPJobPhase.REQUEST:
+            print(f"Job {job_id} still in REQUEST phase. Waiting for seller...")
+        elif job.phase == ACPJobPhase.EVALUATION:
+            print(f"Job {job_id} is in EVALUATION.")
+            job.evaluate(True)
+            print(f"Job {job_id} evaluated")
+        elif job.phase == ACPJobPhase.TRANSACTION:
+            print(f"Job {job_id} is in TRANSACTION. Waiting for seller to deliver...")
+        elif job.phase == ACPJobPhase.COMPLETED:
+            print("Job completed", job)
+            break
+        elif job.phase == ACPJobPhase.REJECTED:
+            print("Job rejected", job)
+            break
+
+    print("\n--- Buyer Script Finished ---")
+
+
+if __name__ == "__main__":
+    buyer()
