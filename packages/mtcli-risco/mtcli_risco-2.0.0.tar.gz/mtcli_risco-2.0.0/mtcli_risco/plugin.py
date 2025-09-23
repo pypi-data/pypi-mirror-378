ad320fd0ce299961f986e998cc06d7395e8caadc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 07:06:40 +0000

    Add package mtcli-risco/2.0.0/mtcli_risco-2.0.0.tar.gz

---
+++ packages/mtcli-risco/mtcli_risco-2.0.0.tar.gz/mtcli_risco-2.0.0/mtcli_risco/plugin.py
@@ -0,0 +1,20 @@
+"""Comando risco adiciona risco ao mtcli."""
+
+import click
+from .checar import checar
+from .monitorar import monitorar
+
+
+@click.group("risco")
+@click.version_option(package_name="mtcli-risco")
+def cli():
+    """Monitora e bloqueia ordens se o limite de prejuízo for atingido."""
+    pass
+
+
+cli.add_command(checar)
+cli.add_command(monitorar)
+
+
+if __name__ == "__main__":
+    cli()
