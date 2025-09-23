a9baa9d2f0e30a26c26f4e95f89b7bdc862a0d45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 14:06:16 +0000

    Add package plain/0.66.0/plain-0.66.0.tar.gz

---
+++ packages/plain/plain-0.66.0.tar.gz/plain-0.66.0/tests/app/settings.py
@@ -0,0 +1,12 @@
+ALLOWED_HOSTS = []
+SECRET_KEY = "secret"
+DEBUG = True
+
+URLS_ROUTER = "app.urls.AppRouter"
+
+INSTALLED_PACKAGES = [
+    "app.test",
+]
+
+EXPLICIT_SETTING = "explicitly changed"
+ENV_OVERRIDDEN_SETTING = "explicitly overridden"
