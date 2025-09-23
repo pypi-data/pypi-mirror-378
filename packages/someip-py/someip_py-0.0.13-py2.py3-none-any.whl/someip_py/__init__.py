2013fdbc4e36af2cf4134158122039b490326adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 15:06:32 +0000

    Add package someip-py/0.0.13/someip_py-0.0.13-py2.py3-none-any.whl

---
+++ packages/someip-py/someip_py-0.0.13-py2.py3-none-any.whl/someip_py/__init__.py
@@ -0,0 +1,5 @@
+from someip_py.interface import SOMEIPService
+
+__version__ = "0.0.13"
+
+__all__ = ["SOMEIPService"]
---
+++ packages/someip-py/someip_py-0.0.13-py2.py3-none-any.whl/someip_py/service_interface/p_2x/ADCU_HmiService/base.py
@@ -0,0 +1,10 @@
+from someip_py.codec import Int32, SomeIpPayload
+
+
+class VehiclePoint(SomeIpPayload):
+    PositionXSeN: Int32
+    PositionYSeN: Int32
+
+    def __init__(self):
+        self.PositionXSeN = Int32(0)
+        self.PositionYSeN = Int32(0)
