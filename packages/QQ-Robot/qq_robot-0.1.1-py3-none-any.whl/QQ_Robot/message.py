57e036f67066325782c408d2d2fcf36708ec82af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 16:06:19 +0000

    Add package QQ-Robot/0.1.1/qq_robot-0.1.1-py3-none-any.whl

---
+++ packages/QQ-Robot/qq_robot-0.1.1-py3-none-any.whl/QQ_Robot/message.py
@@ -0,0 +1,21 @@
+import json
+
+from fastapi import APIRouter, Request
+from starlette.responses import JSONResponse
+
+from QQ_Robot import processor
+
+router = APIRouter(prefix="/message")
+
+#此API端点为接收机器人信息而设计
+@router.post("/recv")
+async def recv_message(request: Request):
+    try:
+        data = await request.json()
+        await processor.covertMessageAndSend(data)
+        return {"status": "ok"}
+    except UnicodeDecodeError as e:
+        return JSONResponse(status_code=400, content={"error": f"Unicode decode error: {str(e)}"})
+    except json.JSONDecodeError as e:
+        return JSONResponse(status_code=400, content={"error": f"Invalid JSON: {str(e)}"})
+
