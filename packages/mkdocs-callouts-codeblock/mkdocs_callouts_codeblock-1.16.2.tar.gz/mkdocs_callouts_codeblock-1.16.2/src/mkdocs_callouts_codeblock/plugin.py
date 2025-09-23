9fe8b1b9c68cc57b8af537e1322a53fdbabbbc1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 15:06:34 +0000

    Add package mkdocs-callouts-codeblock/1.16.2/mkdocs_callouts_codeblock-1.16.2.tar.gz

---
+++ packages/mkdocs-callouts-codeblock/mkdocs_callouts_codeblock-1.16.2.tar.gz/mkdocs_callouts_codeblock-1.16.2/src/mkdocs_callouts_codeblock/plugin.py
@@ -0,0 +1,30 @@
+from mkdocs.plugins import BasePlugin
+from mkdocs.config import config_options
+
+from mkdocs_callouts_codeblock.utils import CalloutParser
+
+
+class CalloutsPlugin(BasePlugin):
+    """
+    Reads your markdown docs for the following style of callout block:
+       > [!INFO] Title
+       > An information callout from Obsidian
+       > inspired by the syntax from the Microsoft Docs
+
+    and converts it into a mkdocs supported admonition:
+       !!! info "Title"
+           An admonition block for MkDocs.
+           Allowing you to edit your notes
+           with confidence using Obsidian.
+    """
+    config_scheme = {  # pragma: no cover
+        ('aliases', config_options.Type(bool, default=True)),
+        ('breakless_lists', config_options.Type(bool, default=True))
+    }
+
+    def on_page_markdown(self, markdown, page, config, files):
+        parser = CalloutParser(
+            convert_aliases=self.config.get('aliases', True),
+            breakless_lists=self.config.get('breakless_lists', True)
+        )
+        return parser.parse(markdown)
