1f95be013e29e73ac73662189d3b32677ef0d57d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 04:05:30 +0000

    Add package wujihandpy/0.5.2/wujihandpy-0.5.2-cp39-cp39-manylinux_2_28_x86_64.whl

---
+++ packages/wujihandpy/wujihandpy-0.5.2-cp39-cp39-manylinux_2_28_x86_64.whl/wujihandpy/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from ._core import __doc__, Hand, Finger, Joint
+from ._version import __version__
+
+__all__ = ["__doc__", "__version__", "Hand", "Finger", "Joint"]
