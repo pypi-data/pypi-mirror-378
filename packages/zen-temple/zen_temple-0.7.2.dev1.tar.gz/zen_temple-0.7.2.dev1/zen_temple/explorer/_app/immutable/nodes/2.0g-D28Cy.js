1c542f46c3658d458a73157f8cf91cd2d5bf2bc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-23 03:17:55 +0000

    Add package zen-temple/0.7.2.dev1/zen_temple-0.7.2.dev1.tar.gz

---
+++ packages/zen-temple/zen_temple-0.7.2.dev1.tar.gz/zen_temple-0.7.2.dev1/zen_temple/explorer/_app/immutable/nodes/2.0g-D28Cy.js
@@ -0,0 +1,1 @@
+import"../chunks/DsnmJJEf.js";import{c as t,f as p,a as m,n as s}from"../chunks/Drxlg-Xn.js";import{s as e}from"../chunks/Hpa8B7Rc.js";function d(a,n){var o=t(),r=p(o);e(r,()=>n.children??s),m(a,o)}export{d as component};
